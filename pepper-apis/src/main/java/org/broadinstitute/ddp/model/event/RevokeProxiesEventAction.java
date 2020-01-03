055667d46b43eec906438e3d208e1c106b9dedb8
Yufeng Wang <yufeng@broadinstitute.org>
2020-01-03 10:19:18 -0500

    DDP-4222 implement revoke-proxies event action

--- pepper-apis/src/main/java/org/broadinstitute/ddp/model/event/RevokeProxiesEventAction.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/model/event/RevokeProxiesEventAction.java
@@ -1,10 +1,19 @@
 package org.broadinstitute.ddp.model.event;
 
+import org.broadinstitute.ddp.db.dao.UserGovernanceDao;
 import org.broadinstitute.ddp.db.dto.EventConfigurationDto;
 import org.broadinstitute.ddp.pex.PexInterpreter;
 import org.jdbi.v3.core.Handle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * An action that disables all proxies for the participant and study so that they are no longer active,
+ * and effectively becomes "former proxies".
+ */
 public class RevokeProxiesEventAction extends EventAction {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RevokeProxiesEventAction.class);
 
     public RevokeProxiesEventAction(EventConfiguration eventConfiguration, EventConfigurationDto dto) {
         super(eventConfiguration, dto);
@@ -12,5 +21,9 @@ public class RevokeProxiesEventAction extends EventAction {
 
     @Override
     public void doAction(PexInterpreter interpreter, Handle handle, EventSignal signal) {
+        int numUpdated = handle.attach(UserGovernanceDao.class)
+                .disableActiveProxies(signal.getParticipantId(), signal.getStudyId());
+        LOG.info("Disabled {} proxies for participant {} in study {}",
+                numUpdated, signal.getParticipantGuid(), signal.getStudyId());
     }
 }
