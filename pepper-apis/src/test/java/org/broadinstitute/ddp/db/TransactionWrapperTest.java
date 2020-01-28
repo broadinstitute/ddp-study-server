be559b95400e53648e74a4c764287d16f18dfc91
Andrew Zimmer <andrew@broadinstitute.org>
2020-01-28 18:00:14 -0500

    DDP-4342 Checkstyle strikes back!

--- pepper-apis/src/test/java/org/broadinstitute/ddp/db/TransactionWrapperTest.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/db/TransactionWrapperTest.java
@@ -97,7 +97,9 @@ public class TransactionWrapperTest {
                     }
                     assertEquals(1, numRows);
                     try {
-                        TransactionWrapper.useTxn((handle2) -> {});
+                        TransactionWrapper.useTxn((handle2) -> {
+                            LOG.info("Got connection when we shouldn't have");
+                        });
                     } catch (Exception e) {
                         gotPoolExhaustedError.set(e.getMessage().toLowerCase().contains("pool"));
                     }
--- pepper-apis/src/test/java/org/broadinstitute/ddp/util/LiquibaseUtilTest.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/util/LiquibaseUtilTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import liquibase.exception.MigrationFailedException;
