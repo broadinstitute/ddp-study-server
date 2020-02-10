08f5682315b9157b53141d0588a7f5b00cd6c59d
Sergei Retsia <Sergei_Retsia@epam.com>
2020-02-10 16:19:59 +0300

    ddp-4366-update-routes-to-respect-language-header: fixed the code obtaining the studyGuid in the filter

--- pepper-apis/src/main/java/org/broadinstitute/ddp/filter/LanguageResolutionFilter.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/filter/LanguageResolutionFilter.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Locale.LanguageRange;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -39,13 +41,20 @@ public class LanguageResolutionFilter implements Filter {
     public void handle(Request request, Response response) {
         try {
             String acceptLanguageHeader = request.headers(RouteConstants.ACCEPT_LANGUAGE);
-            String studyGuid = request.params(RouteConstants.PathParam.STUDY_GUID);
+            Matcher matcher = Pattern.compile("/studies/(\\w+)").matcher(request.url());
+            String studyGuid = matcher.find() ? matcher.group(0) : null;
             // The "supported languages" notion is an attribute of a study, thus is doesn't
             // make any sense outside of the study context
-            boolean supportedLanguagesCantBeDetermined = studyGuid == null;
-            if (supportedLanguagesCantBeDetermined) {
+            boolean supportedLanguagesCanBeDetected = studyGuid != null;
+            if (!supportedLanguagesCanBeDetected) {
+                LOG.warn(
+                        "Supported languages can't be detected because the filter is invoked"
+                        + " before the route that is outside of the study context. Path = {}",
+                        request.url()
+                );
                 return;
             }
+            LOG.info("The supported languages can be detected for the study {}", studyGuid);
             List<LanguageRange> acceptLanguages = StringUtils.isNotEmpty(acceptLanguageHeader)
                     ? LanguageRange.parse(acceptLanguageHeader) : Collections.emptyList();
             Set<Locale> localesSupportedByStudy = TransactionWrapper.withTxn(
@@ -55,6 +64,7 @@ public class LanguageResolutionFilter implements Filter {
                     RouteUtil.getDDPAuth(request).getPreferredLocale(), acceptLanguages, localesSupportedByStudy
             );
             request.attribute(USER_LANGUAGE, preferredLanguage);
+            LOG.info("Added the preferred user language {} to the attribute store", preferredLanguage.getLanguage());
         } catch (Exception e) {
             LOG.error("Error while figuring out the user language", e);
             halt(401);
