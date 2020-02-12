a503fec3cb728369c97953c84d0f1242199442dd
Sergei Retsia <Sergei_Retsia@epam.com>
2020-02-12 16:00:50 +0300

    ddp-4366-update-routes-to-respect-language-header: gave tests more meaningful names

--- pepper-apis/src/test/java/org/broadinstitute/ddp/filter/StudyLanguageResolutionFilterTest.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/filter/StudyLanguageResolutionFilterTest.java
@@ -19,16 +19,15 @@ public class StudyLanguageResolutionFilterTest extends TxnAwareBaseTest {
 
     private static final String LANG_EN = "en";
     private static final String LANG_RU = "ru";
+    private static final String LANG_HEADER_RU = "Accept-Language: ru-RU, ru";
 
     @BeforeClass
     public static void setupClass() {
         testData = TransactionWrapper.withTxn(handle -> TestDataSetupUtil.generateBasicUserTestData(handle));
     }
 
-
-    // When the study supports the language in the user profile, we choose it
     @Test
-    public void test_givenStudySupportsRU_andPreferredLangIsRU_andNoAcceptLangHeader_whenGetPreferredLangIsCalled_thenItReturnsRU() {
+    public void test_whenStudySupportsLanguageInUserProfile_andNoLanguageHeaderIsSpecified_thenWeChooseOneFromProfile() {
         TransactionWrapper.useTxn(
                 handle -> {
                     handle.attach(JdbiStudyLanguage.class).insert(testData.getStudyGuid(), LANG_RU);
@@ -42,9 +41,8 @@ public class StudyLanguageResolutionFilterTest extends TxnAwareBaseTest {
         );
     }
 
-    // When study doesn't suport the language, we fall back to the default one regardless of the user preferences
     @Test
-    public void test_givenStudySupportsNoLang_andPreferredLangIsRU_andNoLanguageHeader_whenGetPreferredLangIsCalled_thenItReturnsEN() {
+    public void test_whenStudyDoesntSupportLanguageInUserProfile_andNoLanguageHeaderIsSpecified_thenWeFallBackToDefaultOne() {
         TransactionWrapper.useTxn(
                 handle -> {
                     long ruLanguageId = handle.attach(JdbiLanguageCode.class).getLanguageCodeId(LANG_RU);
@@ -59,9 +57,8 @@ public class StudyLanguageResolutionFilterTest extends TxnAwareBaseTest {
         );
     }
 
-    // When study doesn't suport the language, we fall back to the default one regardless of the Accept-Language header
     @Test
-    public void test_givenStudySupportsNoLang_andPrefLangIsFR_andLangHeaderContainsRU_whenGetPreferredLangIsCalled_thenItReturnsEN() {
+    public void test_whenStudyDoesntSupportLanguageInLanguageHeader_thenWeFallBackToDefaultOne() {
         TransactionWrapper.useTxn(
                 handle -> {
                     String acceptLanguageHeader = "Accept-Language: fr-FR, fr";
@@ -74,13 +71,12 @@ public class StudyLanguageResolutionFilterTest extends TxnAwareBaseTest {
         );
     }
 
-    // Language header always overrides the language in the user profile
     @Test
-    public void test_givenStudySupportsRU_andPreferredLangIsEN_andLanguageHeaderContainsRU_whenGetPreferredLangIsCalled_thenItReturnsRU() {
+    public void test_whenBothLanguageInUserProfile_andLanguageHeaderAreSpecfied_thenLanguageInHeaderTakesPrecedence() {
         TransactionWrapper.useTxn(
                 handle -> {
                     handle.attach(JdbiStudyLanguage.class).insert(testData.getStudyGuid(), LANG_RU);
-                    String acceptLanguageHeader = "Accept-Language: ru-RU, ru";
+                    String acceptLanguageHeader = LANG_HEADER_RU;
                     LanguageDto languageDto = new StudyLanguageResolutionFilter().getPreferredLanguage(
                             handle, acceptLanguageHeader, Locale.forLanguageTag(LANG_EN), testData.getStudyGuid()
                     );
@@ -90,13 +86,12 @@ public class StudyLanguageResolutionFilterTest extends TxnAwareBaseTest {
         );
     }
 
-    // If the study doesn't support the language in the Accept-Language header, we resort to the one in the user profile
     @Test
-    public void test_givenStudySupportsEN_andPreferredLangIsEN_andLanguageHeaderContainsRU_whenGetPreferredLangIsCalled_thenItReturnsEN() {
+    public void test_whenStudyDoesntSupportLanguageInLanguageHeader_thenWeResortToLanguageInUserProfile() {
         TransactionWrapper.useTxn(
                 handle -> {
                     handle.attach(JdbiStudyLanguage.class).insert(testData.getStudyGuid(), LANG_EN);
-                    String acceptLanguageHeader = "Accept-Language: ru-RU, ru";
+                    String acceptLanguageHeader = LANG_HEADER_RU;
                     LanguageDto languageDto = new StudyLanguageResolutionFilter().getPreferredLanguage(
                             handle, acceptLanguageHeader, Locale.forLanguageTag(LANG_EN), testData.getStudyGuid()
                     );
