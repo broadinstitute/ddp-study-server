171a9c47566591e8571962564f84badff34fed2a
Sergei Retsia <Sergei_Retsia@epam.com>
2020-03-04 18:19:56 +0300

    DDP-2314-update-rendering-for-component-blocks: FormComponent implements Renderable now

--- pepper-apis/src/main/java/org/broadinstitute/ddp/model/activity/instance/FormComponent.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/model/activity/instance/FormComponent.java
@@ -2,13 +2,17 @@ package org.broadinstitute.ddp.model.activity.instance;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import javax.validation.constraints.NotNull;
 
 import com.google.gson.annotations.SerializedName;
+
+import org.broadinstitute.ddp.content.ContentStyle;
+import org.broadinstitute.ddp.content.Renderable;
 import org.broadinstitute.ddp.model.activity.types.ComponentType;
 
-public abstract class FormComponent implements Numberable {
+public abstract class FormComponent implements Numberable, Renderable {
 
     @NotNull
     @SerializedName("componentType")
@@ -42,6 +46,14 @@ public abstract class FormComponent implements Numberable {
     @Override
     public boolean shouldHideNumber() {
         return hideDisplayNumber;
+    }
+
+    @Override
+    public void registerTemplateIds(Consumer<Long> registry) {
+    }
+
+    @Override
+    public void applyRenderedTemplates(Provider<String> rendered, ContentStyle style) {
     }
 
     public ComponentType getComponentType() {
