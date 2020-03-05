94dd87b3f5eacb5628b36cd842b904dc5b070002
Sergei <raiks.com@gmail.com>
2020-03-05 20:25:03 +0300

    DDP-2314-update-rendering-for-component-blocks: passing params bundle… (#112)
    * DDP-2314-update-rendering-for-component-blocks: passing params bundled in InstitutionPhysicianComponentDto now
    
    - ComponentDao doesn't render components' labels anymore
    - TO-DO: implement template rendering in classes that contains those labels instead
    
    * DDP-2314-update-rendering-for-component-blocks: FormComponent implements Renderable now
    
    * DDP-2314-update-rendering-for-component-blocks: implemented overrides of Renderable methods in PhysicianInstitutionComponent
    
    * DDP-2314-update-rendering-for-component-blocks: removed an unused field
    
    * DDP-2314-update-rendering-for-component-blocks: not using an inherited Map for putting fields to be serialized anymore
    
    * DDP-2314-update-rendering-for-component-blocks: made Renderable methods abstract in FormComponent
    
    * DDP-2314-update-rendering-for-component-blocks: fleshed out methods responsible for specifying/applying templates
    
    * DDP-2314-update-rendering-for-component-blocks: fixed an NPE

--- pepper-apis/src/main/java/org/broadinstitute/ddp/model/activity/instance/InstitutionComponent.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/model/activity/instance/InstitutionComponent.java
@@ -1,27 +1,14 @@
 package org.broadinstitute.ddp.model.activity.instance;
 
+import org.broadinstitute.ddp.db.dto.InstitutionPhysicianComponentDto;
 import org.broadinstitute.ddp.model.activity.types.ComponentType;
-import org.broadinstitute.ddp.model.activity.types.InstitutionType;
-
 
 public final class InstitutionComponent extends PhysicianInstitutionComponent {
 
-    public InstitutionComponent(boolean allowMultiple,
-                                String addButtonText,
-                                String titleText,
-                                String subtitleText,
-                                InstitutionType institutionType,
-                                boolean showFields,
-                                boolean hideNumber,
-                                boolean required) {
-        super(ComponentType.INSTITUTION,
-                allowMultiple,
-                addButtonText,
-                titleText,
-                subtitleText,
-                institutionType,
-                showFields,
-                hideNumber,
-                required);
+    public InstitutionComponent(
+            InstitutionPhysicianComponentDto instDto,
+            boolean shouldHideNumber
+    ) {
+        super(ComponentType.INSTITUTION, instDto, shouldHideNumber);
     }
 }
