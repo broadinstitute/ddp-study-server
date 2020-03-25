2f6eb2c8985eec57d6159780c589b665ec4e0360
Marco Ocana <mocana@broadinstitute.org>
2020-03-25 16:25:17 -0400

    fix script

--- pepper-apis/scripts/ci/otherbranchessamecommit.sh
+++ pepper-apis/scripts/ci/otherbranchessamecommit.sh
@@ -7,4 +7,4 @@ git show-ref |  #list all references in current rep
 grep $CURRENT_COMMIT |  #find current commit sha
 grep -Eo "$REMOTE_BRANCH_PREFIX.*" |  #keep only remote branches
 cut -c "$((${#REMOTE_BRANCH_PREFIX} + 1))-"  | #strip out remote prefix
-qgrep -Ev "^${CURRENT_BRANCH}"  #exclude the current working branch
+grep -Ev "^${CURRENT_BRANCH}"  #exclude the current working branch
