0b4d5579fff88a6dffffd55bc929181d6ee7f44b
Marco Ocana <mocana@broadinstitute.org>
2020-03-25 18:02:32 -0400

    Make sure this script returns 0 if everything OK regardless of grep exit code

--- pepper-apis/scripts/ci/otherbranchessamecommit.sh
+++ pepper-apis/scripts/ci/otherbranchessamecommit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -x
 #List other branches that refer to current commit
 CURRENT_BRANCH=$(git branch --contains HEAD | grep -E '^\*' | grep -Eo '\b\w.*' )
 CURRENT_COMMIT=$(git log -1 --format=%H)
@@ -7,5 +6,13 @@ REMOTE_BRANCH_PREFIX='refs/remotes/origin/'
 git show-ref |  #list all references in current rep
 grep $CURRENT_COMMIT |  #find current commit sha
 grep -Eo "$REMOTE_BRANCH_PREFIX.*" |  #keep only remote branches
-cut -c "$((${#REMOTE_BRANCH_PREFIX} + 1))-"  | #strip out remote prefix
+cut -c "$((${#REMOTE_BRANCH_PREFIX} + 1))-" | #remove prefixes
 grep -Ev "^${CURRENT_BRANCH}"  #exclude the current working branch
+
+# turns out grep if no matching lines found returns exit code 1. We don't want that for our script!
+if [ $? == 1 ] || [ $? == 0 ]
+then
+  exit 0
+else
+  exit $?
+fi
