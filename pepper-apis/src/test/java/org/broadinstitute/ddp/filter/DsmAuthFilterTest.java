6028773d56a89d2feefeb8a62702f811000561a0
Sergei Retsia <sretsia@broadinstitute.org>
2020-03-27 19:23:11 +0300

    DDP-4134-auth0-client-id-to-be-scoped-to-tenant: fixed the SQL query

--- pepper-apis/src/test/java/org/broadinstitute/ddp/filter/DsmAuthFilterTest.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/filter/DsmAuthFilterTest.java
@@ -58,7 +58,7 @@ public class DsmAuthFilterTest extends DsmRouteTest {
     @Test
     public void testValidTokenWithInvalidClient() {
         String updateIsRevokedQueryTemplate = "UPDATE client c JOIN auth0_tenant t ON c.auth0_tenant_id = t.auth0_tenant_id "
-                + " SET c.is_revoked = %d WHERE c.auth0_client_id = %s AND t.auth0_domain = %s";
+                + " SET c.is_revoked = %d WHERE c.auth0_client_id = '%s' AND t.auth0_domain = '%s'";
 
         TransactionWrapper.withTxn(handle -> {
             String query = String.format(updateIsRevokedQueryTemplate, 1, dsmClientId, auth0Domain);
