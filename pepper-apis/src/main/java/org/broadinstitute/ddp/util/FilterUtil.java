30a392df086b1cc454da2978499f153b708e89e2
Marco Ocana <mocana@broadinstitute.org>
2020-04-06 17:21:56 -0400

    New home for generic filters

--- pepper-apis/src/main/java/org/broadinstitute/ddp/util/FilterUtil.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/util/FilterUtil.java
@@ -2,9 +2,13 @@ package org.broadinstitute.ddp.util;
 
 import static spark.Spark.before;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+
+import org.broadinstitute.ddp.filter.ExcludePathFilterWrapper;
+import spark.Filter;
 
 public class FilterUtil {
     public static void whitelist(String path, Collection<String> ips) {
@@ -14,5 +18,16 @@ public class FilterUtil {
                 throw ResponseUtil.halt404PageNotFound(response);
             }
         });
+    }
+
+    /**
+     * Allow to specify the exclusion of a path from the execution of a filter
+     *
+     * @param filterPath     the path for which the filter is applicable
+     * @param pathsToExclude the paths to exclude the execution of the filter
+     * @param filter         the filter
+     */
+    public static void beforeWithExclusion(String filterPath, String[] pathsToExclude, Filter filter) {
+        before(filterPath, new ExcludePathFilterWrapper(filter, Arrays.asList(pathsToExclude)));
     }
 }
