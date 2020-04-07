7aa2e99358172a6f713783967c1c153d14491693
Marco Ocana <mocana@broadinstitute.org>
2020-04-07 15:37:28 -0400

    Cleanup and javadocs

--- pepper-apis/src/main/java/org/broadinstitute/ddp/util/FilterUtil.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/util/FilterUtil.java
@@ -1,20 +1,37 @@
 package org.broadinstitute.ddp.util;
 
+import static java.util.stream.Collectors.toList;
 import static spark.Spark.before;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.broadinstitute.ddp.filter.ExcludePathFilterWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Filter;
 
 public class FilterUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(FilterUtil.class);
+
+    /**
+     * Creates "before" filter on specified that checks that IP address matches one of those provided
+     * Filter will return a 404 and a page not found API error message
+     * @param path the URL path
+     * @param ips Collection of IPs to allow
+     */
     public static void whitelist(String path, Collection<String> ips) {
-        Set<String> ipSet = (ips instanceof Set) ? (Set<String>)ips : new HashSet<>(ips);
+        Set<String> ipSet = (ips instanceof Set) ? (Set<String>) ips : new HashSet<>(ips);
         before(path, (request, response) -> {
-            if(!(ipSet.contains(request.ip()))) {
+            if (!(ipSet.contains(request.ip()))) {
+                List<String> headerString = request.headers().stream()
+                        .map(headerName -> headerName + ": " + request.headers(headerName))
+                        .collect(toList());
+                LOG.warn("Unauthorized IP address tried to access service.\nIP: {}\nURL: {}\nMethod: {}\nBody: {}\nHeaders: {}",
+                        request.ip(), request.url(), request.requestMethod(), request.body(), headerString);
                 throw ResponseUtil.halt404PageNotFound(response);
             }
         });
