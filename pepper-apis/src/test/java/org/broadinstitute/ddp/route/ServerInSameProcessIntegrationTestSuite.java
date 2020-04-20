c3775122eeb078fa2a39c9631432de0091e8fc14
Marco Ocana <mocana@broadinstitute.org>
2020-04-20 10:53:20 -0400

    Add comment to new test suite

--- pepper-apis/src/test/java/org/broadinstitute/ddp/route/ServerInSameProcessIntegrationTestSuite.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/route/ServerInSameProcessIntegrationTestSuite.java
@@ -3,6 +3,10 @@ package org.broadinstitute.ddp.route;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * These tests need to have server running in same process. In CircleCi running tests these are treated
+ * differently then other route tests that just need access to the server via http and and the database
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         GetParticipantInfoRouteTest.class,
