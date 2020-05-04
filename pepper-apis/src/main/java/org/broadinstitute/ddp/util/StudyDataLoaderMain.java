48d2bfc0f8ebb652e53e53c0cb90f89a026462a6
Sampath K. Settipalli <sampath@broadinstitute.org>
2020-05-05 01:58:26 +0530

    cleanup

--- pepper-apis/src/main/java/org/broadinstitute/ddp/util/StudyDataLoaderMain.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/util/StudyDataLoaderMain.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.TreeMap;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Blob;
