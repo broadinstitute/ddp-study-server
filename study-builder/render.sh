076d7e563a447f9d0a52b8c13c3d969154e2dad2
Yufeng Wang <yufeng@broadinstitute.org>
2020-05-06 08:43:25 -0400

    Bootstrap testboston study

--- study-builder/render.sh
+++ study-builder/render.sh
@@ -11,6 +11,10 @@ if [[ "$3" == 'tenant' ]]; then
   INPUT_DIR=config OUTPUT_DIR=output-config NO_SYSLOG=true \
     VERSION="$1" ENVIRONMENT="$2" MANIFEST=manifest-tenant.rb \
     ruby ../pepper-apis/configure.rb -y
+elif [[ "$3" == 'testboston' ]]; then
+  INPUT_DIR=config OUTPUT_DIR=output-config NO_SYSLOG=true \
+    VERSION="$1" ENVIRONMENT="$2" STUDY_KEY="$3" MANIFEST=manifest-testboston.rb \
+    ruby ../pepper-apis/configure.rb -y
 else
   INPUT_DIR=config OUTPUT_DIR=output-config NO_SYSLOG=true \
     VERSION="$1" ENVIRONMENT="$2" STUDY_KEY="$3" \
