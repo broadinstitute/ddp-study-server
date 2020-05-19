e2e7934c2d6ddf0108fb623eff1b4d01b2341791
Sampath K. Settipalli <sampath@broadinstitute.org>
2020-05-19 18:18:33 +0530

    functionality to keep track of emails sent

--- pepper-apis/src/test/java/org/broadinstitute/ddp/script/MBCAuth0PasswordResetEmailScript.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/script/MBCAuth0PasswordResetEmailScript.java
@@ -1,5 +1,7 @@
 package org.broadinstitute.ddp.script;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -13,7 +15,7 @@ import org.broadinstitute.ddp.exception.DDPException;
 import org.broadinstitute.ddp.util.Auth0Util;
 import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.Test;
+//import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,10 +29,15 @@ public class MBCAuth0PasswordResetEmailScript extends TxnAwareBaseTest {
     private static final String MESSAGE_SUBJECT = "Metastatic Breast Cancer Project update: Please choose a password for your account";
     private static final String SENDGRID_CONSENTED_TEMPLATE_ID = "fa576354-73b9-41fe-9cfc-ac370fc70556";
     private static final String SENDGRID_NOT_CONSENTED_TEMPLATE_ID = "cd3e9c60-9636-4685-84fd-c1aa3c7a1177";
-    private static final String BASE_WEBPAGE_URL = "https:///mbc.dev.datadonationplatform.org/password-reset-done"; //WATCHOUT.. Its DEV
+    private static final String BASE_WEBPAGE_URL = "https:///mbc.dev.datadonationplatform.org/password-reset-done";
+    //WATCHOUT.. Its DEV sendgrid template ids and Dev url
+
+    private static final Integer TODAYS_LIMIT = 150;
 
     private String[] consentedEmails = {};
     private String[] nonConsentedEmails = {};
+    private String[] consentedEmailsSent = {};
+    private String[] nonConsentedEmailsSent = {};
 
     private Auth0ManagementClient mgmtClient;
 
@@ -41,15 +48,31 @@ public class MBCAuth0PasswordResetEmailScript extends TxnAwareBaseTest {
         });
     }
 
-    //@Ignore
-    @Test
+    @Ignore
+    //@Test
     public void sendPasswordResetsForHardcodedUsersNotConsented() throws Exception {
         StudyPasswordResetEmailGenerator emailGenerator = new StudyPasswordResetEmailGenerator();
         List<ProfileWithEmail> recipientProfiles = Collections.emptyList();
-        // manually add email addresses here
-        //Set<String> ptpEmails = new HashSet<>(Arrays.asList(nonConsentedEmails));
+        //add emails
+        List<String> ncEmailListSent = new ArrayList<>(Arrays.asList(nonConsentedEmailsSent));
+        List<String> ncEmailList = new ArrayList<>(Arrays.asList(nonConsentedEmails));
+        Collections.sort(ncEmailList);
+        ncEmailList.replaceAll(String::toLowerCase);
+        ncEmailListSent.replaceAll(String::toLowerCase);
+
+        //remove emails already sent
+        ncEmailList.removeAll(ncEmailListSent);
+
+        LOG.info("NON consented email ARRAY count: {} non consented email List count: {} already sent count: {}",
+                nonConsentedEmails.length, ncEmailList.size(), ncEmailListSent.size());
+
+        //pick todays emails
         Set<String> ptpEmails = new HashSet<>();
-        LOG.info("Not consented participant email count: {}", ptpEmails.size());
+        for (int counter = 0; counter <= TODAYS_LIMIT; counter++) {
+            ptpEmails.add(ncEmailList.get(counter));
+        }
+
+        LOG.info("TODAYS {} non-consented emails:: {}", ptpEmails.size(), ptpEmails.toString());
 
         try {
             recipientProfiles = TransactionWrapper.withTxn(handle -> {
@@ -69,17 +92,36 @@ public class MBCAuth0PasswordResetEmailScript extends TxnAwareBaseTest {
         } catch (DDPException e) {
             LOG.error("Exception executing MBCAuth0PasswordResetEmailScript", e);
         }
+
+        //add emails sent today to sentEmails and update the nonConsentedEmailsSent array manually;
+        ncEmailListSent.addAll(ptpEmails);
+        LOG.info("ncEmailListSent = {}", ncEmailListSent.toString());
     }
 
-    //@Ignore
-    @Test
+    @Ignore
+    //@Test
     public void sendPasswordResetsForHardcodedUsersConsented() throws Exception {
         StudyPasswordResetEmailGenerator emailGenerator = new StudyPasswordResetEmailGenerator();
         List<ProfileWithEmail> recipientProfiles = Collections.emptyList();
-        // manually add email addresses here
-        //Set<String> ptpEmails = new HashSet<>(Arrays.asList(consentedEmails));
+
+        //add emails
+        List<String> consentedEmailListSent = new ArrayList<>(Arrays.asList(consentedEmailsSent));
+        List<String> consentedEmailList = new ArrayList<>(Arrays.asList(consentedEmails));
+        Collections.sort(consentedEmailList);
+        consentedEmailList.replaceAll(String::toLowerCase);
+        consentedEmailListSent.replaceAll(String::toLowerCase);
+
+        //remove emails already sent
+        consentedEmailList.removeAll(consentedEmailListSent);
+        LOG.info("Consented email ARRAY count: {} consented email list count: {} ", consentedEmails.length, consentedEmailList.size());
+
+        //pick todays emails to send
         Set<String> ptpEmails = new HashSet<>();
-        LOG.info("Consented participant email count: {}", ptpEmails.size());
+        for (int counter = 0; counter <= TODAYS_LIMIT; counter++) {
+            ptpEmails.add(consentedEmailList.get(counter));
+        }
+
+        LOG.info("TODAYS consented users {} emails:: {}", ptpEmails.size(), ptpEmails.toString());
 
         try {
             recipientProfiles = TransactionWrapper.withTxn(handle -> {
@@ -99,6 +141,10 @@ public class MBCAuth0PasswordResetEmailScript extends TxnAwareBaseTest {
         } catch (DDPException e) {
             LOG.error("Exception executing MBCAuth0PasswordResetEmailScript", e);
         }
+
+        //add emails sent today to sentEmails and update the consentedEmailsSent array manually;
+        consentedEmailListSent.addAll(ptpEmails);
+        LOG.info("consentedEmailListSent = {}", consentedEmailListSent.toString());
     }
 
 }
