76de5cce4fd42ff940a90666677dad11f86f895f
Marco Ocana <mocana@broadinstitute.org>
2020-05-21 16:33:43 -0400

    Utility to create servers for tests

---
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/util/TestServer.java
@@ -0,0 +1,34 @@
+package org.broadinstitute.ddp.util;
+
+import java.util.function.Consumer;
+
+import spark.Service;
+
+/**
+ * Little utility for when we you need to create your own server to do stuff
+ */
+public class TestServer {
+    static public int PORT = 6666;
+    private Consumer<Service> setupServer;
+    private Service service;
+
+    public TestServer(Consumer<Service> setup) {
+        this.setupServer = setup;
+
+    }
+    public TestServer startServer() {
+        service = Service.ignite().port(PORT);
+        setupServer.accept(service);
+        service.awaitInitialization();
+        return this;
+    }
+
+    public TestServer stopServer() {
+        service.stop();
+        service.awaitStop();
+        return this;
+    }
+    public String baseUrl() {
+        return "http://localhost:" + PORT;
+    }
+}
