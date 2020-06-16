edbc68a09e5a9746decfcbc2b9222525df0785d8
Yufeng Wang <yufeng@broadinstitute.org>
2020-06-16 13:52:37 -0400

    Allowing configuring invite error message (#230)

---
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/model/study/StudySettings.java
@@ -0,0 +1,26 @@
+package org.broadinstitute.ddp.model.study;
+
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
+import org.jdbi.v3.core.mapper.reflect.JdbiConstructor;
+
+public class StudySettings {
+
+    private long studyId;
+    private Long inviteErrorTemplateId;
+
+    @JdbiConstructor
+    public StudySettings(
+            @ColumnName("umbrella_study_id") long studyId,
+            @ColumnName("invite_error_template_id") Long inviteErrorTemplateId) {
+        this.studyId = studyId;
+        this.inviteErrorTemplateId = inviteErrorTemplateId;
+    }
+
+    public long getStudyId() {
+        return studyId;
+    }
+
+    public Long getInviteErrorTemplateId() {
+        return inviteErrorTemplateId;
+    }
+}
