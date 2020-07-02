a47365e6095bb099c4f164fee23f9a3703430bb3
Marco Ocana <mocana@broadinstitute.org>
2020-07-02 12:33:59 -0400

    Fix merge and checkstyle issues

--- pepper-apis/src/main/java/org/broadinstitute/ddp/route/PatchFormAnswersCollectDataRoute.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/route/PatchFormAnswersCollectDataRoute.java
@@ -25,6 +25,7 @@ import org.broadinstitute.ddp.analytics.GoogleAnalyticsMetrics;
 import org.broadinstitute.ddp.analytics.GoogleAnalyticsMetricsTracker;
 import org.broadinstitute.ddp.constants.ErrorCodes;
 import org.broadinstitute.ddp.constants.RouteConstants.PathParam;
+import org.broadinstitute.ddp.db.ActivityDefStore;
 import org.broadinstitute.ddp.db.TransactionWrapper;
 import org.broadinstitute.ddp.db.dao.AnswerDao;
 import org.broadinstitute.ddp.db.dao.DataExportDao;
@@ -43,7 +44,9 @@ import org.broadinstitute.ddp.db.dao.PickListQuestionCachedDao;
 import org.broadinstitute.ddp.db.dao.PicklistQuestionDao;
 import org.broadinstitute.ddp.db.dao.QuestionCachedDao;
 import org.broadinstitute.ddp.db.dao.UserDao;
+import org.broadinstitute.ddp.db.dto.ActivityDto;
 import org.broadinstitute.ddp.db.dto.ActivityInstanceDto;
+import org.broadinstitute.ddp.db.dto.ActivityVersionDto;
 import org.broadinstitute.ddp.db.dto.AnswerDto;
 import org.broadinstitute.ddp.db.dto.CompositeQuestionDto;
 import org.broadinstitute.ddp.db.dto.LanguageDto;
@@ -59,6 +62,7 @@ import org.broadinstitute.ddp.json.PatchAnswerPayload;
 import org.broadinstitute.ddp.json.PatchAnswerResponse;
 import org.broadinstitute.ddp.json.errors.AnswerValidationError;
 import org.broadinstitute.ddp.json.errors.ApiError;
+import org.broadinstitute.ddp.model.activity.definition.FormActivityDef;
 import org.broadinstitute.ddp.model.activity.instance.answer.AgreementAnswer;
 import org.broadinstitute.ddp.model.activity.instance.answer.Answer;
 import org.broadinstitute.ddp.model.activity.instance.answer.BoolAnswer;
@@ -332,10 +336,18 @@ public class PatchFormAnswersCollectDataRoute implements Route {
                 throw ResponseUtil.haltError(response, 400, new ApiError(ErrorCodes.REQUIRED_PARAMETER_MISSING, e.getMessage()));
             }
 
-            res.setBlockVisibilities(formService.getBlockVisibilities(handle, participantGuid, instanceGuid));
+            ActivityDefStore activityStore = ActivityDefStore.getInstance();
+            ActivityDto activityDto = activityStore.findActivityDto(handle, instanceDto.getActivityId())
+                    .orElseThrow(() -> new DDPException("Could not find activity dto for instance " + instanceGuid));
+            ActivityVersionDto versionDto = activityStore
+                    .findVersionDto(handle, instanceDto.getActivityId(), instanceDto.getCreatedAtMillis())
+                    .orElseThrow(() -> new DDPException("Could not find activity version for instance " + instanceGuid));
+            FormActivityDef def = activityStore.findActivityDef(handle, studyGuid, activityDto, versionDto)
+                    .orElseThrow(() -> new DDPException("Could not find activity definition for instance " + instanceGuid));
+            res.setBlockVisibilities(formService.getBlockVisibilities(handle, def, participantGuid, instanceGuid));
 
             List<ActivityValidationFailure> failures = getActivityValidationFailures(
-                    handle, participantGuid, instanceGuid, languageCodeId
+                    handle, participantGuid, instanceDto, languageCodeId
             );
             if (!failures.isEmpty()) {
                 LOG.info("Activity validation failed, reasons: {}", createValidationFailureSummaries(failures));
@@ -702,11 +714,10 @@ public class PatchFormAnswersCollectDataRoute implements Route {
     }
 
     private List<ActivityValidationFailure> getActivityValidationFailures(
-            Handle handle, String participantGuid, String activityInstanceGuid, long languageCodeId
+            Handle handle, String participantGuid, ActivityInstanceDto instanceDto, long languageCodeId
     ) {
-        long activityId = handle.attach(JdbiActivityInstance.class).getActivityIdByGuid(activityInstanceGuid);
         return actValidationService.validate(
-                handle, interpreter, participantGuid, activityInstanceGuid, activityId, languageCodeId
+                handle, interpreter, participantGuid, instanceDto.getGuid(), instanceDto.getActivityId(), languageCodeId
         );
     }
 }
