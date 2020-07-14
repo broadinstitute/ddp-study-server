b05f2dd14508fecc37ac5215ab3b64b90b59ac75
Marco Ocana <mocana@broadinstitute.org>
2020-07-14 17:44:47 -0400

    Make sure we use the cached version of the method

--- pepper-apis/src/main/java/org/broadinstitute/ddp/db/dao/JdbiBooleanQuestion.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/db/dao/JdbiBooleanQuestion.java
@@ -16,16 +16,16 @@ public interface JdbiBooleanQuestion extends SqlObject {
 
     @SqlUpdate("insert into boolean_question (question_id,true_template_id,false_template_id)"
             + " values(:questionId,:trueTemplateId,:falseTemplateId)")
-    int insert(@Bind("questionId")long questionId, @Bind("trueTemplateId")long trueTemplateId,
+    int insert(@Bind("questionId") long questionId, @Bind("trueTemplateId") long trueTemplateId,
                @Bind("falseTemplateId") long falseTemplateId);
 
     @UseStringTemplateSqlLocator
     @SqlQuery("queryDtoByQuestionId")
     @RegisterConstructorMapper(BooleanQuestionDto.class)
-    Optional<BooleanQuestionDto> findDtoByQuestionId(@Bind("questionId") long questionId);
+    Optional<BooleanQuestionDto> findDtoByQuestion(@Bind("questionId") long questionId);
 
-    default Optional<BooleanQuestionDto> findDtoByQuestionId(QuestionDto questionDto) {
-        return findDtoByQuestionId(questionDto.getId());
+    default Optional<BooleanQuestionDto> findDtoByQuestion(QuestionDto questionDto) {
+        return findDtoByQuestion(questionDto.getId());
     }
 
     @UseStringTemplateSqlLocator
