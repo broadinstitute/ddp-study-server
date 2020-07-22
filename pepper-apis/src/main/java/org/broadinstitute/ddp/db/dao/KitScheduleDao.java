93b4234f0c13a0c91258c7a7447410073c95d761
Yufeng Wang <yufeng@broadinstitute.org>
2020-07-22 16:27:40 -0400

    cleanup

--- pepper-apis/src/main/java/org/broadinstitute/ddp/db/dao/KitScheduleDao.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/db/dao/KitScheduleDao.java
@@ -17,6 +17,7 @@ import org.jdbi.v3.sqlobject.config.RegisterColumnMapperFactory;
 import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.stringtemplate4.UseStringTemplateSqlLocator;
 
 public interface KitScheduleDao {
 
@@ -75,53 +76,14 @@ public interface KitScheduleDao {
     @RegisterConstructorMapper(KitScheduleRecord.class)
     Optional<KitScheduleRecord> findRecord(@Bind("id") long recordId);
 
-    @SqlQuery("select usen.study_id,"
-            + "       (select guid from umbrella_study where umbrella_study_id = usen.study_id) as study_guid,"
-            + "       (select guid from user where user_id = usen.user_id) as user_guid,"
-            + "       addr.address_id,"
-            + "       vs.code as address_validation_status,"
-            + "       rec.*,"
-            + "       (select kit_request_guid from kit_request"
-            + "         where kit_request_id = rec.initial_kit_request_id) as initial_kit_request_guid"
-            + "  from kit_schedule_record as rec"
-            + "  join user_study_enrollment as usen on usen.user_id = rec.participant_user_id"
-            + "  join enrollment_status_type as entype on entype.enrollment_status_type_id = usen.enrollment_status_type_id"
-            + "  join kit_configuration as kc on kc.kit_configuration_id = rec.kit_configuration_id and kc.study_id = usen.study_id"
-            + "  join kit_schedule as ks on ks.kit_configuration_id = kc.kit_configuration_id"
-            + "  left join default_mailing_address as defaddr on defaddr.participant_user_id = usen.user_id"
-            + "  left join mailing_address as addr on addr.address_id = defaddr.address_id"
-            + "  left join mailing_address_validation_status as vs on vs.mailing_address_validation_status_id = addr.validation_status_id"
-            + " where entype.enrollment_status_type_code = 'ENROLLED'"
-            + "   and usen.valid_to is null"
-            + "   and rec.opted_out = false"
-            + "   and rec.num_occurrences < ks.num_occurrences_per_user"
-            + "   and rec.initial_kit_sent_time is null"
-            + " order by usen.study_id")
+    @UseStringTemplateSqlLocator
+    @SqlQuery("findAllEligibleRecordsWaitingForKitStatus")
     @RegisterConstructorMapper(PendingScheduleRecord.class)
     @RegisterColumnMapperFactory(EnumByOrdinalMapperFactory.class)
     Stream<PendingScheduleRecord> findAllEligibleRecordsWaitingForKitStatus();
 
-    @SqlQuery("select usen.study_id,"
-            + "       (select guid from umbrella_study where umbrella_study_id = usen.study_id) as study_guid,"
-            + "       (select guid from user where user_id = usen.user_id) as user_guid,"
-            + "       addr.address_id,"
-            + "       vs.code as address_validation_status,"
-            + "       rec.*,"
-            + "       (select kit_request_guid from kit_request"
-            + "         where kit_request_id = rec.initial_kit_request_id) as initial_kit_request_guid"
-            + "  from kit_schedule_record as rec"
-            + "  join user_study_enrollment as usen on usen.user_id = rec.participant_user_id"
-            + "  join enrollment_status_type as entype on entype.enrollment_status_type_id = usen.enrollment_status_type_id"
-            + "  join kit_configuration as kc on kc.kit_configuration_id = rec.kit_configuration_id and kc.study_id = usen.study_id"
-            + "  join kit_schedule as ks on ks.kit_configuration_id = kc.kit_configuration_id"
-            + "  left join default_mailing_address as defaddr on defaddr.participant_user_id = usen.user_id"
-            + "  left join mailing_address as addr on addr.address_id = defaddr.address_id"
-            + "  left join mailing_address_validation_status as vs on vs.mailing_address_validation_status_id = addr.validation_status_id"
-            + " where entype.enrollment_status_type_code = 'ENROLLED'"
-            + "   and usen.valid_to is null"
-            + "   and rec.kit_configuration_id = :configId"
-            + "   and rec.opted_out = false"
-            + "   and rec.num_occurrences < ks.num_occurrences_per_user")
+    @UseStringTemplateSqlLocator
+    @SqlQuery("findPendingScheduleRecords")
     @RegisterConstructorMapper(PendingScheduleRecord.class)
     @RegisterColumnMapperFactory(EnumByOrdinalMapperFactory.class)
     Stream<PendingScheduleRecord> findPendingScheduleRecords(@Bind("configId") long kitConfigurationId);
