c315572c6b08b5239237e2d38010ab7f6bf7cf6e
Anastasiya Fomina <anastasiya.fomina@quantori.com>
2020-07-22 15:07:37 +0300

    PBD-303: export tenant configuration from Auth0

---
+++ study-builder/tenants/atcp/rules/Register User in Pepper.js
@@ -0,0 +1,224 @@
+function (user, context, callback) {
+    // Environment stabilization. This will save us a number of
+    // overly complex if statements later
+    context.clientMetadata = context.clientMetadata || {};
+
+    context.request = context.request || {};
+    context.request.body = context.request.body || {};
+    context.request.geoip = context.request.geoip || {};
+    context.request.query = context.request.query || {};
+
+    user.app_metadata = user.app_metadata || {};
+    user.app_metadata.pepper_user_guids = user.app_metadata.pepper_user_guids || {};
+
+    var m2mClients = ['dsm', 'Count Me In (Salt CMS)'];
+    if (m2mClients.includes(context.clientName)) {
+        return callback(null, user, context);
+    }
+
+    var pepperUserGuidClaim = 'https://datadonationplatform.org/uid';
+    var pepperClientClaim = 'https://datadonationplatform.org/cid';
+    var pepperTenantClaim = 'https://datadonationplatform.org/t';
+
+    var tenantDomain = 'https://' + context.tenant + '.auth0.com/';
+    context.idToken[pepperTenantClaim] = tenantDomain;
+    context.idToken[pepperClientClaim] = context.clientID;
+
+    var mockRegistration = context.clientMetadata.mockRegistration;
+    var doLocalRegistration = context.clientMetadata.doLocalRegistration;
+
+    if (mockRegistration) {
+        var overrideUserGuid;
+        if (user.app_metadata.testingGuid) {
+            overrideUserGuid = user.app_metadata.testingGuid;
+            console.log('Using hardcoded guid ' + overrideUserGuid + ' during registration for ' + user.user_id + ' for client ' + context.clientID);
+        } else if (context.clientMetadata.overrideUserGuid) {
+            overrideUserGuid = context.clientMetadata.overrideUserGuid;
+            console.log('Using hardcoded guid ' + overrideUserGuid + ' during registration for ' + user.user_id + ' for client ' + context.clientID);
+        } else {
+            console.log('No hardcoded guid found during mock registration for ' + user.user_id + ' from client ' + context.clientID);
+        }
+
+        context.idToken[pepperUserGuidClaim] = overrideUserGuid;
+        return callback(null, user, context);
+    } else if (doLocalRegistration) {
+        var localRegistrationGuid = user.app_metadata.pepper_user_guids[context.clientID];
+
+        if (localRegistrationGuid) {
+            console.log('Using guid ' + localRegistrationGuid + ' during local registration for ' + user.user_id + ' for client ' + context.clientID);
+            context.idToken[pepperUserGuidClaim] = localRegistrationGuid;
+        } else {
+            console.log('No pepper guid available during local registration for ' + user.user_id + ' for client ' + context.clientID);
+        }
+
+        return callback(null, user, context);
+    } else {
+        var pepper_params = {
+            auth0UserId: user.user_id,
+            auth0ClientId: context.clientID,
+            auth0Domain: tenantDomain
+        };
+
+        if (context.request.query.study_guid) {
+            pepper_params.studyGuid = context.request.query.study_guid;
+            console.log('StudyGuid passed in (via query) = ' + pepper_params.studyGuid);
+        } else if (context.request.body.study_guid) {
+            pepper_params.studyGuid = context.request.body.study_guid;
+            console.log('StudyGuid passed in (via body) = ' + pepper_params.studyGuid);
+        } else {
+            console.log('No studyGuid passed in request');
+        }
+
+        if (context.request.query.temp_user_guid) {
+            pepper_params.tempUserGuid = context.request.query.temp_user_guid;
+            console.log('Temp user guid passed in (via query) = ' + pepper_params.tempUserGuid);
+        } else if (context.request.body.temp_user_guid) {
+            pepper_params.tempUserGuid = context.request.body.temp_user_guid;
+            console.log('Temp user guid passed in (via body) = ' + pepper_params.tempUserGuid);
+        } else if (user.user_metadata && user.user_metadata.temp_user_guid) {
+            pepper_params.tempUserGuid = user.user_metadata.temp_user_guid;
+            console.log('Temp user guid passed in (via user_metadata) = ' + pepper_params.tempUserGuid);
+        } else {
+            console.log('No temp user guid passed in request');
+        }
+
+        if (context.request.query.mode) {
+            pepper_params.mode = context.request.query.mode;
+            console.log('Registration Mode passed in (via query) = ' + pepper_params.mode);
+        } else if (context.request.body.mode) {
+            pepper_params.mode = context.request.body.mode;
+            console.log('Registration Mode passed in (via body) = ' + pepper_params.mode);
+        } else {
+            console.log('No Registration Mode passed in request');
+        }
+
+        if (context.request.query.invitation_id) {
+            pepper_params.invitationId = context.request.query.invitation_id;
+            console.log('Invitation id passed in (via query) = ' + pepper_params.invitationId);
+        } else if (context.request.body.invitation_id) {
+            pepper_params.invitationId = context.request.body.invitation_id;
+            console.log('Invitation id passed in (via body) = ' + pepper_params.invitationId);
+        }
+
+        if (context.request.query.language) {
+            pepper_params.languageCode = context.request.query.language;
+            console.log('User language passed in (via query) = ' + pepper_params.languageCode);
+        } else if (context.request.body.language) {
+            pepper_params.languageCode = context.request.body.language;
+            console.log('User language passed in (via body) = ' + pepper_params.languageCode);
+        }
+
+        if (context.request.query.time_zone) {
+            pepper_params.timeZone = context.request.query.time_zone;
+            console.log('User timezone passed in (via query) = ' + pepper_params.timeZone);
+        } else if (context.request.body.time_zone) {
+            pepper_params.timeZone = context.request.body.time_zone;
+            console.log('User timezone passed in (via body) = ' + pepper_params.timeZone);
+        }
+
+        console.log(context);
+
+        // This is the token renewal case. Let's avoid going through pepper registration
+        if (context.request.query.renew_token_only) {
+            context.idToken[pepperUserGuidClaim] = user.app_metadata.user_guid;
+            return callback(null, user, context);
+        }
+
+        // In order to get a refresh token, the client must go through one of the other methods
+        // to get an id/access token first (oauth2-password, oidc-implicit-profile, etc).
+        // This allows us to assume that, if they have a refresh token, they must have gone through the
+        // registration process.
+        //
+        // Errata: This assumption is not valid in a multi-study world. It's possible to fetch a token
+        //  for one study initially, then attempt to renew it when accessing another. For the moment,
+        //  assume that, if a study guid is included with the call, the client wants us to call the
+        //  registration endpoint.
+        var isRefreshTokenExchange = (context.protocol === "oauth2-refresh-token");
+        var needsStudyRegistration = !!(pepper_params.studyGuid);
+        var hasCachedUserGuid = !!(user.app_metadata.user_guid);
+        if (isRefreshTokenExchange && hasCachedUserGuid && !needsStudyRegistration) {
+            console.log('using app_metadata cached user GUID');
+            context.idToken[pepperUserGuidClaim] = user.app_metadata.user_guid;
+            return callback(null, user, context);
+        } else {
+            user.user_metadata = user.user_metadata || {};
+            if (user.user_metadata.first_name) {
+                pepper_params.firstName = user.user_metadata.first_name;
+                console.log('User metadata has first name = ' + pepper_params.firstName);
+            }
+            if (user.user_metadata.last_name) {
+                pepper_params.lastName = user.user_metadata.last_name;
+                console.log('User metadata has last name = ' + pepper_params.lastName);
+            }
+
+            request.post({
+                url: configuration.pepperBaseUrl + '/pepper/v1/register',
+                json: pepper_params,
+                timeout: 15000
+            }, function(err, response, body) {
+                if (err) {
+                    console.log('Error while registering auth0 user ' + user.user_id);
+                    console.log(err);
+                    let error = new Error(JSON.stringify({
+                        code: err.code,
+                        message: err.message,
+                        statusCode: 500
+                    }));
+                    return callback(error);
+                } else if (response && response.statusCode !== 200) {
+                    console.log('Failed to register auth0 user ' + user.user_id + ':' + response.statusCode);
+                    console.log(body);
+                    body = body || {};
+                    let error = new Error(JSON.stringify({
+                        code: body.code,
+                        message: body.message,
+                        statusCode: response.statusCode
+                    }));
+                    return callback(error);
+                } else {
+                    // all is well
+                    var ddpUserGuid = body.ddpUserGuid;
+                    user.app_metadata.user_guid = ddpUserGuid;
+
+                    auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
+                      .then(function(){
+                          context.idToken[pepperUserGuidClaim] = ddpUserGuid;
+                          console.log('Registered pepper user ' + ddpUserGuid + ' for auth0 user ' + user.user_id);
+
+                            // clear temp user guid after registration
+                              if (!!user.user_metadata.temp_user_guid){
+                               console.log('The temp_user_guid will be cleared for user ' + ddpUserGuid);
+                               user.user_metadata.temp_user_guid = null;
+                               auth0.users.updateUserMetadata(user.user_id, user.user_metadata)
+                                 .then(function(){
+                               console.log('Temp_user_guid was cleared for user ' + ddpUserGuid);
+                                  return callback(null, user, context);
+                                 })
+                                 .catch(function(err){
+                                   console.log('Error updating user metadata for auth0 user ' + user.user_id);
+                                                              console.log(err);
+                                                              let error = new Error(JSON.stringify({
+                                                                  code: err.code,
+                                                                  message: err.message,
+                                                                  statusCode: 500
+                                                              }));
+                                                              return callback(error);
+                                 });
+                              }
+                          return callback(null, user, context);
+                      })
+                      .catch(function(err){
+                          console.log('Error updating metadata for auth0 user ' + user.user_id);
+                          console.log(err);
+                          let error = new Error(JSON.stringify({
+                              code: err.code,
+                              message: err.message,
+                              statusCode: 500
+                          }));
+                          return callback(error);
+                      });
+                }
+            });
+        }
+    }
+}
