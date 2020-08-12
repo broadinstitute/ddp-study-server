75d839b426dbcd8029510ff955eb34f41e1af639
Marco Ocana <mocana@broadinstitute.org>
2020-08-12 13:32:39 -0400

    Remove unused import

--- pepper-apis/src/test/java/org/broadinstitute/ddp/service/ActivityValidationServiceTest.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/service/ActivityValidationServiceTest.java
@@ -30,7 +30,6 @@ import org.broadinstitute.ddp.util.TestUtil;
 import org.jdbi.v3.core.Handle;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ActivityValidationServiceTest extends TxnAwareBaseTest {
