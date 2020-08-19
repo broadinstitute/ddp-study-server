428403a733745db4ac59fa0ed47f5afa0c95792e
Sampath K. Settipalli <sampath@broadinstitute.org>
2020-08-19 20:37:14 +0530

    cleanup

--- pepper-apis/src/main/java/org/broadinstitute/ddp/export/collectors/CompositeQuestionFormatStrategy.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/export/collectors/CompositeQuestionFormatStrategy.java
@@ -19,7 +19,6 @@ import org.broadinstitute.ddp.model.activity.instance.answer.BoolAnswer;
 import org.broadinstitute.ddp.model.activity.instance.answer.CompositeAnswer;
 import org.broadinstitute.ddp.model.activity.instance.answer.DateAnswer;
 import org.broadinstitute.ddp.model.activity.instance.answer.DateValue;
-import org.broadinstitute.ddp.model.activity.instance.answer.NumericAnswer;
 import org.broadinstitute.ddp.model.activity.instance.answer.PicklistAnswer;
 import org.broadinstitute.ddp.model.activity.instance.answer.SelectedPicklistOption;
 import org.broadinstitute.ddp.model.activity.instance.answer.TextAnswer;
