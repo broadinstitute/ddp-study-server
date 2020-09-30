e260c924ad33f501a36ac02dcbf6bc1b1e6f7a88
Sampath K. Settipalli <sampath@broadinstitute.org>
2020-09-30 17:03:14 +0530

    Brain Pediatrics functionality

---
+++ study-builder/src/main/java/org/broadinstitute/ddp/studybuilder/task/AddBrainStudyActivities.java
@@ -0,0 +1,9 @@
+package org.broadinstitute.ddp.studybuilder.task;
+
+public class AddBrainStudyActivities extends AddStudyActivities {
+
+    public AddBrainStudyActivities() {
+        super("cmi-brain", "patches/new-study-activities.conf");
+    }
+
+}
