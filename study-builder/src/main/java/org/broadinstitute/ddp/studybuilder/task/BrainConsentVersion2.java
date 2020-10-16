aa5b99bd7e340d3fa8d19de1aaa73bc515732bb9
Pavel Sidorenko <fat.cat.1703@gmail.com>
2020-10-16 17:25:54 +0300

    DDP-5030: Brain pediatrics styling

--- study-builder/src/main/java/org/broadinstitute/ddp/studybuilder/task/BrainConsentVersion2.java
+++ study-builder/src/main/java/org/broadinstitute/ddp/studybuilder/task/BrainConsentVersion2.java
@@ -5,12 +5,14 @@ import java.nio.file.Path;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import com.google.gson.Gson;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.broadinstitute.ddp.cache.LanguageStore;
 import org.broadinstitute.ddp.db.dao.ActivityDao;
+import org.broadinstitute.ddp.db.dao.JdbiActivity;
 import org.broadinstitute.ddp.db.dao.JdbiActivityVersion;
 import org.broadinstitute.ddp.db.dao.JdbiFormSectionBlock;
 import org.broadinstitute.ddp.db.dao.JdbiQuestion;
@@ -21,16 +23,19 @@ import org.broadinstitute.ddp.db.dao.JdbiVariableSubstitution;
 import org.broadinstitute.ddp.db.dao.PdfDao;
 import org.broadinstitute.ddp.db.dao.SectionBlockDao;
 import org.broadinstitute.ddp.db.dao.UserDao;
+import org.broadinstitute.ddp.db.dto.ActivityDto;
 import org.broadinstitute.ddp.db.dto.ActivityVersionDto;
 import org.broadinstitute.ddp.db.dto.QuestionDto;
 import org.broadinstitute.ddp.db.dto.SectionBlockMembershipDto;
 import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.db.dto.TextQuestionDto;
 import org.broadinstitute.ddp.exception.DDPException;
+import org.broadinstitute.ddp.model.activity.definition.FormActivityDef;
 import org.broadinstitute.ddp.model.activity.definition.QuestionBlockDef;
 import org.broadinstitute.ddp.model.activity.definition.i18n.Translation;
 import org.broadinstitute.ddp.model.activity.definition.question.TextQuestionDef;
 import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
+import org.broadinstitute.ddp.model.activity.types.ListStyleHint;
 import org.broadinstitute.ddp.model.pdf.PdfActivityDataSource;
 import org.broadinstitute.ddp.model.pdf.PdfConfigInfo;
 import org.broadinstitute.ddp.model.pdf.PdfVersion;
@@ -42,6 +47,7 @@ import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindList;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.slf4j.Logger;
@@ -54,8 +60,9 @@ public class BrainConsentVersion2 implements CustomTask {
     private static final String BRAIN = "cmi-brain";
 
     private Path cfgPath;
-    private Config cfg;
+    private Config studyCfg;
     private Config dataCfg;
+    private Config varsCfg;
     private Instant timestamp;
     private String versionTag;
     private Gson gson;
@@ -72,37 +79,63 @@ public class BrainConsentVersion2 implements CustomTask {
             throw new DDPException("This task is only for the " + BRAIN + " study!");
         }
 
-        cfg = studyCfg;
+        this.studyCfg = studyCfg;
         versionTag = dataCfg.getString("versionTag");
         timestamp = Instant.now(); //parse(dataCfg.getString("timestamp"));
         this.cfgPath = cfgPath;
+        this.varsCfg = varsCfg;
         gson = GsonUtil.standardGson();
 
     }
 
-    @Override
+    //@Override
     public void run(Handle handle) {
         //creates version: 2 for CONSENT (self/adult consent) activity.
 
         LanguageStore.init(handle);
-        User adminUser = handle.attach(UserDao.class).findUserByGuid(cfg.getString("adminUser.guid")).get();
-        StudyDto studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid(cfg.getString("study.guid"));
-        PdfBuilder builder = new PdfBuilder(cfgPath.getParent(), cfg, studyDto, adminUser.getId());
+        User adminUser = handle.attach(UserDao.class).findUserByGuid(studyCfg.getString("adminUser.guid")).get();
+        StudyDto studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid(studyCfg.getString("study.guid"));
+        PdfBuilder builder = new PdfBuilder(cfgPath.getParent(), studyCfg, studyDto, adminUser.getId());
 
         String activityCode = dataCfg.getString("activityCode");
         String studyGuid = studyDto.getGuid();
         long studyId = studyDto.getId();
         long activityId = ActivityBuilder.findActivityId(handle, studyId, activityCode);
+        SqlHelper helper = handle.attach(SqlHelper.class);
 
+        //first update styling (applies to both versions: v1 and v2)
+        var activityBuilder = new ActivityBuilder(cfgPath.getParent(), studyCfg, varsCfg, studyDto, adminUser.getId());
+        var activityDao = handle.attach(ActivityDao.class);
+        var jdbiActivity = handle.attach(JdbiActivity.class);
+        var jdbiActVersion = handle.attach(JdbiActivityVersion.class);
+        Config definition = activityBuilder.readDefinitionConfig("consent.conf");
+
+        ActivityDto activityDto = jdbiActivity.findActivityByStudyIdAndCode(studyDto.getId(), activityCode).get();
+        ActivityVersionDto versionDto = jdbiActVersion.findByActivityCodeAndVersionTag(studyDto.getId(),
+                activityCode, "v1").get();
+        FormActivityDef activity = (FormActivityDef) activityDao.findDefByDtoAndVersion(activityDto, versionDto);
+
+        UpdateTemplatesInPlace updateTemplatesTask = new UpdateTemplatesInPlace();
+        updateTemplatesTask.traverseActivity(handle, activityCode, definition, activity);
+        LOG.info("updated variables for styling changes");
+
+        //update brain_consent_s3_election_agree listHintStyle to NONE
+        long tmplId = helper.findTemplateIdByTemplateText("$brain_consent_s3_election_agree");
+        helper.updateGroupHeaderStyleHintByTemplateId(tmplId, ListStyleHint.NONE.name());
+
+        //remove consent icons
+        List<Long> iconIds = helper.findSectionIconIdByActivity(activityId);
+        helper.deleteActivityIcons(Set.copyOf(iconIds));
+        LOG.info("deleted form section icons for consent");
+
+        //consent ver: 2 stuff
         String reason = String.format(
                 "Update activity with studyGuid=%s activityCode=%s to versionTag=%s",
                 studyGuid, activityCode, versionTag);
         RevisionMetadata meta = new RevisionMetadata(timestamp.toEpochMilli(), adminUser.getId(), reason);
 
-        SqlHelper helper = handle.attach(SqlHelper.class);
 
         JdbiRevision jdbiRevision = handle.attach(JdbiRevision.class);
-        ActivityDao activityDao = handle.attach(ActivityDao.class);
 
         //change version
         ActivityVersionDto activityVersionDto = activityDao.changeVersion(activityId, versionTag, meta);
@@ -186,8 +219,43 @@ public class BrainConsentVersion2 implements CustomTask {
         @SqlQuery("select template_variable_id from template_variable where template_id = :templateId")
         long findTemplateVariableId(@Bind("templateId") long templateId);
 
+        //WATCH OUT: might cause issue if same variable name/text across multiple studies.. ok for patch though.
+        //used max to handle multiple study versions in lower regions
+        @SqlQuery("select max(template_id) from template where template_text = :text")
+        long findTemplateIdByTemplateText(@Bind("text") String text);
+
+        @SqlUpdate("update block_group_header set list_style_hint_id = "
+                + " (select list_style_hint_id from list_style_hint where list_style_hint_code = :listStyle)"
+                + " where title_template_id = :templateId")
+        int updateGroupHeaderStyleHintByTemplateId(@Bind("templateId") long templateId, @Bind("listStyle") String listStyle);
+
         @SqlUpdate("update form_section__block set display_order = :displayOrder where form_section__block_id = :formSectionBlockId")
         int updateFormSectionBlockDisplayOrder(@Bind("formSectionBlockId") long formSectionBlockId, @Bind("displayOrder") int displayOrder);
+
+        @SqlQuery("select fsi.form_section_icon_id from form_section_icon fsi, form_activity__form_section fafs "
+                + " where fafs.form_section_id = fsi.form_section_id "
+                + " and fafs.form_activity_id = :studyActivityId")
+        List<Long> findSectionIconIdByActivity(@Bind("studyActivityId") long studyActivityId);
+
+        @SqlUpdate("delete from form_section_icon_source where form_section_icon_id in (<ids>)")
+        int _deleteActivityIconSources(@BindList("ids") Set<Long> ids);
+
+        @SqlUpdate("delete from form_section_icon where form_section_icon_id in (<ids>)")
+        int _deleteActivityIcons(@BindList("ids") Set<Long> ids);
+
+        default void deleteActivityIcons(Set<Long> ids) {
+            int numUpdated = _deleteActivityIconSources(ids);
+            if (numUpdated != 6) {
+                throw new DDPException("Expected to delete 6 rows from icon sources ="
+                        + " but deleted " + numUpdated);
+            }
+
+            numUpdated = _deleteActivityIcons(ids);
+            if (numUpdated != 6) {
+                throw new DDPException("Expected to delete 6 rows from form section icons ="
+                        + " but deleted " + numUpdated);
+            }
+        }
 
     }
 }
