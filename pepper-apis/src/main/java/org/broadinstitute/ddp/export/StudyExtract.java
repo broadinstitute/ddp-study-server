7796187a11a78f5c9eef6ef09fdbd97c98fbc273
Marco Ocana <mocana@broadinstitute.org>
2020-10-22 13:15:32 -0400

    Checkstyle fix

--- pepper-apis/src/main/java/org/broadinstitute/ddp/export/StudyExtract.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/export/StudyExtract.java
@@ -3,7 +3,6 @@ package org.broadinstitute.ddp.export;
 import java.util.List;
 import java.util.Map;
 
-import org.broadinstitute.ddp.db.dto.InvitationDto;
 import org.broadinstitute.ddp.model.pdf.PdfConfigInfo;
 import org.broadinstitute.ddp.model.pdf.PdfVersion;
 
