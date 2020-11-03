25bf8b3dd7469a1cee8994932966bbe451ad449b
Andrew Zimmer <andrew@broadinstitute.org>
2020-11-03 14:40:17 -0500

    DDP-5110 slack unfurling and dialing back slack overload (#474)
    DDP-5110 stop unfurling alert messages in slack.  Removed jslack dependency, replaced test with mockserver, and brought over lossy slack appender changes from birch.

---
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/thread/ThreadFactory.java
@@ -0,0 +1,24 @@
+package org.broadinstitute.ddp.thread;
+
+/**
+ * Simple {@link java.util.concurrent.ThreadFactory} that lets
+ * you set the name for the group and the priority.
+ */
+public class ThreadFactory implements java.util.concurrent.ThreadFactory {
+
+    private final String name;
+    private final int priority;
+
+    public ThreadFactory(String name, int priority) {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        ThreadGroup threadGroup = new ThreadGroup(name);
+        Thread t = new Thread(threadGroup, r);
+        t.setPriority(priority);
+        return t;
+    }
+}
---
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/log/SlackAppenderTest.java
@@ -0,0 +1,69 @@
+package org.broadinstitute.ddp.log;
+
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
+
+import ch.qos.logback.classic.spi.LoggingEvent;
+import com.google.gson.Gson;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockserver.client.MockServerClient;
+import org.mockserver.junit.MockServerRule;
+import org.mockserver.model.JsonBody;
+
+public class SlackAppenderTest {
+
+    @Rule
+    public MockServerRule mockServerRule = new MockServerRule(this);
+
+    private MockServerClient mockServerClient;
+
+    private LoggingEvent loggingEvent = new LoggingEvent();
+
+    @Before
+    public void setUp() {
+        loggingEvent.setMessage("Hi there");
+        loggingEvent.setCallerData(new StackTraceElement[] {new StackTraceElement("Foo",
+                "bar",
+                "Foo.java",
+                0)});
+    }
+
+    @Test
+    public void testSuccessfulLoggingToSlack() throws Exception {
+        mockServerClient = mockServerClient.reset();
+
+        if (mockServerClient.hasStarted()) {
+            mockServerClient.when(request().withPath("/mock_slack_test"))
+                    .respond(response()
+                            .withStatusCode(200)
+                            .withBody("ok"));
+
+            SlackAppender slackAppender = new SlackAppender("http://localhost:" + mockServerRule.getPort() + "/mock_slack_test",
+                    "SlackChannel", 100, 10);
+            slackAppender.start();
+            slackAppender.doAppend(loggingEvent);
+
+            slackAppender.waitForClearToQueue(3000);
+
+            mockServerClient.verify(request().withPath("/mock_slack_test").withBody(JsonBody.json(
+                    new Gson().toJson(new SlackAppender.SlackMessagePayload("*Hi there*\n ``````",
+                            "SlackChannel",
+                            "Pepper",
+                            ":nerd_face:")))));
+        } else {
+            Assert.fail("Mock slack not running");
+        }
+    }
+
+    @After
+    public void shutDown() {
+        mockServerClient.stop();
+    }
+
+}
