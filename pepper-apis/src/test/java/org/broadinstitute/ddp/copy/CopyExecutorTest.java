ca99851d8d6f8b99c996c533949e59b36a394f38
Yufeng Wang <yufeng@broadinstitute.org>
2020-11-17 13:13:41 -0500

    DDP-5175 support copying answers from previous instance (#471)

--- pepper-apis/src/test/java/org/broadinstitute/ddp/copy/CopyExecutorTest.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/copy/CopyExecutorTest.java
@@ -12,8 +12,14 @@ import org.broadinstitute.ddp.db.dao.AnswerDao;
 import org.broadinstitute.ddp.db.dao.CopyConfigurationDao;
 import org.broadinstitute.ddp.db.dao.UserProfileDao;
 import org.broadinstitute.ddp.db.dto.ActivityInstanceDto;
+import org.broadinstitute.ddp.model.activity.definition.question.NumericQuestionDef;
+import org.broadinstitute.ddp.model.activity.definition.template.Template;
 import org.broadinstitute.ddp.model.activity.instance.answer.Answer;
+import org.broadinstitute.ddp.model.activity.instance.answer.CompositeAnswer;
+import org.broadinstitute.ddp.model.activity.instance.answer.NumericIntegerAnswer;
 import org.broadinstitute.ddp.model.activity.instance.answer.TextAnswer;
+import org.broadinstitute.ddp.model.activity.types.NumericType;
+import org.broadinstitute.ddp.model.activity.types.QuestionType;
 import org.broadinstitute.ddp.model.copy.CopyAnswerLocation;
 import org.broadinstitute.ddp.model.copy.CopyConfiguration;
 import org.broadinstitute.ddp.model.copy.CopyConfigurationPair;
@@ -46,7 +52,7 @@ public class CopyExecutorTest extends TxnAwareBaseTest {
             var answer = new TextAnswer(null, act.getTextQuestion().getStableId(), null, "new-first-name");
             handle.attach(AnswerDao.class).createAnswer(testData.getUserId(), instanceId, answer);
 
-            var config = new CopyConfiguration(testData.getStudyId(), List.of(new CopyConfigurationPair(
+            var config = new CopyConfiguration(testData.getStudyId(), false, List.of(new CopyConfigurationPair(
                     new CopyAnswerLocation(act.getTextQuestion().getStableId()),
                     new CopyLocation(CopyLocationType.PARTICIPANT_PROFILE_FIRST_NAME))));
             config = handle.attach(CopyConfigurationDao.class).createCopyConfig(config);
@@ -76,7 +82,7 @@ public class CopyExecutorTest extends TxnAwareBaseTest {
                     .build(handle, testData.getUserId(), testData.getStudyGuid());
             long instance2Id = createInstance(handle, act2.getDef().getActivityId()).getId();
 
-            var config = new CopyConfiguration(testData.getStudyId(), List.of(new CopyConfigurationPair(
+            var config = new CopyConfiguration(testData.getStudyId(), false, List.of(new CopyConfigurationPair(
                     new CopyAnswerLocation(act1.getTextQuestion().getStableId()),
                     new CopyAnswerLocation(act2.getTextQuestion().getStableId()))));
             config = handle.attach(CopyConfigurationDao.class).createCopyConfig(config);
@@ -89,6 +95,57 @@ public class CopyExecutorTest extends TxnAwareBaseTest {
             assertNotNull(actual);
             assertNotNull(actual.getAnswerGuid());
             assertEquals("source-text", ((TextAnswer) actual).getValue());
+
+            handle.rollback();
+        });
+    }
+
+    @Test
+    public void testExecute_copyFromPreviousInstance() {
+        TransactionWrapper.useTxn(handle -> {
+            TestFormActivity act = TestFormActivity.builder()
+                    .withTextQuestion(true)
+                    .withCompositeQuestion(true, NumericQuestionDef
+                            .builder(NumericType.INTEGER, "child-num", Template.text("child-num-prompt"))
+                            .build())
+                    .build(handle, testData.getUserId(), testData.getStudyGuid());
+            long instance1Id = createInstance(handle, act.getDef().getActivityId()).getId();
+
+            AnswerDao answerDao = handle.attach(AnswerDao.class);
+            var answer = new TextAnswer(null, act.getTextQuestion().getStableId(), null, "prev-text");
+            answerDao.createAnswer(testData.getUserId(), instance1Id, answer);
+
+            var compAnswer = new CompositeAnswer(null, act.getCompositeQuestion().getStableId(), null);
+            compAnswer.addRowOfChildAnswers(new NumericIntegerAnswer(null, "child-num", null, 1L));
+            compAnswer.addRowOfChildAnswers(new NumericIntegerAnswer(null, "child-num", null, 25L));
+            answerDao.createAnswer(testData.getUserId(), instance1Id, compAnswer);
+
+            var config = new CopyConfiguration(testData.getStudyId(), true, List.of());
+            config = handle.attach(CopyConfigurationDao.class).createCopyConfig(config);
+
+            long instance2Id = createInstance(handle, act.getDef().getActivityId()).getId();
+            new CopyExecutor()
+                    .withTriggeredInstanceId(instance2Id)
+                    .execute(handle, testData.getUserId(), testData.getUserId(), config);
+
+            var actual = handle.attach(ActivityInstanceDao.class)
+                    .findFormResponseWithAnswersByInstanceId(instance2Id)
+                    .orElse(null);
+            assertNotNull(actual);
+            assertEquals(2, actual.getAnswers().size());
+
+            var actualAnswer = actual.getAnswer(act.getTextQuestion().getStableId());
+            assertNotNull(actualAnswer);
+            assertEquals(QuestionType.TEXT, actualAnswer.getQuestionType());
+            assertEquals("prev-text", ((TextAnswer) actualAnswer).getValue());
+
+            actualAnswer = actual.getAnswer(act.getCompositeQuestion().getStableId());
+            assertNotNull(actualAnswer);
+            assertEquals(QuestionType.COMPOSITE, actualAnswer.getQuestionType());
+            var actualCompAnswer = (CompositeAnswer) actualAnswer;
+            assertEquals(2, actualCompAnswer.getValue().size());
+            assertEquals(1L, actualCompAnswer.getValue().get(0).getValues().get(0).getValue());
+            assertEquals(25L, actualCompAnswer.getValue().get(1).getValues().get(0).getValue());
 
             handle.rollback();
         });
