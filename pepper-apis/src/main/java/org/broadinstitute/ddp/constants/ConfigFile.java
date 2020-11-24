caf1adf3b41f38af00879f16eef173f10980b008
Marco Ocana <mocana@broadinstitute.org>
2020-11-23 20:33:07 -0500

    Add new config params for SlackAppender

--- pepper-apis/src/main/java/org/broadinstitute/ddp/constants/ConfigFile.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/constants/ConfigFile.java
@@ -102,6 +102,8 @@ public class ConfigFile {
     public static final String PUBSUB_HKEEP_TASKS_SUB = "pubsub.housekeepingTasksSubscription";
     public static final String SLACK_HOOK = "slack.hook";
     public static final String SLACK_CHANNEL = "slack.channel";
+    public static final String SLACK_QUEUE_SIZE = "slack.queueSize";
+    public static final String SLACK_INTERVAL_IN_MILLIS = "slack.intervalInMillis";
     public static final String TEST_USER_AUTH0_ID = "testUserAuth0Id";
 
     public static final String DOMAIN = "domain";
