ea1e577ebe176f88bc255cc971886d4a840c7b28
Yufeng Wang <yufeng@broadinstitute.org>
2021-01-08 13:00:50 -0500

    DDP-5498 testboston: add vaccine questions to longitudinal v2 (#582)

--- study-builder/src/main/java/org/broadinstitute/ddp/studybuilder/task/TestBostonLongitudinalV2.java
+++ study-builder/src/main/java/org/broadinstitute/ddp/studybuilder/task/TestBostonLongitudinalV2.java
@@ -1,12 +1,17 @@
 package org.broadinstitute.ddp.studybuilder.task;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.broadinstitute.ddp.db.dao.ActivityDao;
+import org.broadinstitute.ddp.db.dao.EventDao;
 import org.broadinstitute.ddp.db.dao.JdbiActivity;
 import org.broadinstitute.ddp.db.dao.JdbiActivityVersion;
 import org.broadinstitute.ddp.db.dao.JdbiBlockNesting;
@@ -20,10 +25,18 @@ import org.broadinstitute.ddp.exception.DDPException;
 import org.broadinstitute.ddp.model.activity.definition.ActivityDef;
 import org.broadinstitute.ddp.model.activity.definition.ConditionalBlockDef;
 import org.broadinstitute.ddp.model.activity.definition.FormActivityDef;
+import org.broadinstitute.ddp.model.activity.definition.FormSectionDef;
 import org.broadinstitute.ddp.model.activity.definition.QuestionBlockDef;
 import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
+import org.broadinstitute.ddp.model.activity.types.BlockType;
+import org.broadinstitute.ddp.model.activity.types.EventActionType;
+import org.broadinstitute.ddp.model.activity.types.EventTriggerType;
+import org.broadinstitute.ddp.model.activity.types.InstanceStatusType;
+import org.broadinstitute.ddp.model.event.ActivityStatusChangeTrigger;
+import org.broadinstitute.ddp.model.event.EventConfiguration;
 import org.broadinstitute.ddp.model.user.User;
 import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
+import org.broadinstitute.ddp.studybuilder.EventBuilder;
 import org.broadinstitute.ddp.util.ConfigUtil;
 import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
@@ -34,6 +47,7 @@ public class TestBostonLongitudinalV2 implements CustomTask {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestBostonLongitudinalV2.class);
     private static final String LONGITUDINAL_V2_FILE = "longitudinal-covid-v2.conf";
+    private static final String VACCINE_COPY_EVENT_FILE = "patches/vaccine-copy-answer-event.conf";
     private static final String STUDY_GUID = "testboston";
     private static final String V1_VERSION_TAG = "v1";
 
@@ -58,6 +72,7 @@ public class TestBostonLongitudinalV2 implements CustomTask {
         StudyDto studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid(STUDY_GUID);
         User adminUser = handle.attach(UserDao.class).findUserByGuid(studyCfg.getString("adminUser.guid")).get();
         var activityBuilder = new ActivityBuilder(cfgPath.getParent(), studyCfg, varsCfg, studyDto, adminUser.getId());
+        var eventBuilder = new EventBuilder(studyCfg, studyDto, adminUser.getId());
 
         var activityDao = handle.attach(ActivityDao.class);
         var sectionBlockDao = handle.attach(SectionBlockDao.class);
@@ -72,13 +87,23 @@ public class TestBostonLongitudinalV2 implements CustomTask {
         activityBuilder.validateDefinition(v2Def);
         LOG.info("Loaded activity definition from file: {}", LONGITUDINAL_V2_FILE);
 
-        // Extract the new question blocks.
-        ConditionalBlockDef question1 = (ConditionalBlockDef) v2Def.getSections().get(0).getBlocks().get(1);
-        Map<String, QuestionBlockDef> mapping = question1.getNested().stream().collect(Collectors.toMap(
-                b -> ((QuestionBlockDef) b).getQuestion().getStableId(), b -> (QuestionBlockDef) b));
-        QuestionBlockDef hospitalizedBlockDef = mapping.get(varsCfg.getString("id.q.longitudinal_hospitalized"));
-        QuestionBlockDef hospitalNameBlockDef = mapping.get(varsCfg.getString("id.q.longitudinal_hospital_name"));
-        QuestionBlockDef hospitalDaysBlockDef = mapping.get(varsCfg.getString("id.q.longitudinal_hospital_days"));
+        // Extract the new question block definitions.
+        FormSectionDef v2SectionDef = v2Def.getSections().get(0);
+        ConditionalBlockDef v2Question1 = (ConditionalBlockDef) v2SectionDef.getBlocks().get(1);
+
+        Map<String, QuestionBlockDef> mapping1 = v2Question1.getNested().stream()
+                .map(b -> (QuestionBlockDef) b)
+                .collect(Collectors.toMap(b -> b.getQuestion().getStableId(), Function.identity()));
+        QuestionBlockDef hospitalizedBlockDef = mapping1.get(varsCfg.getString("id.q.longitudinal_hospitalized"));
+        QuestionBlockDef hospitalNameBlockDef = mapping1.get(varsCfg.getString("id.q.longitudinal_hospital_name"));
+        QuestionBlockDef hospitalDaysBlockDef = mapping1.get(varsCfg.getString("id.q.longitudinal_hospital_days"));
+
+        Map<String, ConditionalBlockDef> mapping2 = v2SectionDef.getBlocks().stream()
+                .filter(b -> b.getBlockType() == BlockType.CONDITIONAL)
+                .map(b -> (ConditionalBlockDef) b)
+                .collect(Collectors.toMap(b -> b.getControl().getStableId(), Function.identity()));
+        ConditionalBlockDef vaccineStudyBlockDef = mapping2.get(varsCfg.getString("id.q.longitudinal_vaccine_study"));
+        ConditionalBlockDef vaccineReceivedBlockDef = mapping2.get(varsCfg.getString("id.q.longitudinal_vaccine_received"));
 
         //
         // Create version 2.
@@ -109,11 +134,13 @@ public class TestBostonLongitudinalV2 implements CustomTask {
         //
 
         FormActivityDef currentDef = (FormActivityDef) activityDao.findDefByDtoAndVersion(activityDto, v2Dto);
-        ConditionalBlockDef question1Def = (ConditionalBlockDef) currentDef.getSections().get(0).getBlocks().get(1);
+        FormSectionDef currentSectionDef = currentDef.getSections().get(0);
+        ConditionalBlockDef question1Def = (ConditionalBlockDef) currentSectionDef.getBlocks().get(1);
+        long sectionId = currentSectionDef.getSectionId();
         long parentBlockId = question1Def.getBlockId();
         long revisionId = v2Dto.getRevId();
 
-        // New question blocks should show up between 3rd and 4th nested blocks, so use display order between 30 and 40.
+        // Hospital question blocks should show up between 3rd and 4th nested blocks, so use display order between 30 and 40.
         // We leave a gap in-between these new display orders so we have room to insert other stuff in the future.
         int displayOrder = 32;
 
@@ -133,6 +160,55 @@ public class TestBostonLongitudinalV2 implements CustomTask {
                 hospitalDaysBlockDef.getQuestion().getStableId(), question1Def.getControl().getStableId(), displayOrder);
         sectionBlockDao.insertBlockByType(activityId, hospitalDaysBlockDef, revisionId);
         jdbiBlockNesting.insert(parentBlockId, hospitalDaysBlockDef.getBlockId(), displayOrder, revisionId);
+
+        // Vaccine question blocks should show up after Flu Shot question which is 7th, so start display order after 70.
+        displayOrder = 73;
+
+        LOG.info("Adding question block {} to section {} with displayOrder={}...",
+                vaccineStudyBlockDef.getControl().getStableId(), sectionId, displayOrder);
+        sectionBlockDao.insertBlockForSection(activityId, sectionId, displayOrder, vaccineStudyBlockDef, revisionId);
+
+        displayOrder = 76;
+        LOG.info("Adding question block {} to section {} with displayOrder={}...",
+                vaccineReceivedBlockDef.getControl().getStableId(), sectionId, displayOrder);
+        sectionBlockDao.insertBlockForSection(activityId, sectionId, displayOrder, vaccineReceivedBlockDef, revisionId);
+
+        //
+        // Ensure we have longitudinal copy answer event configuration.
+        //
+
+        List<EventConfiguration> events = handle.attach(EventDao.class)
+                .getAllEventConfigurationsByStudyId(studyDto.getId());
+
+        LOG.info("Searching for longitudinal copy answer event configuration...");
+        EventConfiguration copyEvent = null;
+        for (var event : events) {
+            if (event.getEventTriggerType() == EventTriggerType.ACTIVITY_STATUS) {
+                var trigger = (ActivityStatusChangeTrigger) event.getEventTrigger();
+                if (trigger.getStudyActivityId() == activityId && trigger.getInstanceStatusType() == InstanceStatusType.CREATED) {
+                    if (event.getEventActionType() == EventActionType.COPY_ANSWER) {
+                        copyEvent = event;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (copyEvent != null) {
+            LOG.info("Already has longitudinal copy answer event config with id={}", copyEvent.getEventConfigurationId());
+        } else {
+            LOG.info("Did not find longitudinal copy answer event, creating...");
+
+            // Load the copy answer event definition.
+            File file = cfgPath.getParent().resolve(VACCINE_COPY_EVENT_FILE).toFile();
+            if (!file.exists()) {
+                throw new DDPException("Data file is missing: " + file);
+            }
+            Config copyEventCfg = ConfigFactory.parseFile(file);
+            copyEventCfg = copyEventCfg.resolveWith(varsCfg);
+
+            eventBuilder.insertEvent(handle, copyEventCfg);
+        }
 
         LOG.info("Done");
     }
