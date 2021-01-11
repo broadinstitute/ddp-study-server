39c260dc75a115d394bf42f7d37e57465a1721c4
Evgenii Priazhennikov <Evgenii_Priazhennikov@epam.com>
2021-01-11 16:52:52 +0300

    DDP-5442 -  log auth0 log events back to to 'info' log

--- pepper-apis/src/main/java/org/broadinstitute/ddp/route/Auth0LogEventRoute.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/route/Auth0LogEventRoute.java
@@ -19,7 +19,7 @@ public class Auth0LogEventRoute implements Route {
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        LOG.error("AUTH0-LOG-EVENT:\n-------------------------\n" + request.body() + "\n-------------------------");
+        LOG.info("AUTH0-LOG-EVENT:\n-------------------------\n" + request.body() + "\n-------------------------");
         return null;
     }
 
