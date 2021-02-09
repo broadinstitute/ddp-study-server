bce8e026fdb74e75de77163ddd3be9224d0047db
Yufeng Wang <yufeng@broadinstitute.org>
2021-02-09 13:52:10 -0500

    [DDP-3709] part 5: file scan result handler (#626)

--- pepper-apis/src/main/java/org/broadinstitute/ddp/client/GoogleBucketClient.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/client/GoogleBucketClient.java
@@ -1,28 +1,59 @@
 package org.broadinstitute.ddp.client;
 
 import com.google.auth.Credentials;
+import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.broadinstitute.ddp.exception.DDPException;
 
 /**
  * A client wrapper around Google Bucket services.
  */
 public class GoogleBucketClient {
 
-    private final StorageOptions storage;
+    private final Storage storage;
 
     public GoogleBucketClient(String gcpProjectId, Credentials credentials) {
         this(StorageOptions.newBuilder()
                 .setCredentials(credentials)
                 .setProjectId(gcpProjectId)
-                .build());
+                .build().getService());
     }
 
-    public GoogleBucketClient(StorageOptions storage) {
+    public GoogleBucketClient(Storage storage) {
         this.storage = storage;
     }
 
     public Bucket getBucket(String bucketName) {
-        return storage.getService().get(bucketName);
+        return storage.get(bucketName);
+    }
+
+    public Blob getBlob(String bucketName, String fileName) {
+        return storage.get(bucketName, fileName);
+    }
+
+    public Blob copyBlob(Blob sourceBlob, String destBucketName, String destFileName) {
+        try {
+            return sourceBlob.copyTo(destBucketName, destFileName).getResult();
+        } catch (Exception e) {
+            throw new DDPException("Failed to copy source blob " + sourceBlob.getBlobId()
+                    + " to destination bucket " + destBucketName);
+        }
+    }
+
+    public Blob moveBlob(Blob sourceBlob, String destBucketName, String destFileName) {
+        // No native support for moving blobs, so have to emulate with a two-step operation.
+        Blob destBlob = copyBlob(sourceBlob, destBucketName, destFileName);
+        deleteBlob(sourceBlob);
+        return destBlob;
+    }
+
+    public void deleteBlob(Blob blob) {
+        try {
+            blob.delete();
+        } catch (Exception e) {
+            throw new DDPException("Failed to delete blob " + blob.getBlobId());
+        }
     }
 }
