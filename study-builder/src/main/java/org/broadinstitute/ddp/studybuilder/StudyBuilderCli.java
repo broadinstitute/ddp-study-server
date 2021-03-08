a4e7789ca200e378659b85a0bf55ea655dd27bcb
Marco Ocana <mocana@broadinstitute.org>
2021-03-08 14:27:42 -0500

    Enable resolving substitutions within study.conf file

--- study-builder/src/main/java/org/broadinstitute/ddp/studybuilder/StudyBuilderCli.java
+++ study-builder/src/main/java/org/broadinstitute/ddp/studybuilder/StudyBuilderCli.java
@@ -118,8 +118,7 @@ public class StudyBuilderCli {
         log("using study configuration file: %s", cfgPath);
 
         log("resolving study configuration...");
-        studyCfg = studyCfg.resolveWith(varsCfg);
-
+        studyCfg = studyCfg.withFallback(varsCfg);
         boolean isDryRun = cmd.hasOption("dry-run");
         if (cmd.hasOption(OPT_RUN_TASK)) {
             String taskName = cmd.getOptionValue(OPT_RUN_TASK);
