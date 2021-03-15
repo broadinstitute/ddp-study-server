7f1bba599edac43274bc0a680d7ba40fe40af4a8
Yuriy Pavlov <ypavlov@broadinstitute.org>
2021-03-15 20:00:34 +0300

    DDP-5676 Add workflow statuses to /status endpoint

--- pepper-apis/src/main/java/org/broadinstitute/ddp/route/GetDsmParticipantStatusRoute.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/route/GetDsmParticipantStatusRoute.java
@@ -5,17 +5,31 @@ import org.broadinstitute.ddp.client.DsmClient;
 import org.broadinstitute.ddp.constants.ErrorCodes;
 import org.broadinstitute.ddp.constants.RouteConstants;
 import org.broadinstitute.ddp.db.TransactionWrapper;
+import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dao.JdbiUserStudyEnrollment;
+import org.broadinstitute.ddp.db.dto.StudyDto;
+import org.broadinstitute.ddp.elastic.ElasticSearchIndexType;
 import org.broadinstitute.ddp.json.errors.ApiError;
 import org.broadinstitute.ddp.model.dsm.ParticipantStatusTrackingInfo;
 import org.broadinstitute.ddp.model.user.EnrollmentStatusType;
 import org.broadinstitute.ddp.util.ResponseUtil;
 import org.broadinstitute.ddp.util.RouteUtil;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.broadinstitute.ddp.util.ElasticsearchServiceUtil.getIndexForStudy;
 
 /**
  * This route calls DSM and returns the DTO after enriching it with certain information
@@ -26,8 +40,11 @@ public class GetDsmParticipantStatusRoute implements Route {
 
     private final DsmClient dsm;
 
-    public GetDsmParticipantStatusRoute(DsmClient dsmClient) {
+    private final RestHighLevelClient esClient;
+
+    public GetDsmParticipantStatusRoute(DsmClient dsmClient, RestHighLevelClient esClient) {
         this.dsm = dsmClient;
+        this.esClient = esClient;
     }
 
     /**
@@ -65,7 +82,16 @@ public class GetDsmParticipantStatusRoute implements Route {
         LOG.info("DSM call completed, study={} and participant={}, status={}", studyGuid, userGuid, result.getStatusCode());
 
         if (result.getStatusCode() == 200) {
-            return new ParticipantStatusTrackingInfo(result.getBody(), status, userGuid);
+            List<ParticipantStatusTrackingInfo.Workflow> workflows;
+            try {
+                workflows = getWorkflowsFromEs(userGuid, studyGuid);
+            } catch (IOException e) {
+                String errMsg = "Something went wrong during fetching the workflow statuses from ES for study "
+                        + studyGuid + " and participant " + userGuid + ".";
+                LOG.error(errMsg, e);
+                throw ResponseUtil.haltError(500, new ApiError(ErrorCodes.SERVER_ERROR, errMsg));
+            }
+            return new ParticipantStatusTrackingInfo(result.getBody(), workflows, status, userGuid);
         } else if (result.getStatusCode() == 404) {
             String errMsg = "Participant " + userGuid + " or study " + studyGuid + " not found";
             LOG.warn(errMsg);
@@ -79,5 +105,31 @@ public class GetDsmParticipantStatusRoute implements Route {
             LOG.error(errMsg);
             throw ResponseUtil.haltError(500, new ApiError(ErrorCodes.SERVER_ERROR, errMsg));
         }
+    }
+
+    private List<ParticipantStatusTrackingInfo.Workflow> getWorkflowsFromEs(String userGuid, String studyGuid) throws IOException {
+        List<ParticipantStatusTrackingInfo.Workflow> result = new ArrayList<>();
+        if (esClient != null) {
+            String esIndex = TransactionWrapper.withTxn(handle -> {
+                StudyDto studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid(studyGuid);
+                return getIndexForStudy(handle, studyDto, ElasticSearchIndexType.PARTICIPANTS_STRUCTURED);
+            });
+            GetRequest getRequest = new GetRequest(esIndex, "_doc", userGuid);
+            GetResponse esResponse = esClient.get(getRequest, RequestOptions.DEFAULT);
+            Map<String, Object> source = esResponse.getSource();
+            if (source != null) {
+                List<?> workflows = (List<?>) source.get("workflows");
+                if (workflows != null) {
+                    for (var wfItem : workflows) {
+                        Map<?, ?> workflow = (Map<?, ?>) wfItem;
+                        result.add(new ParticipantStatusTrackingInfo.Workflow(
+                                (String) workflow.get("workflow"),
+                                (String) workflow.get("status"))
+                        );
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
