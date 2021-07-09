e9026fb3466fcafe9a82ae42777fa79d6a32d14e
Evgenii Priazhennikov <epriazhe@broadinstitute.org>
2021-07-09 19:31:24 +0300

    DDP-6306 - refactor an hierarchy of activity instance creation event sync processors.

--- pepper-apis/src/main/java/org/broadinstitute/ddp/model/event/activityinstancecreation/ActivityInstanceCreationEventSyncProcessor.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/model/event/activityinstancecreation/ActivityInstanceCreationEventSyncProcessor.java
@@ -1,27 +1,120 @@
 package org.broadinstitute.ddp.model.event.activityinstancecreation;
 
+import java.util.List;
+
+import org.broadinstitute.ddp.db.dao.JdbiActivity;
+import org.broadinstitute.ddp.db.dao.JdbiActivityInstance;
+import org.broadinstitute.ddp.db.dao.JdbiActivityInstanceStatus;
+import org.broadinstitute.ddp.db.dto.ActivityDto;
 import org.broadinstitute.ddp.model.activity.types.EventActionType;
+import org.broadinstitute.ddp.model.event.DsmNotificationSignal;
 import org.broadinstitute.ddp.model.event.EventSignal;
 import org.broadinstitute.ddp.service.ActivityInstanceCreationService;
 import org.jdbi.v3.core.Handle;
 
 
 /**
- * Interface defining a synchronous processor for event of type {@link EventActionType#ACTIVITY_INSTANCE_CREATION}
+ * Base class implementing a synchronous processor for event of type {@link EventActionType#ACTIVITY_INSTANCE_CREATION}
  */
-public interface ActivityInstanceCreationEventSyncProcessor {
+public abstract class ActivityInstanceCreationEventSyncProcessor {
 
-    void create();
+    protected final Handle handle;
+    protected final EventSignal signal;
 
-    long createActivityInstance(Long parentActivityId);
+    protected final JdbiActivityInstance jdbiActivityInstance;
+    protected final JdbiActivityInstanceStatus jdbiActivityInstanceStatus;
+    protected final JdbiActivity jdbiActivity;
 
-    int detectPossibleNumberOfInstancesToCreate(int instancesToCreate);
+    protected final long studyActivityId;
+    protected final ActivityInstanceCreationService creationService;
 
-    Handle getHandle();
+    protected ActivityDto activityDto;
 
-    EventSignal getSignal();
 
-    long getStudyActivityId();
+    public ActivityInstanceCreationEventSyncProcessor(
+            Handle handle, EventSignal signal, long studyActivityId, ActivityInstanceCreationService creationService) {
+        this.handle = handle;
+        this.signal = signal;
+        this.studyActivityId = studyActivityId;
+        this.creationService = creationService;
+        this.jdbiActivityInstance = handle.attach(JdbiActivityInstance.class);
+        this.jdbiActivityInstanceStatus = handle.attach(JdbiActivityInstanceStatus.class);
+        this.jdbiActivity = handle.attach(JdbiActivity.class);
+    }
 
-    ActivityInstanceCreationService getCreationService();
+    /**
+     * Create activity instance (or instances).
+     * Do necessary pre- and post-processing (depending on implementation).
+     *
+     */
+    public abstract void create();
+
+    /**
+     * Detect number of instances which can be really created
+     */
+    protected abstract int detectPossibleNumberOfInstancesToCreate(int instancesToCreate);
+
+    /**
+     * Create an activity instance.
+     *
+     * @return long  ID of a new instance
+     */
+    public long createActivityInstance(Long parentActivityId) {
+
+        Long parentInstanceId = creationService.detectParentInstanceId(parentActivityId);
+
+        ActivityInstanceCreationService.ActivityInstanceCreationResult creationResult = creationService.createActivityInstance(
+                studyActivityId,
+                parentInstanceId,
+                jdbiActivityInstance,
+                jdbiActivityInstanceStatus,
+                false,
+                null);
+
+        long newActivityInstanceId = creationResult.getActivityInstanceId();
+        String newActivityInstanceGuid = creationResult.getActivityInstanceGuid();
+
+        if (signal instanceof DsmNotificationSignal) {
+            creationService.saveKitEventData(handle, newActivityInstanceId);
+        }
+
+        creationService.runDownstreamEvents(studyActivityId, newActivityInstanceId, handle);
+
+        createChildActivityInstances(studyActivityId, newActivityInstanceId, newActivityInstanceGuid);
+
+        return newActivityInstanceId;
+    }
+
+    /**
+     * Create child nested activity instances, if any
+     */
+    public void createChildActivityInstances(long parentActivityId, long parentActivityInstanceId, String parentActivityInstanceGuid) {
+        List<Long> childActIdsToCreate = handle.attach(JdbiActivity.class).findChildActivityIdsThatNeedCreation(parentActivityId);
+        for (var activityId : childActIdsToCreate) {
+            ActivityInstanceCreationService.ActivityInstanceCreationResult creationResult = creationService.createActivityInstance(
+                    activityId,
+                    parentActivityInstanceId,
+                    jdbiActivityInstance,
+                    jdbiActivityInstanceStatus,
+                    true,
+                    parentActivityInstanceGuid);
+            creationService.runDownstreamEvents(activityId, creationResult.getActivityInstanceId(), handle);
+        }
+    }
+
+    public Handle getHandle() {
+        return handle;
+    }
+
+    public EventSignal getSignal() {
+        return signal;
+    }
+
+    public long getStudyActivityId() {
+        return studyActivityId;
+    }
+
+    public ActivityInstanceCreationService getCreationService() {
+        return creationService;
+    }
 }
--- pepper-apis/src/main/java/org/broadinstitute/ddp/model/event/activityinstancecreation/ActivityInstanceCreationEventSyncProcessorDefault.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/model/event/activityinstancecreation/ActivityInstanceCreationEventSyncProcessorDefault.java
@@ -1,13 +1,6 @@
 package org.broadinstitute.ddp.model.event.activityinstancecreation;
 
-import java.util.List;
-
-import org.broadinstitute.ddp.db.dao.JdbiActivity;
-import org.broadinstitute.ddp.db.dao.JdbiActivityInstance;
-import org.broadinstitute.ddp.db.dao.JdbiActivityInstanceStatus;
-import org.broadinstitute.ddp.db.dto.ActivityDto;
 import org.broadinstitute.ddp.model.event.ActivityInstanceCreationEventAction;
-import org.broadinstitute.ddp.model.event.DsmNotificationSignal;
 import org.broadinstitute.ddp.model.event.EventSignal;
 import org.broadinstitute.ddp.service.ActivityInstanceCreationService;
 import org.jdbi.v3.core.Handle;
@@ -16,33 +9,16 @@ import org.jdbi.v3.core.Handle;
  * Default activity instance creator (creates one activity instance).
  * Called from {@link ActivityInstanceCreationEventAction}.
  */
-public class ActivityInstanceCreationEventSyncProcessorDefault implements ActivityInstanceCreationEventSyncProcessor {
+public class ActivityInstanceCreationEventSyncProcessorDefault extends ActivityInstanceCreationEventSyncProcessor {
 
-    /** In default case created only 1 activity instance */
+    /**
+     * In default case created only 1 activity instance
+     */
     private static final int INSTANCES_TO_CREATE_COUNT = 1;
 
-    protected final Handle handle;
-    protected final EventSignal signal;
-
-    protected final JdbiActivityInstance jdbiActivityInstance;
-    protected final JdbiActivityInstanceStatus jdbiActivityInstanceStatus;
-    protected final JdbiActivity jdbiActivity;
-
-    protected final long studyActivityId;
-    protected final ActivityInstanceCreationService creationService;
-
-    protected ActivityDto activityDto;
-
-
-    public ActivityInstanceCreationEventSyncProcessorDefault(
-            Handle handle, EventSignal signal, long studyActivityId, ActivityInstanceCreationService creationService) {
-        this.handle = handle;
-        this.signal = signal;
-        this.studyActivityId = studyActivityId;
-        this.creationService = creationService;
-        this.jdbiActivityInstance = handle.attach(JdbiActivityInstance.class);
-        this.jdbiActivityInstanceStatus = handle.attach(JdbiActivityInstanceStatus.class);
-        this.jdbiActivity = handle.attach(JdbiActivity.class);
+    public ActivityInstanceCreationEventSyncProcessorDefault(Handle handle, EventSignal signal, long studyActivityId,
+                                                             ActivityInstanceCreationService creationService) {
+        super(handle, signal, studyActivityId, creationService);
     }
 
     @Override
@@ -51,59 +27,8 @@ public class ActivityInstanceCreationEventSyncProcessorDefault implements Activi
         creationService.checkSignalIfNestedTargetActivity(activityDto.getParentActivityId());
 
         if (detectPossibleNumberOfInstancesToCreate(INSTANCES_TO_CREATE_COUNT) >= INSTANCES_TO_CREATE_COUNT) {
-
             creationService.hideExistingInstancesIfRequired(studyActivityId, handle, activityDto);
-
             createActivityInstance(activityDto.getParentActivityId());
-        }
-    }
-
-    /**
-     * Create an activity instance.
-     *
-     * @return long  ID of a new instance
-     */
-    @Override
-    public long createActivityInstance(Long parentActivityId) {
-
-        Long parentInstanceId = creationService.detectParentInstanceId(parentActivityId);
-
-        ActivityInstanceCreationService.ActivityInstanceCreationResult creationResult = creationService.createActivityInstance(
-                studyActivityId,
-                parentInstanceId,
-                jdbiActivityInstance,
-                jdbiActivityInstanceStatus,
-                false,
-                null);
-
-        long newActivityInstanceId = creationResult.getActivityInstanceId();
-        String newActivityInstanceGuid = creationResult.getActivityInstanceGuid();
-
-        if (signal instanceof DsmNotificationSignal) {
-            creationService.saveKitEventData(handle, newActivityInstanceId);
-        }
-
-        creationService.runDownstreamEvents(studyActivityId, newActivityInstanceId, handle);
-
-        createChildActivityInstances(studyActivityId, newActivityInstanceId, newActivityInstanceGuid);
-
-        return newActivityInstanceId;
-    }
-
-    /**
-     * Create child nested activity instances, if any
-     */
-    public void createChildActivityInstances(long parentActivityId, long parentActivityInstanceId, String parentActivityInstanceGuid) {
-        List<Long> childActIdsToCreate = handle.attach(JdbiActivity.class).findChildActivityIdsThatNeedCreation(parentActivityId);
-        for (var activityId : childActIdsToCreate) {
-            ActivityInstanceCreationService.ActivityInstanceCreationResult creationResult = creationService.createActivityInstance(
-                    activityId,
-                    parentActivityInstanceId,
-                    jdbiActivityInstance,
-                    jdbiActivityInstanceStatus,
-                    true,
-                    parentActivityInstanceGuid);
-            creationService.runDownstreamEvents(activityId, creationResult.getActivityInstanceId(), handle);
         }
     }
 
@@ -114,21 +39,5 @@ public class ActivityInstanceCreationEventSyncProcessorDefault implements Activi
     public int detectPossibleNumberOfInstancesToCreate(int instancesToCreate) {
         Integer numberOfActivitiesLeft = creationService.detectNumberOfActivitiesLeft(studyActivityId, activityDto, jdbiActivityInstance);
         return numberOfActivitiesLeft == null || numberOfActivitiesLeft > 0 ? INSTANCES_TO_CREATE_COUNT : 0;
-    }
-
-    public Handle getHandle() {
-        return handle;
-    }
-
-    public EventSignal getSignal() {
-        return signal;
-    }
-
-    public long getStudyActivityId() {
-        return studyActivityId;
-    }
-
-    public ActivityInstanceCreationService getCreationService() {
-        return creationService;
     }
 }
