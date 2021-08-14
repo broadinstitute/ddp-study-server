023fe66f4e0049e624e1d878965e9c73963676a3
Marco Ocana <mocana@broadinstitute.org>
2021-08-14 12:06:07 -0400

    Batch insert suggestions

--- pepper-apis/src/main/java/org/broadinstitute/ddp/db/dao/JdbiTextQuestionSuggestion.java
+++ pepper-apis/src/main/java/org/broadinstitute/ddp/db/dao/JdbiTextQuestionSuggestion.java
@@ -1,16 +1,20 @@
 package org.broadinstitute.ddp.db.dao;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlBatch;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface JdbiTextQuestionSuggestion extends SqlObject {
-
-    @SqlUpdate("insert into text_question_suggestion (text_question_id, suggestion, display_order)"
-            + " values (:questionId, :suggestion, :displayOrder)"
-    )
-    int insert(@Bind("questionId") long questionId,
-               @Bind("suggestion") String suggestion,
-               @Bind("displayOrder") int displayOrder);
+    @SqlBatch("insert into text_question_suggestion (text_question_id, suggestion, display_order)"
+            + " values (:questionId, :suggestion, :displayOrder)")
+    int[] insert(@Bind("questionId") long questionId,
+               @Bind("suggestion") List<String> suggestions,
+               @Bind("displayOrder") Iterator<Integer> displayOrder);
 
 }
