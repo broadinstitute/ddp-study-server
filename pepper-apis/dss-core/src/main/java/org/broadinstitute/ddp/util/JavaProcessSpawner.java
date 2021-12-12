9f610dfb254a17319bd4f36e932948cd16785d73
Marco Ocana <mocana@broadinstitute.org>
2021-12-12 13:33:11 -0500

    Fix checkstyle warnings

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/JavaProcessSpawner.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/JavaProcessSpawner.java
@@ -22,14 +22,16 @@ public class JavaProcessSpawner {
     /**
      * Starts up a wholly separate java process.  May not be suitable for production code,
      * but good enough for spinning up simulators for testing
+     *
      * @param classWithMainMethod The main class you want to run
-     * @param sendLogsTo class that will appear in the logs as the source of log statements
-     * @param bootTimeInSeconds initial pause (in seconds), needed because spinning up
-     *                          an app takes a moment
+     * @param sendLogsTo          class that will appear in the logs as the source of log statements
+     * @param bootTimeInSeconds   initial pause (in seconds), needed because spinning up
+     *                            an app takes a moment
      * @return The future result of the job
-     * @throws IOException
+     * @throws IOException if there is an IOException
      */
-    public static Future<ProcessResult> spawnMainInSeparateProcess(Class classWithMainMethod, Class sendLogsTo, int bootTimeInSeconds) throws IOException {
+    public static Future<ProcessResult> spawnMainInSeparateProcess(Class classWithMainMethod, Class sendLogsTo, int bootTimeInSeconds)
+                throws IOException {
         if (classWithMainMethod == null) {
             throw new IllegalArgumentException("class to spawn is required");
         }
@@ -37,15 +39,16 @@ public class JavaProcessSpawner {
             throw new IllegalArgumentException("logging class required.  Otherwise your logs will go into outer space.");
         }
         if (bootTimeInSeconds < 1) {
-            logger.warn("With a boot wait time of " + bootTimeInSeconds + ", clients of " + classWithMainMethod + " may discover that the process the need has not been started yet.");
+            logger.warn("With a boot wait time of " + bootTimeInSeconds + ", clients of " + classWithMainMethod + " may discover that the"
+                    + " process the need has not been started yet.");
         }
 
-        String classpath =  System.getProperty("java.class.path");
+        String classpath = System.getProperty("java.class.path");
         String separator = System.getProperty("file.separator");
         String pathToJava = System.getProperty("java.home") + separator + "bin" + separator + "java";
 
         Future<ProcessResult> processResultFuture = new ProcessExecutor()
-                .command(pathToJava, "-cp", classpath,classWithMainMethod.getName())
+                .command(pathToJava, "-cp", classpath, classWithMainMethod.getName())
                 .redirectOutput(Slf4jStream.of(sendLogsTo).asInfo())
                 .redirectError(Slf4jStream.of(sendLogsTo).asError())
                 .readOutput(true)
@@ -54,8 +57,8 @@ public class JavaProcessSpawner {
         try {
             logger.info("Pausing for " + bootTimeInSeconds + " seconds while starting " + classWithMainMethod.getName());
             Thread.sleep(bootTimeInSeconds * 1000);
-        }catch(InterruptedException e) {
-            logger.info("Spawned java class " + classWithMainMethod + " interrupted",e);
+        } catch (InterruptedException e) {
+            logger.info("Spawned java class " + classWithMainMethod + " interrupted", e);
         }
         return processResultFuture;
     }
