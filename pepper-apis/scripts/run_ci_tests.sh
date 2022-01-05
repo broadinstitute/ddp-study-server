42b5050cbf33322562a6a5a9a72479cb9dfa5bc0
Marco Ocana <mocana@broadinstitute.org>
2022-01-05 16:04:40 -0500

    First step to run dss and dsm tests separately

--- pepper-apis/scripts/run_ci_tests.sh
+++ pepper-apis/scripts/run_ci_tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 CI_PROJECT_SLUG='gh/broadinstitute/ddp-study-server'
-DEFAULT_BRANCH=develop
+
+BRANCH=$1
+PROJECT=${2-dss}
 
 CI_TOKEN=$(xargs <  ~/.circleci-token)
 if [[ -z $CI_TOKEN ]]; then
@@ -9,12 +11,13 @@ if [[ -z $CI_TOKEN ]]; then
   exit 1
 fi
 
-BRANCH=${1-$DEFAULT_BRANCH}
+
 
 echo "Will try to run build/test on branch: $BRANCH"
 curl -u "${CI_TOKEN}:" -X POST --header "Content-Type: application/json" -d "{
                                   \"branch\": \"$BRANCH\",
                                   \"parameters\": {
-                                      \"on_demand\": true
+                                      \"on_demand\": true,
+                                      \"project\": \"$PROJECT\"
                                   }
 }" "https://circleci.com/api/v2/project/${CI_PROJECT_SLUG}/pipeline"
