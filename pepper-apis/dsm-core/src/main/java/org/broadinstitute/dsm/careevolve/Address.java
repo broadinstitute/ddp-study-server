8bfad75707d6ce976493d51e3d9445cb21f8feae
Marco Ocana <mocana@broadinstitute.org>
2022-01-28 12:24:51 -0500

    Bogus change to see if we can force DSM to build

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/careevolve/Address.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/careevolve/Address.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
 import org.apache.commons.lang3.StringUtils;
 
+
 public class Address {
 
     @SerializedName("Line1")
