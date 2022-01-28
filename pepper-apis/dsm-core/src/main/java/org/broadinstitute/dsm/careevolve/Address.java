b61e01487a762d9374cfd82c06b6cde615d2a7fe
Marco Ocana <mocana@broadinstitute.org>
2022-01-28 16:35:51 -0500

    Revert changes made to test path-filtering

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/careevolve/Address.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/careevolve/Address.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.annotations.SerializedName;
 import org.apache.commons.lang3.StringUtils;
 
-
 public class Address {
 
     @SerializedName("Line1")
