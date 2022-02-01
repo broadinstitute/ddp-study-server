d64b967e96ccc9c56bd7ef4e6e8abfd6ae6013ae
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-02-01 11:57:59 +0100

    DDP-7306-DECIMAL. Part IV. Decimal Range rule test

---
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/model/activity/instance/validation/DecimalRangeRuleTest.java
@@ -0,0 +1,80 @@
+package org.broadinstitute.ddp.model.activity.instance.validation;
+
+import org.broadinstitute.ddp.model.activity.instance.answer.DecimalAnswer;
+import org.broadinstitute.ddp.model.activity.instance.question.DecimalQuestion;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class DecimalRangeRuleTest {
+
+    private static DecimalQuestion unused;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @BeforeClass
+    public static void setup() {
+        unused = new DecimalQuestion("sid", 1L, 2L, false, false, false, null, null, null, List.of(), List.of());
+    }
+
+    @Test
+    public void testChecksRange() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("range");
+        DecimalRangeRule.of("msg", null, false, BigDecimal.TEN, BigDecimal.ONE);
+    }
+
+    @Test
+    public void testValidate_noValue() {
+        DecimalRangeRule rule = DecimalRangeRule.of("msg", "hint", false, BigDecimal.ZERO, BigDecimal.TEN);
+        assertFalse(rule.validate(unused, null));
+        assertTrue(rule.validate(unused, new DecimalAnswer(1L, "q", "a", null)));
+    }
+
+    @Test
+    public void testValidate_onlyMinimum() {
+        DecimalRangeRule rule = DecimalRangeRule.of("msg", "hint", false, BigDecimal.ZERO, null);
+        assertFalse(run(rule, BigDecimal.ZERO));
+        assertTrue(run(rule, BigDecimal.ONE));
+        assertTrue(run(rule, BigDecimal.TEN));
+    }
+
+    @Test
+    public void testValidate_onlyMaximum() {
+        DecimalRangeRule rule = DecimalRangeRule.of("msg", "hint", false, null, BigDecimal.ONE);
+        assertTrue(run(rule, BigDecimal.ZERO));
+        assertTrue(run(rule, BigDecimal.ONE));
+        assertFalse(run(rule, BigDecimal.TEN));
+    }
+
+    @Test
+    public void testValidate_exactly() {
+        DecimalRangeRule rule = DecimalRangeRule.of("msg", "hint", false, BigDecimal.ONE, BigDecimal.ONE);
+        assertFalse(run(rule, BigDecimal.ZERO));
+        assertFalse(run(rule, BigDecimal.TEN));
+        assertTrue(run(rule, BigDecimal.ONE));
+    }
+
+    @Test
+    public void testValidate_range() {
+        DecimalRangeRule rule = DecimalRangeRule.of("msg", "hint", false, BigDecimal.valueOf(3L), BigDecimal.valueOf(5L));
+        assertFalse(run(rule, BigDecimal.ONE));
+        assertFalse(run(rule, BigDecimal.valueOf(2L)));
+        assertTrue(run(rule, BigDecimal.valueOf(3L)));
+        assertTrue(run(rule, BigDecimal.valueOf(4L)));
+        assertTrue(run(rule, BigDecimal.valueOf(5L)));
+        assertFalse(run(rule, BigDecimal.TEN));
+    }
+
+    // Helper to run the rule.
+    private boolean run(DecimalRangeRule rule, BigDecimal value) {
+        return rule.validate(unused, new DecimalAnswer(1L, "q", "a", value));
+    }
+}
