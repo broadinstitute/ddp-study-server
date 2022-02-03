0bc5fe6a53a2b0e01a61b4ee2563486f15475588
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-02-03 15:47:22 +0100

    DDP-7306-BASIL. Part infinity. Unit test fix

--- pepper-apis/src/test/java/org/broadinstitute/ddp/db/dao/AnswerDaoTest.java
+++ pepper-apis/src/test/java/org/broadinstitute/ddp/db/dao/AnswerDaoTest.java
@@ -8,7 +8,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.Collection;
 import java.util.List;
