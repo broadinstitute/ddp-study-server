de78e8fdacdda548abfafa023c4a4980d1a6bd2d
archilsharashenidzebroad <asharash@broadinstitute.org>
2022-02-04 19:00:35 +0400

    DDP-7417

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/MatrixQuestionDto.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/MatrixQuestionDto.java
@@ -1,6 +1,5 @@
 package org.broadinstitute.ddp.db.dto;
 
-import lombok.Value;
 import org.broadinstitute.ddp.model.activity.types.MatrixSelectMode;
 import org.jdbi.v3.core.mapper.Nested;
 import org.jdbi.v3.core.mapper.reflect.ColumnName;
@@ -9,12 +8,15 @@ import org.jdbi.v3.core.mapper.reflect.JdbiConstructor;
 import java.io.Serializable;
 import java.util.Set;
 
-@Value
-public class MatrixQuestionDto extends QuestionDto implements Serializable {
-    MatrixSelectMode selectMode;
-    boolean renderModal;
-    Long modalTemplateId;
-    Long modalTitleTemplateId;
+/**
+ * DTO class to represent matrix question that includes all base question data.
+ */
+public final class MatrixQuestionDto extends QuestionDto implements Serializable {
+
+    private MatrixSelectMode selectMode;
+    private boolean renderModal;
+    private Long modalTemplateId;
+    private Long modalTitleTemplateId;
 
     @JdbiConstructor
     public MatrixQuestionDto(@Nested QuestionDto questionDto,
@@ -28,6 +30,22 @@ public class MatrixQuestionDto extends QuestionDto implements Serializable {
         this.renderModal = renderModal;
         this.modalTemplateId = modalTemplateId;
         this.modalTitleTemplateId = modalTitleTemplateId;
+    }
+
+    public MatrixSelectMode getSelectMode() {
+        return selectMode;
+    }
+
+    public boolean isRenderModal() {
+        return renderModal;
+    }
+
+    public Long getModalTemplateId() {
+        return modalTemplateId;
+    }
+
+    public Long getModalTitleTemplateId() {
+        return modalTitleTemplateId;
     }
 
     @Override
