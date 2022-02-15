4b790f0aa8393093162cc15d244044b0a41eba36
simone <simone@broadinstitute.org>
2022-02-15 16:11:45 -0500

    changing import class to static import of method

--- pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/constants/RouteConstants.java
+++ pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/constants/RouteConstants.java
@@ -1,6 +1,6 @@
 package org.broadinstitute.ddp.constants;
 
-import org.broadinstitute.ddp.util.MiscUtil;
+import static org.broadinstitute.ddp.util.MiscUtil.fmt;
 
 public class RouteConstants {
 
@@ -30,83 +30,84 @@ public class RouteConstants {
 
         public static final String REGISTRATION = BASE + "/register";
         public static final String TEMP_USERS = BASE + "/temporary-users";
-        public static final String ACTIVITY_INSTANCE_STATUS_TYPE_LIST = BASE + "/activity-instance-status-types";
+        public static final String ACTIVITY_INSTANCE_STATUS_TYPE_LIST = BASE  + "/activity-instance-status-types";
 
         public static final String SENDGRID_EVENT = BASE + "/sendgrid-event";
         public static final String AUTH0_LOG_EVENT = BASE + "/auth0-log-event";
 
         public static final String ADMIN_BASE = BASE + "/admin";
         public static final String ADMIN_STUDY_PARTICIPANTS =
-                MiscUtil.fmt(ADMIN_BASE + "/studies/%s/participants", PathParam.STUDY_GUID);
+                fmt(ADMIN_BASE + "/studies/%s/participants", PathParam.STUDY_GUID);
+
         public static final String ADMIN_STUDY_PARTICIPANTS_LOOKUP =
-                MiscUtil.fmt(ADMIN_BASE + "/studies/%s/participants-lookup", PathParam.STUDY_GUID);
+                fmt(ADMIN_BASE + "/studies/%s/participants-lookup", PathParam.STUDY_GUID);
         public static final String ADMIN_STUDY_PARTICIPANT_LOOKUP_BY_GUID =
-                MiscUtil.fmt(ADMIN_BASE + "/studies/%s/participants/%s", PathParam.STUDY_GUID, PathParam.USER_GUID);
+                fmt(ADMIN_BASE + "/studies/%s/participants/%s", PathParam.STUDY_GUID, PathParam.USER_GUID);
         public static final String ADMIN_STUDY_INVITATION_LOOKUP =
-                MiscUtil.fmt(ADMIN_BASE + "/studies/%s/invitation-lookup", PathParam.STUDY_GUID);
+                fmt(ADMIN_BASE + "/studies/%s/invitation-lookup", PathParam.STUDY_GUID);
         public static final String ADMIN_STUDY_INVITATION_DETAILS =
-                MiscUtil.fmt(ADMIN_BASE + "/studies/%s/invitation-details", PathParam.STUDY_GUID);
+                fmt(ADMIN_BASE + "/studies/%s/invitation-details", PathParam.STUDY_GUID);
         public static final String ADMIN_STUDY_USER_LOGIN_ACCOUNT =
-                MiscUtil.fmt(ADMIN_BASE + "/studies/%s/user/%s/login-account", PathParam.STUDY_GUID, PathParam.USER_GUID);
+                fmt(ADMIN_BASE + "/studies/%s/user/%s/login-account", PathParam.STUDY_GUID, PathParam.USER_GUID);
 
         public static final String STUDY_ALL = BASE + "/studies";
-        public static final String STUDY_DETAIL = MiscUtil.fmt(STUDY_ALL + "/%s", PathParam.STUDY_GUID);
+        public static final String STUDY_DETAIL = fmt(STUDY_ALL + "/%s", PathParam.STUDY_GUID);
         public static final String PARTICIPANTS_INFO_FOR_STUDY =
-                MiscUtil.fmt(STUDY_ALL + "/participant-info" + "/%s", PathParam.STUDY_GUID);
-        public static final String STUDY_LANGUAGES = MiscUtil.fmt(BASE + "/studies/%s/languages", PathParam.STUDY_GUID);
-        public static final String INVITATION_VERIFY = MiscUtil.fmt(BASE + "/studies/%s/invitation-verify", PathParam.STUDY_GUID);
-        public static final String INVITATION_CHECK = MiscUtil.fmt(BASE + "/studies/%s/invitation-check", PathParam.STUDY_GUID);
+                fmt(STUDY_ALL + "/participant-info" + "/%s", PathParam.STUDY_GUID);
+        public static final String STUDY_LANGUAGES = fmt(BASE + "/studies/%s/languages", PathParam.STUDY_GUID);
+        public static final String INVITATION_VERIFY = fmt(BASE + "/studies/%s/invitation-verify", PathParam.STUDY_GUID);
+        public static final String INVITATION_CHECK = fmt(BASE + "/studies/%s/invitation-check", PathParam.STUDY_GUID);
 
-        public static final String USER_ALL = MiscUtil.fmt(BASE + "/user/%s/*", PathParam.USER_GUID);
-        public static final String USER_SPECIFIC = MiscUtil.fmt(BASE + "/user/%s", PathParam.USER_GUID);
+        public static final String USER_ALL = fmt(BASE + "/user/%s/*", PathParam.USER_GUID);
+        public static final String USER_SPECIFIC = fmt(BASE + "/user/%s", PathParam.USER_GUID);
         public static final String UPDATE_USER_PASSWORD = USER_SPECIFIC + "/password";
         public static final String UPDATE_USER_EMAIL = USER_SPECIFIC + "/email";
 
-        public static final String USER_PROFILE = MiscUtil.fmt(BASE + "/user/%s/profile", PathParam.USER_GUID);
+        public static final String USER_PROFILE = fmt(BASE + "/user/%s/profile", PathParam.USER_GUID);
 
         public static final String PARTICIPANT_ADDRESS = USER_PROFILE + "/address";
-        public static final String PARTICIPANT_TEMP_ADDRESS = MiscUtil.fmt(PARTICIPANT_ADDRESS + "/temp/%s",
+        public static final String PARTICIPANT_TEMP_ADDRESS = fmt(PARTICIPANT_ADDRESS + "/temp/%s",
                 PathParam.INSTANCE_GUID);
         public static final String DEFAULT_PARTICIPANT_ADDRESS = PARTICIPANT_ADDRESS + "/default";
 
-        public static final String ADDRESS = MiscUtil.fmt(PARTICIPANT_ADDRESS + "/%s", PathParam.ADDRESS_GUID);
+        public static final String ADDRESS = fmt(PARTICIPANT_ADDRESS + "/%s", PathParam.ADDRESS_GUID);
 
         public static final String ADDRESS_VERIFY = PARTICIPANT_ADDRESS + "/verify";
 
         public static final String ADDRESS_COUNTRIES = BASE + "/addresscountries";
         public static final String ADDRESS_COUNTRY_DETAILS = ADDRESS_COUNTRIES + "/:countryCode";
 
-        public static final String USER_STUDY_WORKFLOW = MiscUtil.fmt(BASE + "/user/%s/studies/%s/workflow",
+        public static final String USER_STUDY_WORKFLOW = fmt(BASE + "/user/%s/studies/%s/workflow",
                 PathParam.USER_GUID, PathParam.STUDY_GUID);
         public static final String USER_STUDY_ANNOUNCEMENTS = String.format(BASE + "/user/%s/studies/%s/announcements",
                 PathParam.USER_GUID, PathParam.STUDY_GUID);
         public static final String USER_STUDY_PARTICIPANTS = String.format(BASE + "/user/%s/studies/%s/participants",
                 PathParam.USER_GUID, PathParam.STUDY_GUID);
 
-        public static final String USER_STUDIES_PREQUALIFIER = MiscUtil.fmt(BASE + "/user/%s/studies/%s/prequalifier",
+        public static final String USER_STUDIES_PREQUALIFIER = fmt(BASE + "/user/%s/studies/%s/prequalifier",
                 PathParam.USER_GUID, PathParam.STUDY_GUID);
-        public static final String USER_STUDIES_ALL_CONSENTS = MiscUtil.fmt(BASE + "/user/%s/studies/%s/consents",
+        public static final String USER_STUDIES_ALL_CONSENTS = fmt(BASE + "/user/%s/studies/%s/consents",
                 PathParam.USER_GUID, PathParam.STUDY_GUID);
-        public static final String USER_STUDIES_CONSENT = MiscUtil.fmt(BASE + "/user/%s/studies/%s/consents/%s",
+        public static final String USER_STUDIES_CONSENT = fmt(BASE + "/user/%s/studies/%s/consents/%s",
                 PathParam.USER_GUID, PathParam.STUDY_GUID, PathParam.ACTIVITY_CODE);
 
-        public static final String USER_ACTIVITIES = MiscUtil.fmt(BASE + "/user/%s/studies/%s/activities",
+        public static final String USER_ACTIVITIES = fmt(BASE + "/user/%s/studies/%s/activities",
                 PathParam.USER_GUID, PathParam.STUDY_GUID);
-        public static final String USER_ACTIVITIES_INSTANCE = MiscUtil.fmt(BASE + "/user/%s/studies/%s/activities/%s",
+        public static final String USER_ACTIVITIES_INSTANCE = fmt(BASE + "/user/%s/studies/%s/activities/%s",
                 PathParam.USER_GUID, PathParam.STUDY_GUID, PathParam.INSTANCE_GUID);
-        public static final String USER_ACTIVITY_SUMMARY = MiscUtil.fmt(BASE + "/user/%s/studies/%s/activities/%s/summary",
+        public static final String USER_ACTIVITY_SUMMARY = fmt(BASE + "/user/%s/studies/%s/activities/%s/summary",
                 PathParam.USER_GUID, PathParam.STUDY_GUID, PathParam.INSTANCE_GUID);
-        public static final String USER_ACTIVITY_ANSWERS = MiscUtil.fmt(BASE + "/user/%s/studies/%s/activities/%s/answers",
+        public static final String USER_ACTIVITY_ANSWERS = fmt(BASE + "/user/%s/studies/%s/activities/%s/answers",
                 PathParam.USER_GUID, PathParam.STUDY_GUID, PathParam.INSTANCE_GUID);
-        public static final String USER_ACTIVITY_UPLOADS = MiscUtil.fmt(BASE + "/user/%s/studies/%s/activities/%s/uploads",
+        public static final String USER_ACTIVITY_UPLOADS = fmt(BASE + "/user/%s/studies/%s/activities/%s/uploads",
                 PathParam.USER_GUID, PathParam.STUDY_GUID, PathParam.INSTANCE_GUID);
-        public static final String USER_MEDICAL_PROVIDERS = MiscUtil.fmt(
+        public static final String USER_MEDICAL_PROVIDERS = fmt(
                 BASE + "/user/%s/studies/%s/medical-providers/%s",
                 PathParam.USER_GUID,
                 PathParam.STUDY_GUID,
                 PathParam.INSTITUTION_TYPE
         );
-        public static final String USER_MEDICAL_PROVIDER = MiscUtil.fmt(
+        public static final String USER_MEDICAL_PROVIDER = fmt(
                 BASE + "/user/%s/studies/%s/medical-providers/%s/%s",
                 PathParam.USER_GUID,
                 PathParam.STUDY_GUID,
@@ -121,8 +122,8 @@ public class RouteConstants {
                 BASE + "/user/%s/studies/%s/exit", PathParam.USER_GUID, PathParam.STUDY_GUID);
 
         public static final String DSM_BASE = BASE + "/dsm";
-        public static final String DSM_STUDY = MiscUtil.fmt(DSM_BASE + "/studies/%s/ddp", PathParam.STUDY_GUID);
-        public static final String DSM_GET_INSTITUTION_REQUESTS = MiscUtil.fmt(DSM_STUDY + "/institutionrequests/%s", PathParam.MAX_ID);
+        public static final String DSM_STUDY = fmt(DSM_BASE + "/studies/%s/ddp", PathParam.STUDY_GUID);
+        public static final String DSM_GET_INSTITUTION_REQUESTS = fmt(DSM_STUDY + "/institutionrequests/%s", PathParam.MAX_ID);
         public static final String DSM_ALL_KIT_REQUESTS = DSM_STUDY + "/kitrequests";
         public static final String DSM_KIT_REQUESTS_STARTING_AFTER = DSM_ALL_KIT_REQUESTS + "/"
                 + PathParam.PREVIOUS_LAST_KIT_REQUEST_ID;
@@ -135,40 +136,40 @@ public class RouteConstants {
         public static final String AUTOCOMPLETE_BASE = BASE + "/autocomplete";
         public static final String AUTOCOMPLETE_INSTITUTION = AUTOCOMPLETE_BASE + "/institution";
         public static final String LIST_CANCERS = BASE + "/cancers";
-        public static final String JOIN_MAILING_LIST = MiscUtil.fmt(BASE + "/mailing-list");
-        public static final String GET_STUDY_MAILING_LIST = MiscUtil.fmt(DSM_BASE + "/studies/%s/ddp/mailinglist", PathParam.STUDY_GUID);
-        public static final String CHECK_IRB_PASSWORD = MiscUtil.fmt(
+        public static final String JOIN_MAILING_LIST = fmt(BASE + "/mailing-list");
+        public static final String GET_STUDY_MAILING_LIST = fmt(DSM_BASE + "/studies/%s/ddp/mailinglist", PathParam.STUDY_GUID);
+        public static final String CHECK_IRB_PASSWORD = fmt(
                 BASE + "/studies/%s/irb-password-check",
                 PathParam.STUDY_GUID
         );
         public static final String DSM_ONDEMAND_ACTIVITIES = DSM_STUDY + "/followupsurveys";
-        public static final String DSM_ONDEMAND_ACTIVITY = MiscUtil.fmt(DSM_STUDY + "/followupsurvey/%s", PathParam.ACTIVITY_CODE);
-        public static final String DSM_NOTIFICATION = MiscUtil.fmt(
+        public static final String DSM_ONDEMAND_ACTIVITY = fmt(DSM_STUDY + "/followupsurvey/%s", PathParam.ACTIVITY_CODE);
+        public static final String DSM_NOTIFICATION = fmt(
                 DSM_STUDY + "/participantevent/%s",
                 PathParam.USER_GUID
         );
 
-        public static final String DSM_TERMINATE_USER = MiscUtil.fmt(
+        public static final String DSM_TERMINATE_USER = fmt(
                 DSM_STUDY + "/exitparticipantrequest/%s",
                 PathParam.USER_GUID
         );
 
-        public static final String PARTICIPANT_STATUS = MiscUtil.fmt(
+        public static final String PARTICIPANT_STATUS = fmt(
                 BASE + "/user/%s/studies/%s/status",
                 PathParam.USER_GUID,
                 PathParam.STUDY_GUID
         );
 
-        public static final String SEND_EMAIL = MiscUtil.fmt(BASE + "/studies/%s/send-email", PathParam.STUDY_GUID);
+        public static final String SEND_EMAIL = fmt(BASE + "/studies/%s/send-email", PathParam.STUDY_GUID);
         public static final String POST_PASSWORD_RESET = BASE + "/post-password-reset";
 
-        public static final String DSM_DRUG_SUGGESTION = MiscUtil.fmt(
+        public static final String DSM_DRUG_SUGGESTION = fmt(
                 STUDY_DETAIL + "/suggestions/drugs"
         );
-        public static final String CANCER_SUGGESTION = MiscUtil.fmt(
+        public static final String CANCER_SUGGESTION = fmt(
                 STUDY_DETAIL + "/suggestions/cancers"
         );
-        public static final String ACTIVITY_INSTANCE_SELECT_SUGGESTION = MiscUtil.fmt(
+        public static final String ACTIVITY_INSTANCE_SELECT_SUGGESTION = fmt(
                 BASE + "/user/%s/studies/%s/activity-instance-select/%s",
                 PathParam.USER_GUID,
                 PathParam.STUDY_GUID,
