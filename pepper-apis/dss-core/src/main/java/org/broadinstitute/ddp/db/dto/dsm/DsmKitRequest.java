c501474b4e9f623b6941a447f868eac60843ac77
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-02-16 10:14:46 +0100

    Comments restored

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/dsm/DsmKitRequest.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/dsm/DsmKitRequest.java
@@ -8,10 +8,29 @@ import lombok.Data;
  */
 @Data
 public class DsmKitRequest {
-    //marked transient to exclude from GSON serialization
+    /**
+     * The database id for the related database row.
+     */
     private transient long id;
+
+    /**
+     * The identifier for the participant, which will be a legacy datstat altpid if present. Otherwise, it will be the user guid.
+     */
     private String participantId;
+
+    /**
+     * The identifier for the kit request used by DSM as a handle.
+     * Check elsewhere for what we set this to.
+     */
     private String kitRequestId;
+
+    /**
+     * The name of the type of kit being requested.
+     */
     private String kitType;
+
+    /**
+     * Does the kit need manual approval?
+     */
     private boolean needsApproval;
 }
