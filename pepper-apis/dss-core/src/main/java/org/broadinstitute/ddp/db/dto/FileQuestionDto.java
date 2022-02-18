9e2c13ca2973c9fed769c60c4847c99e65149d33
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-02-18 14:33:44 +0100

    FileQuestionDto

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/FileQuestionDto.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/FileQuestionDto.java
@@ -2,10 +2,11 @@ package org.broadinstitute.ddp.db.dto;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import lombok.Getter;
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.ddp.interfaces.FileUploadSettings;
 import org.jdbi.v3.core.mapper.Nested;
@@ -13,29 +14,23 @@ import org.jdbi.v3.core.mapper.reflect.ColumnName;
 import org.jdbi.v3.core.mapper.reflect.JdbiConstructor;
 
 
+@Getter(onMethod = @__(@Override))
 public final class FileQuestionDto extends QuestionDto implements Serializable, FileUploadSettings {
-
-    private long maxFileSize;
-    private Set<String> mimeTypes = new LinkedHashSet<>();
+    private final long maxFileSize;
+    private final Set<String> mimeTypes;
 
     @JdbiConstructor
     public FileQuestionDto(@Nested QuestionDto questionDto,
                            @ColumnName("max_file_size") long maxFileSize,
                            @ColumnName("mime_types") String mimeTypes) {
         super(questionDto);
+
         this.maxFileSize = maxFileSize;
-        if (StringUtils.isNotBlank(mimeTypes)) {
-            this.mimeTypes =  new LinkedHashSet(Arrays.stream(mimeTypes.split(",")).collect(Collectors.toList()));
-        }
-    }
-
-    @Override
-    public long getMaxFileSize() {
-        return maxFileSize;
-    }
-
-    @Override
-    public Set<String> getMimeTypes() {
-        return mimeTypes;
+        this.mimeTypes = Optional.ofNullable(mimeTypes)
+                .filter(StringUtils::isNotBlank)
+                .map(types -> types.split(","))
+                .map(Arrays::stream)
+                .map(x -> x.collect(Collectors.toSet()))
+                .orElse(null);
     }
 }
