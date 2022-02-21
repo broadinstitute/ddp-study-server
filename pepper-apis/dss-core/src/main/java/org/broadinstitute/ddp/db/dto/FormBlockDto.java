ef2f2d5da27eb9376e3e7dd4a012964b9f08fa97
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-02-21 11:27:14 +0100

    Unused imports

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/FormBlockDto.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dto/FormBlockDto.java
@@ -11,9 +11,6 @@ import org.broadinstitute.ddp.model.activity.types.BlockType;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 /**
  * A wrapper data object around a block and its optional conditional expression.
  */
@@ -25,18 +22,6 @@ public class FormBlockDto {
     BlockDto blockDto;
     String shownExpr;
     String enabledExpr;
-
-    public static class FormBlockDtoMapper implements RowMapper<FormBlockDto> {
-        @Override
-        public FormBlockDto map(ResultSet rs, StatementContext ctx) throws SQLException {
-            return new FormBlockDto(
-                    (Long) rs.getObject("form_section_id"),
-                    (Long) rs.getObject("parent_block_id"),
-                    new BlockDto.BlockDtoMapper().map(rs, ctx),
-                    rs.getString("shown_expression_text"),
-                    rs.getString("enabled_expression_text"));
-        }
-    }
 
     public BlockType getType() {
         return blockDto.getType();
