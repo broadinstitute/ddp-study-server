108070c7f7e9d3fd9a07e6d561fa081094ef3a91
Sampath K. Settipalli <sampath@broadinstitute.org>
2022-03-15 18:10:28 -0500

    refactor

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/route/GetOptionsForActivityInstanceQuestionRoute.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/route/GetOptionsForActivityInstanceQuestionRoute.java
@@ -4,16 +4,21 @@ import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.ddp.constants.RouteConstants;
 import org.broadinstitute.ddp.content.ContentStyle;
 import org.broadinstitute.ddp.content.I18nContentRenderer;
+import org.broadinstitute.ddp.db.ActivityDefStore;
+import org.broadinstitute.ddp.db.DaoException;
 import org.broadinstitute.ddp.db.TransactionWrapper;
-import org.broadinstitute.ddp.db.dao.JdbiPicklistOption;
-import org.broadinstitute.ddp.db.dao.JdbiQuestion;
+import org.broadinstitute.ddp.db.dao.JdbiActivityInstance;
+import org.broadinstitute.ddp.db.dto.ActivityInstanceDto;
 import org.broadinstitute.ddp.db.dto.LanguageDto;
-import org.broadinstitute.ddp.db.dto.PicklistOptionDto;
 import org.broadinstitute.ddp.json.RemoteAutoCompleteResponse;
+import org.broadinstitute.ddp.model.activity.definition.FormActivityDef;
+import org.broadinstitute.ddp.model.activity.definition.question.PicklistOptionDef;
+import org.broadinstitute.ddp.model.activity.definition.question.PicklistQuestionDef;
 import org.broadinstitute.ddp.model.activity.instance.question.PicklistOption;
 import org.broadinstitute.ddp.security.DDPAuth;
 import org.broadinstitute.ddp.util.PicklistOptionTypeaheadComparator;
 import org.broadinstitute.ddp.util.RouteUtil;
+import org.jdbi.v3.core.Handle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
@@ -23,7 +28,6 @@ import spark.Route;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -61,28 +65,17 @@ public class GetOptionsForActivityInstanceQuestionRoute implements Route {
 
         RemoteAutoCompleteResponse result = TransactionWrapper.withTxn(handle -> {
 
-            //load All options first
-            List<PicklistOption> allOptions = new ArrayList<>();
-            List<PicklistOption> suggestions;
-            JdbiPicklistOption jdbiPicklistOption = handle.attach(JdbiPicklistOption.class);
-            JdbiQuestion jdbiQuestion = handle.attach(JdbiQuestion.class);
-            Optional<Long> questionId = jdbiQuestion.findIdByStableIdAndInstanceGuid(questionStableId, instanceGuid);
-            List<PicklistOptionDto> optionDtos = jdbiPicklistOption.findAllActiveOrderedOptionsByQuestionId(questionId.get());
-            for (PicklistOptionDto optionDto : optionDtos) {
-                PicklistOption option = new PicklistOption(optionDto.getStableId(),
-                        optionDto.getOptionLabelTemplateId(), optionDto.getTooltipTemplateId(), optionDto.getDetailLabelTemplateId(),
-                        optionDto.isAllowDetails(), optionDto.isExclusive(), optionDto.isDefault());
-                allOptions.add(option);
-            }
-            // Render labels using current time to get latest templates.
+            //Get all options first from ActivityDef (already in memory) then render and match
             long timestamp = Instant.now().toEpochMilli();
             long langCodeId = preferredUserLanguage.getId();
-            renderer.bulkRenderAndApply(handle, allOptions, style, langCodeId, timestamp);
-
+            List<PicklistOption> suggestions = new ArrayList<>();
+            List<PicklistOption> allOptions = getPicklistOptions(handle, studyGuid, instanceGuid, questionStableId);
             if (StringUtils.isBlank(autoCompleteQuery)) {
-                LOG.info("Option suggestion query is blank, returning all results");
+                LOG.info("Option suggestion query is blank, returning all results size to default limit");
                 suggestions = allOptions.stream().limit(limit).collect(Collectors.toList());
+                renderer.bulkRenderAndApply(handle, allOptions, style, langCodeId, timestamp);
             } else {
+                renderer.bulkRenderAndApply(handle, allOptions, style, langCodeId, timestamp);
                 suggestions = filterOptions(allOptions, autoCompleteQuery, limit);
             }
 
@@ -102,6 +95,27 @@ public class GetOptionsForActivityInstanceQuestionRoute implements Route {
         return optionMatches.stream()
                 .sorted(new PicklistOptionTypeaheadComparator(autoCompleteQuery))
                 .limit(limit).collect(Collectors.toList());
+    }
+
+    private List<PicklistOption> getPicklistOptions(Handle handle, String studyGuid,
+                                                       String activityInstanceGuid, String questionStableId) {
+
+        ActivityInstanceDto instanceDto = handle.attach(JdbiActivityInstance.class)
+                .getByActivityInstanceGuid(activityInstanceGuid)
+                .orElseThrow(() -> new DaoException("Could not find activity instance using guid " + activityInstanceGuid
+                        + " while loading activity definition "));
+        FormActivityDef formActivityDef = ActivityDefStore.getInstance().findActivityDef(handle, studyGuid, instanceDto)
+                .orElseThrow(() -> new DaoException("Could not find activity def for studyGuid: " + studyGuid
+                        + " and instanceGuid: " + activityInstanceGuid));
+        PicklistQuestionDef questionDef = (PicklistQuestionDef) formActivityDef.getQuestionByStableId(questionStableId);
+        List<PicklistOptionDef> optionDefs = questionDef.getPicklistOptionsIncludingRemoteAutoComplete();
+
+        return optionDefs.stream().map(optionDef -> new PicklistOption(optionDef.getStableId(),
+                optionDef.getOptionLabelTemplate().getTemplateId(),
+                optionDef.getTooltipTemplate() != null ? optionDef.getTooltipTemplate().getTemplateId() : null,
+                optionDef.getDetailLabelTemplate() != null ? optionDef.getDetailLabelTemplate().getTemplateId() : null,
+                optionDef.isDetailsAllowed(), optionDef.isExclusive(), optionDef.isDefault()
+        )).collect(Collectors.toList());
     }
 
 }
