3e01a7f71ba567b22388d817aad27550f78eabd3
Sampath K. Settipalli <sampath@broadinstitute.org>
2022-03-15 18:18:21 -0500

    more refactoring and code cleanup

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/CancerTypeaheadComparator.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/CancerTypeaheadComparator.java
@@ -1,7 +1,6 @@
 package org.broadinstitute.ddp.util;
 
 import java.util.Comparator;
-import java.util.regex.Pattern;
 
 import org.broadinstitute.ddp.model.dsm.Cancer;
 
@@ -14,6 +13,7 @@ import org.broadinstitute.ddp.model.dsm.Cancer;
 public class CancerTypeaheadComparator implements Comparator<Cancer> {
 
     private final String searchTerm;
+    StringSuggestionTypeaheadComparator stringComparator;
 
     /**
      * Create a new one, using the given search term
@@ -22,53 +22,13 @@ public class CancerTypeaheadComparator implements Comparator<Cancer> {
      */
     public CancerTypeaheadComparator(String searchTerm) {
         this.searchTerm = searchTerm.toUpperCase();
+        this.stringComparator = new StringSuggestionTypeaheadComparator(searchTerm);
     }
 
     @Override
     public int compare(Cancer cancer1, Cancer cancer2) {
         String cancer1Name = cancer1.getName().toUpperCase();
         String cancer2Name = cancer2.getName().toUpperCase();
-        boolean cancer1StartsWithSearchTerm = cancer1Name.startsWith(searchTerm);
-        boolean cancer2StartsWithSearchTearm = cancer2Name.startsWith(searchTerm);
-
-        // bias search results so that matches at the start of the name are favored
-        if (cancer1StartsWithSearchTerm && cancer2StartsWithSearchTearm) {
-            return cancer1Name.compareTo(cancer2Name);
-        } else if (cancer1StartsWithSearchTerm) {
-            return -1;
-        } else if (cancer2StartsWithSearchTearm) {
-            return 1;
-        } else {
-            // if the search is not at the start of the term, prefer hits that are at start of words
-            Pattern startOfWordMatch = Pattern.compile("\\b(\\))*" + Pattern.quote(searchTerm));
-            boolean cancer1MatchesAtWordStart = startOfWordMatch.matcher(cancer1Name).find();
-            boolean cancer2MatchesAtWordStart = startOfWordMatch.matcher(cancer2Name).find();
-
-            if (cancer1MatchesAtWordStart && !cancer2MatchesAtWordStart) {
-                return -1;
-            } else if (cancer2MatchesAtWordStart && !cancer1MatchesAtWordStart) {
-                return 1;
-            } else {
-                // prefer left-most match
-                int cancer1MatchPosition = cancer1Name.indexOf(searchTerm);
-                int cancer2MatchPosition = cancer2Name.indexOf(searchTerm);
-
-                if (cancer1MatchPosition > -1 && cancer2MatchPosition > -1) {
-                    if (cancer1MatchPosition == cancer2MatchPosition) {
-                        return cancer1Name.compareTo(cancer2Name);
-                    } else {
-                        return (cancer1MatchPosition < cancer2MatchPosition) ? -1 : 1;
-                    }
-                } else {
-                    if (cancer1MatchPosition > -1) {
-                        return -1;
-                    } else if (cancer2MatchPosition > -1) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-            }
-        }
+        return stringComparator.compare(cancer1Name, cancer2Name);
     }
 }
--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/DrugTypeaheadComparator.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/DrugTypeaheadComparator.java
@@ -1,7 +1,6 @@
 package org.broadinstitute.ddp.util;
 
 import java.util.Comparator;
-import java.util.regex.Pattern;
 
 import org.broadinstitute.ddp.model.dsm.Drug;
 
@@ -14,6 +13,7 @@ import org.broadinstitute.ddp.model.dsm.Drug;
 public class DrugTypeaheadComparator implements Comparator<Drug> {
 
     private final String searchTerm;
+    StringSuggestionTypeaheadComparator stringComparator;
 
     /**
      * Create a new one, using the given search term
@@ -22,58 +22,13 @@ public class DrugTypeaheadComparator implements Comparator<Drug> {
      */
     public DrugTypeaheadComparator(String searchTerm) {
         this.searchTerm = searchTerm.toUpperCase();
+        this.stringComparator = new StringSuggestionTypeaheadComparator(searchTerm);
     }
 
     @Override
     public int compare(Drug drug1, Drug drug2) {
         String drug1Name = drug1.getName().toUpperCase();
         String drug2Name = drug2.getName().toUpperCase();
-        boolean drug1StartsWithSearchTerm = drug1Name.startsWith(searchTerm);
-        boolean drug2StartsWithSearchTearm = drug2Name.startsWith(searchTerm);
-
-
-        // bias search results so that matches at the start of the name are favored
-        if (drug1StartsWithSearchTerm && !drug2StartsWithSearchTearm) {
-            return -1;
-        } else if (drug2StartsWithSearchTearm && !drug1StartsWithSearchTerm) {
-            return 1;
-        } else {
-            if (drug1StartsWithSearchTerm && drug2StartsWithSearchTearm) {
-                return drug1Name.compareTo(drug2Name);
-            } else {
-                // if the search is not at the start of the term, prefer hits that are at
-                // start of words
-                Pattern startOfWordMatch = Pattern.compile("\\b(\\))*" + Pattern.quote(searchTerm));
-                boolean drug1MatchesAtWordStart = startOfWordMatch.matcher(drug1Name).find();
-                boolean drug2MatchesAtWordStart = startOfWordMatch.matcher(drug2Name).find();
-
-                if (drug1MatchesAtWordStart && !drug2MatchesAtWordStart) {
-                    return -1;
-                } else if (drug2MatchesAtWordStart && !drug1MatchesAtWordStart) {
-                    return 1;
-                } else {
-                    // prefer left-most match
-                    int drug1MatchPosition = drug1Name.indexOf(searchTerm);
-                    int drug2MatchPosition = drug2Name.indexOf(searchTerm);
-
-                    if (drug1MatchPosition > -1 && drug2MatchPosition > -1) {
-                        if (drug1MatchPosition == drug2MatchPosition) {
-                            return drug1Name.compareTo(drug2Name);
-                        } else {
-                            return (drug1MatchPosition < drug2MatchPosition) ? -1 : 1;
-                        }
-                    } else {
-                        if (drug1MatchPosition > -1) {
-                            return -1;
-                        } else if (drug2MatchPosition > -1) {
-                            return 1;
-                        } else {
-                            return 0;
-                        }
-                    }
-                }
-
-            }
-        }
+        return stringComparator.compare(drug1Name, drug2Name);
     }
 }
