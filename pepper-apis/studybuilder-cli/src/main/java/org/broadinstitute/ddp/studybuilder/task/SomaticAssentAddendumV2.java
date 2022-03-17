09ba7b933830dec61979c83f10aaa3ecece1fe65
nsakande <nsakande@broadinstitute.org>
2022-03-17 14:47:09 +0400

    added patch, tested and worked successfully

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/SomaticAssentAddendumV2.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/SomaticAssentAddendumV2.java
@@ -4,33 +4,25 @@ import com.google.gson.Gson;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.broadinstitute.ddp.cache.LanguageStore;
-import org.broadinstitute.ddp.db.dao.*;
+import org.broadinstitute.ddp.db.dao.JdbiActivityVersion;
+import org.broadinstitute.ddp.db.dao.JdbiFormActivityFormSection;
+import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
+import org.broadinstitute.ddp.db.dao.SectionBlockDao;
+import org.broadinstitute.ddp.db.dao.UserDao;
 import org.broadinstitute.ddp.db.dto.ActivityVersionDto;
-import org.broadinstitute.ddp.db.dto.BlockContentDto;
 import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.exception.DDPException;
 import org.broadinstitute.ddp.model.activity.definition.FormSectionDef;
-import org.broadinstitute.ddp.model.activity.definition.template.Template;
-import org.broadinstitute.ddp.model.activity.definition.template.TemplateVariable;
-import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
-import org.broadinstitute.ddp.model.pdf.PdfActivityDataSource;
-import org.broadinstitute.ddp.model.pdf.PdfConfigInfo;
-import org.broadinstitute.ddp.model.pdf.PdfVersion;
 import org.broadinstitute.ddp.model.user.User;
 import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
 import org.broadinstitute.ddp.util.ConfigUtil;
 import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
-import org.jdbi.v3.sqlobject.SqlObject;
-import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
-import org.jdbi.v3.sqlobject.customizer.Bind;
-import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.time.Instant;
 
 public class SomaticAssentAddendumV2 implements CustomTask {
 
@@ -88,14 +80,15 @@ public class SomaticAssentAddendumV2 implements CustomTask {
                 .orElseThrow(() -> new DDPException("Could not find version " + versionTag));
         long revisionId = versionDto.getRevId();
 
-        var newDef = gson.fromJson(ConfigUtil.toJson(assentAddendumCfg), FormSectionDef.class);
+        var sectionDef = gson.fromJson(ConfigUtil.toJson(assentAddendumCfg), FormSectionDef.class);
 
-        var id = handle.attach(SectionBlockDao.class)
-                .insertSection(activityId, newDef, revisionId);
+        var sectionId = handle.attach(SectionBlockDao.class)
+                .insertSection(activityId, sectionDef, revisionId);
+
 
         var jdbiActSection = handle.attach(JdbiFormActivityFormSection.class);
 
-        jdbiActSection.insert(activityId, id, revisionId, 0);
+        jdbiActSection.insert(activityId, sectionId, revisionId, 50);
 
     }
 
