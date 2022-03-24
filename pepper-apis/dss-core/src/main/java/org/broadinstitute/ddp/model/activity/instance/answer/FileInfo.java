407dc54d2df3301cabc21e3412c2564315f9a21d
lasha <lkapanad@broadinstitute.org>
2022-03-24 14:46:18 +0400

    DDP-7534: serialize FileInfo uploadGuid

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/model/activity/instance/answer/FileInfo.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/model/activity/instance/answer/FileInfo.java
@@ -19,6 +19,8 @@ public class FileInfo implements Serializable {
     private long fileSize;
 
     private transient long uploadId;
+
+    @SerializedName("uploadGuid")
     private transient String uploadGuid;
 
     @JdbiConstructor
