a28b8b31b2682d072572d8f9b1071a47b74b4ebd
Nikoloz Sakandelidze <nsakande@broadinstitute.org>
2022-03-24 23:20:48 +0300

    Resolved DDP-7377. Created task OsteoAboutYouV2.

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoAboutYouV2.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoAboutYouV2.java
@@ -1,6 +1,8 @@
 package org.broadinstitute.ddp.studybuilder.task;
 
+import com.google.gson.Gson;
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.broadinstitute.ddp.db.DBUtils;
 import org.broadinstitute.ddp.db.dao.ActivityDao;
 import org.broadinstitute.ddp.db.dao.CopyConfigurationSql;
@@ -10,13 +12,17 @@ import org.broadinstitute.ddp.db.dao.JdbiQuestion;
 import org.broadinstitute.ddp.db.dao.JdbiRevision;
 import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dao.QuestionDao;
+import org.broadinstitute.ddp.db.dao.SectionBlockDao;
 import org.broadinstitute.ddp.db.dao.UserDao;
 import org.broadinstitute.ddp.db.dto.QuestionDto;
 import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.exception.DDPException;
+import org.broadinstitute.ddp.model.activity.definition.FormBlockDef;
 import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
 import org.broadinstitute.ddp.model.user.User;
 import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
+import org.broadinstitute.ddp.util.ConfigUtil;
+import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.customizer.Bind;
@@ -26,6 +32,7 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.util.Set;
@@ -33,24 +40,35 @@ import java.util.Set;
 /**
  * One-off task to add adhoc symptom message to TestBoston in deployed environments.
  */
-public class OsteoAboutChildV2 implements CustomTask {
+public class OsteoAboutYouV2 implements CustomTask {
 
-    private static final Logger LOG = LoggerFactory.getLogger(OsteoAboutChildV2.class);
+    private static final Logger LOG = LoggerFactory.getLogger(OsteoAboutYouV2.class);
+    private static final String DATA_FILE = "patches/about-you-v2.conf";
     private static final String STUDY_GUID = "CMI-OSTEO";
-    private static final String ACTIVITY_CODE = "ABOUTCHILD";
+    private static final String ACTIVITY_CODE = "ABOUTYOU";
     private static final String VERSION_TAG = "v2";
 
     private Config studyCfg;
     private Instant timestamp;
+    private Config dataCfg;
+    private Gson gson;
 
     @Override
     public void init(Path cfgPath, Config studyCfg, Config varsCfg) {
+        File file = cfgPath.getParent().resolve(DATA_FILE).toFile();
+        if (!file.exists()) {
+            throw new DDPException("Data file is missing: " + file);
+        }
+
+        this.dataCfg = ConfigFactory.parseFile(file);
+
         if (!studyCfg.getString("study.guid").equals(STUDY_GUID)) {
             throw new DDPException("This task is only for the " + STUDY_GUID + " study!");
         }
 
         this.studyCfg = studyCfg;
         this.timestamp = Instant.now();
+        this.gson = GsonUtil.standardGson();
     }
 
     @Override
@@ -78,35 +96,14 @@ public class OsteoAboutChildV2 implements CustomTask {
         final String reason = String.format(
                 "Update activity with studyGuid=%s activityCode=%s to versionTag=%s",
                 studyGuid, ACTIVITY_CODE, VERSION_TAG);
-
         final RevisionMetadata meta = new RevisionMetadata(timestamp.toEpochMilli(), adminUser.getId(), reason);
 
         //change version
-        activityDao.changeVersion(activityId, VERSION_TAG, meta);
+        var versionDto = activityDao.changeVersion(activityId, VERSION_TAG, meta);
 
-        //add new section
-        final var firstSection =
-                jdbiFormActivityFormSection.findOrderedSectionMemberships(activityId, meta.getTimestamp()).get(0);
-        final int sectionOrder = firstSection.getDisplayOrder() - 1;
-        final long newFormSectionId = jdbiFormSection.insert(jdbiFormSection.generateUniqueCode(), null);
-        jdbiFormActivityFormSection.insert(activityId, newFormSectionId, firstSection.getRevisionId(), sectionOrder);
-
-        LOG.info("New section successfully created with displayOrder={} and revision={}",
-                sectionOrder,
-                firstSection.getRevisionId());
-
-        // Move WHO_IS_FILLING to new section
-        QuestionDto questionFillingDto =
-                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "WHO_IS_FILLING")
-                        .orElseThrow(() -> new DDPException("Could not find question dto by studyId and question sid"));
-
-        final long currFillingBlockId = helper.findQuestionBlockId(questionFillingDto.getId());
-        helper.updateFormSectionBlock(newFormSectionId, currFillingBlockId);
-        LOG.info("Question ('WHO_IS_FILLING') successfully moved to new section={}", newFormSectionId);
-
-        // Disable CHILD_HOW_HEAR
+        // Disable HOW_HEAR
         QuestionDto questionHowHereDto =
-                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "CHILD_HOW_HEAR")
+                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "HOW_HEAR")
                         .orElseThrow(() -> new DDPException("Could not find question dto by studyId and question sid"));
 
         final long terminatedRevId = jdbiRevision.copyAndTerminate(questionHowHereDto.getRevisionId(), meta);
@@ -114,37 +111,51 @@ public class OsteoAboutChildV2 implements CustomTask {
         final long currHowHereBlockId = helper.findQuestionBlockId(questionHowHereDto.getId());
         questionDao.disableTextQuestion(questionHowHereDto.getId(), meta);
         helper.updateFormSectionBlockRevision(currHowHereBlockId, terminatedRevId);
-        LOG.info("Question ('CHILD_HOW_HEAR') successfully disabled");
+        LOG.info("Question ('HOW_HEAR') successfully disabled");
 
-        // Disable CHILD_EXPERIENCE
+        // Disable EXPERIENCE
         QuestionDto questionExperienceDto =
-                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "CHILD_EXPERIENCE")
+                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "EXPERIENCE")
                         .orElseThrow(() -> new DDPException("Could not find question dto by studyId and question sid"));
 
         final long currExperienceBlockId = helper.findQuestionBlockId(questionExperienceDto.getId());
         questionDao.disableTextQuestion(questionExperienceDto.getId(), meta);
         helper.updateFormSectionBlockRevision(currExperienceBlockId, terminatedRevId);
-        LOG.info("Question ('CHILD_EXPERIENCE') successfully disabled");
+        LOG.info("Question ('EXPERIENCE') successfully disabled");
 
-        // Disable CHILD_RACE
+        // Disable RACE
         QuestionDto questionRaceDto =
-                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "CHILD_RACE")
+                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "RACE")
                         .orElseThrow(() -> new DDPException("Could not find question dto by studyId and question sid"));
 
         final long currRaceBlockId = helper.findQuestionBlockId(questionRaceDto.getId());
         questionDao.disablePicklistQuestion(questionRaceDto.getId(), meta);
         helper.updateFormSectionBlockRevision(currRaceBlockId, terminatedRevId);
-        LOG.info("Question ('CHILD_RACE') successfully disabled");
+        LOG.info("Question ('RACE') successfully disabled");
 
-        // Disable CHILD_HISPANIC
+        // Disable HISPANIC
         QuestionDto questionHispanicDto =
-                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "CHILD_HISPANIC")
+                jdbiQuestion.findLatestDtoByStudyIdAndQuestionStableId(studyId, "HISPANIC")
                         .orElseThrow(() -> new DDPException("Could not find question dto by studyId and question sid"));
 
         final long currHispanicBlockId = helper.findQuestionBlockId(questionHispanicDto.getId());
         questionDao.disablePicklistQuestion(questionHispanicDto.getId(), meta);
         helper.updateFormSectionBlockRevision(currHispanicBlockId, terminatedRevId);
-        LOG.info("Question ('CHILD_HISPANIC') successfully disabled");
+        LOG.info("Question ('HISPANIC') successfully disabled");
+
+        //add new section
+        final var firstSection = jdbiFormActivityFormSection
+                .findOrderedSectionMemberships(activityId, meta.getTimestamp()).get(0);
+        final long newFormSectionId = jdbiFormSection.insert(jdbiFormSection.generateUniqueCode(), null);
+        final int sectionOrder = firstSection.getDisplayOrder() - 1;
+        jdbiFormActivityFormSection.insert(activityId, newFormSectionId, versionDto.getRevId(), sectionOrder);
+        LOG.info("New section successfully created with displayOrder={} and revision={}", sectionOrder, versionDto.getRevId());
+
+        //add new WHO_IS_FILLING_ABOUTYOU question
+        SectionBlockDao sectionBlockDao = handle.attach(SectionBlockDao.class);
+        FormBlockDef raceDef = gson.fromJson(ConfigUtil.toJson(dataCfg.getConfig("who_filling_q")), FormBlockDef.class);
+        sectionBlockDao.insertBlockForSection(activityId, newFormSectionId, sectionOrder, raceDef, versionDto.getRevId());
+        LOG.info("Question ('WHO_IS_FILLING_ABOUTYOU') successfully added");
 
         // Delete copy configs
         Set<Long> locationIds = helper.findCopyConfigsByQuestionSid(Set.of(
@@ -175,8 +186,5 @@ public class OsteoAboutChildV2 implements CustomTask {
 
         @SqlUpdate("update form_section__block set revision_id = :revisionId where block_id = :blockId")
         void updateFormSectionBlockRevision(@Bind("blockId") long blockId, @Bind("revisionId") long revisionId);
-
-        @SqlUpdate("update form_section__block set form_section_id = :formSectionId where block_id = :blockId")
-        void updateFormSectionBlock(@Bind("formSectionId") long formSectionId, @Bind("blockId") long blockId);
     }
 }
