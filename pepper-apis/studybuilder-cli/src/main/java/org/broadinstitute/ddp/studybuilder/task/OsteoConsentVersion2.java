a53af7d73b537f61c23ec4c17b36ac0ba77f7998
nsakande <nsakande@broadinstitute.org>
2022-03-28 11:50:17 +0400

    added revision mechanism to assent update in osteo project

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoConsentVersion2.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoConsentVersion2.java
@@ -1,42 +1,27 @@
 package org.broadinstitute.ddp.studybuilder.task;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import org.broadinstitute.ddp.db.dao.*;
+import org.broadinstitute.ddp.db.dto.ActivityVersionDto;
+import org.broadinstitute.ddp.db.dto.StudyDto;
+import org.broadinstitute.ddp.exception.DDPException;
+import org.broadinstitute.ddp.model.activity.definition.i18n.Translation;
+import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
+import org.broadinstitute.ddp.model.user.User;
+import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.sqlobject.SqlObject;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import com.google.gson.Gson;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import org.broadinstitute.ddp.cache.LanguageStore;
-import org.broadinstitute.ddp.db.dao.*;
-import org.broadinstitute.ddp.db.dto.*;
-import org.broadinstitute.ddp.exception.DDPException;
-import org.broadinstitute.ddp.model.activity.definition.ContentBlockDef;
-import org.broadinstitute.ddp.model.activity.definition.FormActivityDef;
-import org.broadinstitute.ddp.model.activity.definition.FormBlockDef;
-import org.broadinstitute.ddp.model.activity.definition.FormSectionDef;
-import org.broadinstitute.ddp.model.activity.definition.i18n.Translation;
-import org.broadinstitute.ddp.model.activity.definition.template.Template;
-import org.broadinstitute.ddp.model.activity.definition.template.TemplateVariable;
-import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
-import org.broadinstitute.ddp.model.pdf.PdfActivityDataSource;
-import org.broadinstitute.ddp.model.pdf.PdfConfigInfo;
-import org.broadinstitute.ddp.model.pdf.PdfVersion;
-import org.broadinstitute.ddp.model.user.User;
-import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
-import org.broadinstitute.ddp.util.ConfigUtil;
-import org.broadinstitute.ddp.util.GsonUtil;
-import org.jdbi.v3.core.Handle;
-import org.jdbi.v3.sqlobject.SqlObject;
-import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
-import org.jdbi.v3.sqlobject.customizer.Bind;
-import org.jdbi.v3.sqlobject.statement.SqlQuery;
-import org.jdbi.v3.sqlobject.statement.SqlUpdate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class OsteoConsentVersion2 implements CustomTask {
@@ -48,8 +33,7 @@ public class OsteoConsentVersion2 implements CustomTask {
     private Config dataCfg;
     private Instant timestamp;
     private String versionTag;
-
-    private static String VARIABLES_V2 = "translation-updates";
+    private Config cfg;
 
     @Override
     public void init(Path cfgPath, Config studyCfg, Config varsCfg) {
@@ -62,45 +46,59 @@ public class OsteoConsentVersion2 implements CustomTask {
         if (!studyCfg.getString("study.guid").equals(OSTEO_STUDY)) {
             throw new DDPException("This task is only for the " + OSTEO_STUDY + " study!");
         }
-
+        cfg = studyCfg;
         versionTag = dataCfg.getString("versionTag");
         timestamp = Instant.now();
     }
 
     @Override
     public void run(Handle handle) {
+        User adminUser = handle.attach(UserDao.class).findUserByGuid(cfg.getString("adminUser.guid")).get();
+
         String activityCode = dataCfg.getString("activityCode");
+        StudyDto studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid(dataCfg.getString("study.guid"));
 
         LOG.info("Changing version of {} to {} with timestamp={}", activityCode, versionTag, timestamp);
+        long ts = this.timestamp.toEpochMilli();
+        updateVariables(handle, adminUser, studyDto, activityCode, ts);
+    }
 
-        updateVariables(handle);
+    private void updateVariables(Handle handle, User adminUser, StudyDto studyDto, String activityCode, long ts) {
+        String VARIABLES_V2 = "translation-updates";
+        List<? extends Config> configList = dataCfg.getConfigList(VARIABLES_V2);
+        System.out.println(configList);
+        String reason = String.format(
+                "Update activity with studyGuid=%s activityCode=%s to versionTag=%s",
+                studyDto.getGuid(), activityCode, versionTag);
+
+        RevisionMetadata meta = new RevisionMetadata(ts, adminUser.getId(), reason);
+        long activityId = ActivityBuilder.findActivityId(handle, studyDto.getId(), activityCode);
+        ActivityVersionDto version2 = handle.attach(ActivityDao.class).changeVersion(activityId, "v2", meta);
+        for (Config config : configList) {
+            revisionVariableTranslation(config.getString(TRANSLATION_KEY), config.getString(TRANSLATION_NEW), handle, meta, version2);
+        }
     }
 
     private static final String TRANSLATION_KEY = "varName";
     private static final String TRANSLATION_NEW = "newValue";
 
-    private void updateVariables(Handle handle) {
-        List<? extends Config> configList = dataCfg.getConfigList(VARIABLES_V2);
-        System.out.println(configList);
-        for (Config config : configList) {
-            revisionVariableTranslation(config.getString(TRANSLATION_KEY), config.getString(TRANSLATION_NEW), handle);
-        }
-    }
-
-    private void revisionVariableTranslation(String varName, String newTemplateText, Handle handle) {
+    private void revisionVariableTranslation(String varName, String newTemplateText, Handle handle, RevisionMetadata meta, ActivityVersionDto version2) {
         long tmplVarId = handle.attach(SqlHelper.class).findTemplateVariableIdByVariableName(varName);
-        System.out.println("template variable id: " + tmplVarId);
-        handle.attach(SqlHelper.class).updateVarValueByTemplateVarId(tmplVarId, newTemplateText);
+        JdbiVariableSubstitution jdbiVarSubst = handle.attach(JdbiVariableSubstitution.class);
+        List<Translation> transList = jdbiVarSubst.fetchSubstitutionsForTemplateVariable(tmplVarId);
+        Translation currTranslation = transList.get(0);
+
+        JdbiRevision jdbiRevision = handle.attach(JdbiRevision.class);
+        long newVarSubRevId = jdbiRevision.copyAndTerminate(currTranslation.getRevisionId().get(), meta);
+        long[] revIds = {newVarSubRevId};
+        jdbiVarSubst.bulkUpdateRevisionIdsBySubIds(Arrays.asList(currTranslation.getId().get()), revIds);
+        jdbiVarSubst.insert(currTranslation.getLanguageCode(), newTemplateText, version2.getRevId(), tmplVarId);
     }
 
 
     private interface SqlHelper extends SqlObject {
         @SqlQuery("select template_variable_id from template_variable where variable_name = :variable_name")
         long findTemplateVariableIdByVariableName(@Bind("variable_name") String variableName);
-
-        @SqlUpdate("update i18n_template_substitution set substitution_value = :value where template_variable_id = :id")
-        int updateVarValueByTemplateVarId(@Bind("id") long templateVarId, @Bind("value") String value);
-
     }
 }
 
