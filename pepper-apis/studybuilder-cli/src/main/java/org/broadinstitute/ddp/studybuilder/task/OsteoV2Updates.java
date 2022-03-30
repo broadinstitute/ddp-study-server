bd7d42e2b168514f2a0220aa10e8cb9c3123f856
Archil Sharashenidze <96410791+archilsharashenidzebroad@users.noreply.github.com>
2022-03-30 13:01:18 +0400

    Ddp 7375/osteo pre qualifier (#1306)
    * create initial env
    
    * add question fix
    
    * add question change logic
    
    * change confs
    
    * change question
    
    * insert prequal update in osteo updates
    
    * Update OsteoV2Updates.java
    
    * add into patches
    
    * fix addednum insert
    
    * reverse prequal locally

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoV2Updates.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoV2Updates.java
@@ -19,6 +19,7 @@ public class OsteoV2Updates implements CustomTask {
         tasks.add(new OsteoAboutChildV2());
         tasks.add(new OsteoAboutYouV2());
         tasks.add(new OsteoConsentAddendumV2());
+        tasks.add(new OsteoPrequalUpdate());
         tasks.forEach(t -> t.init(cfgPath, studyCfg, varsCfg));
     }
 
