e5618d77542002d94ceae23489df96017f8a6734
Jose Manuel Morales Olivera <josemanu@broadinstitute.org>
2022-04-06 22:21:16 -0500

    update suggested changes

--- scripts/ddp-RxTerms-script/src/index.ts
+++ scripts/ddp-RxTerms-script/src/index.ts
@@ -1,6 +1,6 @@
 import * as XLSX from "xlsx";
 import { RxNormDrugOption } from "./models";
-import { parseToHOCON, writeFile } from "./utils/common";
+import * as fs from 'fs';
 
 const inputFileName = process.argv[2] || "RxTerms202202.txt";
 const outputFileName = process.argv[3] || "output.txt";
@@ -13,41 +13,21 @@ const workSheet = workBook.Sheets[workBook.SheetNames[0]];
 
 const rxNormArray = XLSX.utils.sheet_to_json(workSheet) as RxNormDrugOption[];
 
-const filteredRxNormOptions = rxNormArray.map((drug) => ({
-  stableId: drug.RXCUI,
-  displayName: drug.DISPLAY_NAME
-}));
-
-const pickListOptions = filteredRxNormOptions.map((option) => ({
-  stableId: option.stableId,
+const pickListOptions = rxNormArray.map((drug) => ({
+  stableId: drug.RXCUI.toString(),
   optionLabelTemplate: {
-    templateType: "TEXT",
-    templateText: option.displayName
+      templateType: 'TEXT',
+      templateText: '$drug_name',
+      variables: [{
+          name: 'drug_name',
+          translations: translations.map(lang => ({ language: lang, text: drug.DISPLAY_NAME.replace(/'/g, '') }))
+      }]
   }
 }));
 
-const picklistString = pickListOptions.reduce((prev, current) => {
-  const stringBuilder = `{${Object.keys(current)[0]}: ${
-    current.stableId
-  } optionLabelTemplate:{${Object.keys(current.optionLabelTemplate)[0]}: "${
-    current.optionLabelTemplate.templateType
-  }" ${
-    Object.keys(current.optionLabelTemplate)[1]
-  }: "$drug_name"
-  variables: [
-    {
-      name: drug_name
-      translations: [
-         ${translations.map(lang => `{language:  ${lang}  text:  "${current.optionLabelTemplate.templateText.replace(/'/g, "")}"}` )}
-      ]
-    }
-  ]  
-}},`;
-  return (prev += stringBuilder);
-}, "");
-
-
-writeFile(
-  outputFileName,
-  JSON.stringify(parseToHOCON(`picklistOptions : [${picklistString}]`), null, 2)
-);
+fs.writeFile(outputFileName, JSON.stringify({pickListOptions}, null, 2), err => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("The file was saved!");
+});
