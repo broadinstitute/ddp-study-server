69e1467554026620f4a35a650c2b5c3f238d8c0d
Blake Skinner <blake@rootpath.dev>
2022-04-08 08:40:27 -0400

    Remove CF tasks & initial setup

--- cloud-functions/cf-file-scanner/deploy.sh
+++ cloud-functions/cf-file-scanner/deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 #
 # Script to prepare and deploy function.
+#
+# Exit codes:
+#   1: Bad usage (improper or missing arguments)
+#   2: Missing cli tools
+#   -1 or 255: subprocess failure- most likely gcloud
 
 set -euo pipefail
 IFS=$'\n\t'
@@ -12,25 +17,40 @@ fi
 
 if ! command -v jq &> /dev/null; then
   echo "jq could not be found in PATH"
-  exit -1
+  exit 2
 fi
 
 if ! command -v gcloud &> /dev/null; then
   echo "gcloud could not be found in PATH"
-  exit -1
+  exit 2
 fi
 
 PROJECT_ID="${1:-""}"
-SERVICE_NAME="${SERVICE_NAME:-"cf-file-scanner"}"
+# gsc -> Google Serverless Compute
+# open to alternate naming schemes
+SERVICE_NAME="${SERVICE_NAME:-"gsc-file-scanner"}"
 CLOUDSDK_RUN_REGION="${CLOUDSDK_RUN_REGION:-"us-central1"}"
 
-SECRET_ID="$SERVICE_NAME"
-LISTEN_TOPIC_NAME="$SERVICE_NAME"
+SECRET_ID="${SECRET_ID:-$SERVICE_NAME}"
+SECRET_VERSION="latest"
+
+##
+# Topic name the service subscribes to listen for google.storage.object.finalize events
+##
+LISTEN_TOPIC_NAME="${LISTEN_TOPIC_NAME:-$SERVICE_NAME}"
+DEAD_LETTER_TOPIC_NAME="${DEAD_LETTER_TOPIC_NAME:-"$RESULT_TOPIC_NAME-dead-letter"}"
+
+RESULT_TOPIC_NAME="${RESULT_TOPIC_NAME:-"cf-file-scan-result"}"
 CF_NAME="$SERVICE_NAME"
 SA_ACCT="$SERVICE_NAME@$PROJECT_ID.iam.gserviceaccount.com"
+
+##
+# file-scanner container
+##
 CONTAINER_BASE="$CLOUDSDK_RUN_REGION-docker.pkg.dev/$PROJECT_ID/dss/$SERVICE_NAME"
 CONTAINER_TAG="latest"
 CONTAINER_NAME="$CONTAINER_BASE:$CONTAINER_TAG"
+
 
 function check-for-image {
   gcloud container images describe "$CONTAINER_NAME" &> /dev/null
@@ -46,95 +66,193 @@ function create-service-account {
     --description="Service Account for the $SERVICE_NAME service"
 }
 
-function check-for-secret {
-  gcloud --project="$PROJECT_ID" secrets versions describe latest --secret="$SECRET_ID" &> /dev/null
-}
-
 function access-secret {
-  gcloud secrets versions access latest \
+  gcloud secrets versions access "$SECRET_VERSION" \
     --project="$PROJECT_ID" \
-    --secret=$SECRET_ID
+    --secret="$SECRET_ID"
 }
 
 function check-for-subscription-topic {
   gcloud pubsub topics describe "$LISTEN_TOPIC_NAME" &> /dev/null
 }
 
-function check-for-subscriber-role {
-  gcloud pubsub topics get-iam-policy "$PROJECT_ID"
-    --flatten="bindings[].members"
-    --filter="bindings.members=serviceAccount:<serviceAccountId>"
-    --format="value(bindings.role)"
+function add-subscriber-iam-binding {
+  gcloud pubsub topics add-iam-policy-binding "$LISTEN_TOPIC_NAME" \
+    --member "serviceAccount:$SA_NAME" \
+    --role "roles/pubsub.subscriber"
+}
+
+function add-publisher-iam-binding {
+  gcloud pubsub topics add-iam-policy-binding "$RESULT_TOPIC_NAME" \
+    --member "serviceAccount:$SA_NAME" \
+    --role "roles/pubsub.publisher";
+}
+
+function deploy-service {
+  ##
+  # Take a look at the performance once this image is deployed.
+  # The mix of ClamAV and Java were running into OOM errors with
+  # a fair degree of frequency when using 2Gi. I traced the bulk
+  # of the issue to the ConcurrentDatabaseReload config value
+  # in clamd.conf and disabled it, but haven't performed thorough
+  # testing with the reduced memory amount.
+  # 
+  # Try reducing the 4Gi to 2Gi below and see if things are still happy.
+  # This system may also be able to get by with only a single CPU.
+  #
+  ##
+  gcloud run deploy "$SERVICE_NAME" \
+    --service-account="$SA_NAME" \
+    --region="$CLOUDSDK_RUN_REGION" \
+    --platform="managed" \
+    --cpu="2" \
+    --memory="4Gi" \
+    --cpu-throttling \
+    --image="$CONTAINER_NAME" \
+    --min-instances="1" \
+    --max-instances="3" \
+    --env-var-file="env.yaml" \
+    --update-secrets=ENV="$SECRET_ID:$SECRET_VERSION" \
+    --ingress="internal-and-cloud-load-balancing" \
+    --no-allow-unauthenticated \
+    --port="80"
+}
+
+function setup-dead-letter-topic {
+  if ! gcloud pubsub topics describe "$DEAD_LETTER_TOPIC_NAME"; then
+    gcloud pubsub topics create "$DEAD_LETTER_TOPIC_NAME" \
+      --message-retention-duration=7d \
+      --message-storage-policy-allowed-regions="$CLOUDSDK_RUN_REGION"
+  fi
+
+  ## Ensure the permissions are correct for the service account
+  gcloud pubsub topics add-iam-policy-binding \
+    --member "serviceAccount:$SA_NAME" \
+    --role "roles/pubsub.publisher"
+}
+
+function setup-listen-subscription {
+  local service_url=$1
+
+  gcloud pubsub subscriptions create "gsc-file-scanner-trigger" \
+    --topic="$LISTEN_TOPIC_NAME" \
+    --topic-project="$PROJECT_ID" \
+    --ack-deadline="60" \
+    --enable-message-ordering \
+    --expiration-period="never" \
+    --push-auth-service-account="$SA_NAME" \
+    --push-endpoint="$service_url" \
+    --max-delivery-attempts="5" \
+    --dead-letter-topic="$DEAD_LETTER_TOPIC_NAME" \
+    --dead-letter-topic-project="$PROJECT_ID" \
+    --min-retry-delay="10s"
+    --max-retry-delay="5m"
 }
 
 function main {
+  ##
+  # Walk through some of the critical things we can check
+  # which don't result in modifications to the project.
+  ##
+
   if ! check-for-image; then
     echo "Failed to locate a valid image for $CONTAINER_NAME. Ensure the repository exists and the name is correct."
-    exit 2
+    exit -1
+  fi
+  
+  ##
+  # The topic the service will subscribe to, and listen for
+  # bucket object finalization events.
+  # Required the pubsub.subscriber role
+  ##
+  if ! check-for-subscription-topic; then
+    echo "PubSub topic $LISTEN_TOPIC_NAME does not exist"
+    exit -1
   fi
 
+  ##
+  # The topic the service will publish scan results to.
+  # Requires the pubsub.publisher role
+  ##
+  if ! check-for-result-topic; then
+    echo "Pubsub topic $RESULT_TOPIC_NAME does not exist"
+    exit -1
+  fi
+
+  ##
+  # The secret is less critical than it was before. Most
+  # of the properties embedded in the secret are required
+  # for the setup process, so it's entirely feasible to flip
+  # this process on its head and update the secret given
+  # the parameters set in the deploy script
+  ##
+  if ! access-secret > env.yaml; then
+    echo "Failed to access the secret named $SECRET_ID in $PROJECT_ID. Ensure the secret exists and you have access to it."
+    exit -1
+  fi
+
+  ##
+  # This is where the modifications start. I've attempted to arrange
+  # the various steps in the order from least side-effects to most side-effects
+  # except where dependencies require a different order (for example,
+  # subscribing to a topic requires that the service be successfully deployed
+  # first).
+  ##
   if ! check-for-service-account; then
     echo "Service account $SA_ACCT does not exist. Attempting to create it..."
     create-service-account
   else
-    echo "Service account $SA_ACCT exists, skipping account creation"
+    echo "Service account $SA_ACCT found"
   fi
 
-  if ! check-for-secret; then
-    echo "Failed to find the secret named $SECRET_ID in $PROJECT_ID. Ensure the secret exists and you have access to it."
-    exit 2
-  else
-    access-secret > env.yaml
+  ##
+  # I tested the two following IAM binding adds, and gcloud will
+  # still exit with a success if the role already exists. This
+  # has no other side effects if the binding does not need to be
+  # modified, so it's safe to call repeatedly.
+  ##
+  if ! add-subscriber-iam-binding; then
+    echo "failed to grant the pubsub.subscriber role to $SA_NAME on topic $LISTEN_TOPIC_NAME"
+    exit -1
   fi
 
-  ## Deploys a new version of a service
-  if ! gcloud run services describe "$SERVICE_NAME" &> /dev/null; then
-    echo "$SERVICE_NAME does not exist. Attempt to create it..."
-    gcloud run deploy "$SERVICE_NAME" \
-      --region="$CLOUDSDK_RUN_REGION" \
-      --platform="managed" \
-      --cpu="2" \
-      --memory="4Gi" \
-      --cpu-throttling \
-      --image="$CONTAINER_NAME" \
-      --min-instances="1" \
-      --max-instances="3" \
-      --env-var-file="env.yaml" \
-      --update-secrets=ENV="$SECRET_ID:latest" \
-      --ingress="internal-and-cloud-load-balancing" \
-      --port="80"
+  if ! add-result-publisher-iam-binding; then
+    echo "failed to grant the pubsub.publisher role to $SA_NAME on topic $RESULT_TOPIC_NAME"
+    exit -1
   fi
 
-  ## Create the PubSub Topic to listen on
-  # Needs roles/pubsub.subscriber
-  if ! check-for-subscription-topic; then
-    echo "PubSub topic $LISTEN_TOPIC_NAME does not exist"
-    exit 2
+  ##
+  # Deploys a new version of a service. Keep an eye on this spot as
+  # I'm currently unsure of how the deployment will response if
+  # the same image is already deployed, and the invocation is identical
+  # to the previous one
+  ##
+  echo "Deploying $CONTAINER_NAME to service $SERVICE_NAME."
+  if ! deploy-service; then
+    echo "Deploy of $SERVICE_NAME failed."
+    exit -1
   fi
-  
 
-  if ! check-for-
+  ##
+  # Used in setting up the subscription for triggering the service
+  # below.
+  #
+  # It may make a bit more sense to merge it into the
+  # setup-listen-subscription sometime later, but it works here
+  # for now in the interests of time.
+  ##
+  if ! setup-dead-letter-topic; then 
+    echo "failed to setup topic $DEAD_LETTER_TOPIC_NAME"
+  fi
 
-  ## Create a subscription between the service & the new topic
+  local service_url="$(gcloud run services describe $SERVICE_NAME --format=json | jq '.status.address.url')"
+  if ! setup-listen-subscription "$service_url"; then
+    echo "failed to create a subscriber to topic $LISTEN_TOPIC_NAME"
+    exit -1
+  fi
 
-
-  echo 'Rendering env vars file...'
-  gcloud --project=$PROJECT_ID \
-    secrets versions access latest \
-    --secret=$SECRET_ID > env.yaml
-
-  echo 'Uploading function...'
-  echo 'Note: it might take a few minutes to upload clamav binaries.'
-
-  echo ''
-  gcloud --project=$PROJECT_ID functions deploy $CF_NAME \
-    --entry-point org.broadinstitute.ddp.cf.FileScanner \
-    --trigger-topic $LISTEN_TOPIC_NAME \
-    --service-account $SA_ACCT \
-    --env-vars-file env.yaml \
-    --region us-central1 \
-    --runtime java11 \
-    --memory 2048MB \
-    --timeout 5m \
-    --retry
+  echo "$SERVICE_NAME deployment was successful"
+  return 0
 }
+
+main "$@"
