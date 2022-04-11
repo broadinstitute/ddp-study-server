cf98482167641c9a331ce347a1dbff84bd824c6a
pegahtah <ptaheri@broadinstitute.org>
2022-04-11 10:40:59 -0400

    removed tiny change

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/DSMServer.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/DSMServer.java
@@ -160,7 +160,6 @@ import spark.Spark;
 
 public class DSMServer {
 
-    
     public static final String CONFIG = "config";
     public static final String NOTIFICATION_UTIL = "NotificationUtil";
     public static final String KIT_UTIL = "KitUtil";
