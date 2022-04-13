8a9087c3f666ce4a59b9546aa09d88672c6e8477
gmakhara <gugamaxara@github.com>
2022-04-13 20:12:47 +0400

    hotfix/ return 0 if age field is empty

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/export/parse/ValueParser.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/export/parse/ValueParser.java
@@ -6,6 +6,7 @@ public class ValueParser extends BaseParser {
 
     @Override
     protected Object forNumeric(String value) {
+        if (StringUtils.isBlank(value)) return 0;
         return Long.valueOf(value);
     }
 
