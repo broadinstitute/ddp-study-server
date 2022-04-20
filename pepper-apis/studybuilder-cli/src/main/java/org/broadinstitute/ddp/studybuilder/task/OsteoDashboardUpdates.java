dcd2097b82d79c7b83f19184ef0cb0f42f78bba7
nsakande <nsakande@broadinstitute.org>
2022-04-20 10:44:50 +0400

    updated child dashboard

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoDashboardUpdates.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoDashboardUpdates.java
@@ -3,13 +3,23 @@ package org.broadinstitute.ddp.studybuilder.task;
 import com.google.gson.Gson;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.broadinstitute.ddp.db.dao.ActivityI18nDao;
+import org.broadinstitute.ddp.db.dao.JdbiRevision;
+import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
+import org.broadinstitute.ddp.db.dao.UserDao;
+import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.exception.DDPException;
+import org.broadinstitute.ddp.model.activity.definition.i18n.ActivityI18nDetail;
+import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
+import org.broadinstitute.ddp.model.user.User;
 import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -18,11 +28,17 @@ import java.util.List;
 
 public class OsteoDashboardUpdates implements CustomTask {
 
+    private static final Logger LOG = LoggerFactory.getLogger(OsteoDashboardUpdates.class);
 
     private static final String ADULT_CONSENT = "patches/adult-consent-dashboard-update.conf";
     private static final String ADULT_MEDICAL_RELEASE = "patches/adult-mr-dashboard-update.conf";
     private static final String ADULT_ABOUT_YOU = "patches/adult-about-you-dashboard-update.conf";
     private static final String ADULT_ABOUT_YOU_ACTIVITY = "patches/adult-about-you-dashboard-update.conf";
+
+    private static final String CHILD_CONSENT = "patches/child-consent-dashboard-update.conf";
+    private static final String CHILD_MEDICAL_RELEASE = "patches/child-mr-dashboard-update.conf";
+    private static final String CHILD_ABOUT_YOU = "patches/child-about-you-dashboard-update.conf";
+
 
     private static final String OSTEO_STUDY = "CMI-OSTEO";
 
@@ -35,6 +51,12 @@ public class OsteoDashboardUpdates implements CustomTask {
     private Config adultMedicalReleaseCfg;
     private Config adultAboutYouCfg;
     private Config adultAboutYouActivityCfg;
+
+    private Config childConsentCfg;
+    private Config childMedicalReleaseCfg;
+    private Config childAboutYouCfg;
+
+    private ActivityI18nDao activityI18nDao;
 
     private Instant timestamp;
 
@@ -49,7 +71,52 @@ public class OsteoDashboardUpdates implements CustomTask {
             throw new DDPException("This task is only for the " + OSTEO_STUDY + " study!");
         }
 
+        initAdult();
+        initChild();
 
+        cfg = studyCfg;
+        timestamp = Instant.now();
+        gson = GsonUtil.standardGson();
+    }
+
+    @Override
+    public void run(Handle handle) {
+
+        this.activityI18nDao = handle.attach(ActivityI18nDao.class);
+
+        updateDashboard(handle, adultConsentCfg);
+        updateDashboard(handle, adultMedicalReleaseCfg);
+        updateDashboard(handle, adultAboutYouCfg);
+        updateDashboard(handle, adultAboutYouActivityCfg);
+
+        updateDashboard(handle, childConsentCfg);
+        updateDashboard(handle, childMedicalReleaseCfg);
+        updateDashboard(handle, childAboutYouCfg);
+
+        updateAboutYouNameDashboard(handle, adultAboutYouCfg);
+    }
+
+    private void initChild() {
+        File childConsentFile = cfgPath.getParent().resolve(CHILD_CONSENT).toFile();
+        if (!childConsentFile.exists()) {
+            throw new DDPException("Data file is missing: " + childConsentFile);
+        }
+        childConsentCfg = ConfigFactory.parseFile(childConsentFile);
+
+        File childMedicalReleaseFile = cfgPath.getParent().resolve(CHILD_MEDICAL_RELEASE).toFile();
+        if (!childMedicalReleaseFile.exists()) {
+            throw new DDPException("Data file is missing: " + childMedicalReleaseFile);
+        }
+        childMedicalReleaseCfg = ConfigFactory.parseFile(childMedicalReleaseFile);
+
+        File childAboutYouFile = cfgPath.getParent().resolve(CHILD_ABOUT_YOU).toFile();
+        if (!childAboutYouFile.exists()) {
+            throw new DDPException("Data file is missing: " + childAboutYouFile);
+        }
+        childAboutYouCfg = ConfigFactory.parseFile(childAboutYouFile);
+    }
+
+    private void initAdult() {
         File adultConsentFile = cfgPath.getParent().resolve(ADULT_CONSENT).toFile();
         if (!adultConsentFile.exists()) {
             throw new DDPException("Data file is missing: " + adultConsentFile);
@@ -75,29 +142,59 @@ public class OsteoDashboardUpdates implements CustomTask {
         }
         adultAboutYouActivityCfg = ConfigFactory.parseFile(adultAboutYouActivityFile);
 
-        cfg = studyCfg;
-        timestamp = Instant.now();
-        gson = GsonUtil.standardGson();
     }
 
-    @Override
-    public void run(Handle handle) {
-
-        updateAdultDashboard(handle, adultConsentCfg);
-        updateAdultDashboard(handle, adultMedicalReleaseCfg);
-        updateAdultDashboard(handle, adultAboutYouCfg);
-    }
-
-    private void updateAdultDashboard(Handle handle, Config config) {
+    private void updateDashboard(Handle handle, Config config) {
         List<? extends Config> configList = config.getConfigList("newTranslatedSummaries");
-        String studyActivityCode = config.getString("activityCode");
-        int studyActivityId = handle.attach(SqlHelper.class).findStudyActivityId(studyActivityCode);
-        for (Config conf : configList) {
-            String statusCode = conf.getString("statusCode");
-            int typeId = handle.attach(SqlHelper.class).findTypeId(statusCode);
-            String newValue = conf.getString("text");
-            handle.attach(SqlHelper.class).updateTranslationText(newValue, studyActivityId, typeId);
+        List<? extends Config> studyActivityCode = config.getConfigList("activityCode");
+        for (Config conf1 : studyActivityCode) {
+            String activityCode = conf1.getString("code");
+            int studyActivityId = handle.attach(SqlHelper.class).findStudyActivityId(activityCode);
+            for (Config conf : configList) {
+                String statusCode = conf.getString("statusCode");
+                int typeId = handle.attach(SqlHelper.class).findTypeId(statusCode);
+                String newValue = conf.getString("text");
+                handle.attach(SqlHelper.class).updateTranslationText(newValue, studyActivityId, typeId);
+            }
         }
+    }
+
+    private void updateAboutYouNameDashboard(Handle handle, Config config) {
+        String studyActivityCode = config.getConfigList("activityCode").get(0).getString("code");
+        int studyActivityId = handle.attach(SqlHelper.class).findStudyActivityId(studyActivityCode);
+        updateActivityDetails(handle, studyActivityId, studyActivityCode);
+    }
+
+    private void updateActivityDetails(Handle handle, long activityId, String activityCode) {
+
+        long ts = this.timestamp.toEpochMilli();
+        User adminUser = handle.attach(UserDao.class).findUserByGuid(cfg.getString("adminUser.guid")).get();
+
+        StudyDto studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid("CMI-OSTEO");
+        String reasonConsentAssent = String.format(
+                "Update activity with studyGuid=%s activityCode=%s to versionTag=%s",
+                studyDto.getGuid(), activityCode, "V2");
+        RevisionMetadata meta = new RevisionMetadata(ts, adminUser.getId(), reasonConsentAssent);
+
+        ActivityI18nDetail i18nDetail = activityI18nDao
+                .findDetailsByActivityIdAndTimestamp(activityId, Instant.now().toEpochMilli())
+                .iterator().next();
+
+        JdbiRevision jdbiRevision = handle.attach(JdbiRevision.class);
+        long newRevId = jdbiRevision.copyAndTerminate(i18nDetail.getRevisionId(), meta);
+
+        var newI18nDetail = new ActivityI18nDetail(
+                i18nDetail.getId(),
+                i18nDetail.getActivityId(),
+                i18nDetail.getLangCodeId(),
+                i18nDetail.getIsoLangCode(),
+                adultAboutYouCfg.getString("name"),
+                i18nDetail.getSecondName(),
+                i18nDetail.getTitle(),
+                i18nDetail.getSubtitle(),
+                i18nDetail.getDescription(),
+                newRevId);
+        activityI18nDao.insertDetails(List.of(newI18nDetail));
     }
 
     private interface SqlHelper extends SqlObject {
