703615add6f183fbb13898b06bad2cc43c945719
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-04-22 10:39:20 +0200

    Sampath's comment resolved

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/QuestionDao.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/QuestionDao.java
@@ -1546,7 +1546,7 @@ public interface QuestionDao extends SqlObject {
 
     default void insertQuestion(long activityId, CompositeQuestionDef compositeQuestion, long revisionId) {
         if (!compositeQuestion.isAcceptable()) {
-            throw new DaoException("Composite questions are only support following types for children: "
+            throw new DaoException("Composite questions only support following types for children: "
                     + StreamEx.of(QuestionType.values())
                             .filter(QuestionType::isCompositional)
                             .joining());
