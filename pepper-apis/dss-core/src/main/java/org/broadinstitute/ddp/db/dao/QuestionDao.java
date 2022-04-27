43f70bb96aabd686258931f930698ea8e31f7539
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-04-27 12:01:37 +0200

    Filtering

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/QuestionDao.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/QuestionDao.java
@@ -45,6 +45,7 @@ import org.broadinstitute.ddp.db.dto.ActivityInstanceSelectQuestionDto;
 import org.broadinstitute.ddp.db.dto.TypedQuestionId;
 import org.broadinstitute.ddp.db.dto.validation.RuleDto;
 import org.broadinstitute.ddp.equation.QuestionEvaluator;
+import org.broadinstitute.ddp.json.EquationResponse;
 import org.broadinstitute.ddp.model.activity.definition.QuestionBlockDef;
 import org.broadinstitute.ddp.model.activity.definition.question.AgreementQuestionDef;
 import org.broadinstitute.ddp.model.activity.definition.question.BoolQuestionDef;
@@ -919,6 +920,7 @@ public interface QuestionDao extends SqlObject {
                 StreamEx.of(getJdbiEquationQuestion().findEquationsByActivityInstanceGuid(activityInstanceGuid))
                         .map(questionEvaluator::evaluate)
                         .filter(Objects::nonNull)
+                        .filterBy(EquationResponse::getQuestionStableId, dto.getStableId())
                         .map(EquationAnswer::new)
                         .toList(),
                 Collections.emptyList(),
