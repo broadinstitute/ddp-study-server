bc300bec37700c5aebc25f616a8c4d56ec8ac453
gmakhara <gugamaxara@github.com>
2022-05-04 18:53:28 +0400

    DDP-7937 | fix exact, non exact search, create NonExactMatchQueryStrategy for non exact searches, refactor and add tests

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/mapping/TypeExtractor.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/mapping/TypeExtractor.java
@@ -1,7 +1,14 @@
 package org.broadinstitute.dsm.model.elastic.mapping;
 
+import org.broadinstitute.dsm.util.ElasticSearchUtil;
+
 public interface TypeExtractor<T> {
     T extract();
+
+    default String getRightMostFieldName(String fullFieldName) {
+        String[] splittedFieldName = fullFieldName.split(ElasticSearchUtil.ESCAPE_CHARACTER_DOT_SEPARATOR);
+        return splittedFieldName[splittedFieldName.length - 1];
+    }
 
     void setIndex(String index);
 
