23b10506a67e69180f1ffffda0f1e9ab0b72d8c2
Leonid <43238985+leonidrakitin@users.noreply.github.com>
2022-05-11 13:43:42 +0400

    Update prion. (#1602)
    Co-authored-by: leonid.rakitin <leonid.rakitin@quantori.com>
    Co-authored-by: Dmitrii Eliseev <eliseev.dmitry@gmail.com>

---
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/PrionInsertEvents.java
@@ -0,0 +1,7 @@
+package org.broadinstitute.ddp.studybuilder.task;
+
+public class PrionInsertEvents extends InsertStudyEvents {
+    PrionInsertEvents() {
+        super("PRION", "patches/activity_instance_creation_prionmedical_event.conf");
+    }
+}
