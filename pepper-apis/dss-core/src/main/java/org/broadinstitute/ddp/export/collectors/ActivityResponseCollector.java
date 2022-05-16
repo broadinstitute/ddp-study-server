a1d23b3220ef3fa8072f1eed75f37db8224d242f
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-05-16 15:07:41 +0200

    DDP-7713: Checkstyle

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/export/collectors/ActivityResponseCollector.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/export/collectors/ActivityResponseCollector.java
@@ -10,7 +10,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import com.itextpdf.layout.element.Tab;
 import org.broadinstitute.ddp.exception.DDPException;
 import org.broadinstitute.ddp.export.ComponentDataSupplier;
 import org.broadinstitute.ddp.model.activity.definition.ActivityDef;
