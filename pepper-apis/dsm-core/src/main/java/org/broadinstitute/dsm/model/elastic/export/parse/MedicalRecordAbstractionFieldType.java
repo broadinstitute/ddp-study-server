211a1e7c604aec118eca4f9c8be0be7ee832cb0e
nghurtch <nghurtch@broadinstitute.com>
2022-05-16 11:26:59 +0400

    DDP-7993 | add MedicalRecordAbstractionFieldType enum for representing the finite amount of unique data types for abstraction

---
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/export/parse/MedicalRecordAbstractionFieldType.java
@@ -0,0 +1,38 @@
+package org.broadinstitute.dsm.model.elastic.export.parse;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+@Getter
+enum MedicalRecordAbstractionFieldType {
+
+    DATE("date"),
+    BUTTON_SELECT("button_select"),
+    NUMBER("number"),
+    MULTI_OPTIONS("multi_options"),
+    TEXT_AREA("text_area"),
+    MULTI_TYPE_ARRAY("multi_type_array"),
+    TEXT("text"),
+    TABLE("table"),
+    OPTIONS("options"),
+    DRUGS("drugs"),
+    MULTI_TYPE("multi_type"),
+    CHECKBOX("checkbox"),
+    CANCERS("cancers");
+
+
+    private final String innerValue;
+
+    MedicalRecordAbstractionFieldType(String innerValue) {
+        this.innerValue = innerValue;
+    }
+
+    public static MedicalRecordAbstractionFieldType of(String value) {
+        return Arrays.stream(MedicalRecordAbstractionFieldType.values())
+                .filter(fieldType -> fieldType.getInnerValue().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException(String.format("requested type %s does not exist", value)));
+    }
+}
