2e104ecef641b5057afd833499dc4938db541dd8
Sampath K. Settipalli <sampath@broadinstitute.org>
2022-05-18 21:25:49 -0500

    DDP-8034. file answer null fix.

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/AnswerDao.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/AnswerDao.java
@@ -519,8 +519,8 @@ public interface AnswerDao extends SqlObject {
                                 view.getColumn("fa_upload_guid", String.class),
                                 view.getColumn("fa_file_name", String.class),
                                 view.getColumn("fa_file_size", Long.class));
+                        ((FileAnswer) answer).getValue().add(info);
                     }
-                    ((FileAnswer) answer).getValue().add(info);
                     break;
                 case NUMERIC:
                     answer = new NumericAnswer(answerId, questionStableId, answerGuid,
