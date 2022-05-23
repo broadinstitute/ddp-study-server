5502ec89cc7a4d5e82f74a9f0de9086e3640bc3a
archilsharashenidzebroad <asharash@broadinstitute.org>
2022-05-23 16:21:52 +0400

    refactor patch

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoPrequalUpdate.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoPrequalUpdate.java
@@ -1,4 +1,4 @@
-package org.broadinstitute.ddp.studybuilder.task.osteo;
+package org.broadinstitute.ddp.studybuilder.task;
 
 import com.google.gson.Gson;
 import com.typesafe.config.Config;
@@ -6,13 +6,11 @@ import com.typesafe.config.ConfigFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.broadinstitute.ddp.db.dao.ActivityDao;
 import org.broadinstitute.ddp.db.dao.JdbiActivity;
-import org.broadinstitute.ddp.db.dao.JdbiQuestion;
 import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dao.SectionBlockDao;
 import org.broadinstitute.ddp.db.dao.UserDao;
 import org.broadinstitute.ddp.db.dto.ActivityDto;
 import org.broadinstitute.ddp.db.dto.ActivityVersionDto;
-import org.broadinstitute.ddp.db.dto.QuestionDto;
 import org.broadinstitute.ddp.db.dto.RevisionDto;
 import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.exception.DDPException;
@@ -20,10 +18,8 @@ import org.broadinstitute.ddp.model.activity.definition.FormActivityDef;
 import org.broadinstitute.ddp.model.activity.definition.FormBlockDef;
 import org.broadinstitute.ddp.model.activity.definition.FormSectionDef;
 import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
-import org.broadinstitute.ddp.model.activity.types.PicklistSelectMode;
 import org.broadinstitute.ddp.model.user.User;
 import org.broadinstitute.ddp.studybuilder.ActivityBuilder;
-import org.broadinstitute.ddp.studybuilder.task.CustomTask;
 import org.broadinstitute.ddp.util.ConfigUtil;
 import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
@@ -43,6 +39,8 @@ public class OsteoPrequalUpdate implements CustomTask {
     private static final String STUDY_GUID = "CMI-OSTEO";
     private static final String ACTIVITY_CODE = "PREQUAL";
 
+    private Path cfgPath;
+    private Config varsCfg;
     private Config dataCfg;
     private Config studyCfg;
     private Gson gson;
@@ -57,6 +55,8 @@ public class OsteoPrequalUpdate implements CustomTask {
         if (!file.exists()) {
             throw new DDPException("Data file is missing: " + file);
         }
+        this.varsCfg = varsCfg;
+        this.cfgPath = cfgPath;
         this.dataCfg = ConfigFactory.parseFile(file).resolveWith(varsCfg);
         this.studyCfg = studyCfg;
         this.gson = GsonUtil.standardGson();
@@ -75,12 +75,26 @@ public class OsteoPrequalUpdate implements CustomTask {
                 STUDY_GUID, ACTIVITY_CODE, versionTag);
         RevisionMetadata meta = new RevisionMetadata(timestamp.toEpochMilli(), adminUser.getId(), reason);
         log.info("Making revision for new changes in blocks");
-        revisionPrequal(activityId, dataCfg, handle, meta, versionTag);
+        revisionPrequal(activityId, dataCfg, handle, adminUser, studyDto, meta, versionTag);
     }
 
-    private void revisionPrequal(long activityId, Config dataCfg, Handle handle, RevisionMetadata meta, String versionTag) {
+    private void revisionPrequal(long activityId, Config dataCfg, Handle handle, User adminUser,
+                                 StudyDto studyDto, RevisionMetadata meta, String versionTag) {
         ActivityVersionDto version2 = handle.attach(ActivityDao.class).changeVersion(activityId, versionTag, meta);
         insertBlock(handle, dataCfg, activityId, version2, meta);
+        traverseActivity(handle, dataCfg, studyDto, adminUser, activityId, version2);
+    }
+
+    private void traverseActivity(Handle handle, Config dataCfg, StudyDto studyDto,
+                                  User admin, long activityId, ActivityVersionDto versionDto) {
+        ActivityBuilder activityBuilder = new ActivityBuilder(cfgPath.getParent(), studyCfg, varsCfg, studyDto, admin.getId());
+        ActivityDao activityDao = handle.attach(ActivityDao.class);
+        JdbiActivity jdbiActivity = handle.attach(JdbiActivity.class);
+        ActivityDto activityDto = jdbiActivity.findActivityByStudyIdAndCode(studyDto.getId(), ACTIVITY_CODE).get();
+        Config prequal = activityBuilder.readDefinitionConfig("activities/prequal.conf");
+        FormActivityDef activity = (FormActivityDef) activityDao.findDefByDtoAndVersion(activityDto, versionDto);
+        UpdateTemplatesInPlace updateTemplatesInPlace = new UpdateTemplatesInPlace();
+        updateTemplatesInPlace.traverseActivity(handle, ACTIVITY_CODE, prequal, activity, versionDto.getRevStart());
         updateQuestion(handle, dataCfg, activityId);
     }
 
@@ -108,10 +122,6 @@ public class OsteoPrequalUpdate implements CustomTask {
         List<? extends Config> questionUpdates = dataCfg.getConfigList("questionUpdates");
 
         questionUpdates.forEach(config -> {
-            String stableId = config.getString("stableId");
-            QuestionDto questionDto = handle.attach(JdbiQuestion.class)
-                    .findDtoByActivityIdAndQuestionStableId(activityId, stableId).get();
-
             if (!config.getConfig("validation").isEmpty()) {
                 Config validation = config.getConfig("validation");
                 String varName = String.format("%s%s%s", "%", validation.getString("varName"), "%");
@@ -120,75 +130,9 @@ public class OsteoPrequalUpdate implements CustomTask {
                 long templateVariableId = helper.getTemplateVariableIdbyTemplateId(activityValidationTemplateId);
                 helper.updateTemplateText(newVal, templateVariableId);
             }
-
-            List<? extends Config> question = config.getConfigList("question");
-            for (Config config1 : question) {
-                String subsValue = config1.getString("newVal");
-                long templateVariableIdbyTemplateId = helper.getTemplateVariableIdbyTemplateId(questionDto.getPromptTemplateId());
-                helper.updateTemplateText(subsValue, templateVariableIdbyTemplateId);
-            }
-        });
-
-        changeQuetionStyle(handle, activityId, "PREQUAL_SELF_DESCRIBE");
-        changeAgeRestriction(handle, activityId);
-        changePicklist(handle, activityId, dataCfg);
-    }
-
-    private void changeQuetionStyle(Handle handle, long activityId, String stableId) {
-        SqlHelper helper = handle.attach(SqlHelper.class);
-        JdbiQuestion jdbiQuestion = handle.attach(JdbiQuestion.class);
-        QuestionDto questionDto = jdbiQuestion.findDtoByActivityIdAndQuestionStableId(activityId, stableId).get();
-        String value = "MULTIPLE";
-        PicklistSelectMode picklistSelectMode = PicklistSelectMode.valueOf(value);
-        long pickListModeIdByValue = helper.getPickListModeIdByValue(picklistSelectMode);
-        helper.updatePicklistOption(questionDto.getId(), pickListModeIdByValue);
-    }
-
-    private void changePicklist(Handle handle, long activityId, Config dataCfg) {
-        SqlHelper attach = handle.attach(SqlHelper.class);
-        Config picklist = dataCfg.getConfigList("picklist").get(0);
-        String stableId = picklist.getString("stableId");
-        List<? extends Config> list = picklist.getConfigList("list");
-        JdbiQuestion jdbiQuestion = handle.attach(JdbiQuestion.class);
-        QuestionDto questionDto = jdbiQuestion.findDtoByActivityIdAndQuestionStableId(activityId, stableId).get();
-        long id = questionDto.getId();
-        list.forEach(item -> {
-            String pickId = item.getString("stableId");
-            String newVal = item.getString("new");
-            long picklistLabelTemplateId = attach.getPicklistLabelTemplateId(id, pickId);
-            long templateVariableIdbyTemplateId = attach.getTemplateVariableIdbyTemplateId(picklistLabelTemplateId);
-            attach.updateTemplateText(newVal, templateVariableIdbyTemplateId);
         });
     }
 
-    private void changeAgeRestriction(Handle handle, long activityId) {
-        int age = 110;
-        String templateText = "Please enter an age between 0 and 110";
-
-        String stableId1 = "SELF_CURRENT_AGE";
-        String stableId2 = "CHILD_CURRENT_AGE";
-
-        QuestionDto selfQuestion = handle.attach(JdbiQuestion.class).findDtoByActivityIdAndQuestionStableId(activityId, stableId1).get();
-        QuestionDto childQuestion = handle.attach(JdbiQuestion.class).findDtoByActivityIdAndQuestionStableId(activityId, stableId2).get();
-
-        SqlHelper helper = handle.attach(SqlHelper.class);
-
-        var validationIds = helper.getValidationId(selfQuestion.getId());
-        for (long validationId : validationIds) {
-            helper.insertUpperRange(age, validationId);
-            long hintTemplateId = helper.getHintTemplateId(validationId);
-            long templateVariableId = helper.getTemplateVariableIdbyTemplateId(hintTemplateId);
-            helper.updateTemplateText(templateText, templateVariableId);
-        }
-
-        var validationId2 = helper.getValidationId(childQuestion.getId());
-        for (long validationId : validationId2) {
-            helper.insertUpperRange(age, validationId);
-            long hintTemplateId = helper.getHintTemplateId(validationId);
-            long templateVariableId = helper.getTemplateVariableIdbyTemplateId(hintTemplateId);
-            helper.updateTemplateText(templateText, templateVariableId);
-        }
-    }
 
     private interface SqlHelper extends SqlObject {
 
@@ -196,22 +140,9 @@ public class OsteoPrequalUpdate implements CustomTask {
                 + " where template_variable_id = :template_variable_id")
         void updateTemplateText(@Bind("substitution_value") String value, @Bind("template_variable_id") long templateId);
 
-        @SqlQuery("select picklist_select_mode_id from picklist_select_mode where picklist_select_mode_code = :picklist_select_mode_code")
-        long getPickListModeIdByValue(@Bind("picklist_select_mode_code") PicklistSelectMode picklistSelectMode);
-
-        @SqlUpdate("update picklist_question set picklist_select_mode_id = :picklist_select_mode_id where question_id = :question_id")
-        void updatePicklistOption(@Bind("question_id") long questionId, @Bind("picklist_select_mode_id") long picklistselectModeId);
-
-        @SqlQuery("select option_label_template_id from picklist_option where picklist_question_id = :question_id "
-                + "and picklist_option_stable_id = :stableId")
-        long getPicklistLabelTemplateId(@Bind("question_id") long questionId, @Bind("stableId") String stableId);
-
         @SqlQuery("select error_message_template_id from activity_validation "
                 + "where study_activity_id = :activityId and expression_text like :text")
         long getActivityValidationTemplateId(@Bind("activityId") long activityId, @Bind("text") String text);
-
-        @SqlQuery("select correction_hint_template_id from validation where validation_id = :validationId")
-        long getHintTemplateId(@Bind("validationId")long validationId);
 
         @SqlQuery("select template_variable_id from template_variable where template_id = :templateId")
         long getTemplateVariableIdbyTemplateId(@Bind("templateId") long templateId);
@@ -219,10 +150,5 @@ public class OsteoPrequalUpdate implements CustomTask {
         @SqlQuery("select question_id from question where question_stable_code_id = :stableId")
         long getQuestionId(@Bind("stableId") long stableId);
 
-        @SqlQuery("select validation_id from question__validation where question_id = :questionId")
-        List<Long> getValidationId(@Bind("questionId")long questionId);
-
-        @SqlUpdate("update int_range_validation set max = :max where validation_id = :validationId")
-        void insertUpperRange(@Bind("max") int max, @Bind("validationId") long validationId);
     }
 }
