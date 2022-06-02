6d3dd74ad84a148431d93d0caedf66612751a7a7
nghurtch <nghurtch@broadinstitute.com>
2022-06-02 11:14:06 +0400

    DDP-7641 | define NewOsteoParticipantMigrator for migrating data under dsm.newOsteoParticipant

---
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/migration/NewOsteoParticipantMigrator.java
@@ -0,0 +1,28 @@
+package org.broadinstitute.dsm.model.elastic.migration;
+
+import org.broadinstitute.dsm.db.Participant;
+import org.broadinstitute.dsm.db.ParticipantExit;
+import org.broadinstitute.dsm.statics.ESObjectConstants;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.broadinstitute.dsm.model.filter.prefilter.StudyPreFilter.NEW_OSTEO_INSTANCE_NAME;
+import static org.broadinstitute.dsm.model.filter.prefilter.StudyPreFilter.OLD_OSTEO_INSTANCE_NAME;
+
+public class NewOsteoParticipantMigrator extends ParticipantMigrator {
+
+    public NewOsteoParticipantMigrator(String index, String realm) {
+        super(index, realm, ESObjectConstants.NEW_OSTEO_PARTICIPANT);
+    }
+
+    @Override
+    protected Map<String, Participant> getParticipantsByRealm(String realm) {
+        return super.getParticipantsByRealm(NEW_OSTEO_INSTANCE_NAME);
+    }
+
+    @Override
+    protected Map<String, ParticipantExit> getExitedParticipantsByRealm(String realm) {
+        return super.getExitedParticipantsByRealm(NEW_OSTEO_INSTANCE_NAME);
+    }
+}
