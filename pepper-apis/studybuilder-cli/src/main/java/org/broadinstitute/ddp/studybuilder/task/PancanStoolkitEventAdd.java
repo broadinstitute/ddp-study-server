4b59cda430c031fe767ac915a0dc8e2049398632
nsakande <nsakande@broadinstitute.org>
2022-06-09 19:52:08 +0400

    DSS: CMI: Stool Kit Received Email Trigger

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/PancanStoolkitEventAdd.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/PancanStoolkitEventAdd.java
@@ -2,12 +2,14 @@ package org.broadinstitute.ddp.studybuilder.task;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.broadinstitute.ddp.db.DaoException;
 import org.broadinstitute.ddp.db.dao.EventDao;
 import org.broadinstitute.ddp.db.dao.JdbiEventConfiguration;
 import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dao.UserDao;
 import org.broadinstitute.ddp.db.dao.JdbiActivity;
+import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.exception.DDPException;
 import org.broadinstitute.ddp.model.activity.types.EventActionType;
 import org.broadinstitute.ddp.model.activity.types.EventTriggerType;
@@ -16,18 +18,16 @@ import org.broadinstitute.ddp.model.event.ActivityStatusChangeTrigger;
 import org.broadinstitute.ddp.model.event.EventConfiguration;
 import org.broadinstitute.ddp.studybuilder.EventBuilder;
 import org.jdbi.v3.core.Handle;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class PancanStoolkitEventAdd implements CustomTask {
-    private static final Logger LOG = LoggerFactory.getLogger(PancanStoolkitEventAdd.class);
     private static final String STUDY_GUID = "cmi-pancan";
-    private static final String DATA_FILE = "patches/stoolkit-sent-event.conf";
+    private static final String DATA_FILE = "patches/stoolkit-event-add.conf";
 
     private Config dataCfg;
     private Config studyCfg;
@@ -57,29 +57,39 @@ public class PancanStoolkitEventAdd implements CustomTask {
 
         List<EventConfiguration> events = handle.attach(EventDao.class)
                 .getAllEventConfigurationsByStudyId(studyDto.getId());
-        LOG.info("Starting to deactivate events of stoolkit initial creation.");
+        log.info("Starting to deactivate events of stoolkit initial creation.");
 
         deactivateEvents(handle, events);
 
-        LOG.info("Deactivated events of stoolkit initial creation.");
+        log.info("Deactivated events of stoolkit initial creation.");
 
-        LOG.info("Adding new event for DSM stoolkit creation.");
+        log.info("Adding new event for DSM stoolkit creation.");
 
-        Config eventDataCfg = dataCfg.getConfigList("events").get(0);
+        insertEvents(handle, studyDto, adminUser.getId());
 
-        eventBuilder.insertEvent(handle, eventDataCfg);
-
-        LOG.info("Added new event for DSM stoolkit creation.");
+        log.info("Added new event for DSM stoolkit creation.");
     }
 
+    private void insertEvents(Handle handle, StudyDto studyDto, long adminUserId) {
+        if (!dataCfg.hasPath("events")) {
+            throw new DDPException("There is no 'events' configuration.");
+        }
+        log.info("Inserting events configuration...");
+        List<? extends Config> events = dataCfg.getConfigList("events");
+        EventBuilder eventBuilder = new EventBuilder(studyCfg, studyDto, adminUserId);
+        for (Config eventCfg : events) {
+            eventBuilder.insertEvent(handle, eventCfg);
+        }
+        log.info("Events configuration has added in study {}", studyCfg.getString("study.guid"));
+    }
 
 
     private void deactivateEvents(Handle handle, List<EventConfiguration> eventConfigurations) {
         var dao = handle.attach(JdbiEventConfiguration.class);
         var eventsToDeactivate = eventConfigurations.stream()
                 .filter(e -> shouldDeactivateEvent(e, handle))
-                        .collect(Collectors.toList());
-        LOG.info("number of existing pancan STOOL_KIT events to deactivate: " + eventsToDeactivate.size());
+                .collect(Collectors.toList());
+        log.info("number of existing pancan STOOL_KIT events to deactivate: " + eventsToDeactivate.size());
         eventsToDeactivate.forEach(e -> {
             long id = e.getEventConfigurationId();
             dao.updateIsActiveById(id, false);
@@ -95,10 +105,10 @@ public class PancanStoolkitEventAdd implements CustomTask {
             if ((triggerActivityCode.equals("RELEASE")
                     || triggerActivityCode.equals("RELEASE_MINOR"))) {
                 return e.getEventTriggerType() == EventTriggerType.ACTIVITY_STATUS
-                    && e.getEventActionType() == EventActionType.ACTIVITY_INSTANCE_CREATION
-                    && (e.getPreconditionExpression() != null && e.getPreconditionExpression().contains("STOOL_KIT"))
-                    && ((ActivityStatusChangeTrigger) trigger).getInstanceStatusType() == InstanceStatusType.COMPLETE
-                    && e.getMaxOccurrencesPerUser() == 1;
+                        && e.getEventActionType() == EventActionType.ACTIVITY_INSTANCE_CREATION
+                        && (e.getPreconditionExpression() != null && e.getPreconditionExpression().contains("STOOL_KIT"))
+                        && ((ActivityStatusChangeTrigger) trigger).getInstanceStatusType() == InstanceStatusType.COMPLETE
+                        && e.getMaxOccurrencesPerUser() == 1;
             } else {
                 return false;
             }
