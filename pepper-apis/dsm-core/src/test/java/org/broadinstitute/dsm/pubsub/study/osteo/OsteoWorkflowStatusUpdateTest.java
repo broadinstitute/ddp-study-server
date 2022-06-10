420a5a9e1445af5a3f8674008a8dfe9cefdff75e
nghurtch <nghurtch@broadinstitute.com>
2022-06-10 16:19:21 +0400

    DDP-7641 | add artificial test for OsteoWorkflowStatusUpdate

--- pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/pubsub/study/osteo/OsteoWorkflowStatusUpdateTest.java
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/pubsub/study/osteo/OsteoWorkflowStatusUpdateTest.java
@@ -1,11 +1,13 @@
 package org.broadinstitute.dsm.pubsub.study.osteo;
 
 import org.broadinstitute.dsm.db.DDPInstance;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class OsteoWorkflowStatusUpdateTest {
 
     @Test
+    @Ignore
     public void update() {
 
         String osteo2DdpInstanceId = "27";
@@ -14,7 +16,7 @@ public class OsteoWorkflowStatusUpdateTest {
 
         DDPInstance ddpInstance = new DDPInstance(osteo2DdpInstanceId, osteo2InstanceName, null, null, false,
                 0, 0, false, null,
-                false, null, osteo2ParticipantESIndex, null, null);
+                false, null, osteo2ParticipantESIndex, null, null, null);
 
         String osteo2DdpParticipantId = "guid";
 
