31d1c21964107fbc33718c93efe6c8722a3fef11
lasha <lkapanad@broadinstitute.org>
2022-06-17 11:21:50 +0400

    DDP-8091 fix medical record patch

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/SingularReadonlyActivities.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/SingularReadonlyActivities.java
@@ -16,6 +16,7 @@ import org.broadinstitute.ddp.exception.DDPException;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 @Slf4j
@@ -67,13 +68,13 @@ public class SingularReadonlyActivities implements CustomTask {
     private void updateActivity(Handle handle, String activityCode, StudyDto studyDto) {
         var helper = handle.attach(SqlHelper.class);
         long studyActivityId = helper.findActivityIdByStudyIdAndCode(studyDto.getId(), activityCode);
-        helper.updateActivityInstances(studyDto.getId(), studyActivityId);
-        helper.updateStudyActivity(studyDto.getId(), activityCode);
+        List<Long> finishedActivities = helper.getDoneActivities(studyActivityId);
+        helper.updateActivityInstances(finishedActivities);
     }
 
     private interface SqlHelper extends SqlObject {
-        @SqlUpdate("update activity_instance set is_readonly=true where study_activity_id=:studyId and study_activity_id=:studyActivityId")
-        void updateActivityInstances(@Bind("studyId") long studyId, @Bind("studyActivityId") long studyActivityId);
+        @SqlUpdate("update activity_instance set is_readonly=true where study_activity_id in (:studyActivityId)")
+        void updateActivityInstances( @Bind("studyActivityId") List<Long> studyActivityId);
 
         default long findActivityIdByStudyIdAndCode(long studyId, String activityCode) {
             return getHandle().attach(JdbiActivity.class)
@@ -81,8 +82,11 @@ public class SingularReadonlyActivities implements CustomTask {
                     .orElseThrow(() -> new DDPException("Could not find activity id for " + activityCode));
         }
 
-        @SqlUpdate("update study_activity set is_write_once=true where study_id=:studyId and study_activity_code=:studyActivityCode")
-        void updateStudyActivity(@Bind("studyId") long studyId, @Bind("studyActivityCode") String studyActivityCode);
+        @SqlQuery("select ai.activity_instance_id from activity_instance ai join activity_instance_status ais on " +
+                "ai.activity_instance_id = ais.activity_instance_id where study_activity_id=:studyActivityId and " +
+                "ais.activity_instance_status_type_id=(select activity_instance_status_type_id from activity_instance_status_type " +
+                "where activity_instance_status_type_code='COMPLETE')")
+        List<Long> getDoneActivities(@Bind("studyActivityId") long studyActivityId);
     }
 
 }
