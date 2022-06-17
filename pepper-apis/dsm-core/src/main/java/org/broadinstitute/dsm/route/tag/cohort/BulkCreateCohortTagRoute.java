fcb08ef76de60110ff6281ccb82efa8fb65e25b1
Guram Makharashvili <gmakhara@Gurams-MacBook-Pro.local>
2022-06-17 15:26:36 +0400

    DDP-8188 | minor change of FilterFactory

---
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/route/tag/cohort/BulkCreateCohortTagRoute.java
@@ -0,0 +1,12 @@
+package org.broadinstitute.dsm.route.tag.cohort;
+
+import org.broadinstitute.dsm.security.RequestHandler;
+import spark.Request;
+import spark.Response;
+
+public class BulkCreateCohortTagRoute extends RequestHandler {
+    @Override
+    protected Object processRequest(Request request, Response response, String userId) throws Exception {
+        return null;
+    }
+}
