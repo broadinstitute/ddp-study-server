9c08843e8f57f735764ec3085af5609e31802723
simone <simone@broadinstitute.org>
2022-06-23 16:16:20 -0400

    DDP-7869: changing logger changing upsert to match the one from ReceiveKitRequest

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/defaultvalues/BasicDefaultDataMaker.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/defaultvalues/BasicDefaultDataMaker.java
@@ -15,7 +15,7 @@ import org.slf4j.LoggerFactory;
 
 public abstract class BasicDefaultDataMaker implements Defaultable {
 
-    protected static final Logger logger = LoggerFactory.getLogger(RgpAutomaticProbandDataCreator.class);
+    protected static final Logger logger = LoggerFactory.getLogger(BasicDefaultDataMaker.class);
 
     protected final FieldSettings fieldSettings = new FieldSettings();
     protected final BookmarkDao bookmarkDao = new BookmarkDao();
