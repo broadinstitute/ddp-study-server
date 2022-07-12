9d8dc7669cd81759f3ae17bab2d5721ca50cd226
MishaK1204 <107425227+MishaK1204@users.noreply.github.com>
2022-07-12 21:51:43 +0400

    DDP-8315 Added tooltip text to Parent 1 and parent 2. Move clarification text … (#1957)
    * Added tooltip text to Parent 1 and parent 2. Move clarification text under the main question.
    
    * Fix content blocks

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/osteo/OsteoAboutYouChildTitleUpdates.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/osteo/OsteoAboutYouChildTitleUpdates.java
@@ -3,7 +3,6 @@ package org.broadinstitute.ddp.studybuilder.task.osteo;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import lombok.extern.slf4j.Slf4j;
-
 import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dto.BlockContentDto;
 import org.broadinstitute.ddp.exception.DDPException;
@@ -15,20 +14,14 @@ import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
-
 import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
-/**
- * One-off task to add adhoc symptom message to TestBoston in deployed environments.
- */
 @Slf4j
 public class OsteoAboutYouChildTitleUpdates implements CustomTask {
     private static final String STUDY_GUID = "CMI-OSTEO";
-
     private static final String DATA_FILE = "patches/osteo-content-title-updates.conf";
-
     private Config cfg;
     private Config dataCfg;
 
@@ -38,7 +31,6 @@ public class OsteoAboutYouChildTitleUpdates implements CustomTask {
             throw new DDPException("This task is only for the " + STUDY_GUID + " study!");
         }
         this.cfg = studyCfg;
-
         File file = cfgPath.getParent().resolve(DATA_FILE).toFile();
         if (!file.exists()) {
             throw new DDPException("Data file is missing: " + file);
@@ -48,11 +40,9 @@ public class OsteoAboutYouChildTitleUpdates implements CustomTask {
 
     @Override
     public void run(Handle handle) {
-
         SqlHelper helper = handle.attach(SqlHelper.class);
         JdbiUmbrellaStudy jdbiUmbrellaStudy = handle.attach(JdbiUmbrellaStudy.class);
         var studyDto = jdbiUmbrellaStudy.findByStudyGuid(cfg.getString("study.guid"));
-
         long studyId = studyDto.getId();
         List<? extends Config> activities = dataCfg.getConfigList("activities");
         for (Config activity : activities) {
@@ -60,13 +50,12 @@ public class OsteoAboutYouChildTitleUpdates implements CustomTask {
             String bodyText = activity.getString("bodyText");
             long activityId = ActivityBuilder.findActivityId(handle, studyId, activityCode);
             BlockContentDto contentBlock = helper.findContentBlockByBodyText(activityId, bodyText);
-            helper.setNullTitle(contentBlock.getId());
-            log.info("Activity block titleTemplate set to null {}", activityCode);
+            helper.deleteContentBlock(contentBlock.getBlockId());
+            log.info("Deleted content block: {} in activity: {}", contentBlock.getId(), activityCode);
         }
     }
 
     private interface SqlHelper extends SqlObject {
-
         @SqlQuery("select bt.* from block_content as bt"
                 + "  join template as tmpl on tmpl.template_id = bt.body_template_id"
                 + " where tmpl.template_text = :text"
@@ -83,7 +72,28 @@ public class OsteoAboutYouChildTitleUpdates implements CustomTask {
         @RegisterConstructorMapper(BlockContentDto.class)
         BlockContentDto findContentBlockByBodyText(@Bind("activityId") long activityId, @Bind("text") String bodyTemplateText);
 
-        @SqlUpdate("update block_content set title_template_id = null where block_content_id=:id")
-        int setNullTitle(@Bind("id") long blockId);
+        @SqlUpdate("delete from form_section__block where block_id = :blockId")
+        int deleteSectionBlockMembershipByBlockId(@Bind("blockId") long blockId);
+
+        @SqlUpdate("delete from block_content where block_id = :blockId")
+        int deleteBlockContentByBlockId(@Bind("blockId") long blockContentId);
+
+        @SqlUpdate("delete from block where block_id = :id")
+        int deleteBlockById(@Bind("id") long id);
+
+        default void deleteContentBlock(long blockId) {
+            int numDeleted = deleteSectionBlockMembershipByBlockId(blockId);
+            if (numDeleted != 1) {
+                throw new DDPException("Could not remove content block with blockId=" + blockId + " from section");
+            }
+            numDeleted = deleteBlockContentByBlockId(blockId);
+            if (numDeleted != 1) {
+                throw new DDPException("Could not delete content block with blockId=" + blockId);
+            }
+            numDeleted = deleteBlockById(blockId);
+            if (numDeleted != 1) {
+                throw new DDPException("Could not delete block with blockId=" + blockId);
+            }
+        }
     }
 }
