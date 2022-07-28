3edb4d6ceded6d8db12bc6f0fb6ec13a6c35f93f
nghurtch <nghurtch@broadinstitute.com>
2022-07-28 16:53:59 +0400

    DDP-8465 | add two implementations for building the DBElement: default and a special case for "ddp_participant_record" with primary key of "participant_id"

---
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/patch/ParticipantRecordDBElementBuilder.java
@@ -0,0 +1,11 @@
+package org.broadinstitute.dsm.model.patch;
+
+import org.broadinstitute.dsm.db.structure.DBElement;
+
+public class ParticipantRecordDBElementBuilder extends DefaultDBElementBuilder {
+
+    @Override
+    public DBElement fromName(String name) {
+        return super.fromName(name);
+    }
+}
