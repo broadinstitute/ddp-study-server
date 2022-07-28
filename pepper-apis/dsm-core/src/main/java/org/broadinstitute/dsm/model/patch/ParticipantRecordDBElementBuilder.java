f3762f8a5be7b16c8cdbde593aab2300913a6f97
nghurtch <nghurtch@broadinstitute.com>
2022-07-28 18:19:25 +0400

    DDP-8465 | fix comment

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/patch/ParticipantRecordDBElementBuilder.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/patch/ParticipantRecordDBElementBuilder.java
@@ -7,7 +7,7 @@ public class ParticipantRecordDBElementBuilder extends DefaultDBElementBuilder {
 
     // The primary key of DBElement built by this class should equal to "participant_id".
     // The reason behind this is that since in ElasticSearch dsm.participant holds data for `ddp_participant` and `ddp_participant_record`.
-    // We need to make sure that the foreign key for Patch#patch will be `participant_id` for correctly exercising DB operations.
+    // We need to make sure that the primary key for Patch#patch will be `participant_id` for correctly exercising DB operations.
 
     @Override
     public DBElement fromName(String name) {
