34c1ebacb1d30b58223fab03b69d63b9f30ee9cf
Sampath K. Settipalli <sampath@broadinstitute.org>
2022-07-28 10:16:43 -0500

    temp commit

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/JdbiBooleanQuestion.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/JdbiBooleanQuestion.java
@@ -6,8 +6,8 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface JdbiBooleanQuestion extends SqlObject {
 
-    @SqlUpdate("insert into boolean_question (question_id,true_template_id,false_template_id)"
-            + " values(:questionId,:trueTemplateId,:falseTemplateId)")
+    @SqlUpdate("insert into boolean_question (question_id,true_template_id,false_template_id,boolean_render_mode_id)"
+            + " values(:questionId,:trueTemplateId,:falseTemplateId, 1)")
     int insert(@Bind("questionId") long questionId, @Bind("trueTemplateId") long trueTemplateId,
                @Bind("falseTemplateId") long falseTemplateId);
 
