f483ca022c27936a3252bc3f2d5c2219701aadd7
Guram Makharashvili <gmakhara@Gurams-MacBook-Pro.local>
2022-08-18 11:51:56 +0400

    DDP-7941 | extract methods in FinalKitUseCase

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/kit/KitTrackingScanUseCase.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/kit/KitTrackingScanUseCase.java
@@ -13,6 +13,7 @@ import org.broadinstitute.dsm.statics.DBConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//connecting kit barcode to tracking id
 public class KitTrackingScanUseCase extends BaseKitUseCase {
 
     private static final Logger logger = LoggerFactory.getLogger(KitTrackingScanUseCase.class);
@@ -25,21 +26,34 @@ public class KitTrackingScanUseCase extends BaseKitUseCase {
     protected Optional<KitStatusChangeRoute.ScanError> process(ScanPayload scanPayload) {
         String kitLabel = scanPayload.getKitLabel();
         String trackingReturnId = scanPayload.getTrackingReturnId();
+        KitRequestShipping kitRequestShipping = buildKitRequestShippingForInserting(kitLabel, trackingReturnId);
+        Optional<KitStatusChangeRoute.ScanError> maybeScanError =
+                insertKitRequest(kitRequestShipping);
+        if (isKitUpdateSuccessful(maybeScanError)) {
+            exportToElasticSearch(trackingReturnId, kitRequestShipping);
+        }
+        return maybeScanError;
+    }
+
+    private void exportToElasticSearch(String trackingReturnId, KitRequestShipping kitRequestShipping) {
+        try {
+            UpsertPainlessFacade.of(DBConstants.DDP_KIT_REQUEST_ALIAS, kitRequestShipping, kitPayload.getDdpInstanceDto(),
+                    "kitLabel", "kitLabel", trackingReturnId, new PutToNestedScriptBuilder()).export();
+        } catch (Exception e) {
+            logger.error(String.format("Error updating kit label for kit with label: %s", trackingReturnId));
+            e.printStackTrace();
+        }
+    }
+
+    private Optional<KitStatusChangeRoute.ScanError> insertKitRequest(KitRequestShipping kitRequestShipping) {
+        return kitDao.insertKitTracking(kitRequestShipping, String.valueOf(kitPayload.getUserId()));
+    }
+
+    private KitRequestShipping buildKitRequestShippingForInserting(String kitLabel, String trackingReturnId) {
         KitRequestShipping kitRequestShipping = new KitRequestShipping();
         kitRequestShipping.setTrackingId(trackingReturnId);
         kitRequestShipping.setKitLabel(kitLabel);
-        Optional<KitStatusChangeRoute.ScanError> maybeScanError =
-                kitDao.insertKitTracking(kitRequestShipping, String.valueOf(kitPayload.getUserId()));
-        if (isKitUpdateSuccessful(maybeScanError)) {
-            try {
-                UpsertPainlessFacade.of(DBConstants.DDP_KIT_REQUEST_ALIAS, kitRequestShipping, kitPayload.getDdpInstanceDto(),
-                        "kitLabel", "kitLabel", trackingReturnId, new PutToNestedScriptBuilder()).export();
-            } catch (Exception e) {
-                logger.error(String.format("Error updating kit label for kit with label: %s", trackingReturnId));
-                e.printStackTrace();
-            }
-        }
-        return maybeScanError;
+        return kitRequestShipping;
     }
 
 }
