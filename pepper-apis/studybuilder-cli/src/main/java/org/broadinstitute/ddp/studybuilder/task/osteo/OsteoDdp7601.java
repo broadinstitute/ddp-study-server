14d55437616ce8d6cfd6e1fdb3c6394610eb2832
LKapanad <lkapanad@broadinstitute.org>
2022-08-24 13:12:09 +0400

    [DDP-8609] hide only not submitted consents

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/osteo/OsteoDdp7601.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/osteo/OsteoDdp7601.java
@@ -50,33 +50,22 @@ public class OsteoDdp7601 implements CustomTask {
         List<SqlHelper.UserInfo> users = sqlHelper.getUsersInfo();
         JdbiActivity jdbiActivity = handle.attach(JdbiActivity.class);
 
+        Set<Long> consentIds = new HashSet<>();
         Set<Long> toHide = new HashSet<>();
-        Set<Long> toReadOnly = new HashSet<>();
-        Set<Long> activityIds = new HashSet<>();
-
         for (var user : users) {
-            activityIds.addAll(user.getActivities().keySet());
-            for (Long value : user.getActivities().values()) {
-                if (sqlHelper.getActivityStatus(value) > 0) {
-                    toReadOnly.add(value);
-                } else {
-                    toHide.add(value);
+            consentIds.addAll(user.getConsents().keySet());
+            for (Long id : user.getConsents().values()) {
+                if (sqlHelper.getActivityStatus(id) == 0) {
+                    toHide.add(id);
                 }
             }
         }
-
         if (!toHide.isEmpty()) {
             sqlHelper.setInstancesToHide(toHide);
         }
-
-        if (!toReadOnly.isEmpty()) {
-            sqlHelper.setInstancesToReadOnly(toReadOnly);
+        for (var consentId : consentIds) {
+            jdbiActivity.updateMaxInstancesPerUserById(consentId, null);
         }
-
-        for (var activityId : activityIds) {
-            jdbiActivity.updateMaxInstancesPerUserById(activityId, null);
-        }
-
         for (var user : users) {
             var signal = new EventSignal(
                     user.getId(),
@@ -86,15 +75,17 @@ public class OsteoDdp7601 implements CustomTask {
                     studyDto.getId(),
                     studyDto.getGuid(),
                     EventTriggerType.ACTIVITY_STATUS);
-            for (long activityId : user.getActivities().keySet()) {
-                ActivityInstanceCreationEventSyncProcessorDefault activityInstanceCreationEventSyncProcessor =
-                        new ActivityInstanceCreationEventSyncProcessorDefault(handle, signal, activityId,
-                                new ActivityInstanceCreationService(signal));
-                activityInstanceCreationEventSyncProcessor.processInstancesCreation();
+            for (long activityId : user.getConsents().keySet()) {
+                if (sqlHelper.getActivityStatus(activityId) == 0) {
+                    ActivityInstanceCreationEventSyncProcessorDefault activityInstanceCreationEventSyncProcessor =
+                            new ActivityInstanceCreationEventSyncProcessorDefault(handle, signal, activityId,
+                                    new ActivityInstanceCreationService(signal));
+                    activityInstanceCreationEventSyncProcessor.processInstancesCreation();
+                }
             }
         }
-        for (var activityId : activityIds) {
-            jdbiActivity.updateMaxInstancesPerUserById(activityId, 1);
+        for (var consentId : consentIds) {
+            jdbiActivity.updateMaxInstancesPerUserById(consentId, 1);
         }
     }
 
@@ -103,8 +94,14 @@ public class OsteoDdp7601 implements CustomTask {
         @SqlUpdate("update activity_instance set is_hidden=true where activity_instance_id in (<ids>);")
         int setInstancesToHide(@BindList(value = "ids") Set<Long> ids);
 
-        @SqlUpdate("update activity_instance set is_readonly=true where activity_instance_id in (<ids>);")
-        int setInstancesToReadOnly(@BindList(value = "ids") Set<Long> ids);
+        @SqlQuery("select u.user_id, u.guid, ai.activity_instance_id, ai.study_activity_id from\n"
+                + "\tumbrella_study us \n"
+                + "\tjoin study_activity sa on us.guid='CMI-OSTEO' and sa.study_id = us.umbrella_study_id\n"
+                + "\t\tand sa.study_activity_code in ('CONSENT', 'CONSENT_ASSENT', 'PARENTAL_CONSENT')\n"
+                + "    join activity_instance ai on ai.study_activity_id = sa.study_activity_id\n"
+                + "    join user u on u.user_id = ai.participant_id")
+        @UseRowReducer(UserReducer.class)
+        List<UserInfo> getUsersInfo();
 
         @SqlQuery("select count(*) from activity_instance_status ais \n"
                 + "join activity_instance_status_type aist on "
@@ -112,27 +109,19 @@ public class OsteoDdp7601 implements CustomTask {
                 + "where activity_instance_id=:id and aist.activity_instance_status_type_code='COMPLETE'; ")
         int getActivityStatus(@Bind(value = "id") Long id);
 
-        @SqlQuery("select u.user_id, u.guid, ai.activity_instance_id, ai.study_activity_id from\n"
-                + "\tumbrella_study us \n"
-                + "\tjoin study_activity sa on us.guid='CMI-OSTEO' and sa.study_id = us.umbrella_study_id\n"
-                + "\tjoin activity_instance ai on ai.study_activity_id = sa.study_activity_id\n"
-                + "\tjoin user u on u.user_id = ai.participant_id")
-        @UseRowReducer(UserReducer.class)
-        List<UserInfo> getUsersInfo();
-
         class UserInfo {
             long id;
             String guid;
-            Map<Long, Long> activities;
+            Map<Long, Long> consents;
 
             UserInfo(long id, String guid) {
                 this.id = id;
                 this.guid = guid;
-                activities = new HashMap<>();
+                consents = new HashMap<>();
             }
 
-            void addActivity(Long activityId, Long activityInstanceId) {
-                activities.put(activityId, activityInstanceId);
+            void addConsent(Long activityId, Long activityInstanceId) {
+                consents.put(activityId, activityInstanceId);
             }
 
             public long getId() {
@@ -143,8 +132,8 @@ public class OsteoDdp7601 implements CustomTask {
                 return guid;
             }
 
-            public Map<Long, Long> getActivities() {
-                return activities;
+            public Map<Long, Long> getConsents() {
+                return consents;
             }
         }
 
@@ -160,7 +149,7 @@ public class OsteoDdp7601 implements CustomTask {
                     userInfo = new UserInfo(userId, userGuid);
                     container.put(userId, userInfo);
                 }
-                userInfo.addActivity(activityId, activityInstanceId);
+                userInfo.addConsent(activityId, activityInstanceId);
             }
         }
     }
