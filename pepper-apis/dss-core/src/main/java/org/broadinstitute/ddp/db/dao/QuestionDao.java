efaca3a8ae954b4d9e433036b9114dc41827b801
MocanaAtBroad <mocana@broadinstitute.org>
2022-08-31 12:42:01 -0400

    Update pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/QuestionDao.java
    Co-authored-by: Blake (he/him) <bskinner@broadinstitute.org>

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/QuestionDao.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/db/dao/QuestionDao.java
@@ -1646,7 +1646,7 @@ public interface QuestionDao extends SqlObject {
     default boolean deleteDateQuestion(long questionId) {
         getJdbiDateQuestionFieldOrder().deleteForQuestionId(questionId);
         var val = getJdbiDateQuestion().delete(questionId);
-        LOG.info("Deleted date question id? {}", val);
+        LOG.info("Deleted date question id {}? {}", questionId, val);
         return val;
     }
 
