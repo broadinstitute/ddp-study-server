72f2a5e267e871d5ff9eb5703e5ca8924bf55b2b
Sampath K. Settipalli <sampath@broadinstitute.org>
2022-09-08 23:07:28 -0500

    Osteo patch updates.

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoV2Updates.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoV2Updates.java
@@ -17,7 +17,6 @@ import org.broadinstitute.ddp.studybuilder.task.osteo.OsteoGovernanceFix;
 import org.broadinstitute.ddp.studybuilder.task.osteo.OsteoLovedOneV2;
 import org.broadinstitute.ddp.studybuilder.task.osteo.OsteoMRFv2;
 import org.broadinstitute.ddp.studybuilder.task.osteo.OsteoNewActivities;
-import org.broadinstitute.ddp.studybuilder.task.osteo.OsteoNewFamilyHistory;
 import org.broadinstitute.ddp.studybuilder.task.osteo.OsteoPDFv2;
 import org.broadinstitute.ddp.studybuilder.task.osteo.OsteoPdfUpdates;
 import org.jdbi.v3.core.Handle;
@@ -45,11 +44,11 @@ public class OsteoV2Updates implements CustomTask {
         tasks.add(new OsteoAboutYouChildTitleUpdates());
 
         // Last
-        tasks.add(new OsteoNewFamilyHistory());
+        //tasks.add(new OsteoNewFamilyHistory()); //not needed, no FAMILY_HISTORY
         tasks.add(new OsteoPdfUpdates());
         tasks.add(new UpdateStudyNonSyncEvents());
         tasks.add(new OsteoInsertSyncEvents());
-        tasks.add(new UpdateStudyWorkflows());
+        tasks.add(new UpdateStudyWorkflows()); //updated conf file to remove FAMILY_HISTORY reference
 
         tasks.forEach(t -> t.init(cfgPath, studyCfg, varsCfg));
     }
