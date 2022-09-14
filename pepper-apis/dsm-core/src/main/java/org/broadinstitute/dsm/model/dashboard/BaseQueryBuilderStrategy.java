2fea7a31a9b1f897d628770c0c573394102c3c74
gmakharat <75846598+gmakharat@users.noreply.github.com>
2022-09-14 18:38:28 +0400

    [DSM] DDP-8112: dashboard (#1955)
    * DDP-8112 | add liquibase file to create tables for dashboard, add dao/dto class fro dashboard with integration tests
    
    * DDP-8112 | add dashboard package to model, create Size and DisplayType enum classes
    
    * DDP-8112 | add data holder classes, add DashboardUseCase class and start implementation integration test for it
    
    * DDP-8112 | add factories and strategies for different graphs
    
    * DDP-8112 | add more tests, cleaned up the code
    
    * DDP-8112 | add new method to ElasticSearchable to execute multi search request, add loggers, refactor code
    
    * DDP-8112 | add serialized name to BarChartData for x,y because of checkstyle
    
    * DDP-8112 | add VerticalHighlightedBarChart for edge case
    
    * DDP-8112 | remove dead code
    
    * DDP-8112 | add methods to VerticalHighlaghtedBarChartStrategy to set color and value to first index
    
    * DDP-8112 | apply changes according to develop
    
    Co-authored-by: Guram Makharashvili <gmakhara@Gurams-MacBook-Pro.local>

---
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/dashboard/BaseQueryBuilderStrategy.java
@@ -0,0 +1,74 @@
+package org.broadinstitute.dsm.model.dashboard;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.broadinstitute.dsm.model.elastic.filter.FilterStrategy;
+import org.broadinstitute.dsm.model.elastic.filter.Operator;
+import org.broadinstitute.dsm.model.elastic.filter.query.BuildQueryStrategy;
+import org.broadinstitute.dsm.model.elastic.filter.query.QueryPayload;
+import org.broadinstitute.dsm.model.elastic.filter.splitter.SplitterStrategy;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+abstract class BaseQueryBuilderStrategy {
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseQueryBuilderStrategy.class);
+    protected QueryBuildPayload queryBuildPayload;
+
+    public BaseQueryBuilderStrategy(QueryBuildPayload queryBuildPayload) {
+        this.queryBuildPayload = queryBuildPayload;
+    }
+
+    public QueryBuilder build() {
+        QueryBuilder queryBuilder;
+        if (hasAdditionalFilter()) {
+            queryBuilder = buildQueryForAdditionalFilter();
+        } else {
+            queryBuilder = buildQueryForNoAdditionalFilter();
+        }
+        return queryBuilder;
+    }
+
+    private boolean hasAdditionalFilter() {
+        return StringUtils.isNotBlank(queryBuildPayload.getLabel().getDashboardFilterDto().getAdditionalFilter());
+    }
+
+    private QueryBuilder buildQueryForAdditionalFilter() {
+        logger.info("Building search requests for additional filtering...");
+        queryBuildPayload.getSeparator().setFilter(queryBuildPayload.getLabel().getDashboardFilterDto().getAdditionalFilter());
+        Map<String, List<String>> andOrSeparated = queryBuildPayload.getSeparator().parseFiltersByLogicalOperators();
+        BoolQueryBuilder boolQueryBuilder = new BoolQueryBuilder();
+        for (Map.Entry<String, List<String>> parsedFilter : andOrSeparated.entrySet()) {
+            FilterStrategy filterStrategy = FilterStrategy.of(parsedFilter.getKey());
+            for (String filterValue: parsedFilter.getValue()) {
+                Operator operator = Operator.extract(filterValue);
+                SplitterStrategy splitterStrategy = operator.getSplitterStrategy();
+                splitterStrategy.setFilter(filterValue);
+                String[] values = splitterStrategy.getValue();
+                QueryPayload queryPayload = new QueryPayload(queryBuildPayload.getLabel().getDashboardFilterDto().getEsNestedPath(),
+                        queryBuildPayload.getLabel().getDashboardFilterDto().getEsFilterPath(),
+                        values, queryBuildPayload.getEsParticipantsIndex());
+                queryBuildPayload.getBaseQueryBuilder().setPayload(queryPayload);
+                BuildQueryStrategy queryStrategy = operator.getQueryStrategy();
+                queryStrategy.setBaseQueryBuilder(queryBuildPayload.getBaseQueryBuilder());
+                queryBuildPayload.getBaseQueryBuilder().setOperator(operator);
+                filterStrategy.build(boolQueryBuilder, queryBuildPayload.getBaseQueryBuilder().build(queryStrategy.build()));
+            }
+        }
+        return boolQueryBuilder;
+    }
+
+    protected QueryBuilder buildQueryForNoAdditionalFilter() {
+        queryBuildPayload.getBaseQueryBuilder().setPayload(getQueryPayload());
+        BuildQueryStrategy queryStrategy = Operator.EQUALS.getQueryStrategy();
+        queryStrategy.setBaseQueryBuilder(queryBuildPayload.getBaseQueryBuilder());
+        queryBuildPayload.getBaseQueryBuilder().setOperator(Operator.EQUALS);
+        return queryBuildPayload.getBaseQueryBuilder().build(queryStrategy.build());
+    }
+
+    protected abstract QueryPayload getQueryPayload();
+}
