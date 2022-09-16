504ed9d3c8f0e217c1533a56959b9ddeb1badd83
nghurtch <nghurtch@broadinstitute.com>
2022-09-16 13:48:00 +0400

    DDP-8802 | change old clients by leveraging the appropriate study post filter strategy implementations

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/filter/postfilter/osteo/OldOsteoActivityCode.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/filter/postfilter/osteo/OldOsteoActivityCode.java
@@ -1,2 +1,23 @@
-package org.broadinstitute.dsm.model.filter.postfilter.osteo;public enum OldOsteoActivityCode {
+package org.broadinstitute.dsm.model.filter.postfilter.osteo;
+
+import java.util.Arrays;
+
+public enum OldOsteoActivityCode {
+
+    ABOUTCHILD,
+    PARENTAL_CONSENT,
+    RELEASE_MINOR,
+    RELEASE_SELF,
+    PREQUAL,
+    ABOUT_YOU,
+    CONSENT,
+    LOVEDONE,
+    CONSENT_ASSENT;
+
+    public static String[] asStrings() {
+        return Arrays.stream(OldOsteoActivityCode.values())
+                .map(OldOsteoActivityCode::toString)
+                .toArray(String[]::new);
+    }
+
 }
