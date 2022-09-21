dff6399732df948c225b8cd0e5eef4986a6c956d
LKapanad <lkapanad@broadinstitute.org>
2022-09-21 15:43:26 +0400

    DDP-8735 fix checkstyle errors

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/LmsPrequalEmptyBlock.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/LmsPrequalEmptyBlock.java
@@ -3,6 +3,7 @@ package org.broadinstitute.ddp.studybuilder.task;
 public class LmsPrequalEmptyBlock extends PrequalEmptyBlock {
     private static final String STUDY_GUID = "cmi-lms";
     private static final String ACTIVITY_CODE  = "PREQUAL";
+
     public LmsPrequalEmptyBlock() {
         super(ACTIVITY_CODE, STUDY_GUID);
     }
--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoPrequalEmptyBlock.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoPrequalEmptyBlock.java
@@ -3,6 +3,7 @@ package org.broadinstitute.ddp.studybuilder.task;
 public class OsteoPrequalEmptyBlock extends PrequalEmptyBlock {
     private static final String STUDY_GUID = "CMI-OSTEO";
     private static final String ACTIVITY_CODE  = "PREQUAL";
+
     public OsteoPrequalEmptyBlock() {
         super(ACTIVITY_CODE, STUDY_GUID);
     }
