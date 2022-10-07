4a224df280822c2fbcb4a6a9e12c215e365ead0e
ssettipalli <ssettipalli@gmail.com>
2022-10-07 09:52:38 -0500

    DDP-8618 singular AOM updates (#2350)

---
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/SingularInsertAOMEvents.java
@@ -0,0 +1,7 @@
+package org.broadinstitute.ddp.studybuilder.task;
+
+public class SingularInsertAOMEvents extends InsertStudyEvents {
+    public SingularInsertAOMEvents() {
+        super("singular", "patches/singular-aom-new-events.conf");
+    }
+}
