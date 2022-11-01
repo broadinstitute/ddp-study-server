d1f597ae111f42544eb47d54986ccafcf7765523
SimoneMaiwald <simone@broadinstitute.org>
2022-11-01 13:37:59 -0400

    PEPPER-241: epoch date changed to date string (#2378)

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/filter/splitter/DateSplitterStrategy.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/filter/splitter/DateSplitterStrategy.java
@@ -1,9 +1,17 @@
 package org.broadinstitute.dsm.model.elastic.filter.splitter;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 import org.broadinstitute.dsm.model.Filter;
 import org.broadinstitute.dsm.model.elastic.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DateSplitterStrategy extends EqualsSplitterStrategy {
+
+    private static final Logger logger = LoggerFactory.getLogger(DateSplitterStrategy.class);
 
     // DATE(FROM_UNIXTIME(k.scan_date/1000))  = DATE(FROM_UNIXTIME(1640563200))
     @Override
@@ -15,6 +23,12 @@ public class DateSplitterStrategy extends EqualsSplitterStrategy {
                 leftSide.split(Util.ESCAPE_CHARACTER + Filter.OPEN_PARENTHESIS)[2].split(Util.FORWARD_SLASH_SEPARATOR)[0];
         String value = rightSide.split(Util.ESCAPE_CHARACTER + Filter.OPEN_PARENTHESIS)[2].split(
                 Util.ESCAPE_CHARACTER + Filter.CLOSE_PARENTHESIS)[0];
+        try {
+            Instant valueInstance = Instant.ofEpochMilli(Long.parseLong(value) * 1000);
+            return new String[] {fieldWithAlias, DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneId.of("UTC")).format(valueInstance)};
+        } catch (NumberFormatException e) {
+            logger.error("DateSplitterStrategy wasn't able to parse " + value + " into long");
+        }
         return new String[] {fieldWithAlias, value};
     }
 
