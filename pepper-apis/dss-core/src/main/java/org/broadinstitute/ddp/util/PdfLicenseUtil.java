e9f2a9c081eca30b63aeca2f3a038c0f8c3ee130
Blake Skinner <bskinner@broadinstitute.org>
2022-11-08 11:47:26 -0500

    Support loading the iText license from Google Secrets

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/PdfLicenseUtil.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/PdfLicenseUtil.java
@@ -1,71 +1,67 @@
 package org.broadinstitute.ddp.util;
 
 import com.google.cloud.secretmanager.v1.ProjectName;
+import com.google.cloud.secretmanager.v1.Secret;
 import com.google.cloud.secretmanager.v1.SecretVersionName;
 import com.itextpdf.licensekey.LicenseKey;
 
+import com.itextpdf.licensekey.LicenseKeyException;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.ddp.constants.ConfigFile;
 import org.broadinstitute.ddp.exception.DDPException;
+import org.broadinstitute.ddp.secrets.SecretManager;
 
 @Slf4j
-public class PdfLicenseUtil {
-    private static final String GOOGLE_SECRET_PROJECT = "google.secret.project";
-    private static final String GOOGLE_SECRET_NAME = "google.secret.name";
+public final class PdfLicenseUtil {
 
     public static void loadITextLicense() {
-        final var licenseFile = System.getProperty(ConfigFile.ITEXT_FILE_ENV_VAR);
+        final var licenseName = PropertyManager.getProperty(ConfigFile.ITEXT_FILE_ENV_VAR);
 
-        if (StringUtils.isBlank(licenseFile)) {
-            throw new DDPException("No itext license file found.");
-        }
-
-        if (loadITextLicense(licenseFile)) {
-            log.info("successfully loaded the iText license key.");
+        if (licenseName.isEmpty() || StringUtils.isWhitespace(licenseName.get())) {
+            log.warn("no license key specified for property {}, skipping attempting load of iText license. "
+                    + "PDF generation may not function as expected.",
+                    ConfigFile.ITEXT_FILE_ENV_VAR);
             return;
         }
 
-        log.info("license key failed to be loaded from a file, trying as a Google Secret");
-
-        final var projectId = System.getProperty(GOOGLE_SECRET_PROJECT);
-        final var latest = "latest";
-
-        final var googleSecretNameInvalid = StringUtils.isBlank(projectId)
-                && StringUtils.isBlank(latest);
-
-        if (googleSecretNameInvalid) {
-            log.info
+        final var licenseNameValue = licenseName.get();
+        final var licenseFilePath = FileSystems.getDefault().getPath(licenseNameValue);
+        if (Files.exists(licenseFilePath) && loadITextLicense(licenseFilePath)) {
+            log.info("successfully loaded the iText license key from {}", licenseFilePath);
+            return;
         }
-        
-        final var secretName = SecretVersionName.of(projectId, licenseFile, version);
 
+        log.debug("license name does not appear to be a local path, treating property {} as a Google Secret resource id.",
+                ConfigFile.ITEXT_FILE_ENV_VAR);
+
+        final var licenseKeySecret = SecretManager.get(SecretVersionName.parse(licenseNameValue))
+                .map((secret) -> IOUtils.toInputStream(secret, StandardCharsets.UTF_8))
+                .orElseThrow(() -> {
+                    return new DDPException(String.format("failed to load iText licence key from resource '%s'", licenseNameValue));
+                });
+
+        LicenseKey.loadLicenseFile(licenseKeySecret);
+        log.info("successfully loaded the iText license from secret manager resource '{}'", licenseNameValue);
     }
 
-    private static boolean loadITextLicenseSecret(SecretVersionName secretName) {
-        
-    }
-
-    private static boolean loadITextLicense(String path) {
+    private static boolean loadITextLicense(Path licensePath) {
         final Path resolvedPath;
 
         try {
-            resolvedPath = Paths.get(path).toRealPath();
+            resolvedPath = licensePath.toRealPath();
         } catch (IOException ioe) {
-            throw new DDPException(String.format("failed to load license file at %s", path));
+            throw new DDPException(String.format("failed to load license file at %s", licensePath), ioe);
         }
 
-        if (Files.exists(resolvedPath)) {
-            LicenseKey.loadLicenseFile(resolvedPath.toString());
-            return true;
-        }
-
-        return false;
+        LicenseKey.loadLicenseFile(resolvedPath.toString());
+        return true;
     }
 }
