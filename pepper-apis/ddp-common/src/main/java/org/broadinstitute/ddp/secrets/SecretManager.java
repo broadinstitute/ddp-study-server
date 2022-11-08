29ee1ea6cdcc77f4b13b1df8c906452cf3966f0a
Blake Skinner <bskinner@broadinstitute.org>
2022-11-08 16:35:44 -0500

    Simplify the implementation
    We're not calling the get secret often- the added cleanup may not be necessary

--- pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/secrets/SecretManager.java
+++ pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/secrets/SecretManager.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.broadinstitute.ddp.exception.DDPException;
 import org.broadinstitute.ddp.util.GoogleCredentialUtil;
 
 import java.io.IOException;
@@ -16,32 +17,15 @@ import java.lang.ref.Cleaner;
 import java.util.Optional;
 
 @Slf4j
-public final class SecretManager implements AutoCloseable {
-    private static final Cleaner cleaner = Cleaner.create();
-    private static SecretManager sharedInstance;
+public final class SecretManager {
 
-    @Value
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    private static class State implements Runnable {
-        SecretManagerServiceClient secretClient;
+    private static SecretManagerServiceClient createSecretManagerClient() throws IOException {
+        final var credentialsProvider = FixedCredentialsProvider.create(GoogleCredentialUtil.initCredentials(false));
+        final var secretSettings = SecretManagerServiceSettings.newBuilder()
+                .setCredentialsProvider(credentialsProvider)
+                .build();
 
-        public void run() {
-            if (secretClient != null) {
-                secretClient.close();
-            }
-        }
-    }
-
-    public static synchronized SecretManager getInstance() {
-        if (sharedInstance == null) {
-            try {
-                sharedInstance = new SecretManager();
-            } catch (IOException error) {
-                log.error("failed to initialize Google Secret Manager client.", error);
-            }
-        }
-
-        return sharedInstance;
+        return SecretManagerServiceClient.create(secretSettings);
     }
 
     public static Optional<String> get(final String projectName, final String secretName, final String secretVersion) {
@@ -49,30 +33,17 @@ public final class SecretManager implements AutoCloseable {
     }
 
     public static Optional<String> get(final SecretVersionName secret) {
-        final var client = getInstance().state.secretClient;
-        return Optional.of(secret)
-                .map(client::accessSecretVersion)
-                .map(AccessSecretVersionResponse::getPayload)
-                .map(SecretPayload::getData)
-                .map(ByteString::toStringUtf8);
-    }
+        final Optional<String> value;
+        try (final var client = createSecretManagerClient()) {
+            value = Optional.of(secret)
+                    .map(client::accessSecretVersion)
+                    .map(AccessSecretVersionResponse::getPayload)
+                    .map(SecretPayload::getData)
+                    .map(ByteString::toStringUtf8);
+        } catch (IOException error) {
+            throw new DDPException(String.format("failed to load [resource:%s] from Google Secret Manager.", secret), error);
+        }
 
-    private final State state;
-    private final Cleaner.Cleanable cleanerToken;
-
-    private SecretManager() throws IOException {
-        final var credentialsProvider = FixedCredentialsProvider.create(GoogleCredentialUtil.initCredentials(false));
-        final var secretSettings = SecretManagerServiceSettings.newBuilder()
-                .setCredentialsProvider(credentialsProvider)
-                .build();
-        final var secretManager = SecretManagerServiceClient.create(secretSettings);
-
-        this.state = new State(secretManager);
-        this.cleanerToken = SecretManager.cleaner.register(this, this.state);
-    }
-
-    @Override
-    public void close() throws Exception {
-        cleanerToken.clean();
+        return value;
     }
 }
