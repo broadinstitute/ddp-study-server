d424205c4bb085f18eb3550fcc1279e1699bc04d
Dmitrii Eliseev <deliseev@broadinstitute.org>
2022-12-06 11:42:42 +0100

    PEPPER-175: Patch implemented

---
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/OsteoInsertEvents.java
@@ -0,0 +1,52 @@
+package org.broadinstitute.ddp.studybuilder.task;
+
+import lombok.extern.slf4j.Slf4j;
+import one.util.streamex.StreamEx;
+import org.broadinstitute.ddp.db.dao.EventDao;
+import org.broadinstitute.ddp.db.dao.JdbiEventConfiguration;
+import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
+import org.broadinstitute.ddp.model.activity.types.EventActionType;
+import org.broadinstitute.ddp.model.activity.types.EventTriggerType;
+import org.broadinstitute.ddp.model.dsm.DsmNotificationEventType;
+import org.broadinstitute.ddp.model.event.DsmNotificationTrigger;
+import org.broadinstitute.ddp.model.event.EventConfiguration;
+import org.jdbi.v3.core.Handle;
+
+@Slf4j
+public class OsteoInsertEvents extends InsertStudyEvents {
+    OsteoInsertEvents() {
+        super("osteo", "patches/osteo-new-events.conf");
+    }
+
+    @Override
+    public void run(final Handle handle) {
+        removeExistingEvents(handle);
+        super.run(handle);
+    }
+
+    private void removeExistingEvents(final Handle handle) {
+        final var studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid(studyGuid);
+
+        var eventDao = handle.attach(EventDao.class);
+        StreamEx.of(eventDao.getAllEventConfigurationsByStudyId(studyDto.getId()))
+                .filterBy(EventConfiguration::getEventActionType, EventActionType.ACTIVITY_INSTANCE_CREATION)
+                .filterBy(EventConfiguration::getEventTriggerType, EventTriggerType.DSM_NOTIFICATION)
+                .filter(this::hasDSMNotificationTrigger)
+                .filter(this::isExpectedTrigger)
+                .forEach(e -> handle.attach(JdbiEventConfiguration.class).updateIsActiveById(e.getEventConfigurationId(), false));
+
+        log.info("Successfully removed DSM Notification events that create new activities of {}.", studyGuid);
+    }
+
+    private boolean hasDSMNotificationTrigger(final EventConfiguration event) {
+        return event.getEventTrigger() instanceof DsmNotificationTrigger;
+    }
+
+    private boolean isExpectedTrigger(final EventConfiguration event) {
+        return isExpectedTrigger(((DsmNotificationTrigger) event.getEventTrigger()).getDsmEventType());
+    }
+
+    private boolean isExpectedTrigger(final DsmNotificationEventType eventType) {
+        return eventType == DsmNotificationEventType.BLOOD_RECEIVED || eventType == DsmNotificationEventType.SALIVA_RECEIVED;
+    }
+}
