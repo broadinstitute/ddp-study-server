f8681d5e1d1633b4d3410b00af16cb0fafd68afc
Blake Skinner <bskinner@broadinstitute.org>
2022-12-13 22:55:26 -0500

    Adds the final implementation of the patch (and config)

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/PancanAddAdditionalDiagnosis.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/PancanAddAdditionalDiagnosis.java
@@ -2,7 +2,7 @@ package org.broadinstitute.ddp.studybuilder.task;
 
 import java.nio.file.Path;
 import java.time.Instant;
-import java.util.LinkedList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,14 +15,12 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-
-import org.broadinstitute.ddp.db.dao.JdbiPicklistGroup;
 import org.broadinstitute.ddp.db.dao.JdbiPicklistOption;
-import org.broadinstitute.ddp.db.dao.TemplateDao;
+import org.broadinstitute.ddp.db.dao.PicklistQuestionDao;
 import org.broadinstitute.ddp.db.dto.PicklistGroupDto;
 import org.broadinstitute.ddp.db.dto.PicklistOptionDto;
 import org.broadinstitute.ddp.exception.DDPException;
-import org.broadinstitute.ddp.model.activity.definition.question.PicklistGroupDef;
+import org.broadinstitute.ddp.model.activity.definition.question.PicklistOptionDef;
 import org.broadinstitute.ddp.util.ConfigUtil;
 import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
@@ -30,6 +28,7 @@ import org.jdbi.v3.sqlobject.SqlObject;
 import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 @Slf4j
 public final class PancanAddAdditionalDiagnosis implements CustomTask {
@@ -39,7 +38,10 @@ public final class PancanAddAdditionalDiagnosis implements CustomTask {
     private static enum Metadata {
         DESCRIPTION("description"),
         STUDY("study"),
-        OPTION_GROUP("option-group");
+        EXPECTED_OPTIONS("expected-options"),
+        TARGET_GROUP_SID("target-group-sid"),
+        INSERT_AFTER_SID("insert-after-sid"),
+        NEW_OPTIONS("options");
 
         @Getter
         private final String key;
@@ -111,6 +113,19 @@ public final class PancanAddAdditionalDiagnosis implements CustomTask {
                 + "  FROM picklist_option AS po"
                 + "  WHERE po.picklist_option_id = :groupId")
         public long fetchQuestionIdForOption(@Bind("optionId") long optionId);
+
+        @SqlQuery("SELECT"
+                + "    qsc.stable_id"
+                + "  FROM question AS q"
+                + "    JOIN question_stable_code AS qsc ON qsc.question_stable_code_id = q.question_stable_code_id"
+                + " WHERE q.question_id = :questionId")
+        public String fetchQuestionStableIdById(@Bind("questionId") long questionId);
+
+
+        @SqlUpdate("INSERT INTO picklist_grouped_option (picklist_group_id, picklist_option_id)"
+                + " VALUES (:groupId, :optionId)")
+        public void linkOptionToGroupId(@Bind("optionId") long optionId,
+                @Bind("groupId") long groupId);
     }
     
     private Path configurationRoot;
@@ -124,6 +139,7 @@ public final class PancanAddAdditionalDiagnosis implements CustomTask {
     }
 
     protected Path getPatchRoot() {
+        assert configurationRoot != null;
         return getConfigurationRoot().resolve("patches");
     }
 
@@ -132,12 +148,17 @@ public final class PancanAddAdditionalDiagnosis implements CustomTask {
     }
 
     protected String getConfigurationStudy() {
+        assert studyConfiguration != null;
         return studyConfiguration.getString("study.guid");
     }
 
     protected String getTargetStudy() {
         assert patchConfiguration != null;
         return patchConfiguration.getString(Metadata.STUDY.getKey());
+    }
+
+    protected String getTaskName() {
+        return this.getClass().getSimpleName();
     }
 
     @Override
@@ -148,8 +169,10 @@ public final class PancanAddAdditionalDiagnosis implements CustomTask {
         final var patchConfigurationPath = getPatchConfigurationPath();
         try {
             var patchConfiguration = ConfigFactory.parseFile(patchConfigurationPath.toFile());
+
             final var resolveOptions = ConfigResolveOptions.defaults().setAllowUnresolved(true);
             patchConfiguration = patchConfiguration.resolve(resolveOptions);
+
             this.patchConfiguration = patchConfiguration.resolveWith(varsCfg);
         } catch (ConfigException configException) {
             throw failedToLoadPatchError(patchConfigurationPath.toString(), configException);
@@ -162,23 +185,139 @@ public final class PancanAddAdditionalDiagnosis implements CustomTask {
 
     @Override
     public void run(Handle handle) {
-        final var optionGroupConfig = patchConfiguration.getConfig(Metadata.OPTION_GROUP.getKey());
-        final var optionGroupDef = gson.fromJson(ConfigUtil.toJson(optionGroupConfig), PicklistGroupDef.class);
+        log.info("TASK::{}", getTaskName());
 
         final var jdbiPatchHelper = handle.attach(SqlHelper.class);
+        final var jdbiPicklistQuestion = handle.attach(PicklistQuestionDao.class);
+        final var jdbiPicklistOption = handle.attach(JdbiPicklistOption.class);
 
-        final var nowTimestamp = Instant.now().toEpochMilli();
-        final var existingGroups = jdbiPatchHelper.fetchPicklistGroupsByName(getTargetStudy(), optionGroupDef.getStableId())
+        final var picklistGroupName = patchConfiguration.getString(Metadata.TARGET_GROUP_SID.getKey());
+        final var containingQuestionIds = jdbiPatchHelper.fetchPicklistGroupsByName(getTargetStudy(), picklistGroupName)
                 .stream()
-                .filter((group) -> group.getRevisionStartTimestamp() <= nowTimestamp)
-                .filter((group) -> (group.getRevisionEndTimestamp() == null) || (group.getRevisionEndTimestamp() > nowTimestamp))
+                .map((group) -> jdbiPatchHelper.fetchQuestionIdForGroup(group.getId()))
                 .collect(Collectors.toList());
 
-        for (final var group : existingGroups) {
+        final var nowTimestamp = Instant.now().toEpochMilli();
+        final var questionToGroupObjects = jdbiPicklistQuestion.findOrderedGroupAndOptionDtos(containingQuestionIds, nowTimestamp);
 
+        for (final var questionOptionDetails : questionToGroupObjects.entrySet()) {
+            /*
+            * Deserializing the new PicklistOptionDefs inside of the loop (and repeating the serialize/deserialize every loop)
+            *   because PicklistQuestionDao.insertOption(long, PicklistOptionDef, int, long) modifies the PicklistOptionDef
+            *   passed in the 2nd parameter. This is a risky behavior to refactor, so my workaround for now is to reload the entire
+            *   list of new PicklistOptionDefs on every loop.
+            */
+            final var newOptionDefs = patchConfiguration.getConfigList(Metadata.NEW_OPTIONS.getKey())
+                    .stream()
+                    .map((optionDefConfig) -> gson.fromJson(ConfigUtil.toJson(optionDefConfig), PicklistOptionDef.class))
+                    .collect(Collectors.toList());
+            
+            final var questionId = questionOptionDetails.getKey();
+            final var questionStableId = jdbiPatchHelper.fetchQuestionStableIdById(questionId);
+            log.info("Updating picklist options for [study:{}, question:{}]", getTargetStudy(), questionStableId);
+
+            // The picklist option group
+            final var optionGroup = questionOptionDetails.getValue().getGroups()
+                    .stream()
+                    .filter((group) -> picklistGroupName.equals(group.getStableId()))
+                    .findFirst()
+                    .orElseThrow(() -> picklistGroupNotFound(getTargetStudy(), questionStableId, picklistGroupName));
+
+            // The picklist options contained within the previous group
+            final var groupOptions = questionOptionDetails.getValue().getGroupIdToOptions().get(optionGroup.getId());
+
+            // Check to see if all the options expected to be in the group are there (see `expected-options` in the patch configuration).
+            // This check disregards the ordering of the stable ids.
+            final var existingPicklistOptionStableIds = groupOptions.stream()
+                    .map(PicklistOptionDto::getStableId)
+                    .collect(Collectors.toSet());
+            
+            final var expectedOptions = new HashSet<String>(patchConfiguration.getStringList(Metadata.EXPECTED_OPTIONS.getKey()));
+            if (!existingPicklistOptionStableIds.equals(expectedOptions)) {
+                throw studyHasBeenModified(getTargetStudy(),
+                        questionStableId,
+                        expectedOptions,
+                        existingPicklistOptionStableIds);
+            }
+
+            // This is the option after which all the new should be inserted (in display order).
+            // See the `insert-after-sid` option in the patch config file to fine tune this if changes are needed.
+            final var insertAfterOptionStableId = patchConfiguration.getString(Metadata.INSERT_AFTER_SID.getKey());
+            final var targetOption = groupOptions
+                    .stream()
+                    .filter((option) -> option.getStableId().equals(insertAfterOptionStableId))
+                    .findFirst()
+                    .orElseThrow(() -> picklistOptionNotFound(getTargetStudy(), questionStableId, insertAfterOptionStableId));
+
+            var displayOrder = targetOption.getDisplayOrder() + PicklistQuestionDao.DISPLAY_ORDER_GAP;
+            for (final var option : newOptionDefs) {
+                log.info("Inserting picklist option {} for [study:{}, question:{}] at display order {}",
+                        option.getStableId(),
+                        getTargetStudy(),
+                        questionStableId,
+                        displayOrder);
+
+                final var optionId = jdbiPicklistQuestion.insertOption(questionId,
+                        option,
+                        displayOrder,
+                        optionGroup.getRevisionId());
+                
+                log.info("Linking option {}({}) to group {}({})",
+                        option.getStableId(),
+                        optionId,
+                        optionGroup.getStableId(),
+                        optionGroup.getId());
+                jdbiPatchHelper.linkOptionToGroupId(optionId, optionGroup.getId());
+
+                displayOrder += PicklistQuestionDao.DISPLAY_ORDER_GAP;
+            }
+
+            /*
+             * Go back to all the PicklistOptionDtos we originally fetched, and figure out
+             * which ones need to have their display order offset (each option should be
+             * advanced by numberOfInsertedOptions * DISPLAY_ORDER_GAP)
+             */
+            final var optionsToUpdate = groupOptions.subList(groupOptions.indexOf(targetOption) + 1, groupOptions.size());
+            for (final var option : optionsToUpdate) {
+                log.info("Changing display order for picklist option {}({}) from {} to {}",
+                        option.getStableId(),
+                        option.getId(),
+                        option.getDisplayOrder(),
+                        displayOrder);
+                option.setDisplayOrder(displayOrder);
+                jdbiPicklistOption.update(option);
+                displayOrder += PicklistQuestionDao.DISPLAY_ORDER_GAP;
+            }
         }
 
-        return;
+        log.info("TASK::{} completed successfully", getTaskName());
+    }
+
+    private DDPException studyHasBeenModified(String study,
+            String questionStableId,
+            Iterable<String> expectedOptionStableIds,
+            Iterable<String> currentOptionStableIds) {
+        return new DDPException(String.format("Mismatch in expected options for [study:%s, question:%s]. "
+                + "Fetched picklist options [%s], but expected picklist options [%s]. The study has been modified, and"
+                + " the patch's `expected-options` value must be updated to match the current state of the study.",
+                study,
+                questionStableId,
+                String.join(", ", expectedOptionStableIds),
+                String.join(", ", currentOptionStableIds)));
+    }
+
+    private DDPException picklistOptionNotFound(String study, String question, String option) {
+        return new DDPException(String.format("unable to find picklist option %s in [study:%s, question:%s]",
+                option,
+                study,
+                question));
+    }
+
+    private DDPException picklistGroupNotFound(String study, String question, String groupName) {
+        return new DDPException(String.format("unable to find picklist group %s in [study:%s, question:%s]",
+                groupName,
+                study,
+                question));
     }
 
     private DDPException invalidStudyError(String configuredStudy, String targetStudy) {
