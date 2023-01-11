a5e57ff8b940068d47a129c8273b883be598201a
simone <simone@broadinstitute.org>
2023-01-11 15:00:32 -0500

    PEPPER-46: changing logic of how ES nested queries is handled

--- pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/model/elastic/filter/query/CollectionQueryBuilderTest.java
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/model/elastic/filter/query/CollectionQueryBuilderTest.java
@@ -2,6 +2,7 @@ package org.broadinstitute.dsm.model.elastic.filter.query;
 
 import java.util.regex.Pattern;
 
+import org.apache.lucene.queryparser.xml.builders.BooleanQueryBuilder;
 import org.apache.lucene.search.join.ScoreMode;
 import org.broadinstitute.dsm.model.elastic.filter.FilterParser;
 import org.broadinstitute.dsm.model.filter.participant.BaseFilterParticipantList;
@@ -60,6 +61,54 @@ public class CollectionQueryBuilderTest {
                                 .operator(Operator.AND), ScoreMode.Avg))
                 .must(new NestedQueryBuilder("dsm.tissue", new RangeQueryBuilder("dsm.tissue.returnDate").lte("2015-01-01"), ScoreMode.Avg))
                 .must(boolQueryBuilder);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mrNotRequestedYet() {
+
+        String filter = "AND k.receive_date IS NOT NULL AND m.fax_sent IS NULL AND NOT m.mr_problem <=> 1 "
+                        + "AND NOT m.unable_obtain <=> 1 AND NOT m.duplicate <=> 1 AND ( data.status = 'ENROLLED' )";
+
+        AbstractQueryBuilder<?> actual = getAbstractQueryBuilder(filter).build();
+
+
+        AbstractQueryBuilder<BoolQueryBuilder> expected = new BoolQueryBuilder()
+                .must(new NestedQueryBuilder("dsm.kitRequestShipping",
+                new BoolQueryBuilder().must(new ExistsQueryBuilder("dsm.kitRequestShipping.receiveDate")), ScoreMode.Avg))
+                .must(new NestedQueryBuilder("dsm.medicalRecord",
+                new BoolQueryBuilder().mustNot(new ExistsQueryBuilder("dsm.medicalRecord.faxSent")), ScoreMode.Avg))
+                .must(new NestedQueryBuilder("dsm.medicalRecord",
+                        new MatchQueryBuilder("dsm.medicalRecord.mrProblem", false).operator(Operator.AND), ScoreMode.Avg))
+                .must(new NestedQueryBuilder("dsm.medicalRecord",
+                        new MatchQueryBuilder("dsm.medicalRecord.unableObtain", false).operator(Operator.AND), ScoreMode.Avg))
+                .must(new NestedQueryBuilder("dsm.medicalRecord",
+                        new MatchQueryBuilder("dsm.medicalRecord.duplicate", false).operator(Operator.AND), ScoreMode.Avg))
+                .must(new BoolQueryBuilder().should(new MatchQueryBuilder("status", "ENROLLED")));
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mrNotRequestedYetTry() {
+
+        String filter = "AND k.receive_date IS NOT NULL AND m.fax_sent IS NULL AND NOT m.mr_problem <=> 1 "
+                        + "AND NOT m.unable_obtain <=> 1 AND NOT m.duplicate <=> 1 AND ( data.status = 'ENROLLED' )";
+
+        AbstractQueryBuilder<?> actual = getAbstractQueryBuilder(filter).build();
+
+        BoolQueryBuilder nestedPart = new BoolQueryBuilder();
+        nestedPart.must(new BoolQueryBuilder().mustNot(new ExistsQueryBuilder("dsm.medicalRecord.faxSent")))
+                .must(new MatchQueryBuilder("dsm.medicalRecord.mrProblem", false).operator(Operator.AND))
+                .must(new MatchQueryBuilder("dsm.medicalRecord.unableObtain", false).operator(Operator.AND))
+                .must(new MatchQueryBuilder("dsm.medicalRecord.duplicate", false).operator(Operator.AND));
+
+        AbstractQueryBuilder<BoolQueryBuilder> expected = new BoolQueryBuilder()
+                .must(new BoolQueryBuilder().should(new MatchQueryBuilder("status", "ENROLLED")))
+                .must(new NestedQueryBuilder("dsm.medicalRecord", nestedPart, ScoreMode.Avg))
+                .must(new NestedQueryBuilder("dsm.kitRequestShipping",
+                new BoolQueryBuilder().must(new ExistsQueryBuilder("dsm.kitRequestShipping.receiveDate")), ScoreMode.Avg));
 
         Assert.assertEquals(expected, actual);
     }
