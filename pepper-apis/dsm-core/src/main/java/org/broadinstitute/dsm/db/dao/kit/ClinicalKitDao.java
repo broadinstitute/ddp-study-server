b76ca5be0cc659b494bf6e2841c2dbd7e7578af1
simone <simone@broadinstitute.org>
2023-01-19 11:22:23 -0500

    PEPPER-445: changing logic of triggering DSS

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/db/dao/kit/ClinicalKitDao.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/db/dao/kit/ClinicalKitDao.java
@@ -10,15 +10,19 @@ import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.dsm.db.DDPInstance;
+import org.broadinstitute.dsm.db.dao.queue.EventDao;
+import org.broadinstitute.dsm.db.dao.settings.EventTypeDao;
 import org.broadinstitute.dsm.db.dto.kit.ClinicalKitDto;
+import org.broadinstitute.dsm.db.dto.settings.EventTypeDto;
 import org.broadinstitute.dsm.model.gp.ClinicalKitWrapper;
-import org.broadinstitute.dsm.route.ClinicalKitsRoute;
 import org.broadinstitute.dsm.statics.DBConstants;
+import org.broadinstitute.dsm.util.EventUtil;
 import org.broadinstitute.lddp.db.SimpleResult;
 
 @Slf4j
 public class ClinicalKitDao {
     public static final String PECGS = "PE-CGS";
+    public static final String MERCURY = "MERCURY";
     private static final String SQL_GET_CLINICAL_KIT_BASED_ON_SM_ID_VALUE =
             "SELECT p.ddp_participant_id, accession_number, ddp.instance_name, t.collaborator_sample_id, date_px,  "
                     + "kit_type_name, bsp_material_type, bsp_receptacle_type, ddp.ddp_instance_id FROM sm_id sm "
@@ -29,8 +33,24 @@ public class ClinicalKitDao {
                     + "LEFT JOIN ddp_participant as p on (p.participant_id = inst.participant_id) "
                     + "LEFT JOIN ddp_instance as ddp on (ddp.ddp_instance_id = p.ddp_instance_id) "
                     + "LEFT JOIN sm_id_type sit on (sit.sm_id_type_id = sm.sm_id_type_id) "
-                    + "LEFT JOIN kit_type ktype on ( sit.kit_type_id = ktype.kit_type_id) WHERE sm.sm_id_value = ? "
-                    + "AND NOT sm.deleted <=> 1 ";
+                    + "LEFT JOIN kit_type ktype on ( sit.kit_type_id = ktype.kit_type_id) "
+                    + "WHERE sm.sm_id_value = ? AND NOT sm.deleted <=> 1 ";
+    private static final String SQL_GET_CLINICAL_TISSUE_BY_DDP_PARTICIPANT_ID = "SELECT kit.received_date FROM ddp_kit "
+                    + "LEFT JOIN ddp_tissue t on (t.tissue_id  = sm.tissue_id) "
+                    + "LEFT JOIN ddp_onc_history_detail oD on (oD.onc_history_detail_id = t.onc_history_detail_id) "
+                    + "LEFT JOIN ddp_medical_record mr on (mr.medical_record_id = oD.medical_record_id) "
+                    + "LEFT JOIN ddp_institution inst on  (mr.institution_id = inst.institution_id AND NOT mr.deleted <=> 1) "
+                    + "LEFT JOIN ddp_participant as p on (p.participant_id = inst.participant_id) "
+                    + "LEFT JOIN ddp_instance as ddp on (ddp.ddp_instance_id = p.ddp_instance_id) "
+                    + "LEFT JOIN sm_id_type sit on (sit.sm_id_type_id = sm.sm_id_type_id) "
+                    + "LEFT JOIN kit_type ktype on ( sit.kit_type_id = ktype.kit_type_id) "
+                    + "WHERE p.ddp_participant_id = ? AND ddp.instance_name = ? AND NOT sm.deleted <=> 1 AND sm.received_date IS NOT NULL ";
+
+    public static final String SQL_GET_CLINICAL_KITS_BY_DDP_PARTICIPANT_ID = "SELECT received_date "
+                    + "FROM ddp_kit_request req LEFT JOIN ddp_kit kit ON (req.dsm_kit_request_id = kit.dsm_kit_request_id) "
+                    + "LEFT JOIN ddp_instance realm ON (realm.ddp_instance_id = req.ddp_instance_id) "
+                    + "LEFT JOIN kit_type ty ON (req.kit_type_id = ty.kit_type_id) "
+                    + "WHERE req.ddp_participant_id = ? AND realm.instance_name = ? AND received_date IS NOT NULL AND received_by = ?";
     private static final String SQL_SET_ACCESSION_TIME = "UPDATE sm_id SET received_date = ?, received_by = ? WHERE sm_id_value = ? "
                     + "AND NOT deleted <=> 1";
 
@@ -79,6 +99,9 @@ public class ClinicalKitDao {
         clinicalKitDto.setNecessaryParticipantDataToClinicalKit(clinicalKitWrapper.getDdpParticipantId(), ddpInstance);
         if (StringUtils.isNotBlank(clinicalKitDto.getAccessionNumber())) {
             setAccessionTimeForSMID(smIdValue);
+            if (hasKitsAccessioned(clinicalKitWrapper.getDdpParticipantId(), ddpInstance)) {
+                triggerParticipantEvent(ddpInstance, clinicalKitWrapper.getDdpParticipantId(), DBConstants.REQUIRED_SAMPLES_RECEIVED_EVENT);
+            }
             return clinicalKitDto;
         }
         throw new RuntimeException("The kit doesn't have an accession number! SM ID is: " + smIdValue);
@@ -89,7 +112,7 @@ public class ClinicalKitDao {
             SimpleResult dbVals = new SimpleResult();
             try (PreparedStatement stmt = conn.prepareStatement(SQL_SET_ACCESSION_TIME)) {
                 stmt.setLong(1, System.currentTimeMillis());
-                stmt.setString(2, ClinicalKitsRoute.MERCURY);
+                stmt.setString(2, MERCURY);
                 stmt.setString(3, smIdValue);
                 int r = stmt.executeUpdate();
                 if (r != 1) { //number of sm ids with that value
@@ -106,5 +129,81 @@ public class ClinicalKitDao {
         if (results.resultException != null) {
             throw new RuntimeException("Error updating accession time for smId " + smIdValue, results.resultException);
         }
+    }
+
+    public void ifTissueAccessionedTriggerDDP(String ddpParticipantId, DDPInstance ddpInstance) {
+        if (hasTissueAccessioned(ddpParticipantId, ddpInstance)) {
+            triggerParticipantEvent(ddpInstance, ddpParticipantId, DBConstants.REQUIRED_SAMPLES_RECEIVED_EVENT);
+        }
+    }
+
+    public boolean hasTissueAccessioned(String ddpParticipantId, DDPInstance ddpInstance) {
+        SimpleResult results = inTransaction((conn) -> {
+            SimpleResult dbVals = new SimpleResult(0);
+            try (PreparedStatement stmt = conn.prepareStatement(SQL_GET_CLINICAL_TISSUE_BY_DDP_PARTICIPANT_ID)) {
+                stmt.setString(1, ddpParticipantId);
+                stmt.setString(2, ddpInstance.getName());
+                try (ResultSet rs = stmt.executeQuery()) {
+                    rs.last();
+                    dbVals.resultValue = rs.getRow();
+                }
+            } catch (SQLException ex) {
+                dbVals.resultException = ex;
+            }
+            return dbVals;
+        });
+
+        if (results.resultException != null) {
+            log.error("Couldn't get tissue sample information for pt w/ id " + ddpParticipantId, results.resultException);
+        }
+        if ((int) results.resultValue > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasKitsAccessioned(String ddpParticipantId, DDPInstance ddpInstance) {
+        SimpleResult results = inTransaction((conn) -> {
+            SimpleResult dbVals = new SimpleResult(0);
+            try (PreparedStatement stmt = conn.prepareStatement(SQL_GET_CLINICAL_KITS_BY_DDP_PARTICIPANT_ID)) {
+                stmt.setString(1, ddpParticipantId);
+                stmt.setString(2, ddpInstance.getName());
+                stmt.setString(3, MERCURY);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    rs.last();
+                    dbVals.resultValue = rs.getRow();
+                }
+            } catch (SQLException ex) {
+                dbVals.resultException = ex;
+            }
+            return dbVals;
+        });
+
+        if (results.resultException != null) {
+            log.error("Couldn't get tissue sample information for pt w/ id " + ddpParticipantId, results.resultException);
+        }
+        if ((int) results.resultValue > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private void triggerParticipantEvent(DDPInstance ddpInstance, String ddpParticipantId, String eventName) {
+        final EventDao eventDao = new EventDao();
+        final EventTypeDao eventTypeDao = new EventTypeDao();
+        Optional<EventTypeDto> eventType =
+                eventTypeDao.getEventTypeByEventNameAndInstanceId(eventName, ddpInstance.getDdpInstanceId());
+        eventType.ifPresent(eventTypeDto -> {
+            boolean participantHasTriggeredEventByEventType =
+                    eventDao.hasTriggeredEventByEventTypeAndDdpParticipantId(eventName, ddpParticipantId).orElse(false);
+            if (!participantHasTriggeredEventByEventType) {
+                inTransaction((conn) -> {
+                    EventUtil.triggerDDP(conn, eventType, ddpParticipantId);
+                    return null;
+                });
+            } else {
+                log.info("Participant " + ddpParticipantId + " was already triggered for event type " + eventName);
+            }
+        });
     }
 }
--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/route/ClinicalKitsRoute.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/route/ClinicalKitsRoute.java
@@ -1,6 +1,5 @@
 package org.broadinstitute.dsm.route;
 
-import static org.broadinstitute.ddp.db.TransactionWrapper.inTransaction;
 
 import java.util.Optional;
 
@@ -8,18 +7,13 @@ import lombok.NonNull;
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.dsm.db.DDPInstance;
 import org.broadinstitute.dsm.db.dao.kit.ClinicalKitDao;
-import org.broadinstitute.dsm.db.dao.queue.EventDao;
-import org.broadinstitute.dsm.db.dao.settings.EventTypeDao;
 import org.broadinstitute.dsm.db.dto.kit.BSPKitDto;
 import org.broadinstitute.dsm.db.dto.kit.ClinicalKitDto;
-import org.broadinstitute.dsm.db.dto.settings.EventTypeDto;
 import org.broadinstitute.dsm.model.gp.BSPKit;
 import org.broadinstitute.dsm.model.gp.GPReceivedKit;
 import org.broadinstitute.dsm.model.gp.KitInfo;
 import org.broadinstitute.dsm.model.gp.bsp.BSPKitStatus;
-import org.broadinstitute.dsm.statics.DBConstants;
 import org.broadinstitute.dsm.statics.RequestParameter;
-import org.broadinstitute.dsm.util.EventUtil;
 import org.broadinstitute.dsm.util.NotificationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,7 +25,6 @@ public class ClinicalKitsRoute implements Route {
 
     private static final Logger logger = LoggerFactory.getLogger(ClinicalKitsRoute.class);
 
-    public static final String MERCURY = "MERCURY";
     private NotificationUtil notificationUtil;
 
     public ClinicalKitsRoute(@NonNull NotificationUtil notificationUtil) {
@@ -64,7 +57,8 @@ public class ClinicalKitsRoute implements Route {
             //kit not found in ddp_kit table -> check tissue smi-ids
             return new ClinicalKitDao().getClinicalKitBasedOnSmId(kitLabel);
         } else {
-            Optional<KitInfo> maybeKitInfo = GPReceivedKit.receiveKit(kitLabel, optionalBSPKitDto.orElseThrow(), notificationUtil, MERCURY);
+            Optional<KitInfo> maybeKitInfo = GPReceivedKit.receiveKit(kitLabel, optionalBSPKitDto.orElseThrow(), notificationUtil,
+                    ClinicalKitDao.MERCURY);
             String ddpParticipantId = optionalBSPKitDto.get().getDdpParticipantId();
             KitInfo kitInfo = maybeKitInfo.orElseThrow();
             ClinicalKitDto clinicalKit = new ClinicalKitDto();
@@ -79,29 +73,8 @@ public class ClinicalKitsRoute implements Route {
             clinicalKit.setSampleCollection(ClinicalKitDao.PECGS);
             DDPInstance ddpInstance = DDPInstance.getDDPInstance(kitInfo.getRealm());
             clinicalKit.setNecessaryParticipantDataToClinicalKit(ddpParticipantId, ddpInstance);
-            //todo add check for tissue received
-            triggerParticipantEvent(ddpInstance, ddpParticipantId, DBConstants.REQUIRED_SAMPLES_RECEIVED_EVENT);
-
+            new ClinicalKitDao().ifTissueAccessionedTriggerDDP(ddpParticipantId, ddpInstance);
             return clinicalKit;
         }
-    }
-
-     private void triggerParticipantEvent(DDPInstance ddpInstance, String ddpParticipantId, String eventName) {
-        final EventDao eventDao = new EventDao();
-        final EventTypeDao eventTypeDao = new EventTypeDao();
-        Optional<EventTypeDto> eventType =
-                eventTypeDao.getEventTypeByEventNameAndInstanceId(eventName, ddpInstance.getDdpInstanceId());
-        eventType.ifPresent(eventTypeDto -> {
-            boolean participantHasTriggeredEventByEventType =
-                    eventDao.hasTriggeredEventByEventTypeAndDdpParticipantId(eventName, ddpParticipantId).orElse(false);
-            if (!participantHasTriggeredEventByEventType) {
-                inTransaction((conn) -> {
-                    EventUtil.triggerDDP(conn, eventType, ddpParticipantId);
-                    return null;
-                });
-            } else {
-                logger.info("Participant " + ddpParticipantId + " was already triggered for event type " + eventName);
-            }
-        });
     }
 }
