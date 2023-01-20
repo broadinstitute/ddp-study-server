db6abbd69f7da594f66584524c32c493f5bb3108
SimoneMaiwald <simone@broadinstitute.org>
2023-01-20 08:41:49 -0500

    PEPPER-127: os2 filters (#2433)
    * PEPPER-46: changing logic of how ES nested queries is handled
    
    * PEPPER-46: updating unit tests to the new logic change
    
    * PEPPER-127: fixing OS search to add ddpInstanceId to k and mr and oD search
    
    * PEPPER-127: fixing test after rebasing
    
    * PEPPER-127: changed to use dbConstants instead of strings

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/filter/query/BaseAbstractQueryBuilder.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/elastic/filter/query/BaseAbstractQueryBuilder.java
@@ -28,6 +28,7 @@ public class BaseAbstractQueryBuilder {
     protected String filter;
     protected Parser parser;
     protected String esIndex;
+    protected Integer ddpInstanceId;
     protected BoolQueryBuilder boolQueryBuilder;
     protected QueryBuilder queryBuilder;
     protected SplitterStrategy splitter;
@@ -41,6 +42,10 @@ public class BaseAbstractQueryBuilder {
 
     public void setEsIndex(String index) {
         this.esIndex = index;
+    }
+
+    public void setDdpInstanceId(Integer ddpInstanceId) {
+        this.ddpInstanceId = ddpInstanceId;
     }
 
     public void setFilter(String filter) {
@@ -74,45 +79,68 @@ public class BaseAbstractQueryBuilder {
         Map<String, BaseQueryBuilder> correctBaseQueryBuilder = new HashMap<>();
         //all alias are mixed in here. order them per alias?
         for (String filterValue : filterValues) {
-            Operator operator = Operator.extract(filterValue);
-            if (operator == null) {
-                continue;
+            creatingQueryBuilder(filterValue, filterStrategy, nestedQueryBuilderLists, correctBaseQueryBuilder);
+        }
+        //including check for ddpInstanceId only needed for OS studies
+        if (ddpInstanceId != null && esIndex != null && DBConstants.OSTEO_INDEX.equals(esIndex)) {
+            if (nestedQueryBuilderLists.containsKey(ESObjectConstants.DSM + DBConstants.ALIAS_DELIMITER + ESObjectConstants.KIT_REQUEST_SHIPPING)) {
+                creatingQueryBuilder(DBConstants.DDP_KIT_REQUEST_ALIAS + DBConstants.ALIAS_DELIMITER
+                                + DBConstants.DDP_INSTANCE_ID_CAMEL_CASE + Filter.EQUALS + ddpInstanceId,
+                        filterStrategy, nestedQueryBuilderLists, correctBaseQueryBuilder);
             }
-            splitter = operator.getSplitterStrategy();
-            splitter.setFilterSeparator(filterSeparator);
-            splitter.setFilter(filterValue);
-            String path = PropertyInfo.of(splitter.getAlias()).getPropertyName();
-            String alias = Objects.requireNonNull(splitter.getAlias());
-            if (Util.isUnderDsmKey(alias)) {
-                path = DSM_WITH_DOT + PropertyInfo.of(splitter.getAlias()).getPropertyName();
+            if (nestedQueryBuilderLists.containsKey(ESObjectConstants.DSM + DBConstants.ALIAS_DELIMITER + ESObjectConstants.MEDICAL_RECORD)) {
+                creatingQueryBuilder(DBConstants.DDP_MEDICAL_RECORD_ALIAS + DBConstants.ALIAS_DELIMITER
+                                + DBConstants.DDP_INSTANCE_ID_CAMEL_CASE + Filter.EQUALS + ddpInstanceId,
+                        filterStrategy, nestedQueryBuilderLists, correctBaseQueryBuilder);
             }
-            String innerProperty = splitter.getInnerProperty();
-            if (alias.equals(ESObjectConstants.PARTICIPANT_DATA)) {
-                innerProperty = "dynamicFields." + innerProperty;
+            if (nestedQueryBuilderLists.containsKey(ESObjectConstants.DSM + DBConstants.ALIAS_DELIMITER + ESObjectConstants.ONC_HISTORY_DETAIL)) {
+                creatingQueryBuilder(DBConstants.DDP_ONC_HISTORY_DETAIL_ALIAS + DBConstants.ALIAS_DELIMITER
+                                + DBConstants.DDP_INSTANCE_ID_CAMEL_CASE + Filter.EQUALS + ddpInstanceId,
+                        filterStrategy, nestedQueryBuilderLists, correctBaseQueryBuilder);
             }
-            QueryPayload queryPayload = new QueryPayload(path, innerProperty, splitter.getAlias(),
-                            parser.parse(splitter.getValue()), esIndex);
-            baseQueryBuilder = BaseQueryBuilder.of(queryPayload);
-            List<QueryBuilder> queryBuilders =
-                    QueryStrategyFactory.create(new QueryStrategyFactoryPayload(baseQueryBuilder, operator, parser)).build();
-
-            if (StringUtils.isNotBlank(path)) {
-                if (!nestedQueryBuilderLists.containsKey(path)) {
-                    nestedQueryBuilderLists.put(path, queryBuilders);
-                    correctBaseQueryBuilder.put(path, baseQueryBuilder);
-                } else {
-                    List<QueryBuilder> queryBuildersFromMap = nestedQueryBuilderLists.get(path);
-                    queryBuildersFromMap.addAll(queryBuilders);
-                }
-            } else {
-                filterStrategy.build(boolQueryBuilder, baseQueryBuilder.build(queryBuilders));
-            }
+            //tissue and oncHistory objects do currently not have ddpInstanceId set
         }
         if (!nestedQueryBuilderLists.isEmpty() && nestedQueryBuilderLists.size() == correctBaseQueryBuilder.size()) {
             nestedQueryBuilderLists.entrySet().stream().forEach(entrySet -> filterStrategy.build(boolQueryBuilder,
                     correctBaseQueryBuilder.get(entrySet.getKey()).build(entrySet.getValue())));
-
         }
     }
 
+    private void creatingQueryBuilder(String filterValue, FilterStrategy filterStrategy,
+                                      Map<String, List<QueryBuilder>> nestedQueryBuilderLists,
+                                      Map<String, BaseQueryBuilder> correctBaseQueryBuilder) {
+        Operator operator = Operator.extract(filterValue);
+        if (operator == null) {
+            return;
+        }
+        splitter = operator.getSplitterStrategy();
+        splitter.setFilterSeparator(filterSeparator);
+        splitter.setFilter(filterValue);
+        String path = PropertyInfo.of(splitter.getAlias()).getPropertyName();
+        String alias = Objects.requireNonNull(splitter.getAlias());
+        if (Util.isUnderDsmKey(alias)) {
+            path = DSM_WITH_DOT + PropertyInfo.of(splitter.getAlias()).getPropertyName();
+        }
+        String innerProperty = splitter.getInnerProperty();
+        if (alias.equals(ESObjectConstants.PARTICIPANT_DATA)) {
+            innerProperty = "dynamicFields." + innerProperty;
+        }
+        QueryPayload queryPayload = new QueryPayload(path, innerProperty, splitter.getAlias(),
+                parser.parse(splitter.getValue()), esIndex);
+        baseQueryBuilder = BaseQueryBuilder.of(queryPayload);
+        List<QueryBuilder> queryBuilders =
+                QueryStrategyFactory.create(new QueryStrategyFactoryPayload(baseQueryBuilder, operator, parser)).build();
+
+        if (StringUtils.isNotBlank(path)) {
+            if (!nestedQueryBuilderLists.containsKey(path)) {
+                nestedQueryBuilderLists.put(path, queryBuilders);
+                correctBaseQueryBuilder.put(path, baseQueryBuilder);
+            } else {
+                List<QueryBuilder> queryBuildersFromMap = nestedQueryBuilderLists.get(path);
+                queryBuildersFromMap.addAll(queryBuilders);
+            }
+        } else {
+            filterStrategy.build(boolQueryBuilder, baseQueryBuilder.build(queryBuilders));
+        }
+    }
 }
