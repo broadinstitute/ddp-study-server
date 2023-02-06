ba426fa7e375ce9608af032af8fdfa6d0361d16d
simone <simone@broadinstitute.org>
2023-02-06 12:11:10 -0500

    PEPPER-171: want to keep them in case they are needed later...

--- pepper-apis/dsm-core/testsupport/scripts/exportIndexInfo.sh
+++ pepper-apis/dsm-core/testsupport/scripts/exportIndexInfo.sh
@@ -1,44 +1,44 @@
-#!/usr/bin/env bash
-# Export contents of an index to 3 separate files: analyzer, mapping and data
-
-set +e
-# set -x
-
-if [ -z "$1" ] || [ "$1" = "--help" ]; then
-    echo "Usage: $0 <indexName> [dataDirectory]"
-    echo "Options:"
-    echo "  --help      show this help message and exit"
-    exit 0
-fi
-
-INDEX_NAME=$1
-DATA_FILE_DIR=${2:-../esdata}
-
-# Going to extract needed info out of vault
-ES_ADMIN_NAME='pepper_engineering'
-ES_ADMIN_PWD=$(vault read --format=json secret/pepper/dev/v1/elasticsearch | jq -r ".data.accounts[]  | select(.username == \"${ES_ADMIN_NAME}\") | .password")
-ES_SERVER_BASE_URL=$(vault read --format=json secret/pepper/dev/v1/elasticsearch | jq -r .data.endpoint)
-
-ES_AUTH_FILE=$(mktemp)
-echo "user=${ES_ADMIN_NAME}" > ${ES_AUTH_FILE}
-echo "password=${ES_ADMIN_PWD}" >> ${ES_AUTH_FILE}
-
-#create an array with the string values "analyzer", "mapping", and "data"
-DATA_TYPES=("analyzer" "mapping" "data")
-
-#loop through the array and create a variable for each value
-for DATA_TYPE in "${DATA_TYPES[@]}"; do
-  elasticdump \
-    --input=${ES_SERVER_BASE_URL}/${INDEX_NAME} \
-    --output=${DATA_FILE_DIR}/${INDEX_NAME}-${DATA_TYPE}.json \
-    --type=${DATA_TYPE} \
-    --httpAuthFile ${ES_AUTH_FILE}
-
-    #if elasticdump is not found, show a message and exit
-    if [ $? -eq 127 ]; then
-      echo "elasticdump utility was not found. Please install it with 'npm install elasticdump -g'"
-      exit 1
-    fi
-done
-
-
+##!/usr/bin/env bash
+## Export contents of an index to 3 separate files: analyzer, mapping and data
+#
+#set +e
+## set -x
+#
+#if [ -z "$1" ] || [ "$1" = "--help" ]; then
+#    echo "Usage: $0 <indexName> [dataDirectory]"
+#    echo "Options:"
+#    echo "  --help      show this help message and exit"
+#    exit 0
+#fi
+#
+#INDEX_NAME=$1
+#DATA_FILE_DIR=${2:-../esdata}
+#
+## Going to extract needed info out of vault
+#ES_ADMIN_NAME='pepper_engineering'
+#ES_ADMIN_PWD=$(vault read --format=json secret/pepper/dev/v1/elasticsearch | jq -r ".data.accounts[]  | select(.username == \"${ES_ADMIN_NAME}\") | .password")
+#ES_SERVER_BASE_URL=$(vault read --format=json secret/pepper/dev/v1/elasticsearch | jq -r .data.endpoint)
+#
+#ES_AUTH_FILE=$(mktemp)
+#echo "user=${ES_ADMIN_NAME}" > ${ES_AUTH_FILE}
+#echo "password=${ES_ADMIN_PWD}" >> ${ES_AUTH_FILE}
+#
+##create an array with the string values "analyzer", "mapping", and "data"
+#DATA_TYPES=("analyzer" "mapping" "data")
+#
+##loop through the array and create a variable for each value
+#for DATA_TYPE in "${DATA_TYPES[@]}"; do
+#  elasticdump \
+#    --input=${ES_SERVER_BASE_URL}/${INDEX_NAME} \
+#    --output=${DATA_FILE_DIR}/${INDEX_NAME}-${DATA_TYPE}.json \
+#    --type=${DATA_TYPE} \
+#    --httpAuthFile ${ES_AUTH_FILE}
+#
+#    #if elasticdump is not found, show a message and exit
+#    if [ $? -eq 127 ]; then
+#      echo "elasticdump utility was not found. Please install it with 'npm install elasticdump -g'"
+#      exit 1
+#    fi
+#done
+#
+#
--- pepper-apis/dsm-core/testsupport/scripts/importAllIndexInfo.sh
+++ pepper-apis/dsm-core/testsupport/scripts/importAllIndexInfo.sh
@@ -1,26 +1,26 @@
-#!/usr/bin/env bash
-#script will try to load all the index info from the esdata directory into the specified ES server
-set -e
-#set -x
-
-# if first argument is empty or if it is equal to --help, print the command usage
-if [ -z "$1" ] || [ "$1" = "--help" ]; then
-  echo "Usage: $0 <serverBaseUrl> [dataDirectory]"
-  echo "Options:"
-  echo "  --help            show this help message and exit"
-  exit 0
-fi
-
-ES_SERVER_BASE_URL=$1
-DATA_FILE_DIR=${2:-../esdata}
-
-for file in "${DATA_FILE_DIR}"/*-data.json; do
-  #extract the index name from the file name and remove the -data.json suffix
-  INDEX_NAME=$(echo "${file}" | xargs -n 1 basename | rev | cut -c11- | rev)
-  ./importIndexInfo.sh "$ES_SERVER_BASE_URL" "$INDEX_NAME" "$DATA_FILE_DIR" &
-done
-wait
-echo "All done!"
-
-
-
+##!/usr/bin/env bash
+##script will try to load all the index info from the esdata directory into the specified ES server
+#set -e
+##set -x
+#
+## if first argument is empty or if it is equal to --help, print the command usage
+#if [ -z "$1" ] || [ "$1" = "--help" ]; then
+#  echo "Usage: $0 <serverBaseUrl> [dataDirectory]"
+#  echo "Options:"
+#  echo "  --help            show this help message and exit"
+#  exit 0
+#fi
+#
+#ES_SERVER_BASE_URL=$1
+#DATA_FILE_DIR=${2:-../esdata}
+#
+#for file in "${DATA_FILE_DIR}"/*-data.json; do
+#  #extract the index name from the file name and remove the -data.json suffix
+#  INDEX_NAME=$(echo "${file}" | xargs -n 1 basename | rev | cut -c11- | rev)
+#  ./importIndexInfo.sh "$ES_SERVER_BASE_URL" "$INDEX_NAME" "$DATA_FILE_DIR" &
+#done
+#wait
+#echo "All done!"
+#
+#
+#
