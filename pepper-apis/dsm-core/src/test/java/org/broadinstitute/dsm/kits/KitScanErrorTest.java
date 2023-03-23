33e2a30158557fc05e1212f6ab08130b874f685c
pegahtah <ptaheri@broadinstitute.org>
2023-03-23 17:22:45 -0400

    I split the Scan Error test to 3 tests

--- pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/kits/KitScanErrorTest.java
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/kits/KitScanErrorTest.java
@@ -8,19 +8,31 @@ import org.junit.Test;
 public class KitScanErrorTest {
 
     @Test
-    public void testIsScanErrorOnlyBspParticipantId(){
+    public void testShortIdIsNotErrror(){
         String kit = "TEST_KIT_LABEL";
         String error = null;
         String shortId = "TEST_SHORT_ID";
 
         ScanError notRealScanError = new ScanError(kit, error, shortId);
         Assert.assertTrue(notRealScanError.isScanErrorOnlyBspParticipantId("TEST_SHORT_ID"));
+    }
 
-        error = "This is real error";
+    @Test
+    public void testScanErrorAndShortINotNull(){
+        String kit = "TEST_KIT_LABEL";
+        String error = "This is real error";
+        String shortId = "TEST_SHORT_ID";
+
         ScanError realScanError = new ScanError(kit, error, shortId);
         Assert.assertFalse(realScanError.isScanErrorOnlyBspParticipantId("TEST_SHORT_ID"));
+    }
 
-        shortId = null;
+    @Test
+    public void testScanErrorNotNullShortIdNull(){
+        String kit = "TEST_KIT_LABEL";
+        String error = "This is real error";
+        String shortId = null;
+
         ScanError realScanError2 = new ScanError(kit, error, shortId);
         Assert.assertFalse(realScanError2.isScanErrorOnlyBspParticipantId("TEST_SHORT_ID"));
     }
