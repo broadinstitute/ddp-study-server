fb6cb3529ef7bd3dce8ece400a517ffcf7317e6a
Sampath K. Settipalli <sampath@broadinstitute.org>
2023-03-25 15:29:34 -0500

    PEPPER-554 . w-i-p

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/atcp/ATCPConsentVersion2.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/atcp/ATCPConsentVersion2.java
@@ -4,8 +4,11 @@ import com.google.gson.Gson;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.broadinstitute.ddp.content.I18nTemplateConstants;
+import org.broadinstitute.ddp.db.DaoException;
 import org.broadinstitute.ddp.db.dao.ActivityDao;
 import org.broadinstitute.ddp.db.dao.JdbiRevision;
+import org.broadinstitute.ddp.db.dao.JdbiTemplateVariable;
 import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dao.JdbiVariableSubstitution;
 import org.broadinstitute.ddp.db.dao.SectionBlockDao;
@@ -37,8 +40,11 @@ import java.util.List;
 public class ATCPConsentVersion2 implements CustomTask {
     private static final String DATA_FILE = "patches/consent-version-2.conf";
     private static final String ATCP_STUDY = "atcp";
-    private static final String VARIABLES = "variables";
+    private static final String VARIABLES_ADD = "variables-add";
+    private static final String VARIABLES_UPD = "variables-update";
+    private static final String VARIABLES_DEL = "variables-delete";
     private static final String ADULT_TRANSLATION_KEY = "name";
+    private static final String NEW_TEMPL_VAR_LOOKUP = "consent_participation_detail_p4";
     private static final Gson gson = GsonUtil.standardGson();
 
     private Config dataCfg;
@@ -102,18 +108,43 @@ public class ATCPConsentVersion2 implements CustomTask {
     }
 
     private void runConsentUpdate(Handle handle, RevisionMetadata meta, ActivityVersionDto version2) {
+        terminateTemplateVariables(handle, meta, version2, dataCfg);
+        addTemplateVariables(handle, meta, version2, dataCfg);
         updateTemplateVariables(handle, meta, version2, dataCfg);
+
+    }
+
+    private void addTemplateVariables(Handle handle, RevisionMetadata meta,
+                                         ActivityVersionDto version2, Config dataCfg) {
+        List<? extends Config> configList = dataCfg.getConfigList(VARIABLES_ADD);
+        List<Long> templateVariableIdByVariableNames = sqlHelper.findTemplateVariableIdByVariableNames(NEW_TEMPL_VAR_LOOKUP);
+        //get first ID
+        Long templateId = sqlHelper.findTemplateIdByVariableId(templateVariableIdByVariableNames.get(0));
+
+        for (Config config : configList) {
+            TemplateVariable templateVariable = gson.fromJson(ConfigUtil.toJson(config), TemplateVariable.class);
+            addVariable(handle, null, templateId, version2.getRevId(), templateVariable);
+        }
     }
 
     private void updateTemplateVariables(Handle handle, RevisionMetadata meta,
                                          ActivityVersionDto version2, Config dataCfg) {
-        List<? extends Config> configList = dataCfg.getConfigList(VARIABLES);
-
+        List<? extends Config> configList = dataCfg.getConfigList(VARIABLES_UPD);
         for (Config config : configList) {
             //String varName = config.getString(ADULT_TRANSLATION_KEY);
             TemplateVariable templateVariable = gson.fromJson(ConfigUtil.toJson(config), TemplateVariable.class);
             //List<Translation> translations = templateVariable.getTranslations();
             revisionVariableTranslation(templateVariable, meta, version2);
+        }
+    }
+
+    private void terminateTemplateVariables(Handle handle, RevisionMetadata meta,
+                                      ActivityVersionDto version2, Config dataCfg) {
+
+        List<? extends Config> configList = dataCfg.getConfigList(VARIABLES_DEL);
+        for (Config config : configList) {
+            TemplateVariable templateVariable = gson.fromJson(ConfigUtil.toJson(config), TemplateVariable.class);
+            terminateTemplateVariable(templateVariable, meta, version2);
         }
     }
 
@@ -154,13 +185,102 @@ public class ATCPConsentVersion2 implements CustomTask {
         }
     }
 
+    private void terminateTemplateVariable(TemplateVariable templateVariable,
+                                             RevisionMetadata meta, ActivityVersionDto version2) {
+        log.info("revisioning and terminating template variable: {}", templateVariable.getName());
+        List<Long> templateVariableIdByVariableNames = sqlHelper.findTemplateVariableIdByVariableNames(templateVariable.getName());
+        //todo if 0/variable not found.. insert new one 3/24
+        log.info("Tmpl variableId count: {} ", templateVariableIdByVariableNames.size());
+        for (Long tmplVarId : templateVariableIdByVariableNames) {
+            List<Translation> transList = jdbiVarSubst.fetchSubstitutionsForTemplateVariable(tmplVarId);
+            //all translations has same revision.. hence using first
+            long revId = jdbiRevision.copyAndTerminate(transList.get(0).getRevisionId().get(), meta);
+            long[] revIds = new long[transList.size()];
+            Arrays.fill(revIds, revId);
+            List<Long> substitutionIds = new ArrayList<>();
+            log.info("subs/translations count: {} for varId : {} ", transList.size(), tmplVarId);
+            for (var currTranslation : transList) {
+                //String language = currTranslation.getLanguageCode();
+                //String currentText = currTranslation.getText();
+
+                //todo .. iterate and update all languages
+                //String newTxt = templateVariable.getTranslation(language).get().getText();
+                //log.info(" language: {} revId: {} new Text: {} currText: {}", language, currTranslation.getRevisionId(),
+                //        newTxt, currentText);
+
+                substitutionIds.add(currTranslation.getId().get());
+                //jdbiVarSubst.bulkUpdateRevisionIdsBySubIds(Arrays.asList(currTranslation.getId().get()), revIds);
+                //jdbiVarSubst.insert(currTranslation.getLanguageCode(), templateVariable.getTranslation(language).get().getText(),
+                //        version2.getRevId(), tmplVarId);
+            }
+            int[] ids = jdbiVarSubst.bulkUpdateRevisionIdsBySubIds(substitutionIds, revIds);
+            if (ids.length != revIds.length) {
+                throw new DDPException("returned ids length " + ids.length + "  doesnt match revIds passed length " + revIds.length);
+            }
+            log.info("ids: {}", ids);
+            log.info("revisioned and terminated template variable: {}", tmplVarId);
+        }
+    }
+
+    private void deleteVariable(Handle handle, String tag, TemplateVariable variable) {
+        //just revision and terminate current revision
+
+        var jdbiTemplateVariable = handle.attach(JdbiTemplateVariable.class);
+        var jdbiVariableSubstitution = handle.attach(JdbiVariableSubstitution.class);
+        String variableName = variable.getName();
+        long variableId = variable.getId().get();
+
+        for (var translation : variable.getTranslations()) {
+            String language = translation.getLanguageCode();
+            boolean deleted = jdbiVariableSubstitution.delete(translation.getId().get());
+            if (deleted) {
+                log.info("[{}] variable {} language {}: deleted substitution", tag, variableName, language);
+            } else {
+                throw new DDPException(String.format(
+                        "Could not delete substitution for %s variable %s language %s",
+                        tag, variableName, language));
+            }
+        }
+
+        boolean deleted = jdbiTemplateVariable.delete(variableId);
+        if (deleted) {
+            log.info("[{}] deleted variable {}", tag, variableName);
+        } else {
+            throw new DDPException(String.format("Could not delete %s variable %s", tag, variableName));
+        }
+    }
+
+    private void addVariable(Handle handle, String tag, long templateId, long revisionId, TemplateVariable variable) {
+        var jdbiTemplateVariable = handle.attach(JdbiTemplateVariable.class);
+        var jdbiVariableSubstitution = handle.attach(JdbiVariableSubstitution.class);
+        String variableName = variable.getName();
+
+        if (I18nTemplateConstants.DDP.equals(variableName) || I18nTemplateConstants.LAST_UPDATED.equals(variableName)) {
+            throw new DaoException("Variable name '" + variableName + "' is not allowed");
+        }
+
+        long variableId = jdbiTemplateVariable.insertVariable(templateId, variableName);
+        log.info("[{}] inserted variable {}", tag, variableName);
+
+        for (var translation : variable.getTranslations()) {
+            String language = translation.getLanguageCode();
+            jdbiVariableSubstitution.insert(language, translation.getText(), revisionId, variableId);
+            log.info("[{}] variable {} language {}: inserted substitution",
+                    tag, variableName, language);
+        }
+        log.info("inserted template variable for all translations");
+    }
 
     private interface SqlHelper extends SqlObject {
 
         //todo filter by studyId so that we query only latest study variables
-        @SqlQuery("select template_variable_id from template_variable where variable_name = :variable_name "
+        @SqlQuery("select template_variable_id from template_variable where variable_name = :variableName "
                 + "order by template_variable_id desc")
-        List<Long> findTemplateVariableIdByVariableNames(@Bind("variable_name") String variableName);
+        List<Long> findTemplateVariableIdByVariableNames(@Bind("variableName") String variableName);
+
+        @SqlQuery("select template_id from template_variable where template_variable_id = :variableId "
+                + "order by template_id desc")
+        Long findTemplateIdByVariableId(@Bind("variableId") long variableId);
 
     }
 
