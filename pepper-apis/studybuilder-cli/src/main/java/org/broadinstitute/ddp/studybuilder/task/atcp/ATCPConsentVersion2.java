7be5b5af5e6e3313522348124a9a8e685f4e7204
Sampath K. Settipalli <sampath@broadinstitute.org>
2023-03-27 20:57:48 -0500

    PEPPER-554 . self-consent version2

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/atcp/ATCPConsentVersion2.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/atcp/ATCPConsentVersion2.java
@@ -42,16 +42,13 @@ import java.util.List;
 @Slf4j
 public class ATCPConsentVersion2 implements CustomTask {
 
-    private static final String OLD_TEMPLATE_KEY = "old_template_search_text";
-    private static final String BLOCK_KEY = "blockNew";
-    private static final String BLOCK_UPDATES = "block-updates";
-
     private static final String DATA_FILE = "patches/consent-version-2.conf";
     private static final String ATCP_STUDY = "atcp";
-    private static final String VARIABLES_ADD = "variables-add";
     private static final String VARIABLES_UPD = "variables-update";
-    private static final String VARIABLES_DEL = "variables-delete";
-    private static final String NEW_TEMPL_VAR_LOOKUP = "consent_participation_detail_p4";
+    private static final String BLOCK_KEY = "blockNew";
+    private static final String BLOCK_UPDATES = "block-updates";
+    private static final String OLD_TEMPLATE_KEY = "old_template_search_text";
+
     private static final Gson gson = GsonUtil.standardGson();
 
     private Config dataCfg;
@@ -72,9 +69,7 @@ public class ATCPConsentVersion2 implements CustomTask {
         if (!file.exists()) {
             throw new DDPException("Data file is missing: " + file);
         }
-        //dataCfg = ConfigFactory.parseFile(file);
         dataCfg = ConfigFactory.parseFile(file).resolveWith(varsCfg);
-
         this.cfgPath = cfgPath;
         this.varsCfg = varsCfg;
 
@@ -112,39 +107,6 @@ public class ATCPConsentVersion2 implements CustomTask {
 
     }
 
-
-
-/*    @Override
-    public void run(Handle handle) {
-        User adminUser = handle.attach(UserDao.class).findUserByGuid(cfg.getString("adminUser.guid")).get();
-
-        String activityCodeConsent = "CONSENT";
-        StudyDto studyDto = handle.attach(JdbiUmbrellaStudy.class).findByStudyGuid(dataCfg.getString("study.guid"));
-        log.info("Changing version of {} to {} with timestamp={}", activityCodeConsent, versionTag, timestamp);
-        long ts = this.timestamp.toEpochMilli();
-
-        String reasonConsent = String.format(
-                "Update activity with studyGuid=%s activityCode=%s to versionTag=%s",
-                studyDto.getGuid(), activityCodeConsent, versionTag);
-        RevisionMetadata metaConsent = new RevisionMetadata(ts, adminUser.getId(), reasonConsent);
-        this.activityDao = handle.attach(ActivityDao.class);
-        this.sqlHelper = handle.attach(SqlHelper.class);
-        this.sectionBlockDao = handle.attach(SectionBlockDao.class);
-        this.jdbiVarSubst = handle.attach(JdbiVariableSubstitution.class);
-        this.jdbiRevision = handle.attach(JdbiRevision.class);
-
-        //ActivityVersionDto version2ForConsent = getVersion2(handle, studyDto, metaConsent, activityCodeConsent);
-        //runConsentUpdate(handle, metaConsent, version2ForConsent);
-
-        //String oldBlockTemplateText = "<p class=\"normal\">$consent_participation_detail_p1</p>";
-        String oldBlockTemplateText = "$consent_contact_detail_email_help";
-        String templateSearchParam = String.format("%s%s%s", "%", oldBlockTemplateText, "%");
-        BlockContentDto contentBlock = handle.attach(SqlHelper.class)
-                .findContentBlockByBodyText(2757, templateSearchParam);
-        log.info("contentBlk : {} ", contentBlock.getRevisionId());
-
-    }*/
-
     private ActivityVersionDto getVersion2(Handle handle, StudyDto studyDto, RevisionMetadata meta, String activityCode) {
         long activityId = ActivityBuilder.findActivityId(handle, studyDto.getId(), activityCode);
         return activityDao.changeVersion(activityId, "v2", meta);
@@ -152,11 +114,11 @@ public class ATCPConsentVersion2 implements CustomTask {
 
     private void runConsentUpdate(Handle handle, RevisionMetadata meta, ActivityVersionDto version2) {
         updateTemplates(handle, meta, version2);
-        terminateTemplateVariables(meta, version2, dataCfg);
         updateTemplateVariables(meta, version2, dataCfg);
     }
 
     private void updateTemplates(Handle handle, RevisionMetadata meta, ActivityVersionDto version2) {
+        log.info("UPDATE Templates/Content Blocks");
         List<? extends Config> configList = dataCfg.getConfigList(BLOCK_UPDATES);
         for (Config config : configList) {
             revisionContentBlockTemplate(handle, meta, version2, config);
@@ -165,7 +127,7 @@ public class ATCPConsentVersion2 implements CustomTask {
 
     private void updateTemplateVariables(RevisionMetadata meta,
                                          ActivityVersionDto version2, Config dataCfg) {
-        log.info("UPDATE variables");
+        log.info("UPDATE Template variables");
         List<? extends Config> configList = dataCfg.getConfigList(VARIABLES_UPD);
         for (Config config : configList) {
             TemplateVariable templateVariable = gson.fromJson(ConfigUtil.toJson(config), TemplateVariable.class);
@@ -173,27 +135,14 @@ public class ATCPConsentVersion2 implements CustomTask {
         }
     }
 
-    private void terminateTemplateVariables(RevisionMetadata meta,
-                                      ActivityVersionDto version2, Config dataCfg) {
-        log.info("Deleting variables");
-        List<? extends Config> configList = dataCfg.getConfigList(VARIABLES_DEL);
-        for (Config config : configList) {
-            TemplateVariable templateVariable = gson.fromJson(ConfigUtil.toJson(config), TemplateVariable.class);
-            terminateTemplateVariable(templateVariable, meta, version2);
-        }
-    }
-
     private void revisionVariableTranslation(TemplateVariable templateVariable,
                                              RevisionMetadata meta, ActivityVersionDto version2) {
         log.info("revisioning and updating template variable: {}", templateVariable.getName());
         List<Long> templateVariableIdByVariableNames = sqlHelper.findTemplateVariableIdByVariableNames(templateVariable.getName());
-        //todo if 0/variable not found.. insert new one 3/24
         log.info("Tmpl variableId count: {} ", templateVariableIdByVariableNames.size());
         for (Long tmplVarId : templateVariableIdByVariableNames) {
-        //Long tmplVarId = templateVariableIdByVariableNames.get(0);
-            //todo.. use just first variableId and ignore rest !! to handle scenarios where study was reloaded and invalidated
             List<Translation> transList = jdbiVarSubst.fetchSubstitutionsForTemplateVariable(tmplVarId);
-            //all translations has same revision.. hence using first
+            //all translations have same revision.. hence using first
             long revId = jdbiRevision.copyAndTerminate(transList.get(0).getRevisionId().get(), meta);
             long[] revIds = new long[transList.size()];
             Arrays.fill(revIds, revId);
@@ -203,13 +152,11 @@ public class ATCPConsentVersion2 implements CustomTask {
                 String language = currTranslation.getLanguageCode();
                 String currentText = currTranslation.getText();
 
-                //todo .. iterate and update all languages
                 String newTxt = templateVariable.getTranslation(language).get().getText();
                 log.info("terminated: language: {} revId: {} new Text: {} currText: {}", language, currTranslation.getRevisionId(),
                         newTxt, currentText);
 
                 substitutionIds.add(currTranslation.getId().get());
-                //jdbiVarSubst.bulkUpdateRevisionIdsBySubIds(Arrays.asList(currTranslation.getId().get()), revIds);
                 jdbiVarSubst.insert(currTranslation.getLanguageCode(), templateVariable.getTranslation(language).get().getText(),
                         version2.getRevId(), tmplVarId);
             }
@@ -219,31 +166,6 @@ public class ATCPConsentVersion2 implements CustomTask {
             }
             log.info("updated ids: {}", ids);
             log.info("revisioned and updated template variable: {}", tmplVarId);
-        }
-    }
-
-    private void terminateTemplateVariable(TemplateVariable templateVariable,
-                                             RevisionMetadata meta, ActivityVersionDto version2) {
-        log.info("revisioning and terminating template variable: {}", templateVariable.getName());
-        List<Long> templateVariableIdByVariableNames = sqlHelper.findTemplateVariableIdByVariableNames(templateVariable.getName());
-        log.info("Tmpl variableId count: {} ", templateVariableIdByVariableNames.size());
-        for (Long tmplVarId : templateVariableIdByVariableNames) {
-            List<Translation> transList = jdbiVarSubst.fetchSubstitutionsForTemplateVariable(tmplVarId);
-            //all translations has same revision.. hence using first
-            long revId = jdbiRevision.copyAndTerminate(transList.get(0).getRevisionId().get(), meta);
-            long[] revIds = new long[transList.size()];
-            Arrays.fill(revIds, revId);
-            List<Long> substitutionIds = new ArrayList<>();
-            log.info("subs/translations count: {} for varId : {} ", transList.size(), tmplVarId);
-            for (var currTranslation : transList) {
-                substitutionIds.add(currTranslation.getId().get());
-            }
-            int[] ids = jdbiVarSubst.bulkUpdateRevisionIdsBySubIds(substitutionIds, revIds);
-            if (ids.length != revIds.length) {
-                throw new DDPException("returned ids length " + ids.length + "  doesnt match revIds passed length " + revIds.length);
-            }
-            log.info("terminated ids: {}", ids);
-            log.info("revisioned and terminated template variable: {}", tmplVarId);
         }
     }
 
@@ -276,7 +198,6 @@ public class ATCPConsentVersion2 implements CustomTask {
             templateDao.disableTemplate(contentBlock.getTitleTemplateId(), meta);
         }
         Long newBodyTemplateId = templateDao.insertTemplate(newBodyTemplate, versionDto.getRevId());
-
         Long newTitleTemplateId = null;
         if (newTitleTemplate != null) {
             newTitleTemplateId = templateDao.insertTemplate(newTitleTemplate, versionDto.getRevId());
@@ -290,14 +211,10 @@ public class ATCPConsentVersion2 implements CustomTask {
 
     private interface SqlHelper extends SqlObject {
 
-        //todo filter by studyId so that we query only latest study variables
+        //would be nice if we can filter by studyId so that we query only latest active study variables
         @SqlQuery("select template_variable_id from template_variable where variable_name = :variableName "
                 + "order by template_variable_id desc")
         List<Long> findTemplateVariableIdByVariableNames(@Bind("variableName") String variableName);
-
-        @SqlQuery("select template_id from template_variable where template_variable_id = :variableId "
-                + "order by template_id desc")
-        Long findTemplateIdByVariableId(@Bind("variableId") long variableId);
 
         /**
          * Find the content block that has the given body template text. Make sure it is from a block that belongs in the expected activity
@@ -320,7 +237,6 @@ public class ATCPConsentVersion2 implements CustomTask {
         @RegisterConstructorMapper(BlockContentDto.class)
         BlockContentDto findContentBlockByBodyText(@Bind("activityId") long activityId, @Bind("text") String bodyTemplateText);
 
-
         @SqlQuery("select bt.* from block_content as bt"
                 + "  join template as tmpl on tmpl.template_id = bt.body_template_id"
                 + " where tmpl.template_text like :text")
@@ -329,4 +245,3 @@ public class ATCPConsentVersion2 implements CustomTask {
     }
 
 }
-
