ad485f02b5b7c4f3988b44d2bc9d2059e37922a5
pegahtah <ptaheri@broadinstitute.org>
2023-04-05 12:54:30 -0400

    fixing the CreateLabelTool

--- pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/util/tools/CreateLabelTool.java
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/util/tools/CreateLabelTool.java
@@ -15,39 +15,37 @@ public class CreateLabelTool {
 
     public static void main(String[] args) {
         //select the method you want to run!
-                createEasyPostLabel();
+        //        createEasyPostLabel();
         //        getAddressFromID();
         //        verifyAddress();
     }
 
     public static void createEasyPostLabel() {
-        String apiKey = "MSEFjvz6YEdV3cpemY3gGg"; //in vault file!
-        String billingRef = null; // or set to null if you don't want a billing reference
-        String carrier = "FedEx"; //FedEx
-        String carrierID = "ca_15d3f957f78747e0b14bc21e4543d08e"; //in dsm db!
-        String service = "FEDEX_2_DAY"; //FEDEX_2_DAY
+        String apiKey = "API_KEY__EASYPOST"; //in vault file!
+        String billingRef = "BILLING_REF"; // or set to null if you don't want a billing reference
+        String carrier = "CARRIER_NAME"; //FedEx
+        String carrierID = "CARRIER_ID"; //in dsm db!
+        String service = "SERVICE_NAME"; //FEDEX_2_DAY
 
         DDPParticipant participant = new DDPParticipant();
-        participant.setFirstName("P");
-        participant.setLastName("T");
-        participant.setStreet1("1178 Stone Gate turn");
-        participant.setStreet2("");
-        participant.setPostalCode("19060-1840");
-        participant.setCity("Garnet Valley");
-        participant.setState("PA");
-        participant.setCountry("US");
+        participant.setFirstName("S");
+        participant.setLastName("M");
+        participant.setStreet1("415 Main St");
+        participant.setStreet2("Floor 7");
+        participant.setPostalCode("02142");
+        participant.setCity("Cambridge");
+        participant.setState("MA");
+        participant.setCountry("USA");
 
         try {
             EasyPostUtil easyPostUtil = new EasyPostUtil(null, apiKey);
-            Address toAddress = easyPostUtil.createAddress(participant, "413-404-9083");// pegah's email
-            Address returnAddress =
-                    easyPostUtil.createBroadAddress("Broad Institute", "320 Charles St - Lab 181", "Attn. Broad Genomics", "Cambridge",
-                            "02141", "MA", "US", "617-714-8952");
-            // inches and oz
-            Parcel parcel = easyPostUtil.createParcel("8", "2", "7.3", "9.5"); //stool kit sizes
+            Address toAddress = easyPostUtil.createAddress(participant, "617-714-8952");
+            Address returnAddress = easyPostUtil.createBroadAddress("Broad Institute", "320 Charles St - Lab 181", "Attn. Broad Genomics",
+                    "Cambridge", "02141", "MA", "US", "617-714-8952");
+            Parcel parcel = easyPostUtil.createParcel("3.2", "6.9", "1.3", "5.2");
             logger.info("Going to buy label");
-            Shipment shipment2Participant =
-                    easyPostUtil.buyShipment(carrier, carrierID, service, toAddress, returnAddress, parcel, billingRef, null);
+            Shipment shipment2Participant = easyPostUtil.buyShipment(carrier, carrierID, service,
+                    toAddress, returnAddress, parcel, billingRef, null);
             logger.info(shipment2Participant.getLabelUrl());
         } catch (EasyPostException e) {
             logger.error(e.toString());
@@ -67,7 +65,7 @@ public class CreateLabelTool {
     }
 
     public static void verifyAddress() {
-        String apiKey = "API_KEY_EASYPOST";
+        String apiKey = "API_KEY__EASYPOST";
 
         DDPParticipant participant = new DDPParticipant();
         participant.setFirstName("S");
