fac6a94fd2bc9686ae1f7fb80d9374444f047ec8
rfricke-asymmetrik <rfricke@asymmetrik.com>
2023-05-10 13:19:03 -0400

    added block updates to new generic task and updated name

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/SimpleActivityRevisionTask.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/SimpleActivityRevisionTask.java
@@ -17,14 +17,19 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.broadinstitute.ddp.db.dao.ActivityDao;
+import org.broadinstitute.ddp.db.dao.JdbiBlockContent;
 import org.broadinstitute.ddp.db.dao.JdbiRevision;
 import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dao.JdbiVariableSubstitution;
+import org.broadinstitute.ddp.db.dao.TemplateDao;
 import org.broadinstitute.ddp.db.dao.UserDao;
 import org.broadinstitute.ddp.db.dto.ActivityVersionDto;
+import org.broadinstitute.ddp.db.dto.BlockContentDto;
 import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.exception.DDPException;
+import org.broadinstitute.ddp.model.activity.definition.ContentBlockDef;
 import org.broadinstitute.ddp.model.activity.definition.i18n.Translation;
+import org.broadinstitute.ddp.model.activity.definition.template.Template;
 import org.broadinstitute.ddp.model.activity.definition.template.TemplateVariable;
 import org.broadinstitute.ddp.model.activity.revision.RevisionMetadata;
 import org.broadinstitute.ddp.model.user.User;
@@ -33,14 +38,18 @@ import org.broadinstitute.ddp.util.ConfigUtil;
 import org.broadinstitute.ddp.util.GsonUtil;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.sqlobject.SqlObject;
+import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
 @Slf4j
-public class SimpleTextRevisionTask implements CustomTask {
+public class SimpleActivityRevisionTask implements CustomTask {
     private static final String VARIABLES_UPD = "variables-update";
 
     private static final String STUDY_UPDATES = "activity-updates";
+    private static final String BLOCK_KEY = "blockNew";
+    private static final String BLOCK_UPDATES = "block-updates";
+    private static final String OLD_TEMPLATE_KEY = "old_template_search_text";
     private static final String VARIABLES_UPD_QS = "question-variables-update";
 
     private static final Gson gson = GsonUtil.standardGson();
@@ -55,9 +64,11 @@ public class SimpleTextRevisionTask implements CustomTask {
     private Config studyCfg;
 
     private ActivityDao activityDao;
-    private SimpleTextRevisionTask.SqlHelper sqlHelper;
+    private TemplateDao templateDao;
+    private SimpleActivityRevisionTask.SqlHelper sqlHelper;
     private JdbiVariableSubstitution jdbiVarSubst;
     private JdbiRevision jdbiRevision;
+    private JdbiBlockContent jdbiBlockContent;
 
     private StudyDto studyDto;
 
@@ -100,10 +111,12 @@ public class SimpleTextRevisionTask implements CustomTask {
 
         List<? extends Config> activityUpdateConfigs = dataCfg.getConfigList(STUDY_UPDATES);
 
-        this.sqlHelper = handle.attach(SimpleTextRevisionTask.SqlHelper.class);
+        this.sqlHelper = handle.attach(SimpleActivityRevisionTask.SqlHelper.class);
         this.activityDao = handle.attach(ActivityDao.class);
         this.jdbiVarSubst = handle.attach(JdbiVariableSubstitution.class);
         this.jdbiRevision = handle.attach(JdbiRevision.class);
+        this.templateDao = handle.attach(TemplateDao.class);
+        this.jdbiBlockContent = handle.attach(JdbiBlockContent.class);
 
         activitiesUpdate(handle, activityUpdateConfigs);
     }
@@ -127,23 +140,77 @@ public class SimpleTextRevisionTask implements CustomTask {
             RevisionMetadata metaConsent = new RevisionMetadata(ts, adminUser.getId(), reason);
 
             ActivityVersionDto activityVersion = getVersion(handle, metaConsent, activityCode, versionTag);
-            activityUpdate(metaConsent, activityVersion);
+            activityUpdate(metaConsent, activityVersion, activityConfig);
         }
     }
 
-    private void activityUpdate(RevisionMetadata meta, ActivityVersionDto version) {
-        updateTemplateVariables(meta, version, dataCfg); //revision only variables in the Template
-        updateQuestionTemplateVariables(meta, version, dataCfg); //revision only variables in the Question Template
+    private void activityUpdate(RevisionMetadata meta, ActivityVersionDto version, Config activityConfig) {
+        updateTemplates(meta, version, activityConfig);
+        updateTemplateVariables(meta, version, activityConfig); //revision only variables in the Template
+        updateQuestionTemplateVariables(meta, version, activityConfig); //revision only variables in the Question Template
+    }
+
+    private void updateTemplates(RevisionMetadata meta, ActivityVersionDto version, Config activityConfig) {
+        log.info("UPDATE Templates/Content Blocks");
+        List<? extends Config> configList = null;
+
+        try {
+            configList = activityConfig.getConfigList(BLOCK_UPDATES);
+        } catch (Exception e) {
+            log.info("No Template updates found for activity={}", version.getActivityId());
+            return;
+        }
+
+        for (Config config : configList) {
+            revisionContentBlockTemplate(meta, version, config);
+        }
+    }
+
+    private void revisionContentBlockTemplate(RevisionMetadata meta, ActivityVersionDto versionDto, Config conf) {
+        Config config = conf.getConfig(BLOCK_KEY);
+        ContentBlockDef contentBlockDef = gson.fromJson(ConfigUtil.toJson(config), ContentBlockDef.class);
+        Template newBodyTemplate = contentBlockDef.getBodyTemplate();
+        Template newTitleTemplate = contentBlockDef.getTitleTemplate();
+
+        String oldBlockTemplateText = conf.getString(OLD_TEMPLATE_KEY);
+
+        String templateSearchParam = String.format("%s%s%s", "%", oldBlockTemplateText, "%");
+        BlockContentDto contentBlock = this.sqlHelper
+                .findContentBlockByBodyText(versionDto.getActivityId(), templateSearchParam);
+
+        long newRevId = jdbiRevision.copyAndTerminate(contentBlock.getRevisionId(), meta);
+        int numUpdated = jdbiBlockContent.updateRevisionById(contentBlock.getId(), newRevId);
+        if (numUpdated != 1) {
+            throw new DDPException(String.format(
+                    "Unable to terminate active block_content with id=%d, blockId=%d, bodyTemplateId=%d",
+                    contentBlock.getId(), contentBlock.getBlockId(), contentBlock.getBodyTemplateId()));
+        }
+
+        templateDao.disableTemplate(contentBlock.getBodyTemplateId(), meta);
+        if (contentBlock.getTitleTemplateId() != null) {
+            templateDao.disableTemplate(contentBlock.getTitleTemplateId(), meta);
+        }
+        Long newBodyTemplateId = templateDao.insertTemplate(newBodyTemplate, versionDto.getRevId());
+        Long newTitleTemplateId = null;
+        if (newTitleTemplate != null) {
+            newTitleTemplateId = templateDao.insertTemplate(newTitleTemplate, versionDto.getRevId());
+        }
+        long newBlockContentId = jdbiBlockContent.insert(contentBlock.getBlockId(), newBodyTemplateId,
+                newTitleTemplateId, versionDto.getRevId());
+
+        log.info("Created block_content with id={}, blockId={}, bodyTemplateId={} for bodyTemplateText={}",
+                newBlockContentId, contentBlock.getBlockId(), newBodyTemplateId, contentBlockDef.getBodyTemplate().getTemplateText());
     }
 
     private void updateTemplateVariables(RevisionMetadata meta,
-                                         ActivityVersionDto version, Config dataCfg) {
+                                         ActivityVersionDto version, Config activityConfig) {
         log.info("UPDATE Template variables");
         List<? extends Config> configList = null;
 
         try {
-            configList = dataCfg.getConfigList(VARIABLES_UPD);
+            configList = activityConfig.getConfigList(VARIABLES_UPD);
         } catch (Exception e) {
+            log.info("No Template Variable updates found for activity={}", version.getActivityId());
             return;
         }
 
@@ -154,13 +221,14 @@ public class SimpleTextRevisionTask implements CustomTask {
     }
 
     private void updateQuestionTemplateVariables(RevisionMetadata meta,
-                                                 ActivityVersionDto version, Config dataCfg) {
+                                                 ActivityVersionDto version, Config activityConfig) {
         log.info("UPDATE QUESTION Template variables");
         List<? extends Config> configList = null;
 
         try {
-            configList = dataCfg.getConfigList(VARIABLES_UPD_QS);
+            configList = activityConfig.getConfigList(VARIABLES_UPD_QS);
         } catch (Exception e) {
+            log.info("No Question updates found for activity={}", version.getActivityId());
             return;
         }
 
@@ -168,7 +236,7 @@ public class SimpleTextRevisionTask implements CustomTask {
             TemplateVariable templateVariable = gson.fromJson(ConfigUtil.toJson(config), TemplateVariable.class);
 
             Long variableId = sqlHelper.findVariableIdByQuestion(templateVariable.getName(), config.getString("questionStableId"),
-                    dataCfg.getString("activityCode"), studyDto.getGuid());
+                    activityConfig.getString("activityCode"), studyDto.getGuid());
             revisionVariable(templateVariable, meta, version, variableId);
         }
 
@@ -213,11 +281,27 @@ public class SimpleTextRevisionTask implements CustomTask {
 
     private interface SqlHelper extends SqlObject {
 
-
-        /* Find the content block that has the given body template text. Make sure it is from a block that belongs in the expected activity
+        /**
+         * Find the content block that has the given body template text. Make sure it is from a block that belongs in the expected activity
          * (and thus the expected study). This is done using a `union` subquery to find all the top-level and nested block ids for the
          * activity and using that to match on the content block.
          */
+        @SqlQuery("select bt.* from block_content as bt"
+                + "  join template as tmpl on tmpl.template_id = bt.body_template_id"
+                + " where tmpl.template_text like :text"
+                + "   and bt.block_id in (select fsb.block_id"
+                + "                         from form_activity__form_section as fafs"
+                + "                         join form_section__block as fsb on fsb.form_section_id = fafs.form_section_id"
+                + "                        where fafs.form_activity_id = :activityId"
+                + "                        union"
+                + "                       select bn.nested_block_id"
+                + "                         from form_activity__form_section as fafs"
+                + "                         join form_section__block as fsb on fsb.form_section_id = fafs.form_section_id"
+                + "                         join block_nesting as bn on bn.parent_block_id = fsb.block_id"
+                + "                        where fafs.form_activity_id = :activityId)")
+        @RegisterConstructorMapper(BlockContentDto.class)
+        BlockContentDto findContentBlockByBodyText(@Bind("activityId") long activityId, @Bind("text") String bodyTemplateText);
+
         @SqlQuery("select tv.template_variable_id from block_content as bt"
                 + "  join template as tmpl on tmpl.template_id = bt.body_template_id"
                 + "  join template_variable tv on tv.template_id = tmpl.template_id "
