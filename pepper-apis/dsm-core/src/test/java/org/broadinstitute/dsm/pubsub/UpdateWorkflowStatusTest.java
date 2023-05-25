e4aa1c4775235c89d79269f6b5aac2b40602de04
Dennis <cunningh@broadinstitute.org>
2023-05-25 13:19:22 -0400

    Restructured UpdateWorkflowStatusTest for data independence among tests

--- pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/pubsub/UpdateWorkflowStatusTest.java
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/pubsub/UpdateWorkflowStatusTest.java
@@ -29,17 +29,14 @@ import org.junit.Test;
 public class UpdateWorkflowStatusTest extends DbTxnBaseTest {
 
     public static final String UPDATE_WORKFLOW_TEST = "UpdateWorkflowTest";
-    public static final String RGP = "RGP";
-    private static final Map<String, String> participantDataMap = new HashMap<>();
+    public static final String fieldTypeId = UPDATE_WORKFLOW_TEST;
+
     private static final DDPInstanceDao ddpInstanceDao = new DDPInstanceDao();
     private static final FieldSettingsDao fieldSettingsDao = FieldSettingsDao.of();
     private static final UserDao userDao = new UserDao();
     private static final ParticipantDataDao participantDataDao = new ParticipantDataDao();
     private static Gson gson;
     private static UserDto userDto;
-    private static String participantId;
-    private static int participantDataId;
-    private static ParticipantData participantData;
     private static DDPInstanceDto ddpInstanceDto;
     private static String instanceName;
     private static int fieldSettingsId;
@@ -52,15 +49,11 @@ public class UpdateWorkflowStatusTest extends DbTxnBaseTest {
         ddpInstanceDto = DBTestUtil.createTestDdpInstance(ddpInstanceDao, instanceName);
         userDto = DBTestUtil.createTestDsmUser("UpdateWorkflowUser", "UpdateWorkflow@status.com", userDao, userDto);
 
-        createParticipantData();
         createFieldSettings();
     }
 
     @AfterClass
     public static void tearDown() {
-        if (participantDataId > 0) {
-            participantDataDao.delete(participantDataId);
-        }
         if (fieldSettingsId > 0) {
             fieldSettingsDao.delete(fieldSettingsId);
         }
@@ -68,67 +61,93 @@ public class UpdateWorkflowStatusTest extends DbTxnBaseTest {
         ddpInstanceDao.delete(ddpInstanceDto.getDdpInstanceId());
     }
 
-    private static void createParticipantData() {
+    private static ParticipantData createParticipantData(String participantIdSeed) {
+        Map<String, String> participantDataMap = new HashMap<>();
         participantDataMap.put("REGISTRATION_STATUS", "REGISTERED");
         participantDataMap.put("MEMBER_TYPE", "SELF");
 
-        participantId = String.format("WorkflowUpdateStatusTest_%d", Instant.now().toEpochMilli());
-        participantData = new ParticipantData.Builder()
+        String participantId = genParticipantId(participantIdSeed);
+        ParticipantData participantData = new ParticipantData.Builder()
                 .withDdpParticipantId(participantId).withDdpInstanceId(ddpInstanceDto.getDdpInstanceId())
-                .withFieldTypeId(UPDATE_WORKFLOW_TEST).withData(gson.toJson(participantDataMap))
+                .withFieldTypeId(fieldTypeId).withData(gson.toJson(participantDataMap))
                 .withLastChanged(System.currentTimeMillis()).withChangedBy(userDto.getEmail().orElse("")).build();
 
-        participantDataId = participantDataDao.create(participantData);
-        participantData.setParticipantDataId(participantDataId);
+        participantData.setParticipantDataId(participantDataDao.create(participantData));
+        return participantData;
+    }
+
+    private static String genParticipantId(String seed) {
+        return String.format("WorkflowUpdateStatusTest_%s_%d", seed, Instant.now().toEpochMilli());
+    }
+
+    private static void deleteParticipantData(int participantDataId) {
+        if (participantDataId >= 0) {
+            participantDataDao.delete(participantDataId);
+        }
     }
 
     private static void createFieldSettings() {
         FieldSettingsDto fieldSettingsDto = new FieldSettingsDto.Builder(ddpInstanceDto.getDdpInstanceId())
-                .withFieldType(UPDATE_WORKFLOW_TEST)
+                .withFieldType(fieldTypeId)
                 .withColumnName(MEMBER_TYPE).build();
         fieldSettingsId = FieldSettingsDao.of().create(fieldSettingsDto);
     }
 
     @Test
     public void testUpdateCustomWorkflow() {
-        String messageData =
-                String.format("{\"participantGuid\":\"%s\",\"instanceName\":\"%s\","
+        ParticipantData participantData = createParticipantData("ucf");
+        int participantDataId = participantData.getParticipantDataId();
+        String participantId = participantData.getDdpParticipantId().orElseThrow();
+        try {
+            String messageData = String.format("{\"participantGuid\":\"%s\",\"instanceName\":\"%s\","
                         + "\"data\":{\"workflow\":\"%s\",\"status\":\"COMPLETED\"}}",
                         participantId, instanceName, MEMBER_TYPE);
-        log.info("TEMP {}", messageData);
-        JsonObject messageJsonObject = new Gson().fromJson(messageData, JsonObject.class);
-        String dataString = messageJsonObject.get("data").getAsJsonObject().toString();
-        Map<String, String> attributeMap = EditParticipantPublisherRoute.getStringStringMap("TEST", messageJsonObject);
-        WorkflowStatusUpdate.updateCustomWorkflow(attributeMap, dataString);
-        String data = participantDataDao.get(participantDataId).orElseThrow().getData().orElseThrow();
-        JsonObject dataJsonObject = gson.fromJson(data, JsonObject.class);
-        //checking that value was updated
-        Assert.assertEquals("COMPLETED", dataJsonObject.get("MEMBER_TYPE").getAsString());
-        //checking that updated value did not remove other fields
-        Assert.assertEquals("REGISTERED", dataJsonObject.get("REGISTRATION_STATUS").getAsString());
+
+            JsonObject messageJsonObject = new Gson().fromJson(messageData, JsonObject.class);
+            String dataString = messageJsonObject.get("data").getAsJsonObject().toString();
+            Map<String, String> attributeMap = EditParticipantPublisherRoute.getStringStringMap("TEST", messageJsonObject);
+            WorkflowStatusUpdate.updateCustomWorkflow(attributeMap, dataString);
+            String data = participantDataDao.get(participantDataId).orElseThrow().getData().orElseThrow();
+            JsonObject dataJsonObject = gson.fromJson(data, JsonObject.class);
+            //checking that value was updated
+            Assert.assertEquals("COMPLETED", dataJsonObject.get("MEMBER_TYPE").getAsString());
+            //checking that updated value did not remove other fields
+            Assert.assertEquals("REGISTERED", dataJsonObject.get("REGISTRATION_STATUS").getAsString());
+        } finally {
+            deleteParticipantData(participantDataId);
+        }
     }
 
     @Test
     public void testUpdateProbandStatusInDB() {
-        String workflow = "REGISTRATION_STATUS";
-        String status = "ENROLLED";
-        String fieldType = participantData.getFieldTypeId().orElseThrow();
-        WorkflowStatusUpdate.updateProbandStatusInDB(workflow, status, participantData, fieldType);
-        String data = participantDataDao.get(participantDataId).orElseThrow().getData().orElseThrow();
-        JsonObject dataJsonObject = gson.fromJson(data, JsonObject.class);
-        Assert.assertEquals(status, dataJsonObject.get(workflow).getAsString());
+        ParticipantData participantData = createParticipantData("ups");
+        int participantDataId = participantData.getParticipantDataId();
+        try {
+            String workflow = "REGISTRATION_STATUS";
+            String status = "ENROLLED";
+            String fieldType = participantData.getFieldTypeId().orElseThrow();
+            WorkflowStatusUpdate.updateProbandStatusInDB(workflow, status, participantData, fieldType);
+            String data = participantDataDao.get(participantDataId).orElseThrow().getData().orElseThrow();
+            JsonObject dataJsonObject = gson.fromJson(data, JsonObject.class);
+            Assert.assertEquals(status, dataJsonObject.get(workflow).getAsString());
+        } finally {
+            deleteParticipantData(participantDataId);
+        }
     }
 
     @Test
     public void testAddNewParticipantDataWithStatus() {
-        String workflow = "REGISTRATION_TYPE";
-        String status = "SELF2";
-        String fieldType = participantData.getFieldTypeId().orElseThrow();
-        int participantDataId = WorkflowStatusUpdate.addNewParticipantDataWithStatus(workflow, status, participantId,
-                ddpInstanceDto.getDdpInstanceId(), fieldType);
-        String data = participantDataDao.get(participantDataId).orElseThrow().getData().orElse("");
-        JsonObject dataJsonObject = gson.fromJson(data, JsonObject.class);
-        Assert.assertEquals(status, dataJsonObject.get(workflow).getAsString());
-        participantDataDao.delete(participantDataId);
+        int participantDataId = -1;
+        try {
+            String workflow = "REGISTRATION_TYPE";
+            String status = "SELF2";
+            participantDataId = WorkflowStatusUpdate.addNewParticipantDataWithStatus(workflow, status,
+                    genParticipantId("anp"), ddpInstanceDto.getDdpInstanceId(), fieldTypeId);
+            String data = participantDataDao.get(participantDataId).orElseThrow().getData().orElse("");
+            JsonObject dataJsonObject = gson.fromJson(data, JsonObject.class);
+            Assert.assertEquals(status, dataJsonObject.get(workflow).getAsString());
+        } finally {
+            deleteParticipantData(participantDataId);
+        }
     }
 }
