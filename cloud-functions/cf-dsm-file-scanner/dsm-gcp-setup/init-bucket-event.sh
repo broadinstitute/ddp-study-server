aa1f2d1b22b3aed99f16199770174340ef7398f4
pegahtah <47120870+pegahtah@users.noreply.github.com>
2023-06-22 10:12:12 -0400

    [DSM] PEPPER-883 (#2585)
    * PEPPER-883 changes for building and deploying
    
    * PEPPER-883 changes for building and deploying
    
    * PEPPER-883 changes for building and deploying
    
    * PEPPER-883 changes for building and deploying
    
    * PEPPER-883 changes for building and deploying
    
    * PEPPER-883
    Adding the --no-cpu-throttling option to make sure CPU is always allocated!
    Changing the clamav version to 1.1.0
    Setting the CLAMD_STARTUP_TIMEOUT to 7200
    
    * PEPPER-883
    Changing the create_topic_and_sub.sh to not create subscription in case the name is not given. This is used later to create push subscription and notifications for the trigger topic in dsm-setup.sh
    Adding the storage.objectAdmin to the service account creation
    Adding one more line of logs into DSMSomaticFileScanner
    
    * PEPPER-883
    Changed the pubsub message to a JSON
    Surrounding the move and delete operations with try-catch
    
    * PEPPER-883
    Final bucket name updated
    
    * PEPPER-883
    Removed study for creation, one service for all
    adding --region flag for service describe

--- cloud-functions/cf-dsm-file-scanner/dsm-gcp-setup/init-bucket-event.sh
+++ cloud-functions/cf-dsm-file-scanner/dsm-gcp-setup/init-bucket-event.sh
@@ -3,8 +3,6 @@
 # Currently, gcloud console does not support configuring notifications on
 # buckets, so have to use gsutil command-line.
 
-set -e
-
 if (( $# != 3 )); then
   echo 'usage: init-bucket-event.sh <GCP_PROJECT> <BUCKET> <TOPIC>'
   exit 1
@@ -14,10 +12,17 @@ PROJECT_ID="$1"
 BUCKET="$2"
 TOPIC="$3"
 
-gsutil notification create \
-  -f json \
-  -e OBJECT_FINALIZE \
-  -t "projects/$PROJECT_ID/topics/$TOPIC" \
-  "gs://$BUCKET"
+# Check if the notification already exists
+echo "Check if the notification already exists from $BUCKET to $TOPIC"
+
+existing_notification=" $(gsutil notification list gs://$BUCKET | grep -w "/$TOPIC")"
+
+if [[ "$existing_notification" = " " ]]; then
+  gsutil notification create -f json -e OBJECT_FINALIZE -t "projects/$PROJECT_ID/topics/$TOPIC" "gs://$BUCKET";
+  echo "Notification added successfully.";
+else
+  echo "Notification already exists.";
+fi
+
 
 gsutil notification list "gs://$BUCKET"
