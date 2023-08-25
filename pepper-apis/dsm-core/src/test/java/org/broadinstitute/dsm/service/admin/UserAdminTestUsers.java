919ce92aa3153d5f0fdecd7bd0df3f08e9be1a88
Dennis <cunningh@broadinstitute.org>
2023-08-25 11:10:19 -0400

    Add UserAdminTestUsers and adjust UserAdminServiceTest to use it

--- pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/service/admin/UserAdminTestUsers.java
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/service/admin/UserAdminTestUsers.java
@@ -1,6 +1,5 @@
 package org.broadinstitute.dsm.service.admin;
 
-import static org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace;
 import static org.broadinstitute.ddp.db.TransactionWrapper.inTransaction;
 
 import java.sql.PreparedStatement;
@@ -11,7 +10,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
 import org.broadinstitute.dsm.db.dao.user.UserDao;
@@ -21,18 +19,37 @@ import org.broadinstitute.lddp.db.SimpleResult;
 import org.junit.Assert;
 
 /**
- * Provides utility methods to help set up and tear down test users
+ * Provides utility methods to help set up and tear down test realms, test study roles, test users and their roles.
+ * This class keeps track of the entities it creates and will tear them down as requested and/or all at once
+ * when the close() method is called.
+ * If you create users and roles directly with the UserAdminService, you need to manage their lifecycle.
+ * <p></p>
+ * The basic usage is:
+ *  Setup infrastructure (typically in a @BeforeClass or @Before method), but only once per class instance
+ *      Create an instance of this class and call createRealmAndStudyGroup()
+ *      Call setStudyAdminAndRoles() to set up a study user admin and study roles (these include all user roles you plan to assign)
+ *      Note: This class expects actual DSM roles.
+ * <p></p>
+ *  Create users and roles (typically in test)
+ *      Via UserAdminService if you manage the user and role lifecycle
+ *      Or call createTestUser() and methods to add and remove user roles, which are managed by this class
+ * <p></p>
+ *  Delete users and roles (typically in a test or in an @After method)
+ *      Via UserAdminService if you manage the user and role lifecycle
+ *      Or all deleteTestUser() for users managed by this class
+ * <p></p>
+ *  Delete all entities created by this class (typically in an @After or @AfterClass method)
+ *      Call close()
  */
 @Slf4j
 public class UserAdminTestUsers {
     private final Map<Integer, List<Integer>> createdUserRoles = new HashMap<>();
     private final List<Integer> createdGroupRoles = new ArrayList<>();
-    private final String adminRole;
-    private final String studyGroup;
-    private int studyGroupId;
+    private int studyGroupId = -1;
     private int ddpInstanceId = -1;
     private int userAdminRoleId;
-    private Map<String, UserAdminService.RoleInfo> studyRoles;
+    private int userAdminId = -1;
+    private Map<String, Integer> allRoles;
 
     private boolean initialized = false;
 
@@ -49,40 +66,133 @@ public class UserAdminTestUsers {
             "DELETE FROM ddp_instance WHERE ddp_instance_id = ?";
 
 
-    public UserAdminTestUsers(String studyGroup, String adminRole) {
-        this.studyGroup = studyGroup;
-        this.adminRole = adminRole;
-    }
+    public UserAdminTestUsers() {}
 
+    /**
+     * Call this to teardown this class, typically in an @After or @AfterClass method
+     */
     public void close() {
-        UserDao userDao = new UserDao();
-        for (var entry: createdUserRoles.entrySet()) {
-            int userId = entry.getKey();
-            List<Integer> userRoles = entry.getValue();
-            for (int roleId: userRoles) {
-                UserAdminService.deleteUserRole(userId, roleId, studyGroupId);
-            }
-            userDao.delete(userId);
-        }
+        deleteStudyAdminAndRoles();
         deleteInstance(ddpInstanceId);
         UserAdminService.deleteStudyGroup(studyGroupId);
     }
 
     private void initialize() {
         if (!initialized) {
-            studyGroupId = UserAdminService.verifyStudyGroup(studyGroup);
-            studyRoles = UserAdminService.getAllRolesForStudy(studyGroupId);
-            userAdminRoleId = UserAdminService.getRoleId(adminRole);
+            allRoles = getAllRoles();
             initialized = true;
         }
     }
 
-    public int createTestUser(String email, List<String> roles) {
+    private void assertInitialized() {
+        if (!initialized) {
+            throw new DsmInternalError("Test users not initialized: call createRealmAndStudyGroup");
+        }
+    }
+
+    /**
+     * Initialize class and create a test DDP realm/instance and study group
+     *
+     * @param realmName name of study realm that is not already in use
+     * @param studyGroup name of study group that is not already in use to associate with realm
+     */
+    public void createRealmAndStudyGroup(String realmName, String studyGroup) {
+        if (ddpInstanceId != -1 || studyGroupId != -1) {
+            throw new DsmInternalError("Realm and study group already initialized");
+        }
         initialize();
+        studyGroupId = UserAdminService.addStudyGroup(studyGroup);
+        ddpInstanceId = createTestInstance(realmName, studyGroupId);
+    }
+
+    /**
+     * Setup study admin and study roles
+     *
+     * @param adminEmail for new admin user account
+     * @param adminRole PEPPER_ADMIN or STUDY_USER_ADMIN
+     * @param rolesToManage actual DSM roles
+     * @return study admin user ID
+     */
+    public int setStudyAdminAndRoles(String adminEmail, String adminRole, List<String> rolesToManage) {
+        assertInitialized();
+        if (userAdminId != -1) {
+            throw new DsmInternalError("setStudyAdminAndRoles already initialized");
+        }
+        userAdminRoleId = UserAdminService.getRoleId(adminRole);
+        userAdminId = createUser(adminEmail);
+        UserAdminService.addUserRole(userAdminId, userAdminRoleId, studyGroupId);
+
+        Map<String, UserAdminService.RoleInfo> adminRoles = UserAdminService.getOperatorAdminRoles(userAdminId, studyGroupId);
+        Assert.assertTrue("adminRoles = " + adminRoles.keySet(), adminRoles.isEmpty());
+
+        if (rolesToManage != null) {
+            addStudyRoles(rolesToManage);
+        }
+        return userAdminId;
+    }
+
+    /**
+     * Add roles for study
+     * @param roles actual DSM roles
+     */
+    public void addStudyRoles(List<String> roles) {
+        assertInitialized();
+        for (String role: roles) {
+            addGroupRole(getRoleId(role), userAdminRoleId);
+        }
+    }
+
+    private int addGroupRole(int roleId, int adminRoleId) {
+        int groupRoleId = UserAdminService.addGroupRole(studyGroupId, roleId, adminRoleId);
+        createdGroupRoles.add(groupRoleId);
+        return groupRoleId;
+    }
+
+    /**
+     * Remove roles for study
+     * @param roles existing study roles
+     */
+    public void removeStudyRoles(List<String> roles) {
+        assertInitialized();
+        for (String role: roles) {
+            removeGroupRole(getRoleId(role));
+        }
+    }
+
+    private void removeGroupRole(int groupRoleId) {
+        UserAdminService.deleteGroupRole(groupRoleId);
+        createdGroupRoles.remove(groupRoleId);
+    }
+
+    /**
+     * Delete all study users, study admin and study roles (typically called from an @After or @AfterClass method)
+     */
+    public void deleteStudyAdminAndRoles() {
+        assertInitialized();
+        deleteAllTestUsers();
+
+        UserAdminService.deleteUserRole(userAdminId, userAdminRoleId, studyGroupId);
+        new UserDao().delete(userAdminId);
+        userAdminId = -1;
+
+        for (int groupRoleId: createdGroupRoles) {
+            UserAdminService.deleteGroupRole(groupRoleId);
+        }
+    }
+
+    /**
+     * Create a test user
+     * @param roles list of actual DSM roles for user
+     * @return user ID
+     */
+    public int createTestUser(String email, List<String> roles) {
+        assertInitialized();
         int userId = createUser(email);
         List<Integer> roleIds = new ArrayList<>();
         for (String role: roles) {
-            roleIds.add(getRoleId(role));
+            int roleId = getRoleId(role);
+            UserAdminService.addUserRole(userId, roleId, studyGroupId);
+            roleIds.add(roleId);
         }
         createdUserRoles.put(userId, roleIds);
         return userId;
@@ -98,138 +208,109 @@ public class UserAdminTestUsers {
         return userDao.create(userDto);
     }
 
-    public int addUserRole(int userId, String role) {
-        initialize();
-        int roleId = getRoleId(role);
-        int userRoleId = UserAdminService.addUserRole(userId, roleId, studyGroupId);
-        addRoleForUser(roleId, userId);
-        return userRoleId;
+    /**
+     * Delete test user and their roles
+     */
+    public void deleteTestUser(int userId) {
+        assertInitialized();
+        _deleteTestUser(userId, new UserDao());
+        createdUserRoles.remove(userId);
     }
 
-    private int getRoleId(String roleName) {
-        UserAdminService.RoleInfo roleInfo = studyRoles.get(roleName);
-        if (roleInfo == null) {
-            throw new DsmInternalError("Invalid role name for study");
-        }
-        return roleInfo.getRoleId();
-    }
-
-    private void addRoleForUser(int roleId, int userId) {
+    private void _deleteTestUser(int userId, UserDao userDao) {
         List<Integer> roleIds = createdUserRoles.get(userId);
+        if (roleIds == null) {
+            throw new DsmInternalError("Invalid user: " + userId);
+        }
+        for (int roleId: roleIds) {
+            UserAdminService.deleteUserRole(userId, roleId, studyGroupId);
+        }
+        userDao.delete(userId);
+    }
+
+    public void deleteAllTestUsers() {
+        assertInitialized();
+        UserDao userDao = new UserDao();
+        for (var entry: createdUserRoles.entrySet()) {
+            _deleteTestUser(entry.getKey(), userDao);
+        }
+        createdUserRoles.clear();
+    }
+
+    /**
+     * Add a roles for user
+     * @param roles actual DSM roles
+     */
+    public void addUserRoles(int userId, List<String> roles) {
+        assertInitialized();
+        for (String role : roles) {
+            int roleId = getRoleId(role);
+            UserAdminService.addUserRole(userId, roleId, studyGroupId);
+            addRoleForUser(roleId, userId);
+        }
+    }
+
+    protected void addRoleForUser(int roleId, int userId) {
+        List<Integer> roleIds = createdUserRoles.get(userId);
+        if (roleIds == null) {
+            throw new DsmInternalError("Invalid user: " + userId);
+        }
         List<Integer> newRoleIds = new ArrayList<>(roleIds);
         newRoleIds.add(roleId);
         createdUserRoles.put(userId, newRoleIds);
     }
 
-    private void removeRoleForUser(int roleId, int userId) {
+    protected void addRoleForUser(String role, int userId) {
+        addRoleForUser(getRoleId(role), userId);
+    }
+
+    /**
+     * Remove role for user
+     * @param role existing user role
+     */
+    public void removeUserRole(int userId, String role) {
+        assertInitialized();
+        int roleId = getRoleId(role);
+        UserAdminService.deleteUserRole(userId, roleId, studyGroupId);
+        removeRoleForUser(roleId, userId);
+    }
+
+    protected void removeRoleForUser(int roleId, int userId) {
         List<Integer> roleIds = createdUserRoles.get(userId);
+        if (roleIds == null) {
+            throw new DsmInternalError("Invalid user: " + userId);
+        }
         List<Integer> newRoleIds = new ArrayList<>(roleIds);
         newRoleIds.removeAll(List.of(roleId));
         createdUserRoles.put(userId, newRoleIds);
     }
 
-    private int addGroupRole(int roleId, int adminRoleId) {
-        int groupRoleId = UserAdminService.addGroupRole(studyGroupId, roleId, adminRoleId);
-        createdGroupRoles.add(groupRoleId);
-        return groupRoleId;
+    protected void removeRoleForUser(String role, int userId) {
+        removeRoleForUser(getRoleId(role), userId);
     }
 
-    private void removeGroupRole(int groupRoleId) {
-        UserAdminService.deleteGroupRole(groupRoleId);
-        createdGroupRoles.remove(groupRoleId);
+    public void verifyUserHasRole(int userId, String role) {
+        assertInitialized();
+        int userRoleId = UserAdminService.getUserRole(userId, getRoleId(role), studyGroupId);
+        Assert.assertNotEquals(-1, userRoleId);
     }
 
-    private int createAdminUser(String email, int adminRoleId) {
-        int userId = createTestUser(email, adminRoleId);
-        int groupId = UserAdminService.verifyStudyGroup(studyGroup);
-        try {
-            addUserRole(userId, adminRoleId, groupId);
-            log.info("Created admin user {} wih id {} and adminRoleId={}", email, userId, adminRoleId);
-        } catch (Exception e) {
-            Assert.fail("Exception from UserAdminService.addUserRole: " +  getStackTrace(e));
+    public int getRoleId(String roleName) {
+        assertInitialized();
+        Integer roleId = allRoles.get(roleName);
+        if (roleId == null) {
+            throw new DsmInternalError("Invalid role name: " + roleName);
         }
-        return userId;
+        return roleId;
     }
 
-    private int setupAdmin(String adminEmail, List<String> rolesToManage) {
-        int operatorId = createAdminUser(adminEmail, userAdminRoleId);
-        try {
-            Map<String, UserAdminService.RoleInfo> adminRoles = UserAdminService.getOperatorAdminRoles(operatorId, studyGroupId);
-            Assert.assertTrue("adminRoles = " + adminRoles.keySet(), adminRoles.isEmpty());
-        } catch (Exception e) {
-            Assert.fail("Exception from UserAdminService.verifyOperatorForGroup: " +  getStackTrace(e));
-        }
-
-        if (rolesToManage != null) {
-            for (String role: rolesToManage) {
-                addGroupRole(getRoleId(role), userAdminRoleId);
-            }
-        }
-        return operatorId;
-    }
-
-    private Map<String, Integer> setupUsers(List<String> users, List<String> roles) {
-
-        Map<String, Integer> userToId = new HashMap<>();
-        for (String user: users) {
-            int userId = createTestUser(user, roles);
-            userToId.put(user, userId);
-        }
-        return userToId;
-    }
-
-
-    private Map<String, Integer> getRoleIds(List<String> roles) {
-
-        Map<String, Integer> roleToId = new HashMap<>();
-        for (String role: roles) {
-            int roleId = UserAdminService.getRoleId(role);
-            Assert.assertTrue(roleId > 0);
-            roleToId.put(role, roleId);
-        }
-        return roleToId;
-    }
-
-    private void addAdminRoles(int adminUserId, int groupId, List<String> roles) {
-        try {
-            for (String role : roles) {
-                int roleId = UserAdminService.getRoleId(role);
-                UserAdminService.addUserRole(adminUserId, roleId, groupId);
-            }
-        } catch (Exception e) {
-            Assert.fail("Exception from UserAdminService.addUserRole: " +  getStackTrace(e));
-        }
-    }
-
-    private void setUserRoles(int userId, List<String> roles, int groupId) {
-        try {
-            for (String role : roles) {
-                int roleId = UserAdminService.getRoleId(role);
-                UserAdminService.addUserRole(userId, roleId, groupId);
-            }
-        } catch (Exception e) {
-            Assert.fail("Exception from UserAdminService.addUserRole: " +  getStackTrace(e));
-        }
-    }
-
-    private static Map<String, Map<String, Boolean>> getUserRoles(List<UserInfo> userInfoList) {
-        Map<String, Map<String, Boolean>> userRoles = new HashMap<>();
-        for (UserInfo userInfo: userInfoList) {
-            Map<String, Boolean> roles = userInfo.getRoles().stream()
-                    .collect(Collectors.toMap(UserRole::getName, UserRole::isHasRole));
-            userRoles.put(userInfo.getEmail(), roles);
-        }
-        return userRoles;
-    }
-
-    private static List<String> getAllRoles() {
+    protected static Map<String, Integer> getAllRoles() {
         return inTransaction(conn -> {
-            List<String> roles = new ArrayList<>();
+            Map<String, Integer> roles = new HashMap<>();
             try (PreparedStatement stmt = conn.prepareStatement("SELECT ar.role_id, ar.name FROM access_role ar")) {
                 try (ResultSet rs = stmt.executeQuery()) {
                     while (rs.next()) {
-                        // TODO: temp until we have display text
-                        roles.add(rs.getString(2));
+                        roles.put(rs.getString(2), rs.getInt(1));
                     }
                 }
             } catch (SQLException e) {
@@ -239,15 +320,12 @@ public class UserAdminTestUsers {
         });
     }
 
-    private int createTestInstance(String instanceName) {
-        if (ddpInstanceId != -1) {
-            throw new DsmInternalError("Test instance already created");
-        }
-        ddpInstanceId = createInstance(instanceName);
+    private static int createTestInstance(String instanceName, int studyGroupId) {
+        int instanceId = createInstance(instanceName);
         SimpleResult res = inTransaction(conn -> {
             SimpleResult dbVals = new SimpleResult();
             try (PreparedStatement stmt = conn.prepareStatement(SQL_INSERT_DDP_INSTANCE_GROUP, Statement.RETURN_GENERATED_KEYS)) {
-                stmt.setInt(1, ddpInstanceId);
+                stmt.setInt(1, instanceId);
                 stmt.setInt(2, studyGroupId);
                 int result = stmt.executeUpdate();
                 if (result != 1) {
@@ -265,9 +343,10 @@ public class UserAdminTestUsers {
         });
 
         if (res.resultException != null) {
+            _deleteInstance(instanceId);
             throw new DsmInternalError("Error adding DDP instance group " + instanceName, res.resultException);
         }
-        return ddpInstanceId;
+        return instanceId;
     }
 
     private static int createInstance(String instanceName) {
@@ -307,6 +386,10 @@ public class UserAdminTestUsers {
             }
         });
 
+        _deleteInstance(instanceId);
+    }
+
+    private static void _deleteInstance(int instanceId) {
         inTransaction(conn -> {
             try (PreparedStatement stmt = conn.prepareStatement(SQL_DELETE_DDP_INSTANCE)) {
                 stmt.setInt(1, instanceId);
@@ -317,4 +400,5 @@ public class UserAdminTestUsers {
             }
         });
     }
+
 }
