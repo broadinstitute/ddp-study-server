91662643d370c416e8afa5713c33bb241ef59caf
Andrew Zimmer <andrew@broadinstitute.org>
2023-09-05 12:41:04 -0400

    PEPPER-1071 updating to take advantage of newly updated admin test utilities.  And snuck in PR template as we discussed at a meeting earlier in the day.

--- pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/AuthenticationTest.java
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/AuthenticationTest.java
@@ -1,14 +1,8 @@
 package org.broadinstitute.dsm;
 
-
-import org.broadinstitute.dsm.db.dao.user.UserDao;
-import org.broadinstitute.dsm.db.dto.ddp.instance.DDPInstanceDto;
-import org.broadinstitute.dsm.db.dto.user.UserDto;
 import org.broadinstitute.dsm.model.NameValue;
 import org.broadinstitute.dsm.model.patch.Patch;
-import org.broadinstitute.dsm.service.admin.SetUserRoleRequest;
-import org.broadinstitute.dsm.service.admin.UserAdminService;
-import org.broadinstitute.dsm.service.admin.UserAdminServiceTest;
+import org.broadinstitute.dsm.service.admin.UserAdminTestUtil;
 import org.broadinstitute.dsm.statics.DBConstants;
 import org.broadinstitute.dsm.util.UserUtil;
 import org.junit.AfterClass;
@@ -17,8 +11,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collections;
 
 import static org.broadinstitute.dsm.service.admin.UserAdminService.USER_ADMIN_ROLE;
 import static org.broadinstitute.dsm.statics.DBConstants.KIT_SHIPPING;
@@ -26,25 +19,16 @@ import static org.broadinstitute.dsm.statics.DBConstants.PT_LIST_VIEW;
 
 public class AuthenticationTest extends DbTxnBaseTest {
 
-    private static UserDto dsmAdminUser;
+    private static String dsmAdminUserId;
 
-    private static UserDto cmiKitShippingOnlyUser;
+    private static String cmiKitShippingOnlyUserId;
 
-    private static DDPInstanceDto ddpInstance;
+    private static final UserAdminTestUtil cmiAdminUtil = new UserAdminTestUtil();
 
-    private static int pecgsStudyGroupId;
+    private static String studyInstanceName;
 
-    private static int cmiStudyGroupId;
-
-    private static final UserDao userDao = new UserDao();
-
-    private static final Set<Integer> groupRoleIdsToDelete = new HashSet<>();
-
-    private static UserDto generateUser() {
-        UserDto user = new UserDto("AuthTest." + System.currentTimeMillis(), "fake+" + System.currentTimeMillis() + "@broad.dev", "5555551212");
-        int userId = userDao.create(user);
-        user.setId(userId);
-        return user;
+    private static String generateUserEmail() {
+        return "AuthTest-" + System.currentTimeMillis() + "@broad.dev";
     }
 
     /**
@@ -53,57 +37,31 @@ public class AuthenticationTest extends DbTxnBaseTest {
      */
     @BeforeClass
     public static void setup() {
-        dsmAdminUser = generateUser();
-        cmiKitShippingOnlyUser = generateUser();
-
         String nameAppend = "." + System.currentTimeMillis();
-        String pecgsStudyGroup = "pecgs" + nameAppend;
         String studyInstanceName = "instance." + nameAppend;
-        pecgsStudyGroupId = UserAdminService.addStudyGroup(pecgsStudyGroup);
-
         String cmiStudyGroup = "cmi" + nameAppend;
-        cmiStudyGroupId = UserAdminService.addStudyGroup(cmiStudyGroup);
 
-        int ddpInstanceId = UserAdminServiceTest.createTestInstance(studyInstanceName, cmiStudyGroupId);
-        ddpInstance = new DDPInstanceDto.Builder().withDdpInstanceId(ddpInstanceId).withInstanceName(studyInstanceName).build();
+        cmiAdminUtil.createRealmAndStudyGroup(studyInstanceName, cmiStudyGroup);
+        cmiAdminUtil.setStudyAdminAndRoles(generateUserEmail(), USER_ADMIN_ROLE, Arrays.asList(KIT_SHIPPING, PT_LIST_VIEW));
 
-        UserAdminService adminServiceForDsmAdminUser = new UserAdminService(dsmAdminUser.getIdAsString(), pecgsStudyGroup);
-        UserAdminService adminServiceForKitShippingUser = new UserAdminService(dsmAdminUser.getIdAsString(), cmiStudyGroup);
-
-        int adminRoleId = UserAdminService.getRoleId(USER_ADMIN_ROLE);
-        int kitShippingRoleId = UserAdminService.getRoleId(KIT_SHIPPING);
-        int participantListRoleId = UserAdminService.getRoleId(PT_LIST_VIEW);
-
-        groupRoleIdsToDelete.add(UserAdminService.addGroupRole(cmiStudyGroupId, kitShippingRoleId, adminRoleId));
-        groupRoleIdsToDelete.add(UserAdminService.addGroupRole(cmiStudyGroupId, participantListRoleId, adminRoleId));
-        groupRoleIdsToDelete.add(UserAdminService.addGroupRole(pecgsStudyGroupId, kitShippingRoleId, adminRoleId));
-        groupRoleIdsToDelete.add(UserAdminService.addGroupRole(pecgsStudyGroupId, participantListRoleId, adminRoleId));
-
-        adminServiceForDsmAdminUser.setGroupAdminRole(dsmAdminUser.getId(), pecgsStudyGroupId);
-        adminServiceForKitShippingUser.setGroupAdminRole(dsmAdminUser.getId(), cmiStudyGroupId);
-
-        adminServiceForKitShippingUser.setUserRoles(new SetUserRoleRequest(dsmAdminUser.getEmailOrThrow(), Arrays.asList(KIT_SHIPPING, PT_LIST_VIEW)));
-        adminServiceForKitShippingUser.setUserRoles(new SetUserRoleRequest(cmiKitShippingOnlyUser.getEmailOrThrow(), KIT_SHIPPING));
+        dsmAdminUserId = Integer.toString(cmiAdminUtil.createTestUser(generateUserEmail(), Arrays.asList(KIT_SHIPPING, PT_LIST_VIEW)));
+        cmiKitShippingOnlyUserId = Integer.toString(cmiAdminUtil.createTestUser(generateUserEmail(), Collections.singletonList(KIT_SHIPPING)));
     }
 
     @Test
     public void testKitShipperRoleCanChangeKitInfo() {
-        String realm = ddpInstance.getInstanceName(); // should be a ddp instance in the pe-cgs group
-
-        Patch patch = new Patch("0",  "dsmKitRequestId", "0", cmiKitShippingOnlyUser.getIdAsString() , new NameValue("kit.collectionDate",  "2023-04-24"), null, "XSZSRS1MS3D4OAEK2DPM") ;
+        Patch patch = new Patch("0",  "dsmKitRequestId", "0", cmiKitShippingOnlyUserId , new NameValue("kit.collectionDate",  "2023-04-24"), null, "XSZSRS1MS3D4OAEK2DPM") ;
         patch.setTableAlias("kit");
 
-        Assert.assertTrue(UserUtil.checkKitShippingAccessForPatch(realm, cmiKitShippingOnlyUser.getIdAsString(), null, patch));
+        Assert.assertTrue(UserUtil.checkKitShippingAccessForPatch(studyInstanceName, cmiKitShippingOnlyUserId, null, patch));
     }
 
     @Test
     public void getKitShipperRoleCannotChangeParticipantInfo(){
-        String realm = ddpInstance.getInstanceName();
-
-        Patch patch = new Patch("0", "participantId", "0", cmiKitShippingOnlyUser.getIdAsString() , new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
+        Patch patch = new Patch("0", "participantId", "0", cmiKitShippingOnlyUserId, new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
         patch.setTableAlias("oD");
 
-        Assert.assertFalse(UserUtil.checkKitShippingAccessForPatch(realm, cmiKitShippingOnlyUser.getIdAsString(), null, patch));
+        Assert.assertFalse(UserUtil.checkKitShippingAccessForPatch(studyInstanceName, cmiKitShippingOnlyUserId, null, patch));
     }
 
     /**
@@ -113,35 +71,33 @@ public class AuthenticationTest extends DbTxnBaseTest {
      */
     @Test
     public void mismatchAccessTest(){
-        String realm = ddpInstance.getInstanceName();
-
-        Patch patch1 = new Patch("0", "participantId", "0", dsmAdminUser.getIdAsString() , new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
+        Patch patch1 = new Patch("0", "participantId", "0", dsmAdminUserId , new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
         patch1.setTableAlias("oD");
 
         try {
-            UserUtil.checkUserAccessForPatch(realm, cmiKitShippingOnlyUser.getIdAsString(), PT_LIST_VIEW, null, patch1);
+            UserUtil.checkUserAccessForPatch(studyInstanceName, cmiKitShippingOnlyUserId, PT_LIST_VIEW, null, patch1);
             Assert.fail("Did not throw expected exception");
         } catch (Exception e) {
             Assert.assertTrue(e.getMessage().contains("User id in patch did not match the one in token"));
         }
         try {
-            UserUtil.checkUserAccessForPatch(realm, cmiKitShippingOnlyUser.getIdAsString(), DBConstants.MR_VIEW, null, patch1);
+            UserUtil.checkUserAccessForPatch(studyInstanceName, cmiKitShippingOnlyUserId, DBConstants.MR_VIEW, null, patch1);
             Assert.fail("Did not throw expected exception");
         } catch (Exception e) {
             Assert.assertTrue(e.getMessage().contains("User id in patch did not match the one in token"));
         }
         try {
-            UserUtil.checkUserAccessForPatch(realm, cmiKitShippingOnlyUser.getIdAsString(), DBConstants.MR_ABSTRACTER, null, patch1);
+            UserUtil.checkUserAccessForPatch(studyInstanceName, cmiKitShippingOnlyUserId, DBConstants.MR_ABSTRACTER, null, patch1);
             Assert.fail("Did not throw expected exception");
         } catch (Exception e) {
             Assert.assertTrue(e.getMessage().contains("User id in patch did not match the one in token"));
         }
 
-        Patch gpPatch = new Patch("0",  "dsmKitRequestId", "0", cmiKitShippingOnlyUser.getIdAsString() , new NameValue("kit.collectionDate",  "2023-04-24"), null, "XSZSRS1MS3D4OAEK2DPM") ;
+        Patch gpPatch = new Patch("0",  "dsmKitRequestId", "0", cmiKitShippingOnlyUserId, new NameValue("kit.collectionDate",  "2023-04-24"), null, "XSZSRS1MS3D4OAEK2DPM") ;
         gpPatch.setTableAlias("kit");
 
         try {
-            UserUtil.checkKitShippingAccessForPatch(realm, dsmAdminUser.getIdAsString(), null, gpPatch);
+            UserUtil.checkKitShippingAccessForPatch(studyInstanceName, dsmAdminUserId, null, gpPatch);
             Assert.fail("Did not throw expected exception");
         } catch (Exception e) {
             Assert.assertTrue(e.getMessage().contains("User id in patch did not match the one in token"));
@@ -155,31 +111,19 @@ public class AuthenticationTest extends DbTxnBaseTest {
      */
     @Test
     public void userPatchAccessTest(){
-        String realm = ddpInstance.getInstanceName();
-
-        Patch patch = new Patch("0", "participantId", "0", dsmAdminUser.getIdAsString() , new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
+        Patch patch = new Patch("0", "participantId", "0", dsmAdminUserId, new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
         patch.setTableAlias("oD");
 
-        Assert.assertTrue(UserUtil.checkUserAccessForPatch(realm, dsmAdminUser.getIdAsString(), PT_LIST_VIEW, null, patch));
+        Assert.assertTrue(UserUtil.checkUserAccessForPatch(studyInstanceName, dsmAdminUserId, PT_LIST_VIEW, null, patch));
 
-        Patch patch2 = new Patch("0", "participantId", "0", dsmAdminUser.getIdAsString() , new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
+        Patch patch2 = new Patch("0", "participantId", "0", dsmAdminUserId, new NameValue("oD.locationPx",  "location"), null, "XSZSRS1MS3D4OAEK2DPM") ;
         patch.setTableAlias("m");
 
-        Assert.assertTrue(UserUtil.checkUserAccessForPatch(realm, dsmAdminUser.getIdAsString(), PT_LIST_VIEW, null, patch2));
+        Assert.assertTrue(UserUtil.checkUserAccessForPatch(studyInstanceName, dsmAdminUserId, PT_LIST_VIEW, null, patch2));
     }
 
     @AfterClass
     public static void teardown() {
-        UserAdminService.deleteUserRoles(dsmAdminUser.getId());
-        UserAdminService.deleteUserRoles(cmiKitShippingOnlyUser.getId());
-        for (Integer groupRoleId : groupRoleIdsToDelete) {
-            UserAdminService.deleteGroupRole(groupRoleId);
-        }
-        UserAdminServiceTest.deleteInstance(ddpInstance.getDdpInstanceId());
-        UserAdminService.deleteStudyGroup(pecgsStudyGroupId);
-        UserAdminService.deleteStudyGroup(cmiStudyGroupId);
-
-        userDao.delete(dsmAdminUser.getId());
-        userDao.delete(cmiKitShippingOnlyUser.getId());
+        cmiAdminUtil.deleteGeneratedData();
     }
 }
