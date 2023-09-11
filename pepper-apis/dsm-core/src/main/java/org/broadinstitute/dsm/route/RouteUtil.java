24ff770c8284ca8ce39aeabbbe6b67d1d9306a58
Dennis <cunningh@broadinstitute.org>
2023-09-11 15:30:25 -0400

    WIP

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/route/RouteUtil.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/route/RouteUtil.java
@@ -1,7 +1,9 @@
 package org.broadinstitute.dsm.route;
 
+import com.google.gson.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.dsm.exception.DSMBadRequestException;
+import org.broadinstitute.dsm.exception.DsmInternalError;
 import org.broadinstitute.dsm.statics.RoutePath;
 import spark.QueryParamsMap;
 import spark.Request;
@@ -16,13 +18,33 @@ public class RouteUtil {
         if (!queryParams.hasKey(RoutePath.REALM)) {
             throw new DSMBadRequestException("Request must include realm parameter");
         }
-        return requireRealm(queryParams.value(RoutePath.REALM));
+        return requireParam(RoutePath.REALM, queryParams.value(RoutePath.REALM));
     }
 
-    public static String requireRealm(String realm) {
-        if (StringUtils.isEmpty(realm)) {
-            throw new DSMBadRequestException("Invalid realm parameter: blank");
+    public static String requireParam(String paramName, String paramValue) {
+        if (StringUtils.isEmpty(paramValue)) {
+            throw new DSMBadRequestException("Missing request parameter: " + paramName);
         }
-        return realm;
+        return paramValue;
+    }
+
+    public static String requireRequestBody(Request request) {
+        String payload = request.body();
+        if (StringUtils.isBlank(payload)) {
+            throw new DSMBadRequestException("Request must include a body/payload");
+        }
+        return payload;
+    }
+
+    public static String requireStringFromJsonObject(JsonObject jsonObject, String field) {
+        String val = jsonObject.get(field).getAsString();
+        if (StringUtils.isBlank(val)) {
+            throw new DSMBadRequestException(String.format("Request body must include %s field", field));
+        }
+        return val;
+    }
+
+    public static void handleInvalidRouteMethod(Request request, String routeName) {
+        throw new DsmInternalError(String.format("Invalid HTTP method for %s: %s", routeName, request.requestMethod()));
     }
 }
