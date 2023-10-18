c0cd94baebd2116b0f656436e35c5a4c9630c20c
Andrew Zimmer <andrew@broadinstitute.org>
2023-10-18 16:33:59 -0400

    PEPPER-1182 PR feedback

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/route/ListCancersRoute.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/route/ListCancersRoute.java
@@ -3,7 +3,9 @@ package org.broadinstitute.ddp.route;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import org.broadinstitute.ddp.cache.LanguageStore;
 import org.broadinstitute.ddp.db.dto.CancerItem;
+import org.broadinstitute.ddp.db.dto.LanguageDto;
 import org.broadinstitute.ddp.service.CancerService;
 
 import spark.Request;
@@ -18,7 +20,11 @@ public class ListCancersRoute implements Route {
 
     @Override
     public List<CancerItem> handle(Request request, Response response) {
-        String cancerLanguage = getUserLanguage(request).getIsoCode();
+        String cancerLanguage = LanguageStore.DEFAULT_LANG_CODE;
+        LanguageDto userLanguage = getUserLanguage(request);
+        if (userLanguage != null) {
+            cancerLanguage = userLanguage.getIsoCode();
+        }
         return cancerService.fetchCancers(cancerLanguage);
     }
 }
