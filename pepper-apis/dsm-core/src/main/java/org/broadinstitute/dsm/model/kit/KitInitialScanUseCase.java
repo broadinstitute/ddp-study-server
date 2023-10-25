40f8312da747297420fceffac7f7d9b2d42ef466
ptaheri <ptaheri@broadinstitute.org>
2023-10-25 09:55:30 -0400

    PEPPER-1193_grammar_fix

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/kit/KitInitialScanUseCase.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/kit/KitInitialScanUseCase.java
@@ -35,7 +35,7 @@ public class KitInitialScanUseCase extends BaseKitUseCase {
             }
 
             if (kitList.size() == 0) {
-                return Optional.ofNullable(new ScanError(kit, "No kit for participant with ShortId \"" + hruid + "\" was not found.\n"
+                return Optional.ofNullable(new ScanError(kit, "kit for participant with ShortId \"" + hruid + "\" was not found.\n"
                         + UserErrorMessages.IF_QUESTIONS_CONTACT_DEVELOPER));
             }
             Optional<KitRequestShipping> kitRequestWithPrefix =
@@ -57,7 +57,7 @@ public class KitInitialScanUseCase extends BaseKitUseCase {
                 }
             }
         }
-        return Optional.ofNullable(new ScanError(kit, "No kit for participant with ShortId \"" + hruid + "\" was not found.\n"
+        return Optional.ofNullable(new ScanError(kit, "kit for participant with ShortId \"" + hruid + "\" was not found.\n"
                 + UserErrorMessages.IF_QUESTIONS_CONTACT_DEVELOPER));
     }
 
