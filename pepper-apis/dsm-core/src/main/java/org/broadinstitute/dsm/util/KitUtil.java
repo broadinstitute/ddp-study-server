aa439660a553f96850b73e10aefddc748d257eea
ptaheri <ptaheri@broadinstitute.org>
2023-10-26 14:32:20 -0400

    added one extra line to make it work

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/util/KitUtil.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/util/KitUtil.java
@@ -689,6 +689,7 @@ public class KitUtil {
 
     public static String messageBasedOnLocationAndInstance(String errorMessage, DDPInstanceDto ddpInstanceDto, Address toAddress) {
         //overwriting the error message if study is PE-CGS and Participant is CA or NY otherwise keep the old error message
+
         if (!ddpInstanceDto.getResearchProject().isEmpty() && StringUtils.isNotBlank(ddpInstanceDto.getResearchProject().get())) {
             if (toAddress.getCountry().equals("CA") || toAddress.getState().equals("NY")) {
                 return PECGS_RESEARCH;
