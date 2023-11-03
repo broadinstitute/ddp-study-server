1132c60b330d2053ea0e357b5d10bdef20862c6e
Sampath K. Settipalli <sampath@broadinstitute.org>
2023-11-03 12:43:09 -0500

    PEPPER-1212 . Add governed user preferred language test case

--- pepper-apis/dss-core/src/test/java/org/broadinstitute/ddp/route/GovernedParticipantRegistrationRouteStandaloneTest.java
+++ pepper-apis/dss-core/src/test/java/org/broadinstitute/ddp/route/GovernedParticipantRegistrationRouteStandaloneTest.java
@@ -1,34 +1,23 @@
 package org.broadinstitute.ddp.route;
 
-import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import com.google.api.core.SettableApiFuture;
 import com.google.cloud.pubsub.v1.Publisher;
 import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
+import org.broadinstitute.ddp.cache.LanguageStore;
 import org.broadinstitute.ddp.constants.ConfigFile;
 import org.broadinstitute.ddp.constants.RouteConstants;
 import org.broadinstitute.ddp.db.TransactionWrapper;
 import org.broadinstitute.ddp.db.dao.DataExportDao;
 import org.broadinstitute.ddp.db.dao.JdbiUser;
 import org.broadinstitute.ddp.db.dao.JdbiUserStudyEnrollment;
+import org.broadinstitute.ddp.db.dao.StudyLanguageDao;
 import org.broadinstitute.ddp.db.dao.UserGovernanceDao;
 import org.broadinstitute.ddp.db.dao.UserProfileDao;
 import org.broadinstitute.ddp.event.publish.pubsub.PubSubPublisherInitializer;
 import org.broadinstitute.ddp.json.GovernedUserRegistrationPayload;
 import org.broadinstitute.ddp.model.governance.Governance;
+import org.broadinstitute.ddp.model.user.UserProfile;
 import org.broadinstitute.ddp.util.ConfigManager;
 import org.broadinstitute.ddp.util.TestDataSetupUtil;
 import org.junit.After;
@@ -37,6 +26,20 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class GovernedParticipantRegistrationRouteStandaloneTest extends IntegrationTestSuite.TestCase {
 
@@ -51,6 +54,11 @@ public class GovernedParticipantRegistrationRouteStandaloneTest extends Integrat
         TransactionWrapper.useTxn(handle -> {
             testData = TestDataSetupUtil.generateBasicUserTestData(handle);
             token = testData.getTestingUser().getToken();
+            //insert study languages
+            StudyLanguageDao studyLanguageDao = handle.attach(StudyLanguageDao.class);
+            studyLanguageDao.insert(testData.getStudyGuid(), "en", "English");
+            studyLanguageDao.insert(testData.getStudyGuid(), "es", "Spanish");
+            studyLanguageDao.setAsDefaultLanguage(testData.getStudyId(), "en");
         });
 
         String endpoint = RouteConstants.API.USER_STUDY_PARTICIPANTS
@@ -105,20 +113,56 @@ public class GovernedParticipantRegistrationRouteStandaloneTest extends Integrat
 
     @Test
     public void createFew() {
-        GovernedUserRegistrationPayload payload1 = new GovernedUserRegistrationPayload("it", "John", "Doe",
+        GovernedUserRegistrationPayload payload1 = new GovernedUserRegistrationPayload("es", "John", "Doe",
                 "Europe/Amsterdam");
         GovernedUserRegistrationPayload payload2 = new GovernedUserRegistrationPayload("en", "Frank", "Johnson",
                 "Europe/Moscow");
+
+        //no payload
+        GovernedUserRegistrationPayload payload3 = new GovernedUserRegistrationPayload(null, null, null, null);
+
         List<String> governedUserGuids = new ArrayList<>();
-        governedUserGuids.add(postRequest(testData.getStudyGuid(), payload1)
+        String governedUser1Guid = postRequest(testData.getStudyGuid(), payload1)
                 .then().assertThat()
                 .statusCode(200)
-                .extract().path("ddpUserGuid"));
+                .extract().path("ddpUserGuid");
+        governedUserGuids.add(governedUser1Guid);
+        //check governed users preferred language
+        UserProfile child1Profile = TransactionWrapper.withTxn(handle ->
+                handle.attach(UserProfileDao.class).findProfileByUserGuid(governedUser1Guid).get());
+        assertTrue("Governed User preferred language does not match",
+                child1Profile.getPreferredLangCode().equals("es"));
 
         governedUserGuids.add(postRequest(testData.getStudyGuid(), payload2)
                 .then().assertThat()
                 .statusCode(200)
                 .extract().path("ddpUserGuid"));
+
+        //update parents preferred language to "es"
+        UserProfile parentProfile = testData.getProfile();
+        UserProfile newProfileES = UserProfile.builder()
+                .userId(parentProfile.getUserId())
+                .firstName(parentProfile.getFirstName())
+                .lastName(parentProfile.getLastName())
+                .birthDate(parentProfile.getBirthDate())
+                .doNotContact(parentProfile.getDoNotContact())
+                .preferredLangCode("es")
+                .preferredLangId(LanguageStore.get("es").getId())
+                .build();
+
+        parentProfile = TransactionWrapper.withTxn(handle ->
+                handle.attach(UserProfileDao.class).updateProfile(newProfileES));
+
+        String governedUserParentLanguageGuid = postRequest(testData.getStudyGuid(), payload3)
+                .then().assertThat()
+                .statusCode(200)
+                .extract().path("ddpUserGuid");
+
+        governedUserGuids.add(governedUserParentLanguageGuid);
+        UserProfile childProfile = TransactionWrapper.withTxn(handle ->
+                handle.attach(UserProfileDao.class).findProfileByUserGuid(governedUserParentLanguageGuid).get());
+        assertTrue("Governed User preferred language doesnt match parent preferred language",
+                childProfile.getPreferredLangCode().equals(parentProfile.getPreferredLangCode()));
 
         userGuidsToDelete.addAll(governedUserGuids);
         List<Governance> governances = TransactionWrapper.withTxn(handle -> handle.attach(UserGovernanceDao.class)
