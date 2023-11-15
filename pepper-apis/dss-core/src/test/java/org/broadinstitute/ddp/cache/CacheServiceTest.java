e3f9d392d00e1739519429926a45aeb933eb7422
Andrew Zimmer <andrew@broadinstitute.org>
2023-11-15 09:13:07 -0500

    PEPPER-1188 Third time's the charm with redisson upgrade.  Updated test to remove mac specific checks and then started playing version whackamole and discovered that the redisson 3.19.1 -> 3.19.2 version change is where the failure was introduced.  Traced it to our use of an incompatible version of netty-common, that lacked the NetUtil.NETWORK_INTERFACES field.  Once I upgraded to a version of netty-common that had the NetUtil.NETWORK_INTERFACES field, things started to work again.

--- pepper-apis/dss-core/src/test/java/org/broadinstitute/ddp/cache/CacheServiceTest.java
+++ pepper-apis/dss-core/src/test/java/org/broadinstitute/ddp/cache/CacheServiceTest.java
@@ -3,6 +3,7 @@ package org.broadinstitute.ddp.cache;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
+import org.redisson.client.RedisConnectionException;
 
 @Slf4j
 public class CacheServiceTest {
@@ -13,25 +14,14 @@ public class CacheServiceTest {
     public void testGetInstanceDoesNotThrowNoSuchMethodError() {
         try {
             CacheService.getInstance();
-        } catch (NoSuchMethodError e) {
+        } catch (RedisConnectionException e) {
+            log.info("As expected, cannot connect to local instance that does not exist.");
+        } catch (NoSuchFieldError | NoSuchMethodError e) {
             e.printStackTrace();
-            Assert.fail("Transitive dependency error with redisson: " + e.getMessage());
-        } catch (NoSuchFieldError e) {
             Assert.fail("Could not get CacheService instance: " + e.getMessage());
         } catch (Throwable e) {
-            boolean hasConfigFileError = false;
-            if (e.getCause() != null) {
-                if (e.getCause().getMessage() != null) {
-                    if (!e.getCause().getMessage().contains("Path for configuration file file")
-                            || !e.getCause().getMessage().contains("config file")) {
-                        hasConfigFileError = true;
-                    }
-                }
-            }
-            if (!hasConfigFileError) {
-                e.printStackTrace();
-            }
-            Assert.assertTrue(hasConfigFileError);
+            e.printStackTrace();
+            Assert.fail("Unexpected error when getting CacheService instance.");
         }
     }
 }
