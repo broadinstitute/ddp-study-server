2dd4eba42094585394d4b6dbf0ad059611a73b0b
denniscunningham <129115982+denniscunningham@users.noreply.github.com>
2024-02-21 13:25:30 -0500

    [PEPPER-1161] Implement FamilyIdProvider for RGP (#2802)
    * Implement FamilyIdProvider for RGP
    
    * Fix whitespace

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/defaultvalues/RgpAutomaticProbandDataCreator.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/defaultvalues/RgpAutomaticProbandDataCreator.java
@@ -2,6 +2,7 @@ package org.broadinstitute.dsm.model.defaultvalues;
 
 import org.broadinstitute.dsm.exception.ESMissingParticipantDataException;
 import org.broadinstitute.dsm.model.bookmark.Bookmark;
+import org.broadinstitute.dsm.service.participantdata.RgpFamilyIdProvider;
 import org.broadinstitute.dsm.service.participantdata.RgpParticipantDataService;
 
 
@@ -15,12 +16,8 @@ public class RgpAutomaticProbandDataCreator extends BasicDefaultDataMaker {
      */
     @Override
     protected boolean setDefaultData(String ddpParticipantId) {
-        // ensure we can get a family ID before writing things to the DB (to avoid concurrency issues)
-        // This will increment the family ID value but will leave an unused family ID if we abort later.
-        // As things stand now that is not a concern.
-        long familyId = RgpParticipantDataService.getNextFamilyId(ddpParticipantId, new Bookmark());
-
-        RgpParticipantDataService.createDefaultData(ddpParticipantId, elasticSearchParticipantDto, familyId, instance);
+        RgpParticipantDataService.createDefaultData(ddpParticipantId, elasticSearchParticipantDto, instance,
+                new RgpFamilyIdProvider());
         return true;
     }
 }
