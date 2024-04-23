d12bb67c18524beeee021d384fe7843cf0fe1aae
Andrew Zimmer <andrew@broadinstitute.org>
2024-04-23 10:05:29 -0400

    PEPPER-1392 _ah/start utility PR feedback

--- pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/db/TransactionWrapper.java
+++ pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/db/TransactionWrapper.java
@@ -3,7 +3,6 @@ package org.broadinstitute.ddp.db;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
