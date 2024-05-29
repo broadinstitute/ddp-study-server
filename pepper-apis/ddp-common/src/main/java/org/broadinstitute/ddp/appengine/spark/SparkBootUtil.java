d2731e9eb840f50af6525066f8b8272deb7a14f3
Andrew Zimmer <andrew@broadinstitute.org>
2024-05-29 09:28:18 -0400

    PEPPER-1413 delay the shutdown to give the db more time to clear connections ahead of starting up replacement instance.

--- pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/appengine/spark/SparkBootUtil.java
+++ pepper-apis/ddp-common/src/main/java/org/broadinstitute/ddp/appengine/spark/SparkBootUtil.java
@@ -79,8 +79,7 @@ public class SparkBootUtil {
                     // as a failure
                     if (numShutdownAttempts == 0) {
                         final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-                        // temporary experiment to see if this gives the database enough time to free connections
-                        executor.schedule(() -> stopRouteCallback.onAhStop(), 500, TimeUnit.MILLISECONDS).get();
+                        executor.schedule(() -> stopRouteCallback.onAhStop(), 5, TimeUnit.SECONDS);
                     } else {
                         log.info("Ignoring shutdown attempt {}", numShutdownAttempts);
                     }
