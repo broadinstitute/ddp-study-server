8320f2fa840845c06f7ef7dd42b934dfda791578
pegahtah <47120870+pegahtah@users.noreply.github.com>
2024-06-07 17:17:33 -0400

    [DSM] PEPPER-294 Fix for RGP kits (#2888)
    * PEPPER-294 fix for RGP upload with subject id and some new tests
    
    * PEPPER-294 one small fix
    
    * PEPPER-294 PR comments
    
    * PEPPER-294 PR comments

---
+++ pepper-apis/dsm-core/src/test/java/org/broadinstitute/dsm/util/model/ParticipantUtilTest.java
@@ -0,0 +1,37 @@
+package org.broadinstitute.dsm.util.model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.broadinstitute.dsm.util.ParticipantUtil;
+import org.junit.Test;
+
+public class ParticipantUtilTest {
+
+    @Test
+    public void testIsHruid() {
+        //test valid HRUIDs
+        assertTrue(ParticipantUtil.isHruid("P12340")); // Valid HRUID with numbers after 'P'
+        assertTrue(ParticipantUtil.isHruid("Pabcde")); // Valid HRUID with lowercase letters
+        assertTrue(ParticipantUtil.isHruid("PABCDE")); // Valid HRUID with upper letters
+        assertTrue(ParticipantUtil.isHruid("PX45RI")); // Valid HRUID with a mix of letters and numbers
+
+        //test invalid HRUIDs
+        assertFalse(ParticipantUtil.isHruid("P1234"));  // Less than 5 characters after 'P'
+        assertFalse(ParticipantUtil.isHruid("P123456")); // More than 5 characters after 'P'
+        assertFalse(ParticipantUtil.isHruid("P?123A"));   // Special character within HRUID
+        assertFalse(ParticipantUtil.isHruid("12345P"));  // 'P' not at the beginning
+        assertFalse(ParticipantUtil.isHruid("123456"));   // No 'P' at all
+        assertFalse(ParticipantUtil.isHruid("RGP_5883_3"));   // RGP subject id
+        assertFalse(ParticipantUtil.isHruid("5883_3"));   // RGP subject id without the RGP prefix
+
+        //testing edge cases
+        assertFalse(ParticipantUtil.isHruid(""));          // Empty string
+        assertFalse(ParticipantUtil.isHruid("P"));         // Only 'P'
+        assertFalse(ParticipantUtil.isHruid("P1234 "));    // Space at the end
+        assertFalse(ParticipantUtil.isHruid(" P12345"));   // Space at the beginning
+        assertFalse(ParticipantUtil.isHruid(" P1234 "));   // Space around
+        assertFalse(ParticipantUtil.isHruid("P 1234"));    // Space in the middle
+    }
+
+}
