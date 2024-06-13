a8e79a721a35a4e4b94c29c3fad8de70ef11355b
Andrew Zimmer <andrew@broadinstitute.org>
2024-06-13 13:41:01 -0400

    Pulling over PEPPER-1397 changes from develop (#2898)

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/util/DSMConfig.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/util/DSMConfig.java
@@ -3,35 +3,47 @@ package org.broadinstitute.dsm.util;
 import com.typesafe.config.Config;
 import lombok.NonNull;
 
+/**
+ * Wrapper around the config used for DSM
+ */
 public class DSMConfig {
 
     private static Config config;
 
-    public DSMConfig(Config config) {
-        this.config = config;
+    public static void setConfig(Config cfg) {
+        config = cfg;
     }
 
     public static String getSqlFromConfig(@NonNull String queryName) {
-        if (config == null) {
-            throw new RuntimeException("Config is null ");
-        }
-
+        checkNullConfig();
         if (!config.hasPath(queryName)) {
             throw new RuntimeException("Conf is missing query named " + queryName);
         }
-
         return config.getString(queryName);
+    }
+
+    private static void checkNullConfig() {
+        if (config == null) {
+            throw new RuntimeException("Config is null ");
+        }
+    }
+
+    public static int getIntFromConfig(String path) {
+        checkNullConfig();
+        return config.getInt(path);
+    }
+
+    public static double getDoubleFromConfig(String path) {
+        checkNullConfig();
+        return config.getDouble(path);
     }
 
     public static boolean hasConfigPath(@NonNull String configPath) {
         if (configPath == null) {
             throw new NullPointerException("configPath");
         } else {
-            if (config == null) {
-                throw new RuntimeException("Conf has not been configured");
-            } else {
-                return config.hasPath(configPath);
-            }
+            checkNullConfig();
+            return config.hasPath(configPath);
         }
     }
 }
