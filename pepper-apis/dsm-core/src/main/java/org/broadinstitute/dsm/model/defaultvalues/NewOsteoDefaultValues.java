17da7dae6cdae5ccfe3bd0b1d2795377352d6037
denniscunningham <129115982+denniscunningham@users.noreply.github.com>
2024-07-23 14:36:06 -0400

    [PEPPER-1482] Get Osteo1 study name from message payload (#2912)
    Get Osteo1 study name from message payload

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/defaultvalues/NewOsteoDefaultValues.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/model/defaultvalues/NewOsteoDefaultValues.java
@@ -10,13 +10,14 @@ import org.broadinstitute.dsm.service.participant.OsteoParticipantService;
 public class NewOsteoDefaultValues extends BasicDefaultDataMaker {
 
     @Override
-    protected boolean setDefaultData(String ddpParticipantId, ElasticSearchParticipantDto esParticipant) {
+    protected boolean setDefaultData(String ddpParticipantId, ElasticSearchParticipantDto esParticipant,
+                                     String payload) {
         if (esParticipant.getDsm().isEmpty() || esParticipant.getActivities().isEmpty()) {
             throw new ESMissingParticipantDataException(String.format("Participant %s does not yet have DSM data and "
                     + "activities in ES", ddpParticipantId));
         }
         OsteoParticipantService osteoParticipantService = new OsteoParticipantService();
-        osteoParticipantService.setOsteoDefaultData(ddpParticipantId, esParticipant);
+        osteoParticipantService.setOsteoDefaultData(ddpParticipantId, esParticipant, payload);
         return true;
     }
 }
