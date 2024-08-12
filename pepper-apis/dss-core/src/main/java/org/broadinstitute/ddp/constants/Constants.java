567b668d1d917ade3fd4a8b4dea7223f8594a5fd
ssettipalli <ssettipalli@gmail.com>
2024-08-12 08:44:05 -0500

    PEPPER-1365 . Special handling of lms release v2 pdfs config with consent v1 (#2925)

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/constants/Constants.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/constants/Constants.java
@@ -2,16 +2,22 @@ package org.broadinstitute.ddp.constants;
 
 public class Constants {
     public static final String PANCAN_GUID = "cmi-pancan";
+    public static final String LMS_GUID = "cmi-lms";
     public static final String COUNTMEIN_RELEASE = "countmein-release";
+    public static final String COUNTMEIN_RELEASE_PARENTAL = "countmein-release-parental";
+    public static final String COUNTMEIN_RELEASE_ASSENT = "countmein-release-assent";
+    public static final String LMS_RELEASE = "lmsproject-release";
+    public static final String LMS_RELEASE_PEDIATRIC = "lmsproject-release-pediatric";
+    public static final String LMS_RELEASE_ASSENT = "lmsproject-release-pediatric-assent";
     public static final String RELEASE = "RELEASE";
+    public static final String MEDICAL_RELEASE = "MEDICAL_RELEASE";
     public static final String VERSION_2 = "v2";
     public static final String CONSENT = "CONSENT";
     public static final String VERSION_1 = "v1";
-    public static final String COUNTMEIN_RELEASE_PARENTAL = "countmein-release-parental";
     public static final String RELEASE_MINOR = "RELEASE_MINOR";
     public static final String CONSENT_PARENTAL = "CONSENT_PARENTAL";
-    public static final String COUNTMEIN_RELEASE_ASSENT = "countmein-release-assent";
     public static final String CONSENT_ASSENT = "CONSENT_ASSENT";
+    public static final String PARENTAL_CONSENT = "PARENTAL_CONSENT";
 
     /**
      * Available profile fields that can be used for profile substitution.
