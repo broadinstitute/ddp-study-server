ce15f7e920d4ef0e530fa2cee46233878fad19cb
Sampath K. Settipalli <sampath@broadinstitute.org>
2024-09-09 09:54:22 -0500

    PEPPER-1388 . remove angio study redirect from pancan

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/pancan/PancanAngioRedirectRemoval.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/pancan/PancanAngioRedirectRemoval.java
@@ -51,18 +51,18 @@ public class PancanAngioRedirectRemoval implements CustomTask {
 
     private interface SqlHelper extends SqlObject {
 
-        @SqlQuery("select trans.workflow_transition_id\n" +
-                "  from workflow_transition as trans\n" +
-                "  join umbrella_study as s on s.umbrella_study_id = trans.umbrella_study_id\n" +
-                "  join workflow_state as next_state on next_state.workflow_state_id = trans.next_state_id\n" +
-                "  join workflow_state_type as next_state_type on next_state_type.workflow_state_type_id = next_state.workflow_state_type_id\n" +
-                "  left join workflow_activity_state as next_act_state on next_act_state.workflow_state_id = next_state.workflow_state_id\n" +
-                "  left join expression as expr on expr.expression_id = trans.precondition_expression_id\n" +
-                "  left join workflow_study_redirect_state as next_redirect_state on next_redirect_state.workflow_state_id = next_state.workflow_state_id\n" +
-                " where s.guid = 'cmi-pancan'\n" +
-                "   and trans.is_active\n" +
-                "   and next_state_type.workflow_state_type_code = 'STUDY_REDIRECT'\n" +
-                "   and study_guid = 'ANGIO'")
+        @SqlQuery("select trans.workflow_transition_id "
+                + "  from workflow_transition as trans "
+                + "  join umbrella_study as s on s.umbrella_study_id = trans.umbrella_study_id "
+                + "  join workflow_state as next_state on next_state.workflow_state_id = trans.next_state_id "
+                + "  join workflow_state_type as next_state_type on next_state_type.workflow_state_type_id = next_state.workflow_state_type_id "
+                + "  left join workflow_activity_state as next_act_state on next_act_state.workflow_state_id = next_state.workflow_state_id "
+                + "  left join expression as expr on expr.expression_id = trans.precondition_expression_id "
+                + "  left join workflow_study_redirect_state as next_redirect_state on next_redirect_state.workflow_state_id = next_state.workflow_state_id "
+                + " where s.guid = 'cmi-pancan' "
+                + "   and trans.is_active "
+                + "   and next_state_type.workflow_state_type_code = 'STUDY_REDIRECT' "
+                + "   and study_guid = 'ANGIO'")
         long findPancanAngioStudyRedirectWorkflowId();
 
         @SqlUpdate("delete workflow_transition  "
