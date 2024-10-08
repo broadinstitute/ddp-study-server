e02c1af192f3f1d45278b9820cb6acf59443ebfe
Sampath K. Settipalli <sampath@broadinstitute.org>
2024-10-08 14:03:26 -0500

    PEPPER-1389 . cleanup

--- pepper-apis/studybuilder-cli/config/manifest-tenant.rb
+++ pepper-apis/studybuilder-cli/config/manifest-tenant.rb
@@ -1,13 +1,8 @@
 render "cmi-config.json.ctmpl"
-#render "testboston-config.json.ctmpl"
 render "prion-config.json.ctmpl"
 render "rgp-config.json.ctmpl"
-#render "rarex-config.json.ctmpl"
-#render "circadia-config.json.ctmpl"
 render "atcp-config.json.ctmpl"
 render "brugada-config.json.ctmpl"
-#render "singular-config.json.ctmpl"
-#render "fon-config.json.ctmpl"
 
 if $env == "dev"
   render "basil-config.json.ctmpl"
