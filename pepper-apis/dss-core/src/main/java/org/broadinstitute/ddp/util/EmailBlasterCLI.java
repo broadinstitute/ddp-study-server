5f354fa0c9f9122e7672b9f4dc2dc78d417ecc56
Sampath K. Settipalli <sampath@broadinstitute.org>
2025-01-08 20:39:07 -0600

    PEPPER-1567 . update EmailBlasterCLI

--- pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/EmailBlasterCLI.java
+++ pepper-apis/dss-core/src/main/java/org/broadinstitute/ddp/util/EmailBlasterCLI.java
@@ -16,11 +16,13 @@ import org.broadinstitute.ddp.db.TransactionWrapper;
 import org.broadinstitute.ddp.db.dao.JdbiAuth0Tenant;
 import org.broadinstitute.ddp.db.dao.JdbiUmbrellaStudy;
 import org.broadinstitute.ddp.db.dao.JdbiUser;
+import org.broadinstitute.ddp.db.dao.UserGovernanceDao;
 import org.broadinstitute.ddp.db.dao.UserProfileDao;
 import org.broadinstitute.ddp.db.dto.Auth0TenantDto;
 import org.broadinstitute.ddp.db.dto.StudyDto;
 import org.broadinstitute.ddp.db.dto.UserDto;
 import org.broadinstitute.ddp.exception.DDPException;
+import org.broadinstitute.ddp.model.governance.Governance;
 import org.broadinstitute.ddp.model.user.UserProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,15 +37,23 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.broadinstitute.ddp.constants.NotificationTemplateVariables.DDP_ACTIVITY_INSTANCE_GUID;
 import static org.broadinstitute.ddp.constants.NotificationTemplateVariables.DDP_BASE_WEB_URL;
 import static org.broadinstitute.ddp.constants.NotificationTemplateVariables.DDP_PARTICIPANT_FIRST_NAME;
 import static org.broadinstitute.ddp.constants.NotificationTemplateVariables.DDP_PARTICIPANT_GUID;
+import static org.broadinstitute.ddp.constants.NotificationTemplateVariables.DDP_PROXY_FIRST_NAME;
 
 /**
  * CLI for sending an email template from sendgrid
  * to a list of guids.
+ * limitations/expectations:
+ * As of now only english, legacy templates are supported
+ * self/pediatric guids should be separated into different runs
+ * For pediatric participants, the participant (pediatric) guid should be provided. Code will lookup proxy/parent and will get the email.
+ * Not all template substitutions are supported. Check the substitutions in the template and add them to the code if needed.
  */
 public class EmailBlasterCLI {
 
@@ -67,6 +77,7 @@ public class EmailBlasterCLI {
         options.addOption("t", "template-id", true, "sendgrid template id");
         options.addOption("a", "activity-code", true, "activity code");
         options.addOption("sub", "subject", true, "email subject");
+        options.addOption("p", "pediatric", false, "pediatric participants");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(options, args);
@@ -90,6 +101,7 @@ public class EmailBlasterCLI {
         String subject = cmd.getOptionValue("sub");
         String activityCode = cmd.getOptionValue("a");
         File guidsFile = new File(cmd.getOptionValue("g"));
+        boolean isPediatric = cmd.hasOption("p");
         LOG.debug("passed subject: " + subject);
 
         List<String> guids = null;
@@ -99,12 +111,13 @@ public class EmailBlasterCLI {
             LOG.error("Could not read " + guidsFile.getAbsolutePath(), e);
             System.exit(-1);
         }
-        new EmailBlasterCLI(sendgridApiKey).sendEmail(fromName, fromEmail, templateId, studyGuid, subject, activityCode, guids);
+        new EmailBlasterCLI(sendgridApiKey).sendEmail(fromName, fromEmail, templateId, studyGuid, subject,
+                activityCode, guids, isPediatric);
         System.exit(0);
     }
 
     public void sendEmail(String fromName, String fromEmail, String sendgridTemplateId, String studyGuid,
-                          String subject, String activityCode, Collection<String> recipientGuids) {
+                          String subject, String activityCode, Collection<String> recipientGuids, boolean isPediatric) {
 
         final Set<String> auth0UserIds = new TreeSet<>();
         final Map<String, Map<String, String>> personalizationByAuth0Id = new HashMap<>();
@@ -127,15 +140,34 @@ public class EmailBlasterCLI {
                     LOG.error("Could not find user with guid " + recipientGuid);
                     continue;
                 }
+
+                UserDto proxyUserDto = null;
                 UserProfile userProfile = handle.attach(UserProfileDao.class).findProfileByUserGuid(userDto.getUserGuid()).get();
-                // todo add other template vars
+                // todo add other template vars if the template requires
                 String userAuth = userDto.getAuth0UserId().orElse(null);
+                if (isPediatric) {
+                    //find the parent/proxy
+                    List<Governance> governances;
+                    LOG.debug("participantId: {} .. studyID:{}", userDto.getUserId(), studyDto.getId());
+                    try (Stream<Governance> governanceStream = handle.attach(UserGovernanceDao.class)
+                            .findActiveGovernancesByParticipantAndStudyIds(userDto.getUserId(), studyDto.getId())) {
+                        governances = governanceStream.collect(Collectors.toList());
+                    }
+                    String proxyGuid = governances.get(0).getProxyUserGuid();
+                    LOG.info("Proxy user guid: " + proxyGuid);
+                    proxyUserDto = userDao.findByUserGuid(proxyGuid);
+                    userAuth = proxyUserDto.getAuth0UserId().orElse(null);
+                    LOG.info("Proxy user auth0 id: " + userAuth);
+                    userProfile = handle.attach(UserProfileDao.class).findProfileByUserGuid(proxyUserDto.getUserGuid()).get();
+                    LOG.debug("Proxy user firstName: " + userProfile.getFirstName());
+                }
+
                 if (StringUtils.isNotBlank(userAuth)) {
                     auth0UserIds.add(userAuth);
                     personalizationByAuth0Id.put(userAuth, new HashMap<>());
                     personalizationByAuth0Id.get(userAuth).put(DDP_PARTICIPANT_FIRST_NAME, userProfile.getFirstName());
                     personalizationByAuth0Id.get(userAuth).put(DDP_BASE_WEB_URL, studyDto.getWebBaseUrl());
-                    personalizationByAuth0Id.get(userAuth).put(DDP_PARTICIPANT_GUID, userDto.getUserGuid());
+                    personalizationByAuth0Id.get(userAuth).put(DDP_PARTICIPANT_GUID, recipientGuid);
 
                     if (activityCode != null) {
                         //load activity instance
@@ -143,8 +175,11 @@ public class EmailBlasterCLI {
                         String instanceGuid = activityInstanceDao.getGuidOfLatestInstanceForUserAndActivity(
                                 handle, userDto.getUserGuid(), activityCode,
                                 studyDto.getId()).get();
-                        LOG.debug("Found instance guid: " + instanceGuid);
+                        LOG.info("Found instance guid: " + instanceGuid);
                         personalizationByAuth0Id.get(userAuth).put(DDP_ACTIVITY_INSTANCE_GUID, instanceGuid);
+                    }
+                    if (isPediatric) {
+                        personalizationByAuth0Id.get(userAuth).put(DDP_PROXY_FIRST_NAME, userProfile.getFirstName());
                     }
 
                 } else {
