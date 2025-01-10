83af45671369e58693c79e022e7cb4bb757f1f18
Sampath K. Settipalli <sampath@broadinstitute.org>
2025-01-10 16:43:07 -0600

    PEPPER-1088 . cleanup.

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/service/onchistory/OncHistoryUploadService.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/service/onchistory/OncHistoryUploadService.java
@@ -184,13 +184,11 @@ public class OncHistoryUploadService {
                         .append(exitedParticipants)
                         .append(" and ")
                         .append(nonTissueConsentedParticipants);
-            }
-            if (!exitedParticipants.isEmpty()) {
+            } else if (!exitedParticipants.isEmpty()) {
                 message.append("a withdrawn participant. "
                                 + "Please remove onc histories for these withdrawn participants from the file and upload it again: ")
                         .append(exitedParticipants);
-            }
-            if (!nonTissueConsentedParticipants.isEmpty()) {
+            } else if (!nonTissueConsentedParticipants.isEmpty()) {
                 message.append("a participant who did not consent to tissue sample. "
                                 + "Please remove onc histories for these participants from the file and upload it again: ")
                         .append(nonTissueConsentedParticipants);
