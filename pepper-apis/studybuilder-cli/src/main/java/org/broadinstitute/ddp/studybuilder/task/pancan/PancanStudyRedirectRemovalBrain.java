52959f830e107ea0b8bed94801922253a7376c41
Sampath K. Settipalli <sampath@broadinstitute.org>
2025-05-12 15:04:56 -0500

    PEPPER-1608 pancan -> brain tumor redirect  removal to support brain enrollment end

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/pancan/PancanStudyRedirectRemovalBrain.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/pancan/PancanStudyRedirectRemovalBrain.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class PancanStudyRedirectRemovalBrain extends PancanStudyRedirectRemovalSupport {
 
-    private static final String searchPexExpr = "hasOptionStartsWith(\"C_BRAIN_\")";
-    private static final String currentPexExpr = "hasOptionStartsWith(\"C_BRAIN_\")";
-    private static final String newPexExpr = "hasOptionStartsWith(\"C_BRAIN_ENROLL_ENDED\")";
+    private static final String searchPexExpr = "hasOptionStartsWith(\"C_BRAIN_\"";
+    private static final String currentPexExpr = "hasOptionStartsWith(\"C_BRAIN_\"";
+    private static final String newPexExpr = "hasOptionStartsWith(\"C_BRAIN_ENROLL_ENDED\"";
 
     public PancanStudyRedirectRemovalBrain() {
         super("cmi-brain", searchPexExpr, currentPexExpr, newPexExpr);
