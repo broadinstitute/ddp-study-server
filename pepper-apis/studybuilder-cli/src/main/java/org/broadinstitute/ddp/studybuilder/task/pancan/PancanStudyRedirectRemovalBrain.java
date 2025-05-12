aa4e2338d8a1ae5706e564f3f10b3f764cad813c
ssettipalli <ssettipalli@gmail.com>
2025-05-12 16:49:40 -0500

    PEPPER-1608 Pancan -> Brain tumor study redirect removal (#2969)

---
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/pancan/PancanStudyRedirectRemovalBrain.java
@@ -0,0 +1,17 @@
+package org.broadinstitute.ddp.studybuilder.task.pancan;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class PancanStudyRedirectRemovalBrain extends PancanStudyRedirectRemovalSupport {
+
+    private static final String searchPexExpr = "hasOptionStartsWith(\"C_BRAIN_\"";
+    private static final String currentPexExpr = "hasOptionStartsWith(\"C_BRAIN_\"";
+    private static final String newPexExpr = "hasOptionStartsWith(\"C_BRAIN_ENROLL_ENDED\"";
+
+    public PancanStudyRedirectRemovalBrain() {
+        super("cmi-brain", searchPexExpr, currentPexExpr, newPexExpr);
+        log.info("TASK:: PancanStudyRedirectRemovalBrain ");
+    }
+
+}
