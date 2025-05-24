ddf0f82d3d0bcedb3aea54c890a215573796682b
Sampath K. Settipalli <sampath@broadinstitute.org>
2025-05-24 18:08:38 -0500

    PEPPER-1617 LMS germline pediatric consent version#3 .

---
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/lms/LmsGermlineConsentPediatricV3.java
@@ -0,0 +1,24 @@
+package org.broadinstitute.ddp.studybuilder.task.lms;
+
+import lombok.extern.slf4j.Slf4j;
+import org.broadinstitute.ddp.model.activity.definition.i18n.ActivityI18nDetail;
+import org.broadinstitute.ddp.studybuilder.task.RevisionStudyActivityVariablesSupport;
+
+import java.time.Instant;
+import java.util.List;
+
+@Slf4j
+public class LmsGermlineConsentPediatricV3 extends RevisionStudyActivityVariablesSupport {
+
+    private static final String DATA_FILE = "patches/germline-consent-addendum-pediatric-v3.conf";
+    private static final String STUDY_LMS = "cmi-lms";
+    private static final String ACTIVITY_CODE = "GERMLINE_CONSENT_ADDENDUM_PEDIATRIC";
+    private static final String ACTIVITY_NAME = "Learning About Your Child’s DNA with Genome Medical and Invitae";
+    private static final String ACTIVITY_TITLE = "Additional Consent & Assent:<br> Learning More About Your Child's DNA with Genome Medical and Invitae";
+
+    public LmsGermlineConsentPediatricV3() {
+        super(STUDY_LMS, ACTIVITY_CODE, DATA_FILE, ACTIVITY_NAME, ACTIVITY_TITLE);
+    }
+
+
+}
