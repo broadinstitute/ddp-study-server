42d73e869aa8cd0c035b6a744f3229fed6596a06
Sampath K. Settipalli <sampath@broadinstitute.org>
2025-08-07 20:57:48 -0500

    PEPPER-1632 . PEPPER-1636 osteo adult germline consent and pdfs v4

--- pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/RevisionStudyActivityVariablesSupportV2.java
+++ pepper-apis/studybuilder-cli/src/main/java/org/broadinstitute/ddp/studybuilder/task/RevisionStudyActivityVariablesSupportV2.java
@@ -153,7 +153,7 @@ public class RevisionStudyActivityVariablesSupportV2 implements CustomTask {
 
     private void revisionVariableTranslation(TemplateVariable tmplVar,
                                                   RevisionMetadata meta, ActivityVersionDto newVersion) {
-        log.info("Revisioning and updating template variable: {} .. \n New en Text: {} ", tmplVar.getTranslation("en").get().getText());
+        log.info("Revisioning and updating template variable: {} .. \n New EN Text: {} ", tmplVar.getName(), tmplVar.getTranslation("en").get().getText());
         String varName = tmplVar.getName();
         Long tmplVarId = sqlHelper.findBlockTemplateVariableIdByNameAndActivityId(varName, newVersion.getActivityId());
 
