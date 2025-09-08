b14f3f51f19d950ea226736e259645c8907c1a09
Andrew Zimmer <andrew@broadinstitute.org>
2025-09-08 19:04:31 -0400

    PEPPER-1642 initial sketch of legacy kit integration with juniper.

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/db/DDPInstance.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/db/DDPInstance.java
@@ -102,7 +102,7 @@ public class DDPInstance {
     private InstanceSettings instanceSettings;
     private final String studyGuid;
     // marked final to avoid state management complexity with lombok Builder
-    private final LegacyKits legacyKits;
+    private final LegacyKits legacyKits = new LegacyKits(new ArrayList<>());
 
     public DDPInstance(String ddpInstanceId, String name, String baseUrl, String collaboratorIdPrefix, boolean hasRole,
                        int daysMrAttentionNeeded, int daysTissueAttentionNeeded, boolean hasAuth0Token, List<String> notificationRecipient,
@@ -128,7 +128,7 @@ public class DDPInstance {
         this.displayName = displayName;
         this.instanceSettings = instanceSettings;
         this.studyGuid = studyGuid;
-        this.legacyKits = legacyKits;
+        this.legacyKits.initFrom(legacyKits);
     }
 
 
@@ -152,7 +152,6 @@ public class DDPInstance {
         this.mercuryOrderCreator = null;
         this.displayName = null;
         this.studyGuid = null;
-        this.legacyKits = null;
     }
 
     /**
