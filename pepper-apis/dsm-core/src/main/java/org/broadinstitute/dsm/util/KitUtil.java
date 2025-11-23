deb92b3021d8184b5746be54ca404afc1f18874a
Andrew Zimmer <andrew@broadinstitute.org>
2025-11-23 15:15:06 -0500

    PEPPER-1646 generalizing easypost retry with legacy api key

--- pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/util/KitUtil.java
+++ pepper-apis/dsm-core/src/main/java/org/broadinstitute/dsm/util/KitUtil.java
@@ -567,7 +567,7 @@ public class KitUtil {
                                     }
                                 }
                             } catch (EasyPostException epe) {
-                                logger.error("Couldn't get shipment information from Easypost for shipment ", epe);
+                                logger.error("Couldn't get shipment information from Easypost for shipment " + kitRequest.getEasypostToId(),  epe);
                             }
 
                         }
